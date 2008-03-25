ec21a789084888f5b8cc538eb651ee9ca7dad145
shalott <shalott@gmail.com>
2008-03-25 22:19:31 +0000

    Initial commit of Rails skeleton

---
+++ app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => 'ac9a60d7583c3455f6ac2ad6ba21d83e'
+end
