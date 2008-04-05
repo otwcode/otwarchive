49b9c09f249e4cd2b0706018fb376d4befef1831
shalott <shalott@gmail.com>
2008-04-05 06:20:07 +0000

    Cleaned up the routes a little bit and edited the default index page. We'll want to delete it entirely soon.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :works
 
-  map.resources :works
-
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -29,7 +27,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  # map.root :controller => :works
 
   # See how all your routes lay out with "rake routes"
 
