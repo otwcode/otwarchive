15fda8416d66691448fb0ca1fbb1e07b1ffe9916
shalott <shalott@gmail.com>
2008-04-05 06:15:03 +0000

    Fix to the flash error message.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,59 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 2) do
+ActiveRecord::Schema.define(:version => 3) do
+
+  create_table "globalize_countries", :force => true do |t|
+    t.string "code",                   :limit => 2
+    t.string "english_name"
+    t.string "date_format"
+    t.string "currency_format"
+    t.string "currency_code",          :limit => 3
+    t.string "thousands_sep",          :limit => 2
+    t.string "decimal_sep",            :limit => 2
+    t.string "currency_decimal_sep",   :limit => 2
+    t.string "number_grouping_scheme"
+  end
+
+  add_index "globalize_countries", ["code"], :name => "index_globalize_countries_on_code"
+
+  create_table "globalize_languages", :force => true do |t|
+    t.string  "iso_639_1",             :limit => 2
+    t.string  "iso_639_2",             :limit => 3
+    t.string  "iso_639_3",             :limit => 3
+    t.string  "rfc_3066"
+    t.string  "english_name"
+    t.string  "english_name_locale"
+    t.string  "english_name_modifier"
+    t.string  "native_name"
+    t.string  "native_name_locale"
+    t.string  "native_name_modifier"
+    t.boolean "macro_language"
+    t.string  "direction"
+    t.string  "pluralization"
+    t.string  "scope",                 :limit => 1
+  end
+
+  add_index "globalize_languages", ["iso_639_1"], :name => "index_globalize_languages_on_iso_639_1"
+  add_index "globalize_languages", ["iso_639_2"], :name => "index_globalize_languages_on_iso_639_2"
+  add_index "globalize_languages", ["iso_639_3"], :name => "index_globalize_languages_on_iso_639_3"
+  add_index "globalize_languages", ["rfc_3066"], :name => "index_globalize_languages_on_rfc_3066"
+
+  create_table "globalize_translations", :force => true do |t|
+    t.string  "type"
+    t.string  "tr_key"
+    t.string  "table_name"
+    t.integer "item_id"
+    t.string  "facet"
+    t.boolean "built_in"
+    t.integer "language_id"
+    t.integer "pluralization_index"
+    t.text    "text"
+    t.string  "namespace"
+  end
+
+  add_index "globalize_translations", ["tr_key", "language_id"], :name => "index_globalize_translations_on_tr_key_and_language_id"
+  add_index "globalize_translations", ["table_name", "item_id", "language_id"], :name => "globalize_translations_table_name_and_item_and_language"
 
   create_table "works", :force => true do |t|
     t.string   "title",                       :default => "", :null => false
