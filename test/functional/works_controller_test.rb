14228483bdafcabbbf953449d79b8d64de529cb2
zooey-glass <zooey.glass04@googlemail.com>
2008-04-17 15:53:48 +0000

    Very minor edits to fix some failures in the works_controller test.

--- test/functional/works_controller_test.rb
+++ test/functional/works_controller_test.rb
@@ -8,11 +8,13 @@ class WorksControllerTest < ActionController::TestCase
   end
 
   def test_should_get_new
+    login_as_user(:user1)
     get :new
     assert_response :success
   end
 
   def test_should_create_work
+    login_as_user(:user1)
     assert_difference('Work.count') do
       post :create, :work => { }
     end
@@ -21,23 +23,24 @@ class WorksControllerTest < ActionController::TestCase
   end
 
   def test_should_show_work
-    get :show, :id => works(:one).id
+    get :show, :id => works(:basic_work).id
     assert_response :success
   end
 
   def test_should_get_edit
-    get :edit, :id => works(:one).id
+    login_as_user(:user1)
+    get :edit, :id => works(:basic_work).id
     assert_response :success
   end
 
   def test_should_update_work
-    put :update, :id => works(:one).id, :work => { }
+    put :update, :id => works(:basic_work).id, :work => { }
     assert_redirected_to work_path(assigns(:work))
   end
-
+  
   def test_should_destroy_work
     assert_difference('Work.count', -1) do
-      delete :destroy, :id => works(:one).id
+      delete :destroy, :id => works(:basic_work).id
     end
 
     assert_redirected_to works_path
