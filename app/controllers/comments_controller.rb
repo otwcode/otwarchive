51a5285f38f8bd8804e453dcd25c133ab8c6d991
elzj <elzj78@gmail.com>
2008-04-24 04:12:36 +0000

    Began making comments more AJAXy and making the Javascript links more accessible.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,33 @@
-class CommentsController < ApplicationController
+class CommentsController < ApplicationController 
+  before_filter :load_commentable, :only => [ :index, :new ]
+  
+  # Get the parent of the desired comment(s) 
+  # Just covering all the bases here for now
+  def load_commentable
+    if params[:work_id]
+      @commentable = Work.find(params[:work_id])
+    elsif params[:chapter_id]
+      @commentable = Chapter.find(params[:chapter_id])
+    elsif params[:comment_id]
+      @commentable = Comment.find(params[:comment_id])
+    elsif params[:user_id]
+      @commentable = User.find(params[:user_id])
+    elsif params[:pseud_id]
+      @commentable = Pseud.find(params[:pseud_id])
+    end    
+  end
+  
   # GET /comments
   # GET /comments.xml
   def index 
-    @work = Work.find(:first)
-    @comments = @work.find_all_comments
+    @comments = @commentable.find_all_comments
   end
   
   # GET /comments/1
   # GET /comments/1.xml
   def show
-    @comment = Comment.find(params[:id])
-    @comments = @comment.all_children
+    comment = Comment.find(params[:id])
+    @comments = comment.full_set
   end
   
   # GET /comments/new
@@ -33,9 +50,9 @@ class CommentsController < ApplicationController
     if @comment.set_and_save
       if @comment.approved?
         flash[:notice] = 'Comment was successfully created.'
-        redirect_to(@comment)
+        redirect_to(@comment.commentable)
       else
-        flash[:notice] = 'Comment was marked as spam by Askimet.'
+        flash[:notice] = 'Comment was marked as spam by Akismet.'
         redirect_to(comments_url)
       end
     else
@@ -50,7 +67,7 @@ class CommentsController < ApplicationController
     
     if @comment.update_attributes(params[:comment])
       flash[:notice] = 'Comment was successfully updated.'
-      redirect_to(@comment)
+      redirect_to(@comment.commentable)
     else
       render :action => "edit" 
     end
