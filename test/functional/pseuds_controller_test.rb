7152037ad1763e52e6d3c8088d4033e343706c7e
dasbero <dasbero@gmail.com>
2008-05-04 20:32:03 +0000

    Played around with fixtures and a few unit test for the User model. Destroyed Naomi's tidy naming scheme.
    This should not have broken test that were previously working.

--- test/functional/pseuds_controller_test.rb
+++ test/functional/pseuds_controller_test.rb
@@ -2,51 +2,51 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class PseudsControllerTest < ActionController::TestCase
   def test_should_get_index
-    get :index, :locale => 'en', :user_id => users(:basic_user).id
+    get :index, :locale => 'en', :user_id => users(:mary).id
     assert_response :success
     assert_not_nil assigns(:pseuds)
   end
 
   def test_should_get_new
-    login_as_user(:basic_user)
-    get :new, :locale => 'en', :user_id => users(:basic_user).id
+    login_as_user(:mary)
+    get :new, :locale => 'en', :user_id => users(:mary).id
     assert_response :success
   end
 
   def test_should_create_pseud
-    login_as_user(:basic_user)
+    login_as_user(:mary)
     assert_difference('Pseud.count') do
-      post :create, :locale => 'en', :user_id => users(:basic_user).id, :pseud => { :name => 'New Pseud' }
+      post :create, :locale => 'en', :user_id => users(:mary).id, :pseud => { :name => 'New Pseud' }
     end
 
-    assert_redirected_to user_pseud_path(users(:basic_user), assigns(:pseud))
+    assert_redirected_to user_pseud_path(users(:mary), assigns(:pseud))
   end
 
   def test_should_show_pseud
-    get :show, :locale => 'en', :user_id => users(:basic_user).id, :id => pseuds(:default_pseud).id
+    get :show, :locale => 'en', :user_id => users(:mary).id, :id => pseuds(:mary_sue).id
     assert_response :success
   end
 
   def test_should_get_edit
-    get :edit, :locale => 'en', :user_id => users(:basic_user).id, :id => pseuds(:default_pseud).id
+    get :edit, :locale => 'en', :user_id => users(:mary).id, :id => pseuds(:mary_sue).id
     assert_response :success
   end
 
   def test_should_update_pseud
-    login_as_user(:basic_user)
+    login_as_user(:mary)
     put :update, :locale => 'en', 
-                 :user_id => users(:basic_user).id, 
-                 :id => pseuds(:default_pseud).id, 
+                 :user_id => users(:mary).id, 
+                 :id => pseuds(:mary_sue).id, 
                  :pseud => { :name => 'Changed Pseud' }
-    assert_redirected_to user_pseud_path(users(:basic_user), assigns(:pseud))
+    assert_redirected_to user_pseud_path(users(:mary), assigns(:pseud))
   end
 
   def test_should_destroy_pseud
-    login_as_user(:basic_user)
+    login_as_user(:mary)
     assert_difference('Pseud.count', -1) do
-      delete :destroy, :locale => 'en', :user_id => users(:basic_user).id, :id => pseuds(:non_default_pseud).id
+      delete :destroy, :locale => 'en', :user_id => users(:mary).id, :id => pseuds(:marie).id
     end
 
-    assert_redirected_to user_pseuds_path(users(:basic_user))
+    assert_redirected_to user_pseuds_path(users(:mary))
   end
 end
