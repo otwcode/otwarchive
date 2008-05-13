29bc57874d92730e644d83e27098677482964a0f
ambtus <alice@alum.mit.edu>
2008-05-13 08:44:20 +0000

    the previous version of action_mailer_tls broke non-ssl email.  this fixes it.
    Also cleaned up the config files a little

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,11 +1,19 @@
 # Email settings
 ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  :address => ArchiveConfig.SMTP_SERVER,
-  :user_name => ArchiveConfig.SMTP_USER,
-  :password => ArchiveConfig.SMTP_PASSWORD,
-  :domain => ArchiveConfig.SMTP_DOMAIN,
-  :port => ArchiveConfig.SMTP_PORT,
-  :authentication => ArchiveConfig.SMTP_AUTHENTICATION,
-}
+if ArchiveConfig.SMTP_AUTHENTICATION
+  ActionMailer::Base.smtp_settings = {
+    :address => ArchiveConfig.SMTP_SERVER,
+    :domain => ArchiveConfig.SMTP_DOMAIN,
+    :port => ArchiveConfig.SMTP_PORT,
+    :user_name => ArchiveConfig.SMTP_USER,
+    :password => ArchiveConfig.SMTP_PASSWORD,
+    :authentication => ArchiveConfig.SMTP_AUTHENTICATION,
+   }
+else
+  ActionMailer::Base.smtp_settings = {
+    :address => ArchiveConfig.SMTP_SERVER,
+    :domain => ArchiveConfig.SMTP_DOMAIN,
+    :port => ArchiveConfig.SMTP_PORT,
+  }
+end
 
--- vendor/plugins/action_mailer_tls/lib/smtp_tls.rb
+++ vendor/plugins/action_mailer_tls/lib/smtp_tls.rb
@@ -6,35 +6,29 @@ Net::SMTP.class_eval do
   def do_start(helodomain, user, secret, authtype)
     raise IOError, 'SMTP session already started' if @started
     check_auth_args user, secret, authtype if user or secret
-
     sock = timeout(@open_timeout) { TCPSocket.open(@address, @port) }
     @socket = Net::InternetMessageIO.new(sock)
     @socket.read_timeout = 60 #@read_timeout
-    @socket.debug_output = STDERR #@debug_output
-
     check_response(critical { recv_response() })
     do_helo(helodomain)
-
-    raise 'openssl library not installed' unless defined?(OpenSSL)
-    starttls
-    ssl = OpenSSL::SSL::SSLSocket.new(sock)
-    ssl.sync_close = true
-    ssl.connect
-    @socket = Net::InternetMessageIO.new(ssl)
-    @socket.read_timeout = 60 #@read_timeout
-    @socket.debug_output = STDERR #@debug_output
-    do_helo(helodomain)
-
+    if starttls
+      raise 'openssl library not installed' unless defined?(OpenSSL)
+      ssl = OpenSSL::SSL::SSLSocket.new(sock)
+      ssl.sync_close = true
+      ssl.connect
+      @socket = Net::InternetMessageIO.new(ssl)
+      @socket.read_timeout = 60 #@read_timeout
+      do_helo(helodomain)
+    end
     authenticate user, secret, authtype if user
     @started = true
   ensure
     unless @started
       # authentication failed, cancel connection.
-        @socket.close if not @started and @socket and not @socket.closed?
+      @socket.close if not @started and @socket and not @socket.closed?
       @socket = nil
     end
   end
-
   def do_helo(helodomain)
      begin
       if @esmtp
@@ -51,11 +45,10 @@ Net::SMTP.class_eval do
       raise
     end
   end
-
   def starttls
-    getok('STARTTLS')
+    getok('STARTTLS') rescue return false
+    return true
   end
-
   def quit
     begin
       getok('QUIT')
