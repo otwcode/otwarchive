945d8f3bfe87389fa3a6a0931980d01e275fbead
elzj <elzj78@gmail.com>
2008-07-12 14:59:09 +0000

    Adding Ultrasphinx plugin

--- test/functional/related_works_controller_test.rb
+++ test/functional/related_works_controller_test.rb
@@ -1,45 +1,5 @@
 require 'test_helper'
 
 class RelatedWorksControllerTest < ActionController::TestCase
-  def test_should_get_index
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:related_works)
-  end
-
-  def test_should_get_new
-    get :new
-    assert_response :success
-  end
-
-  def test_should_create_related_work
-    assert_difference('RelatedWork.count') do
-      post :create, :related_work => { }
-    end
-
-    assert_redirected_to related_work_path(assigns(:related_work))
-  end
-
-  def test_should_show_related_work
-    get :show, :id => related_works(:one).id
-    assert_response :success
-  end
-
-  def test_should_get_edit
-    get :edit, :id => related_works(:one).id
-    assert_response :success
-  end
-
-  def test_should_update_related_work
-    put :update, :id => related_works(:one).id, :related_work => { }
-    assert_redirected_to related_work_path(assigns(:related_work))
-  end
-
-  def test_should_destroy_related_work
-    assert_difference('RelatedWork.count', -1) do
-      delete :destroy, :id => related_works(:one).id
-    end
-
-    assert_redirected_to related_works_path
-  end
+# Placeholder
 end
--- test/functional/series_controller_test.rb
+++ test/functional/series_controller_test.rb
@@ -1,45 +1,5 @@
 require 'test_helper'
 
 class SeriesControllerTest < ActionController::TestCase
-  def test_should_get_index
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:series)
-  end
-
-  def test_should_get_new
-    get :new
-    assert_response :success
-  end
-
-  def test_should_create_series
-    assert_difference('Series.count') do
-      post :create, :series => { }
-    end
-
-    assert_redirected_to series_path(assigns(:series))
-  end
-
-  def test_should_show_series
-    get :show, :id => series(:one).id
-    assert_response :success
-  end
-
-  def test_should_get_edit
-    get :edit, :id => series(:one).id
-    assert_response :success
-  end
-
-  def test_should_update_series
-    put :update, :id => series(:one).id, :series => { }
-    assert_redirected_to series_path(assigns(:series))
-  end
-
-  def test_should_destroy_series
-    assert_difference('Series.count', -1) do
-      delete :destroy, :id => series(:one).id
-    end
-
-    assert_redirected_to series_path
-  end
+# Placeholder
 end
