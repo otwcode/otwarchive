db781bb6d18b5cd184e83ecbf79f57d863d36bdc
zooey-glass <zooey.glass04@googlemail.com>
2008-07-18 16:38:01 +0000

    Adding 'paste from Word' to TinyMCE toolbar as interim measure while we wait to get Word imports off the ground.

--- public/javascripts/mce_editor.js
+++ public/javascripts/mce_editor.js
@@ -4,10 +4,13 @@ tinyMCE.init({
 	theme:"advanced",
 	mode:"none",
 	editor_selector:"mce-editor",
+	plugins : "paste",
+	
+
 
 	
 	// Theme options - using the advanced theme for now and just limiting the buttons used - we may want to create a custom theme in future.
-	theme_advanced_buttons1 : "bold,italic,underline,strikethrough,fontselect,fontsizeselect,|,link,unlink,image,|,outdent,indent,blockquote,|,bullist,numlist,|,justifyleft,justifycenter,justifyright,justifyfull,|,undo,redo",
+	theme_advanced_buttons1 : "bold,italic,underline,strikethrough,fontselect,fontsizeselect,|,link,unlink,image,|,outdent,indent,blockquote,|,bullist,numlist,|,justifyleft,justifycenter,justifyright,justifyfull,|,paste,pasteword,|,undo,redo",
 	theme_advanced_buttons2 : "",
 	theme_advanced_buttons3 : "",
 	theme_advanced_toolbar_location : "top",
@@ -26,6 +29,7 @@ function addEditor(id) {
 function removeEditor(id) {
 		tinyMCE.execCommand('mceRemoveControl', false,  id)
 }
+
 
 
 
