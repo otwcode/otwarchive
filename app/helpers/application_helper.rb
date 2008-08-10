8c36347e4cc33410b6425cab87e29ac314d23c0d
zooey-glass <zooey.glass04@googlemail.com>
2008-08-10 12:46:43 +0000

    Adding a new sanitise helper method so summaries can have limited html but still have some formatting help. There must be a DRYer way of doing this, but this seems to work, at least.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -134,6 +134,12 @@ module ApplicationHelper
     close_tags(sanitize(simple_format(content)))
   end
   
+  #santizes and formats a string with paragraphs, and limits the range of allowed tags.
+  def sanitize_limited(content)
+    limit_tags(sanitize(simple_format(content)))
+  end
+  
+  
   #closes tags in html (uses http://snippets.dzone.com/posts/show/3822, but
   #modified)
   def close_tags(html)
@@ -190,6 +196,83 @@ module ApplicationHelper
     
     result
   end
+  
+  # Closes tags in html and limits allowed tags to a small subgroup.
+  def limit_tags(html, okTags = 'a href, b, br, p, i, em')
+    # no closing tag necessary for these
+    soloTags = ["br","hr"]
+
+    # Build hash of allowed tags with allowed attributes
+    tags = okTags.downcase().split(',').collect!{ |s| s.split(' ') }
+    allowed = Hash.new
+    tags.each do |s|
+      key = s.shift
+      allowed[key] = s
+    end
+
+    # Analyze all <> elements
+    stack = Array.new
+    result = html.gsub( /(<.*?>)/m ) do | element |
+      if element =~ /\A<\/(\w+)/ then
+        # </tag>
+        tag = $1.downcase
+        if allowed.include?(tag) && stack.include?(tag) then
+          # If allowed and on the stack
+          # Then pop down the stack
+          top = stack.pop
+          out = "</#{top}>"
+          until top == tag do
+            top = stack.pop
+            out << "</#{top}>"
+          end
+          out
+        end
+      elsif element =~ /\A<(\w+)\s*\/>/
+        # <tag />
+        tag = $1.downcase
+        if allowed.include?(tag) then
+          "<#{tag} />"
+        end
+      elsif element =~ /\A<(\w+)/ then
+        # <tag ...>
+        tag = $1.downcase
+        if allowed.include?(tag) then
+          if ! soloTags.include?(tag) then
+            stack.push(tag)
+          end
+          if allowed[tag].length == 0 then
+            # no allowed attributes
+            "<#{tag}>"
+          else
+            # allowed attributes?
+            out = "<#{tag}"
+            while ( $' =~ /(\w+)=("[^"]+")/ )
+              attr = $1.downcase
+              valu = $2
+              if allowed[tag].include?(attr) then
+                out << " #{attr}=#{valu}"
+              end
+            end
+            out << ">"
+          end
+        end
+      end
+    end
+
+    # eat up unmatched leading >
+    while result.sub!(/\A([^<]*)>/m) { $1 } do end
+
+    # eat up unmatched trailing <
+    while result.sub!(/<([^>]*)\Z/m) { $1 } do end
+
+    # clean up the stack
+    if stack.length > 0 then
+      result << "</#{stack.reverse.join('></')}>"
+    end
+
+    result
+  end
+
   
   def translation_button
     if current_user.has_role? 'translator'
