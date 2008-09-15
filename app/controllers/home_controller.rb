c998577e5f634f79a2d922e7f088795636a90e39
shalott <shalott@gmail.com>
2008-09-15 04:21:58 +0000

    New homepage -- style not quite working but the code is all in, yay.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -13,8 +13,20 @@ class HomeController < ApplicationController
   # home page itself
   def index
     @user_count = User.count(:all)
+    @random_user = User.find_by_sql( "SELECT * FROM users WHERE id NOT IN 
+                                      ( SELECT users.id FROM users INNER JOIN pseuds ON users.id = pseuds.user_id 
+                                        INNER JOIN creatorships ON 
+                                        (pseuds.id = creatorships.pseud_id AND creatorships.creation_type = 'Work') 
+                                        INNER JOIN works ON 
+                                        (creatorships.creation_id = works.id AND works.restricted = true) )
+                                      ORDER BY RAND()
+                                      LIMIT 1" ).first
     @work_count = Work.count(:all)
     @works_today = Work.count(:all, :conditions => (['created_at > ?', 1.day.ago]))
+    fandom_category = TagCategory.find_or_create_by_name("Fandom")
+    @fandom_count = fandom_category.tags.count(:all, :conditions => {:canonical => true})
+    @latest_work = Work.find(:first, :conditions => {:restricted => false}, :order => "updated_at DESC")
+    @latest_fandom = fandom_category.tags.find(:first, :conditions => {:name => @latest_work.Fandom})
     render :action => "index", :layout => "home"
   end
   
