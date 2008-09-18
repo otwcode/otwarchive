c582203823934d889057b5188b1d099bd3a18ebf
zooey-glass <zooey.glass04@googlemail.com>
2008-09-18 08:25:18 +0000

    Adding text align buttons back into rich text editor, since we're now allowing align tags through.

--- public/javascripts/mce_editor.js
+++ public/javascripts/mce_editor.js
@@ -15,7 +15,7 @@ tinyMCE.init({
 
 	
 	// Theme options - using the advanced theme for now and just limiting the buttons used - we may want to create a custom theme in future.
-	theme_advanced_buttons1 : "pasteword,|,bold,italic,underline,strikethrough,|,link,unlink,image,|,blockquote,|,bullist,numlist,|,undo,redo",
+	theme_advanced_buttons1 : "pasteword,|,bold,italic,underline,strikethrough,|,link,unlink,image,|,blockquote,|,bullist,numlist,|,justifyleft,justifycenter,justifyright,justifyfull,|,undo,redo",
 	theme_advanced_buttons2 : "",
 	theme_advanced_buttons3 : "",
 	theme_advanced_toolbar_location : "top",
