a13357c26927cb15ef75882e9e7eb896e342abb4
shalott <shalott@gmail.com>
2008-09-18 18:11:17 +0000

    Added "common_timestring" to application helper to get the typical timestring used in strftime and Localize, and moved error-message helpers into validation_helper instead.

--- app/helpers/validation_helper.rb
+++ app/helpers/validation_helper.rb
@@ -1,5 +1,55 @@
 module ValidationHelper
 
+  # Set a custom error-message handler that puts the errors on 
+  # their respective fields instead of on the top of the page
+  ActionView::Base.field_error_proc = Proc.new {|html_tag, instance|
+    # don't put errors on the labels, duh
+    if !html_tag.match(/label/)
+      %(<span class='error'>#{html_tag}</span>)
+    elsif instance.error_message.kind_of?(Array)
+      %(<span class='error'>#{html_tag} <ul class='error'><li>#{instance.error_message.join('</li><li>')}</li></ul></span>)
+    else
+      %(<span class='error'>#{html_tag} #{instance.error_message}</span>)
+    end
+  }
+  
+  # A custom version of the error message display when something goes wrong 
+  # with model validation. Currently this is actually just the same as the
+  # default Rails method with translation included 
+  def error_messages_for(*params)
+    options = params.extract_options!.symbolize_keys
+    if object = options.delete(:object)
+      objects = [object].flatten
+    else
+      objects = params.collect {|object_name| instance_variable_get("@#{object_name}") }.compact
+    end
+    error_count   = objects.inject(0) {|sum, obj| sum + obj.errors.count }
+    unless error_count.zero?
+      html = {}
+      [:id, :class].each do |key|
+        if options.include?(key)
+          value = options[key]
+          html[key] = value unless value.blank?
+        else
+          html[key] = 'error'
+        end
+      end
+      options[:object_name] ||= params.first
+      options[:header_message] = "We couldn't save this %s, sorry!"/options[:object_name].to_s.gsub('_', ' ').t unless options.include?(:header_message)
+      options[:message] ||= 'Here are the problems we found:'.t unless options.include?(:message)
+      error_messages = objects.sum {|obj| obj.errors.full_messages.map {|msg| content_tag(:li, msg) } }.join
+      
+      contents = ''
+      contents << content_tag(options[:header_tag] || :h2, options[:header_message]) unless options[:header_message].blank?
+      contents << content_tag(:p, options[:message]) unless options[:message].blank?
+      contents << content_tag(:ul, error_messages)
+      
+      content_tag(:div, contents, html)
+    else
+          ''
+    end
+  end
+
   # use to make sure we have consistent name throughout
   def live_validation_varname(id)
     "validation_for_#{id}"
