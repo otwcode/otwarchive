ffed44693dcbd37907f186af435ccd9dc1c7336f
elzj <elzj78@gmail.com>
2008-09-26 00:53:49 +0000

    Tidying up the Javascript a bit.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+// This file is automatically included by javascript_include_tag :defaults 
+
+//things to do when the page loads
+document.observe("dom:loaded", function () {
+	hideFormFields(); 
+	hideFilters(); 
+	initSelect('languages_menu');
+});
 
 
 // Toggles the chaptered story section of the work form and resets the wip_length field
