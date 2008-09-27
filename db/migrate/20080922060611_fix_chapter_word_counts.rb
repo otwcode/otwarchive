c17e880c676b46e74267834aad34ae1c855dd6d6
ambtus <alice@alum.mit.edu>
2008-09-27 05:50:43 +0000

    prevent race commit on deploy when sphinx hasn't been started in the new releases directory - also, prevent each change from making a delta update

--- db/migrate/20080922060611_fix_chapter_word_counts.rb
+++ db/migrate/20080922060611_fix_chapter_word_counts.rb
@@ -1,8 +1,10 @@
 class FixChapterWordCounts < ActiveRecord::Migration
   def self.up
+    ThinkingSphinx.deltas_enabled=false
     full_sanitizer = HTML::FullSanitizer.new
     Chapter.all.each {|c| c.update_attribute(:word_count, full_sanitizer.sanitize(c.content).split.length)}
     Work.all.each {|w| w.update_attribute(:word_count, w.chapters.collect(&:word_count).compact.sum)}
+    ThinkingSphinx.deltas_enabled=true
   end
 
   def self.down
