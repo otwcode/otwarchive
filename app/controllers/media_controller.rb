3c1be991d2eb2bcd6a320096a896a51b51cf5b6a
elzj <elzj78@gmail.com>
2008-09-28 14:43:45 +0000

    Fixed last migration and tweaked media controller to get only canonical tags.

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -4,12 +4,12 @@ class MediaController < ApplicationController
     @media = TagCategory.find_or_create_by_name("Media").tags.find(:all, :order => 'taggings_count DESC')
     @fandom_listing = {}
     @media.each do |medium|
-      @fandom_listing[medium] = medium.tags.valid.find(:all, :limit => 5, :order => 'taggings_count DESC')
+      @fandom_listing[medium] = medium.tags.canonical.find(:all, :limit => 5, :order => 'taggings_count DESC')
     end
   end
   
   def show
     @medium = Tag.find(params[:id])
-    @fandoms = @medium.tags.valid.find(:all, :order => :name).paginate(:page => params[:page])
+    @fandoms = @medium.tags.canonical.find(:all, :order => :name).paginate(:page => params[:page])
   end
 end
--- db/migrate/20080927191115_add_boolean_defaults.rb
+++ db/migrate/20080927191115_add_boolean_defaults.rb
@@ -1,5 +1,31 @@
 class AddBooleanDefaults < ActiveRecord::Migration
   def self.up
+    Bookmark.update_all('hidden_by_admin=0', 'hidden_by_admin IS NULL')
+    Chapter.update_all('posted=0', 'posted IS NULL')
+    Chapter.update_all('hidden_by_admin=0', 'hidden_by_admin IS NULL')
+    Comment.update_all('hidden_by_admin=0', 'hidden_by_admin IS NULL')
+    Comment.update_all('is_deleted=0', 'is_deleted IS NULL')
+    Comment.update_all('approved=0', 'approved IS NULL')
+    ExternalWork.update_all('dead=0', 'dead IS NULL')
+    ExternalWork.update_all('hidden_by_admin=0', 'hidden_by_admin IS NULL')
+    Preference.update_all('edit_emails_off=0', 'edit_emails_off IS NULL')
+    Preference.update_all('comment_emails_off=0', 'comment_emails_off IS NULL')
+    Preference.update_all('hide_warnings=0', 'hide_warnings IS NULL')
+    Pseud.update_all('is_default=0', 'is_default IS NULL')
+    RelatedWork.update_all('reciprocal=0', 'reciprocal IS NULL')
+    Series.update_all('hidden_by_admin=0', 'hidden_by_admin IS NULL')
+    TagCategory.update_all('required=0', 'required IS NULL')
+    TagCategory.update_all('official=0', 'official IS NULL')
+    TagCategory.update_all('exclusive=0', 'exclusive IS NULL')
+    TagRelationshipKind.update_all('reciprocal=0', 'reciprocal IS NULL')
+    Tag.update_all('canonical=0', 'canonical IS NULL')
+    Tag.update_all('banned=0', 'banned IS NULL')
+    User.update_all('suspended=0', 'suspended IS NULL')
+    User.update_all('banned=0', 'banned IS NULL')
+    User.update_all('recently_reset=0', 'recently_reset IS NULL')
+    Work.update_all('posted=0', 'posted IS NULL')
+    Work.update_all('hidden_by_admin=0', 'hidden_by_admin IS NULL')
+    
     change_column :bookmarks, :hidden_by_admin, :boolean, :default => false, :null => false
     change_column :chapters, :posted, :boolean, :default => false, :null => false
     change_column :chapters, :hidden_by_admin, :boolean, :default => false, :null => false
