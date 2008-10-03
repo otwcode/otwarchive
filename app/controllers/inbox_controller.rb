bc606040ab4a098c5bcd1c65ee4e51873c66ee79
parenthetical <sarkymoocow@gmail.com>
2008-10-03 09:06:24 +0000

    Adding check that user is author of comment or work before they can delete a comment from a work or mark it as spam via their inbox. Notes under issue 329.

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -17,13 +17,23 @@ class InboxController < ApplicationController
       flash[:warning] = "Please select something first".t
     else
       if params[:commit] == "delete comments from story"
-        @selected_comment_ids.each {|c| Comment.find(c).destroy_or_mark_deleted }
+        @selected_comment_ids.each {|c| 
+        @comment = Comment.find(c)
+        if current_user.is_author_of?(@comment) || current_user.is_author_of?(@comment.ultimate_parent)
+          @comment.destroy_or_mark_deleted
+        else flash[:error] = "Sorry, you don't have permission to delete some of those comments".t
+        end }
       elsif params[:commit] == "read"
         @selected_comment_ids.each {|c| Comment.find(c).update_attribute(:is_read, true) }
       elsif params[:commit] == "unread"
         @selected_comment_ids.each {|c| Comment.find(c).update_attribute(:is_read, false) }
       elsif params[:commit] == "spam"
-        @selected_comment_ids.each {|c| Comment.find(c).mark_as_spam! }
+        @selected_comment_ids.each {|c| 
+        @comment = Comment.find(c)
+        if current_user.is_author_of?(@comment) || current_user.is_author_of?(@comment.ultimate_parent)      
+        @comment.mark_as_spam!
+        else flash[:error] = "Sorry, you don't have permission to mark some of those comments as spam".t
+        end }
       end
     end
     redirect_to user_inbox_path(@user)
