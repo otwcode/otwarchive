a5fbef70d5d2031aec06d99e03692b87a7e28aee
ambtus <alice@alum.mit.edu>
2008-11-22 10:25:29 +0000

    another nil error during deploy

--- app/models/pairing.rb
+++ app/models/pairing.rb
@@ -11,10 +11,12 @@ class Pairing < Tag
     if names.size > 1
       names.each do |character_name|
         character = Character.find_or_create_by_name(character_name)
-        character.update_attribute(:fandom_id, self.fandom_id) unless character.fandom
-        character.wrangle_canonical(update_works) if self.canonical
-        character.update_attribute(:wrangled, true) if self.wrangled
-        self.wrangle_parent(character, update_works)
+        if character
+          character.update_attribute(:fandom_id, self.fandom_id) unless character.fandom
+          character.wrangle_canonical(update_works) if self.canonical
+          character.update_attribute(:wrangled, true) if self.wrangled
+          self.wrangle_parent(character, update_works)
+        end
       end
     end
   end
