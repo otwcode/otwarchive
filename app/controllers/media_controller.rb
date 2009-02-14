63347c6453667bddb6528ecae6cfb45fee9062d8
ambtus <alice@alum.mit.edu>
2009-02-14 07:11:44 +0000

    issue 561

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,17 +1,17 @@
 class MediaController < ApplicationController
 
   def index
-    @fandom_listing = Fandom.canonical.group_by(&:media).sort_by{|array| array[1].size}.reverse + [[nil,Fandom.canonical.no_parent]]
+    @fandom_listing = Fandom.canonical.group_by(&:media).sort_by{|array| array[1].size}.reverse + [[nil,Fandom.no_parent]]
   end
 
   def show
     if params[:id] == "0"
       @medium_name = "Uncategorized Fandoms".t
-      @fandoms = Fandom.canonical.find(:all, :order => :name, :conditions => {:media_id => nil}).paginate(:page => params[:page])
+      @fandoms = Fandom.no_parent.by_name.paginate(:page => params[:page])
     else
       medium = Media.find_by_name(params[:id])
       @medium_name = medium.name
-      @fandoms = medium.fandoms.canonical.find(:all, :order => :name).paginate(:page => params[:page])
+      @fandoms = medium.fandoms.canonical.by_name.paginate(:page => params[:page])
     end
   end
 end
