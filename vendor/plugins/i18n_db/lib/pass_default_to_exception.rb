e7d1902496967d823bd70654df546d660360da9d
elzj <elzj78@gmail.com>
2009-03-15 22:16:29 +0000

    Backtracking some things while I work out a bug.

--- vendor/plugins/i18n_db/lib/pass_default_to_exception.rb
+++ vendor/plugins/i18n_db/lib/pass_default_to_exception.rb
@@ -23,18 +23,21 @@ module I18n
           if entry.nil?
             raise(I18n::MissingTranslationData.new(locale, key, options))
           else
-            Translation.add_default_to_db(locale, key, options) if Locale.find_main_cached.short == locale.to_s && options[:saved_default]
+            #Translation.add_default_to_db(locale, key, options) if Locale.find_main_cached.short == locale.to_s && options[:saved_default]
           end
         else
           if Locale.find_main_cached.short == locale.to_s && options[:saved_default] && entry != options[:saved_default]
+            if false
             entry = options[:saved_default]
-            n = key.split('.')
-            tr_key = n.last
-            namespace = (n - [n.last]).join('.')
-            translation = Locale.find_main_cached.translations.find(:first, :conditions => {:tr_key => tr_key, :namespace => namespace})
-            translation.text = options[:saved_default]
-            translation.updated = true
-            translation.save
+            #if key.is_a?(String)
+              n = key.split('.')
+              tr_key = n.last
+              namespace = (n - [n.last]).join('.')
+              translation = Locale.find_main_cached.translations.find(:first, :conditions => {:tr_key => tr_key, :namespace => namespace})
+              translation.text = options[:saved_default]
+              translation.updated = true
+              translation.save
+            end
           end
         end
         entry = pluralize(locale, entry, count)
