48c9f4a1c5c080b8d90f0a7efd6c1b5f8f858f56
zooey-glass <zooey.glass04@googlemail.com>
2009-03-16 22:09:07 +0000

    Requiring the latest version of will_paginate. Run rake gems:install.

--- config/environment.rb
+++ config/environment.rb
@@ -101,7 +101,7 @@ Rails::Initializer.run do |config|
 
   config.gem 'ruby-openid', :lib => 'openid'
   config.gem 'chronic'
-  config.gem 'mislav-will_paginate', :version => '~> 2.3.4', :lib => 'will_paginate', :source => 'http://gems.github.com'
+  config.gem 'mislav-will_paginate', :version => '~> 2.3.8', :lib => 'will_paginate', :source => 'http://gems.github.com'
   config.gem 'hpricot'
   config.gem 'thoughtbot-shoulda', :lib => 'shoulda/rails', :source => "http://gems.github.com", :version => '~> 2.10.0'
   config.gem 'andand'
