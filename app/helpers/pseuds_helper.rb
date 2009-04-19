f030bc79605cd2c0f575c04d91a94c0d7e169a08
elzj <elzj78@gmail.com>
2009-04-19 20:38:37 +0000

    Added formatting to translation notes and tweaked the pseud section of the user sidebar a tiny bit.

--- app/helpers/pseuds_helper.rb
+++ app/helpers/pseuds_helper.rb
@@ -9,7 +9,8 @@ module PseudsHelper
   # used in the sidebar
   def print_pseud_selector(pseuds)
     pseuds -= [@pseud] if @pseud && @pseud.new_record?
-    pseuds.sort.collect {|pseud| "<li>" + link_to_unless_current(pseud.name, [pseud.user, pseud]) + "</li>"}.join("")
+    list = "<li>Pseuds</li>"
+    list += pseuds.sort.collect {|pseud| "<li>" + link_to_unless_current(pseud.name, [pseud.user, pseud]) + "</li>"}.join("")
   end
 
   # For tag list on /people page
