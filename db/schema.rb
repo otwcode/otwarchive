17aa0b64f5945ecfeea9da6cdc367d042fec43f0
elzj <elzj78@gmail.com>
2009-05-25 02:22:30 +0000

    Schema update

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090524195217) do
+ActiveRecord::Schema.define(:version => 20090524201025) do
 
   create_table "abuse_reports", :force => true do |t|
     t.string   "email"
