9f131108e351efd4f1c7e6535ba8c06b0afb718a
cesy <cesy.avon@gmail.com>
2009-10-24 06:13:05 +0000

    Rest of fix for issue 184

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -11,8 +11,8 @@ class PasswordsController < ApplicationController
       @user.reset_user_password
       @user.save
       UserMailer.deliver_reset_password(@user)
-      flash[:notice] = t('check_email', :default => 'Check your email for your new password.')
-      redirect_to login_path 
+      flash[:notice] = t('check_email', :default => 'Check your email for your new password.') + ' <a href="session/new">' + t('layout.header.sign_in', :default => 'Sign in') + '</a>'
+      render :action => "new"
     rescue
       flash[:login] = '<br /><br /><div class="flash notice">' + t('try_again', :default => "We couldn't find an account with that email address or username. Please try again?") + "</div>"
       render :action => "new"
