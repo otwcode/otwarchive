995a513bde78d20f88ad65ab0322c34a4c07dfd2
shalott <shalott@gmail.com>
2009-11-28 18:49:05 +0000

    Some very simple code that can be used to redirect users coming from other sites to the imported version here (so that links don't need to break all over). Not really meant to be user-facing for the most part (this should be invoked by scripts running on the former domains), but it works along the same principles as http://archive.org if someone does want to manually look up a work.

---
+++ app/controllers/redirect_controller.rb
@@ -0,0 +1,32 @@
+class RedirectController < ApplicationController
+  before_filter :get_url_to_look_for
+  
+  def get_url_to_look_for
+    @original_url = params[:original_url] || ""
+    @minimal_url = @original_url
+    # strip it down to the most basic URL
+    @minimal_url.gsub!(/\?.*$/, "")
+    @minimal_url.gsub!(/\#.*$/, "")
+  end
+  
+  def show
+    if @original_url.blank?
+      flash[:error] = t('redirect.none', :default => "What url did you want to look up?")
+    else
+      @work = Work.find_by_imported_from_url(@original_url) || Work.find(:first, :conditions => ["imported_from_url LIKE :url", {:url => "%#{@minimal_url}%"}])
+      if @work
+        redirect_to work_path(@work) and return
+      else 
+        flash[:error] = t('redirect.failed', :default => "We could not find a work imported from that url in the Archive of Our Own, sorry! Try another url?")
+      end
+    end
+    redirect_to :action => :index
+  end 
+  
+  def index
+    if !@original_url.blank?
+      redirect_to :action => :show, :original_url => @original_url and return
+    end
+  end
+  
+end
