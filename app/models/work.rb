dbe4a7433a74d35afa5fa5fe825cbfbb2db177c8
amelia4otw <amelia4OTW@gmail.com>
2009-12-07 22:22:56 +0000

    issue 1004 changing hardcoded tag length references

--- app/models/work.rb
+++ app/models/work.rb
@@ -582,7 +582,7 @@ class Work < ActiveRecord::Base
   # Add an error message if the user tried to add invalid tags to the work
   def check_for_invalid_tags
     unless self.invalid_tags.blank?
-      errors.add_to_base("The following tags are invalid: " + self.invalid_tags.collect(&:name).join(', ') + ". Please make sure that your tags are less than 42 characters long.")
+      errors.add_to_base("The following tags are invalid: " + self.invalid_tags.collect(&:name).join(', ') + ". Please make sure that your tags are less than 100 characters long.")
     end
     self.invalid_tags.blank?  
   end
