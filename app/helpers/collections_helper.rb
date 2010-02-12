9d4c17e1dfab5061f2494d25cb132c29491d5506
enigel <enigel@gmail.com>
2010-02-12 01:19:17 +0000

    Issue 1282; moving some logic to the collections helper.

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -11,5 +11,26 @@ module CollectionsHelper
       link_to_remote text, {:url => fallback_url, :method => :get, :href => fallback_url}
     end
   end
+  
+  # show a section if it's not empty or if the parent collection has it
+  def show_collection_section(collection, section)
+    if ["intro", "faq", "rules"].include?(section) # just a check that we're not using a bogus section string
+      !eval("collection.collection_profile.#{section}").blank? || collection.parent && !eval("collection.parent.collection_profile.#{section}").blank?
+    end
+  end
 
+  # show collection preface if at least one section of the profile (or the parent collection's profile) is not empty
+  def show_collection_preface(collection)
+    show_collection_section(collection, "intro") || show_collection_section(collection, "faq") || show_collection_section(collection, "rules")
+  end
+  
+  # show navigation to relevant sections of the profile if needed
+  def show_collection_profile_navigation(collection, section)
+    ["intro", "faq", "rules"].each do |s|
+      if show_collection_section(collection, s) && s != section
+        return true # if at least one other section than the current one is not blank, we need the navigation; break out of the each...do
+      end
+    end
+    return false # if it passed through all tests above and not found a match, then we don't need the navigation
+  end
 end
