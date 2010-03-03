a64ffdd440af21a6075c705e135289f2a87e5f98
shalott <shalott@gmail.com>
2010-03-03 16:28:13 +0000

    Added custom validations module and moved the URL-validation from External Work out into that module for reuse in other places. Adds two new validations:
    validates_url_format_of
    Checks a URL against a regular expression to make sure it is both legal and reasonable (ie, we don't let people submit "http://localhost"). 
    
    validates_active_url_status_of
    Tries to hit the given URL with a quick HEAD request to make sure it is there and active. By default, this times out in 15 seconds but can take a different timeout as argument. 
    
    Also adds useful helper reformat_url for making URLs consistent (adds on http:// at the start if missing). 
    
    ExternalWork test updated to exercise this validation some.

--- app/models/external_work.rb
+++ app/models/external_work.rb
@@ -1,4 +1,5 @@
 class ExternalWork < ActiveRecord::Base
+  
   has_bookmarks
   has_many :user_tags, :through => :bookmarks, :source => :tags
   
@@ -35,35 +36,19 @@ class ExternalWork < ActiveRecord::Base
   validates_length_of :author, :maximum => AUTHOR_LENGTH_MAX, 
     :too_long=> t('author_too_long', :default => "must be less than {{max}} characters long.", :max => AUTHOR_LENGTH_MAX)
 
-  before_save :validate_url
+  # From the custom validations in config/initializers/validations.rb
+  validates_url_format_of :url
+  validates_url_active_status_of :url
+
+  before_validation :cleanup_url
   
-  # Standardizes format of urls so they're easier to validate and compare
-  def self.format_url(url)
-    url = "http://" + url if /http/.match(url[0..3]).nil?
-    url.chop! if url.last == "/"
-    url  
-  end 
-  
-  # Makes sure urls are valid and checks to see if they're active or not
-  def validate_url
-    self.url = ExternalWork.format_url(self.url)
-    errors.add_to_base(t('invalid_url', :default => "Not a valid URL")) unless self.url_active?
+  def cleanup_url
+    self.url = reformat_url(self.url) if self.url
   end
-  
+    
   # Sets the dead? attribute to true if the link is no longer active
   def set_url_status
     self.update_attribute(:dead, true) unless self.url_active?
-  end
-  
-  # Checks the status of the webpage at the external work's url
-  def url_active?
-    begin
-      response = Net::HTTP.get_response URI.parse(self.url)
-      active_status = %w(200 301 302)
-      active_status.include? response.code
-    rescue
-      false
-    end          
   end
   
   
--- test/unit/external_work_test.rb
+++ test/unit/external_work_test.rb
@@ -2,6 +2,9 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class ExternalWorkTest < ActiveSupport::TestCase
 
+  VALID_URLS    = ['http://godaddy.com', 'http://www.godaddy.com','www.godaddy.com', 'godaddy.com', 'http://archiveofourown.org/works/65087?show_comments=true#comments', ]
+  INVALID_URLS  = ['https://godaddy.com', 'ftp://godaddy.com', 'adfsadfsd', 'localhost', 'http://godaddy.com/" onclick="whee">']
+
   context "An external work" do
     setup do
       assert create_external_work
@@ -10,6 +13,8 @@ class ExternalWorkTest < ActiveSupport::TestCase
     should_validate_presence_of :author
     should_validate_presence_of :url
     should_ensure_length_in_range :title, (ArchiveConfig.TITLE_MIN..ArchiveConfig.TITLE_MAX), :long_message => /must be less/, :short_message => /must be at least/
+    should_allow_values_for :url, *VALID_URLS
+    should_not_allow_values_for :url, *INVALID_URLS
     context "with a summary" do
       setup do
         create_external_work(:summary => random_paragraph)
