00c6144eb4b306807df77868ec4226c4d19deadc
shalott <shalott@gmail.com>
2010-03-03 17:02:46 +0000

    Ugh, sorry, getting rid of auto-generated test folder.

--- test/unit/helpers/challenge_signups_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class ChallengeSignupsHelperTest < ActionView::TestCase
-end
--- test/unit/helpers/collection_profile_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class CollectionProfileHelperTest < ActionView::TestCase
-end
