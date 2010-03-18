c916fa5c55702be15cb53f06a933e346d5f78734
ambtus <alice@alum.mit.edu>
2010-03-18 19:22:41 +0000

    fix for potential memcached corruption. not testable

--- config/environment.rb
+++ config/environment.rb
@@ -152,3 +152,13 @@ begin
   end
 rescue NameError
 end
+
+if defined?(PhusionPassenger)
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+    if forked
+      Rails.cache.instance_variable_get(:@data).reset if Rails.cache.class == ActiveSupport::Cache::MemCacheStore
+    else
+      # No need to do anything.
+    end
+  end
+end
