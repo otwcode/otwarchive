7690500bd33a7e5de21c765bcb24b2ce39a34a7b
kendappa.ou <kendappa.ou@gmail.com>
2010-04-09 19:04:11 +0000

    Redirects bot, which have no referer, to homepage.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -93,7 +93,12 @@ class TagsController < ApplicationController
     else
       # This is just a quick fix to avoid script barf if JavaScript is disabled
       flash[:error] = t('need_javascript', :default => "Sorry, you need to have JavaScript enabled for this.")
-      redirect_to :back
+      if request.env["HTTP_REFERER"]
+				redirect_to :back
+      else
+				# else branch needed to deal with bots, which don't have a referer
+				redirect_to '/'
+			end
     end
   end
   
