6e9899214b17f642dff8c154b295328468c31467
elzj <elzj78@gmail.com>
2010-04-19 15:02:11 +0000

    Fix for issue 1496 - should now be able to access edit pages for tags with semicolons in them.

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -149,7 +149,7 @@ module TagsHelper
       last_comment = " (last comment: " + tag.total_comments.find(:first, :order => 'created_at DESC').created_at.to_s + ")"
     end
     link_text = count + " comments" + last_comment
-    link_to link_text, tag_comments_path(tag)
+    link_to link_text, {:controller => :comments, :action => :index, :tag_id => tag.name}
   end 
   
   def show_wrangling_dashboard
