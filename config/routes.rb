a3fb7309795538bd75869ef713fb5068df19edef
ambtus <alice@alum.mit.edu>
2010-05-01 18:08:00 +0000

    put back top level /search route. redirect to /works/search for now

--- config/routes.rb
+++ config/routes.rb
@@ -188,6 +188,8 @@ ActionController::Routing::Routes.draw do |map|
 
   map.devmode "/devmode", :controller => "devmode", :action => "index", :view => "index"
 
+  map.search "/search", :controller => "works", :action => "search"
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
