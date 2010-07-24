2e5de4cf0499c3e5923221b89d32815d72605d1c
maryann573 <maryann573@gmail.com>
2010-07-24 17:52:19 +0000

    slight change in redirect to help cucumber test issue 1781

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -34,7 +34,7 @@ class FeedbacksController < ApplicationController
           UserMailer.deliver_feedback(@feedback)
         end
         flash[:notice] = t('successfully_sent', :default => 'Your message was sent to the archive team - thank you!')
-        format.html { redirect_to '' }
+        format.html { redirect_back_or_default(root_path) }
       else
         flash[:error] = t('failure_send', :default => 'Sorry, your message could not be saved - please try again!')
         format.html { render :action => "new" }
