4053d2ffb018c806c542e31972e95c0ba8214259
shalott <shalott@gmail.com>
2010-09-01 18:36:22 +0000

    WAIT WAIT migrate after THIS update, sorry, turns out mysql on windows is unhappy with default values for text/blob fields. D:

--- db/migrate/20100901154501_add_description_to_skins.rb
+++ db/migrate/20100901154501_add_description_to_skins.rb
@@ -1,6 +1,6 @@
 class AddDescriptionToSkins < ActiveRecord::Migration
   def self.up
-    add_column :skins, :description, :text, :null => false, :default => ""
+    add_column :skins, :description, :text
   end
 
   def self.down
