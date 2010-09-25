7d14f953a0d65576a7745402ceceb2b9d5fd26f4
shalott <shalott@gmail.com>
2010-09-25 03:37:00 +0000

    Removing old version of project_search that was not ruby 1.9.2 compatible.

--- vendor/plugins/project_search/install.rb
+++
@@ -1,7 +0,0 @@
-require 'fileutils'
-
-source = File.join(RAILS_ROOT, 'vendor', 'plugins', 'project_search', 'script', 'find')
-target = File.join(RAILS_ROOT, 'script', 'find')
-
-FileUtils.cp_r(source, target)
-FileUtils.chmod(0755, target)
--- vendor/plugins/project_search/uninstall.rb
+++
@@ -1,2 +0,0 @@
-require 'fileutils'
-FileUtils.rm_f(File.join(RAILS_ROOT, "script", "find"))
