da35a416a94a88539ecc87c119c8d21d7d6b79c5
rbreu <rebecca@rbreu.de>
2010-09-30 19:31:09 +0000

    replacing deprecated .find(:all, ...) by .all(...)

--- app/controllers/archive_faqs_controller.rb
+++ app/controllers/archive_faqs_controller.rb
@@ -7,7 +7,7 @@ class ArchiveFaqsController < ApplicationController
   # GET /archive_faqs
   # GET /archive_faqs.xml
   def index
-    @archive_faqs = ArchiveFaq.find(:all, :order => 'position ASC')
+    @archive_faqs = ArchiveFaq.all(:order => 'position ASC')
 
     respond_to do |format|
       format.html # index.html.erb
@@ -44,7 +44,7 @@ class ArchiveFaqsController < ApplicationController
   
   # GET /archive_faqs/manage
   def manage
-    @archive_faqs = ArchiveFaq.find(:all, :order => 'position ASC')    
+    @archive_faqs = ArchiveFaq.all(:order => 'position ASC')    
   end
 
   # POST /archive_faqs
