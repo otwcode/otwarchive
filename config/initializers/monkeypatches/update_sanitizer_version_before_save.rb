36a0d1cae45fbd005b2a05881456c455cd584008
shalott <shalott@gmail.com>
2010-11-09 20:16:52 +0000

    Fixed the "can't mass-assign protected attributes" problem that prevented sanitizer version from actually being set and saved. :P

--- config/initializers/monkeypatches/update_sanitizer_version_before_save.rb
+++ config/initializers/monkeypatches/update_sanitizer_version_before_save.rb
@@ -2,13 +2,11 @@ module ActiveRecord
   class Base
     before_save :update_sanitizer_version
     def update_sanitizer_version
-      sanitizer_attrs = {}
       ArchiveConfig.FIELDS_ALLOWING_HTML.each do |field|
         if self.respond_to?("#{field}_changed?") && self.send("#{field}_changed?")
-          sanitizer_attrs["#{field}_sanitizer_version"] = ArchiveConfig.SANITIZER_VERSION
+          self.send("#{field}_sanitizer_version=", ArchiveConfig.SANITIZER_VERSION)
         end
       end
-      self.attributes = sanitizer_attrs
     end    
   end 
 end 
