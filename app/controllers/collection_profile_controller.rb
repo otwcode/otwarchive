2bf2911ac4fecfd32858012803f9c42f1ee952ee
shalott <shalott@gmail.com>
2010-11-09 22:36:08 +0000

    Fix for 500 errors when viewing profile of collection that doesn't exist.

--- app/controllers/collection_profile_controller.rb
+++ app/controllers/collection_profile_controller.rb
@@ -3,6 +3,10 @@ class CollectionProfileController < ApplicationController
   before_filter :load_collection
 
   def show
+    unless @collection
+      flash[:error] = "What collection did you want to look at?"
+      redirect_to collections_path
+    end
   end
 
 end
