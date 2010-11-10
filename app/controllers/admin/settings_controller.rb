44cc7f951affdb82860ebbbb74a44c4c3090bd07
ambtus <alice@alum.mit.edu>
2010-11-10 18:06:51 +0000

    fix for previous fix :(

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -3,13 +3,13 @@ class Admin::SettingsController < ApplicationController
   before_filter :admin_only
 
   def index
-    @admin_setting = AdminSetting.first || AdminSetting.new
+    @admin_setting = AdminSetting.first || AdminSetting.create
   end
 
   # PUT /admin_settings/1
   # PUT /admin_settings/1.xml
   def update
-    @admin_setting = AdminSetting.first || AdminSetting.new
+    @admin_setting = AdminSetting.first || AdminSetting.create
     if @admin_setting.update_attributes(params[:admin_setting])
       flash[:notice] = 'Archive settings were successfully updated.'
       redirect_to admin_settings_path
