4a5266bb14cfcfe7d58df8a4dccf0990c4cfee04
shalott <shalott@gmail.com>
2010-11-18 22:27:26 +0000

    Improvement in work cache sweeper: no longer expires blurb when no relevant modifications have been made.

--- app/sweepers/work_sweeper.rb
+++ app/sweepers/work_sweeper.rb
@@ -1,20 +1,17 @@
 class WorkSweeper < ActionController::Caching::Sweeper
   observe Work
   
-  def after_create(work)
-    expire_cache_for(work)
-  end
-
-  def after_update(work)
-    expire_cache_for(work)
+  def after_save(work)
+    changelist = work.changed.empty? ? [] : work.changed - %w(updated_at delta)
+    expire_work_blurb_cache_for(work) unless changelist.empty?
   end
 
   def after_destroy(work)
-    expire_cache_for(work)
+    expire_work_blurb_cache_for(work)
   end
   
   private
-  def expire_cache_for(work)
+  def expire_work_blurb_cache_for(work)
     # expire all the blurbs for this work
     expire_fragment("work-#{work.id}-nowarn-nofreeform")
     expire_fragment("work-#{work.id}-showwarn-nofreeform")
