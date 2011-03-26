793110f9559f6b0c3d277588733b2968f0f0d0d0
Cesy <cesy.avon@gmail.com>
2011-03-26 15:30:04 +0000

    Hopefully fixing the error 500 on challenge signups

--- app/controllers/challenge_signups_controller.rb
+++ app/controllers/challenge_signups_controller.rb
@@ -91,8 +91,10 @@ class ChallengeSignupsController < ApplicationController
       format.html {
           if @challenge.user_allowed_to_see_signups?(current_user)
             @challenge_signups = @collection.signups.joins(:pseud).paginate(:page => params[:page], :per_page => 20, :order => "pseuds.name")
-          else
+          elsif !current_user.nil?
             @challenge_signups = @collection.signups.by_user(current_user)
+          else
+            not_allowed
           end
       }
       format.xls {
