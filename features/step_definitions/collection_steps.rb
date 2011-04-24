0618278608876e7ef8f190ad4d5e26be99b76e05
pixel <thecatwasnot@gmail.com>
2011-04-24 08:01:11 -0500

    Fix rebase gone wrong, duplicate code in some step defs

--- features/step_definitions/collection_steps.rb
+++ features/step_definitions/collection_steps.rb
@@ -11,38 +11,6 @@ Given /^mod1 lives in Alaska$/ do
 end
 
 ### WHEN
-When /^I sign up for Battle 12$/ do
-  When "I go to the collections page"
-    And "I follow \"Battle 12\""
-    And "I follow \"Sign Up\""
-    And "I check \"challenge_signup_requests_attributes_0_fandom_28\""
-    And "I check \"challenge_signup_requests_attributes_1_fandom_28\""
-    And "I check \"challenge_signup_requests_attributes_1_anonymous\""
-    And "I fill in \"challenge_signup_requests_attributes_0_tag_set_attributes_freeform_tagnames\" with \"Something else weird\""
-    And "I press \"Submit\""
-end
-
-When /^I add prompt (\d+)$/ do |number|
-  When "I follow \"Add another prompt\""
-    And "I check \"challenge_signup_requests_attributes_#{number}_fandom_54\""
-    And "I press \"Submit\""
-end
-
-When /^I set up a basic promptmeme "([^\"]*)"$/ do |title|
-  visit new_collection_path
-  fill_in("collection_name", :with => "promptcollection")
-  fill_in("collection_title", :with => title)
-  select("Prompt Meme", :from => "challenge_type")
-  click_button("Submit")
-  check("prompt_meme_signup_open")
-  fill_in("prompt_meme_requests_num_allowed", :with => ArchiveConfig.PROMPT_MEME_PROMPTS_MAX)
-  fill_in("prompt_meme_requests_num_required", :with => 1)
-  fill_in("prompt_meme_request_restriction_attributes_fandom_num_required", :with => 1)
-  fill_in("prompt_meme_request_restriction_attributes_fandom_num_allowed", :with => 2)
-  click_button("Submit")
-  Then "I should see \"Challenge was successfully created\""
-end
-
 When /^I set up the collection "([^\"]*)"$/ do |title|
   visit new_collection_url
   fill_in("collection_name", :with => "testcollection")
