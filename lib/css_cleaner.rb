830aadf804d6924e7100018e30beb10eeec56b79
shalott <shalott@gmail.com>
2011-05-12 22:03:25 -0400

    FINALLY finished fixing the CSS parser... I HOPE OMG D:
    Added more and clearer tests, changed the cleaner to tokenize values so space-and-comma-separated values can be handled, allowed image urls for content, shifted a bunch of the value checking into regexps.
    
    Signed-off-by: shalott <shalott@gmail.com>

--- lib/css_cleaner.rb
+++ lib/css_cleaner.rb
@@ -4,6 +4,29 @@ include CssParser
 
 module CssCleaner
 
+  # constant regexps for css values
+  ALPHA_REGEX = Regexp.new('[a-z\-]+')
+  UNITS_REGEX = Regexp.new('deg|cm|em|ex|in|mm|pc|pt|px|s|%', Regexp::IGNORECASE)
+  NUMBER_REGEX = Regexp.new('-?\.?\d{1,3}\.?\d{0,3}')
+  NUMBER_WITH_UNIT_REGEX = Regexp.new("#{NUMBER_REGEX}\s*#{UNITS_REGEX}?\s*,?\s*")
+  PAREN_NUMBER_REGEX = Regexp.new('\(\s*' + NUMBER_WITH_UNIT_REGEX.to_s + '+\s*\)')
+  
+  FUNCTION_NAME_REGEX = Regexp.new('scalex?y?|translatex?y?|skewx?y?|rotate|matrix', Regexp::IGNORECASE)
+  TRANSFORM_FUNCTION_REGEX = Regexp.new("#{FUNCTION_NAME_REGEX}#{PAREN_NUMBER_REGEX}")
+  
+  RGBA_REGEX = Regexp.new('rgba?' + PAREN_NUMBER_REGEX.to_s, Regexp::IGNORECASE)
+  COLOR_REGEX = Regexp.new('#[0-9a-f]{3,6}|' + ALPHA_REGEX.to_s + '|' + RGBA_REGEX.to_s)
+  COLOR_STOP_FUNCTION_REGEX = Regexp.new('color-stop\s*\(' + NUMBER_WITH_UNIT_REGEX.to_s + '\s*\,?\s*' + COLOR_REGEX.to_s + '\s*\)', Regexp::IGNORECASE)
+
+  # from the ICANN list at http://www.icann.org/en/registries/top-level-domains.htm
+  TOP_LEVEL_DOMAINS = %w(ac ad ae aero af ag ai al am an ao aq ar arpa as asia at au aw ax az ba bb bd be bf bg bh bi biz bj bm bn bo br bs bt bv bw by bz ca cat cc cd cf cg ch ci ck cl cm cn co com coop cr cu cv cx cy cz de dj dk dm do dz ec edu ee eg er es et eu fi fj fk fm fo fr ga gb gd ge gf gg gh gi gl gm gn gov gp gq gr gs gt gu gw gy hk hm hn hr ht hu id ie il im in info int io iq ir is it je jm jo jobs jp ke kg kh ki km kn kp kr kw ky kz la lb lc li lk lr ls lt lu lv ly ma mc md me mg mh mil mk ml mm mn mo mobi mp mq mr ms mt mu museum mv mw mx my mz na name nc ne net nf ng ni nl no np nr nu nz om org pa pe pf pg ph pk pl pm pn pr pro ps pt pw py qa re ro rs ru rw sa sb sc sd se sg sh si sj sk sl sm sn so sr st su sv sy sz tc td tel tf tg th tj tk tl tm tn to tp tr travel tt tv tw tz ua ug uk us uy uz va vc ve vg vi vn vu wf ws xn xxx ye yt za zm zw)
+  URI_REGEX = Regexp.new('https?://\w[\w\-\.]+\.(' + TOP_LEVEL_DOMAINS.join('|') + ')/[\w\-\.\/]*[\w\-]\.(' + ArchiveConfig.SUPPORTED_EXTERNAL_URLS.join('|') + ')')
+  URL_REGEX = Regexp.new('"' + URI_REGEX.to_s + '"|\'' + URI_REGEX.to_s + '\'')
+  URL_FUNCTION_REGEX = Regexp.new('url\(\s*' + URL_REGEX.to_s + '\s*\)')
+  
+  VALUE_REGEX = Regexp.new("#{TRANSFORM_FUNCTION_REGEX}|#{URL_FUNCTION_REGEX}|#{COLOR_STOP_FUNCTION_REGEX}|#{COLOR_REGEX}|#{NUMBER_WITH_UNIT_REGEX}|#{ALPHA_REGEX}")
+
+
   # For use in ActiveRecord models 
   # We parse and clean the CSS line by line in order to provide more helpful error messages.
   # The prefix is used if you want to make sure a particular prefix appears on all the selectors in 
@@ -76,102 +99,100 @@ module CssCleaner
       end
     elsif property == "content"
       clean = sanitize_css_content(value)
+    elsif value.match(/\burl\b/) && (!ArchiveConfig.SUPPORTED_CSS_KEYWORDS.include?("url") || !%w(background background-image border border-image list-style list-style-image).include?(property))
+      # check whether we can use urls in this property
+      clean = ""
     elsif property.match(/#{ArchiveConfig.SUPPORTED_CSS_SHORTHAND_PROPERTIES.join('|')}/)
-      if value.match(/#{ArchiveConfig.SUPPORTED_CSS_FUNCTIONS.join('|')}/)
-        clean = sanitize_css_function(value)
-      else
-        clean = sanitize_css_shorthand_value(value)
-      end
+      clean = tokenize_and_sanitize_css_value(value)
     elsif ArchiveConfig.SUPPORTED_CSS_PROPERTIES.include?(property)
-      if property == "content"
-        # sanitize content here
-      else 
-        clean = sanitize_css_value(value)
-      end
+      clean = sanitize_css_value(value)
     end
     clean.strip
   end
 
+  # divide a css value into tokens and clean them individually
+  def tokenize_and_sanitize_css_value(value)
+    cleanval = ""
+    scanner = StringScanner.new(value)
+    
+    # we scan until we find either a space, a comma, or an open parenthesis
+    while scanner.exist?(/\s+|,|\(/)
+      # we have some tokens left to break up
+      in_paren = 0
+      token = scanner.scan_until(/\s+|,|\(/)
+      if token.blank? || token == ","
+        cleanval += token
+        next
+      end
+      in_paren = 1 if token.match(/\($/)
+      while in_paren > 0
+        # scan until closing paren or another opening paren
+        nextpart = scanner.scan_until(/\(|\)/)
+        if nextpart
+          token += nextpart
+          in_paren += 1 if token.match(/\($/)
+          in_paren -= 1 if token.match(/\)$/)
+        else
+          # mismatched parens
+          return ""
+        end
+      end
+      
+      # we now have a single token
+      separator = token.match(/(\s|,)$/) || ""
+      token.strip! 
+      token.chomp!(',')
+      cleantoken = sanitize_css_token(token)
+      return "" if cleantoken.blank?
+      cleanval += cleantoken + separator.to_s
+    end
+    
+    token = scanner.rest
+    if token && !token.blank?
+      cleantoken = sanitize_css_token(token)
+      return "" if cleantoken.blank?
+      cleanval += cleantoken
+    end
+    
+    return cleanval
+  end
+      
+  def sanitize_css_token(token)
+    cleantoken = ""
+    if token.match(/gradient/)
+      cleantoken = sanitize_css_gradient(token)
+    else
+      cleantoken = sanitize_css_value(token)
+    end
+    return cleantoken
+  end
 
-  # sanitize a CSS function, eg, gradient
-  # background:-moz-linear-gradient(bottom, rgba(120,120,120,1) 5%, rgba(94,94,94,1) 50%, rgba(108,108,108,1) 55%, rgba(137,137,137,1) 100%) ;
-  def sanitize_css_function(value)
+  # sanitize a CSS gradient
+  # background:-webkit-gradient( linear, left bottom, left top, color-stop(0, rgb(82,82,82)), color-stop(1, rgb(125,124,125)));
+  # -moz-linear-gradient(bottom, rgba(120,120,120,1) 5%, rgba(94,94,94,1) 50%, rgba(108,108,108,1) 55%, rgba(137,137,137,1) 100%);
+  def sanitize_css_gradient(value)
     if value.match(/^([a-z\-]+)\((.*)\)/)
       function = $1
-      cleaned_interior = sanitize_css_shorthand_value($2)
-      if function.match(/#{ArchiveConfig.SUPPORTED_CSS_FUNCTIONS.join('|')}/) && !cleaned_interior.blank?
-        return "#{function}(#{cleaned_interior});"
+      interior = $2
+      cleaned_interior = tokenize_and_sanitize_css_value(interior)
+      if function.match(/gradient/) && !cleaned_interior.blank?
+        return "#{function}(#{cleaned_interior})"
       end
     end
     return ""
   end
-
-
-  # Shorthand rule -- might have multiple space-separated and/or comma-separated values 
-  # AND might have rgba values in there also, aagh
-  def sanitize_css_shorthand_value(value)
-    cleanval = []
-    rgba_value = ""
-    # get rid of spaces after commas
-    value.split(", ").join(",").split(",").each do |value_section|        
-      # Rejoin rgba values before checking
-      if rgba_value.blank? && value_section.match(/rgba?\(/)
-        rgba_value = value_section
-        next
-      elsif !rgba_value.blank?
-        rgba_value += "," + value_section
-        if rgba_value.match(/rgba?\(.*\)/)
-          # we've completed the rgba value, go ahead and sanitize it
-          value_section = rgba_value
-          rgba_value = ""
-        else
-          next
-        end
-      end
-      
-      clean_section = []
-      value_section.split.each do |keyword|
-        if sanitize_css_value(keyword).blank?
-          # bad value somewhere, break
-          clean_section = []
-          break
-        else
-          clean_section << sanitize_css_value(keyword) 
-        end
-      end
-      if clean_section.empty?
-        cleanval = []
-        break
-      else
-        cleanval << clean_section.join(' ')
-      end
-    end
-    cleanval.join(', ')
-  end
-
+  
 
   # all values must either appear in ArchiveConfig.SUPPORTED_CSS_KEYWORDS, be urls of the format url(http://url/) or be 
   # rgba(), hex (#), or numeric values, or a comma-separated list of same
   def sanitize_css_value(value)
     value_stripped = value.downcase.gsub(/(!important)/, '').strip
 
-    # handle urls if we want to support them
-    if value_stripped.match(/\burl\b/)
-      if ArchiveConfig.SUPPORTED_CSS_KEYWORDS.include?("url")
-        return sanitize_css_url(value)
-      else
-        return ""
-      end
-    end
-
-    # If it's an ordinary alphabetic string, it's fine
-    return value if value_stripped.split(',').all? {|subval| subval.strip =~ /^[a-z\-]+$/}
+    # if it's a comma-separated set of valid values it's fine
+    return value if value_stripped =~ /^(#{VALUE_REGEX}\,?)+$/i
 
     # If it's explicitly in our keywords it's fine
     return value if value_stripped.split(',').all? {|subval| ArchiveConfig.SUPPORTED_CSS_KEYWORDS.include?(subval.strip)}
-
-    # if it's an rgba, hex, percentage, or numeric value it's fine
-    return value if value_stripped =~ /^(#[0-9a-f]+|rgba?\(\d+%?,? ?\d*%?,? ?\d*%?,? ?\d{0,3}?\.?\d{0,3}?\)|\-?\d{0,3}\.?\d{0,3}(deg|cm|em|ex|in|mm|pc|pt|px|s|%|,)?)$/
 
     return ""
   end
@@ -181,6 +202,9 @@ module CssCleaner
     # For now we only allow a single completely quoted string
     return value if value =~ /^\'([^\']*)\'$/      
     return value if value =~ /^\"([^\"]*)\"$/
+    
+    # or a valid img url
+    return value if value.match(URL_FUNCTION_REGEX)
     
     return ""
   end
@@ -195,23 +219,5 @@ module CssCleaner
       return ""
     end
   end 
-
-  # URL values must be of the format:
-  # url(url name)
-  # http:// or https:// protocol
-  # can be inside single or double quotes but must match
-  # extra space inside or outside the enclosing parentheses is fine
-  # must end in an allowed type (eg, jpg, png, gif)
-  def sanitize_css_url(value)
-    if value.downcase.match(/^\s*url\s*\(\s*([\"|\'])?(https?:\/\/[\-\w\.\/]+)([\"|\'])?\s*\)\s*$/)
-      url = $2
-      # make sure url ends in an allowed type and quotes are balanced if present
-      if $1 == $3 && url.match(/\.(#{ArchiveConfig.SUPPORTED_EXTERNAL_URLS.join('|')})$/)
-        return value
-      end
-    else
-      return ""
-    end
-  end
   
 end
