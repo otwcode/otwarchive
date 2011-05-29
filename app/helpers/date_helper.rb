4bb079a15f76a6572c49a1c87e08c0591f650345
Gabriele Schulz <gschulz@gschulz.de>
2011-05-29 10:13:28 +0000

    Issue 2292 Display revision date in work blurb in time zone of current user
    http://code.google.com/p/otwarchive/issues/detail?id=2292

--- app/helpers/date_helper.rb
+++ app/helpers/date_helper.rb
@@ -6,7 +6,12 @@ module DateHelper
     if datetime > 30.days.ago && !AdminSetting.enable_test_caching?
       time_ago_in_words(datetime)
     else
-      datetime.to_date.to_formatted_s(:rfc822)
+      adjusted_time = datetime
+      if logged_in? && current_user.preference.time_zone
+        zone = current_user.preference.time_zone
+        adjusted_time = datetime.in_time_zone(current_user.preference.time_zone)
+      end
+      adjusted_time.to_date.to_formatted_s(:rfc822)
     end
   end
 
