c68b96e6971f2e0025c8ad9da74807593c4d0746
Rebecca <rebecca@rbreu.de>
2011-06-23 17:51:12 +0200

    basic spec test for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,28 @@ require 'spec_helper'
 
 describe User do
 
-  it "should create a new user" do
-    user = User.create(:login => "myname",
-                        :age_over_13 => true,
-                        :terms_of_service => true,
-                        :email => "foo1@archiveofourown.org",
-                        :password => "password")
-    user.login.should == "myname"
+  describe "save" do
+  
+    before(:each) do
+      @user = User.new
+      @user.login = "myname"
+      @user.age_over_13 = "1"
+      @user.terms_of_service = "1"
+      @user.email = "foo1@archiveofourown.org"
+      @user.password = "password"
+    end
+    
+    it "should save a minimalistic user" do
+      @user.save
+      @user.save.should == true
+  end
+    
+    it "should not save user with too short login" do
+      @user.login = "a"
+      @user.save.should == false
+      @user.errors[:login].should_not be_empty
+    end
+    
   end
 
 end
