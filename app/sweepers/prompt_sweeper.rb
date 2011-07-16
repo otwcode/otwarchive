d4c59f7df3ddba4e0f8beb921e2650402dc394f2
Cesy <cesy.avon@gmail.com>
2011-07-16 08:51:27 +0000

    Issue 2472 prompt caching

---
+++ app/sweepers/prompt_sweeper.rb
@@ -0,0 +1,52 @@
+class PromptSweeper < ActionController::Caching::Sweeper
+  observe Prompt
+  
+  def after_save(record)
+    expire_challenge_signup_cache_for(record)
+  end
+
+  def after_destroy(record)
+    expire_challenge_signup_cache_for(record)
+  end
+  
+  private
+  # return one or many collections associated with the changed record 
+  # converted into an array
+  def get_collections_from_record(record)
+    if record.is_a?(Collection)
+      # send collection, its parent, and any children 
+      ([record, record.parent] + record.children).compact
+    elsif record.respond_to?(:collection) && !record.collection.nil?
+      ([record.collection, record.collection.parent] + record.collection.children).compact
+    elsif record.respond_to?(:collections)
+      (record.collections + record.collections.collect(&:parent) + record.collections.collect(&:children).flatten).compact
+    else
+      []
+    end
+  end
+  
+  def get_requests_from_record(record, collection)
+    Prompt.find(:collection => collection, :type => 'request', :challenge_signup => get_signup_from_record(record))
+  end
+  
+  def get_signup_from_record(record)
+    ChallengeSignup.find_by_id(record.challenge_signup_id)
+  end
+      
+  # Whenever these records are updated, we need to blank out the collections cache
+  def expire_challenge_signup_cache_for(record)
+    collections = get_collections_from_record(record)
+    collections.each do |collection|
+      requests = get_requests_from_record(record, collection)
+      requests.each do |request|
+        # expire the request summary
+        expire_fragment("collection-#{collection.id}-request-#{request.id}")
+        # expire the prompt summary too
+        expire_fragment("collection-#{collection.id}-prompt-#{request.id}")
+      end
+      # expire the signup summary, for prompt meme challenge prompts index
+      expire_fragment("collection-#{collection.id}-signup-#{record.id}")
+    end
+  end
+
+end
