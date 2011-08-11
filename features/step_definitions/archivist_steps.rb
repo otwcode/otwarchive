b030f0efe2d7564a2f91cd8eaca40a71715999c5
Cesy <cesy.avon@gmail.com>
2011-08-11 17:59:14 +0000

    Tidying bits suggested in code review

--- features/step_definitions/archivist_steps.rb
+++ features/step_definitions/archivist_steps.rb
@@ -4,7 +4,7 @@ Given /^I have an archivist "([^\"]*)"$/ do |name|
   Given %{I am logged in as "#{name}"}
     And %{I have loaded the "roles" fixture}
   When %{I am logged in as an admin}
-      And %{I fill in "query" with "elynross"}
+      And %{I fill in "query" with "#{name}"}
       And %{I press "Find"}
     When %{I check "user_roles_4"}
       And %{I press "Update"}
