8ba0d9cfd126ab4e9344c70a9a056180967f0b85
maryann573 <maryann573@gmail.com>
2011-10-10 16:56:17 +0000

    changing failed login attempts to 50 and lockout time to 5 minutes

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,4 +1,4 @@
 class UserSession < Authlogic::Session::Base
-consecutive_failed_logins_limit 5
-failed_login_ban_for 30.minutes
+consecutive_failed_logins_limit 50
+failed_login_ban_for 5.minutes
 end
