5596d185876edd163791143c0eeb28b575433596
Phoenix Wolfe <Phoenix.wolfe@gmail.com>
2011-11-22 22:08:36 +0000

    Missed one thing

--- db/migrate/20111115225340_add_comment_to_psueds.rb
+++
@@ -1,9 +0,0 @@
-class AddCommentToPsueds < ActiveRecord::Migration
-  def self.up
-  add_column :psueds, :icon_comment_text, :string, :default => "" :null
-  end
-
-  def self.down
-  remove_column :psueds, :icon_comment_text
-  end
-end
