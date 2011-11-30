9fcdf2dcd40830a77e08ee4e32a22b586309da41
Cesy <cesy.avon@gmail.com>
2011-11-30 20:25:05 +0000

    Updating view test

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,14 +1,42 @@
 require 'spec_helper'
  	
 describe "home/index.html.erb" do
-  it "should display the welcome box" do
+
+  it "should display the typical boxes and key words" do
     view.stub(:logged_in?).and_return(false)
     view.stub(:logged_in_as_admin?).and_return(false)
     render
     rendered.should contain 'Welcome'
-    rendered.should contain 'News'
+    rendered.should_not contain 'News'
     rendered.should contain 'Archive'
-    rendered.should contain 'fandoms'
+    rendered.should have_selector('ul', :class => 'navigation', :content => 'fandoms')
+  end
+  
+  context "when logged in as a regular user" do
+
+      before do
+        view.stub(:logged_in?).and_return(true)
+        view.stub(:logged_in_as_admin?).and_return(false)
+        render
+      end
+      
+      it "should display invites link" do
+        rendered.should contain "Invite a friend"
+      end
+      
+  end
+  
+  context "when admin posts exist" do
+
+      before do
+        @adminpost = Factory.create(:admin_post)
+        view.stub(:logged_in?).and_return(false)
+        view.stub(:logged_in_as_admin?).and_return(false)
+        render
+      end
+      
+      it "should display news posts"
+      
   end
 
 end
