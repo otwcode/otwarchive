52b1baeb07ae541c2e0b469e95639ad1f90f3fc7
Phoenix Wolfe <Phoenix.wolfe@gmail.com>
2011-12-06 20:33:02 +0000

    Adding Alt and Comment boxes for collection/challenge icons

--- db/migrate/20111122225341_add_icontext_to_collections.rb
+++ db/migrate/20111122225341_add_icontext_to_collections.rb
@@ -1,9 +1,9 @@
 class AddIcontextToCollections < ActiveRecord::Migration
   def self.up
-  add_column :pseuds, :icon_alt_text, :string, :default => ""
+  add_column :collections, :icon_alt_text, :string, :default => ""
   end
 
   def self.down
-  remove_column :pseuds, :icon_alt_text
+  remove_column :collections, :icon_alt_text
   end
 end
