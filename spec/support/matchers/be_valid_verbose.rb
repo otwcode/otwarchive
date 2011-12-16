08730642323ef58d51208163eb7d47b4c3d68770
Cesy <cesy.avon@gmail.com>
2011-12-16 18:20:52 +0000

    Pseud spec first draft

---
+++ spec/support/matchers/be_valid_verbose.rb
@@ -0,0 +1,17 @@
+RSpec::Matchers.define :be_valid_verbose do
+  match do |model|
+    model.valid?
+  end
+
+  failure_message_for_should do |model|
+    "#{model.class} expected to be valid but had errors:n #{model.errors.full_messages.join("n ")}"
+  end
+
+  failure_message_for_should_not do |model|
+    "#{model.class} expected to have errors, but it did not"
+  end
+
+  description do
+    "be valid"
+  end
+end
