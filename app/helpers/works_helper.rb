2727b7381afd2ddbd667099f1cf6aed081edc422
elz <elzj78@gmail.com>
2011-12-19 22:45:47 -0500

    More formatting tweaks

--- app/helpers/works_helper.rb
+++ app/helpers/works_helper.rb
@@ -169,21 +169,26 @@ module WorksHelper
   # Generates a list of a work's tags and details for use in feeds
   def feed_summary(work)
     tags = work.tags.group_by(&:type)
-    %w(Fandom Rating Warning Category Character Relationship Freeform).each do |type|
-      tags[type] ||= []
-    end
-    fandoms = tags['Fandom'].map{ |t| link_to_tag_works(t) }.join(', ')
-    rating_and_warnings = (tags['Rating'] + tags['Warning'] + tags['Category']).map{ |t| link_to_tag_works(t) }.join(', ')
-    other_tags = (tags['Character'] + tags['Relationship'] + tags['Freeform']).map{ |t| link_to_tag_works(t) }.join(', ')
-    text = work.summary
-    text << "<ul>"
-    text << "<li>Author: #{byline(work, :visibility => 'public')}</li>"
-    text << "<li>Fandom: #{fandoms}</li>"
-    text << "<li>Rating, warnings, and categories: #{rating_and_warnings}</li>"
-    text << "<li>Characters, relationships, and other tags: #{other_tags}</li>"
-    text << "<li>Words: #{work.word_count}, Chapters: #{work.chapter_total_display}, Language: #{work.language ? work.language.name : 'English'}</li>"
+    text = "<p>by #{byline(work, :visibility => 'public')}</p>"
+    text << work.summary
+    text << "<p>Words: #{work.word_count}, Chapters: #{work.chapter_total_display}, Language: #{work.language ? work.language.name : 'English'}</p>"
     unless work.series.count == 0
-      text << "<li>Series: #{series_list_for_feeds(work)}</li>"
+      text << "<p>Series: #{series_list_for_feeds(work)}</p>"
+    end
+    # Create list of tags
+    text << "<ul>"
+    %w(Fandom Rating Warning Category Character Relationship Freeform).each do |type|
+      if tags[type]
+        label = case type
+        when 'Freeform'
+          'Additional Tags'
+        when 'Rating'
+          'Rating'
+        else
+          type.pluralize
+        end
+        text << "<li>#{label}: #{tags[type].map{ |t| link_to_tag_works(t) }.join(', ')}</li>"
+      end
     end
     text << "</ul>"
     text
