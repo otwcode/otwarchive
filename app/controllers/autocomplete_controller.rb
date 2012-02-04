542eacc0c00bd17f93a982dc0347029b3d2b1ce9
shalott <shalott@gmail.com>
2012-02-03 21:18:51 -0500

    autocomplete tweak for pseud autocomplete
    selects user's own pseuds when no argument passed to autocomplete
    
    Signed-off-by: shalott <shalott@gmail.com>

--- app/controllers/autocomplete_controller.rb
+++ app/controllers/autocomplete_controller.rb
@@ -23,7 +23,13 @@ class AutocompleteController < ApplicationController
   
   # PSEUDS
   def pseud
-    render_output(Pseud.autocomplete_lookup(:search_param => params[:term], :autocomplete_prefix => "autocomplete_pseud").map {|res| Pseud.fullname_from_autocomplete(res)})
+    if params[:term].blank?
+      # get the user's own pseuds
+      set_current_user
+      render_output(current_user.pseuds.collect(&:byline))
+    else
+      render_output(Pseud.autocomplete_lookup(:search_param => params[:term], :autocomplete_prefix => "autocomplete_pseud").map {|res| Pseud.fullname_from_autocomplete(res)})
+    end
   end
   
   ## TAGS  
