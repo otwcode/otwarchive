21f61645b931ae6d638c7d6c9da92931620ff604
sarken <sarken@gmail.com>
2012-02-10 23:29:27 +0000

    Partial fix for issue 2887

--- app/helpers/series_helper.rb
+++ app/helpers/series_helper.rb
@@ -1,20 +1,23 @@
 module SeriesHelper 
   
   def show_series_data(work)
-    # this should only show prev and next works visible to the current user
+    series_data = series_data_for_work(work)
+    series_data.join(ArchiveConfig.DELIMITER_FOR_OUTPUT).html_safe
+  end
+  
+  # this should only show prev and next works visible to the current user
+  def series_data_for_work(work)
     series = work.series.select{|s| s.visible?(current_user)}
-    series_data = series.map do |serial|
-      # cull visible works
+    series.map do |serial|
       serial_works = serial.serial_works.find(:all, :include => :work, :conditions => ['works.posted = ?', true], :order => :position).select{|sw| sw.work.visible(current_user)}.collect{|sw| sw.work}
       visible_position = serial_works.index(work) || serial_works.length     
       unless !visible_position
-        previous_link = (visible_position > 0) ? link_to("&laquo; ".html_safe, serial_works[visible_position - 1]) : ""
+        previous_link = (visible_position > 0) ? link_to("&laquo; ".html_safe, serial_works[visible_position - 1]) : "".html_safe
         main_link = ("Part " + (visible_position+1).to_s + " of the " + link_to(serial.title, serial) + " series").html_safe
-        next_link = (visible_position < serial_works.size-1) ? link_to(" &raquo;".html_safe, serial_works[visible_position + 1]) : ""
+        next_link = (visible_position < serial_works.size-1) ? link_to(" &raquo;".html_safe, serial_works[visible_position + 1]) : "".html_safe
         previous_link + main_link + next_link
       end
     end
-    series_data.join(ArchiveConfig.DELIMITER_FOR_OUTPUT).html_safe
   end
   
   def work_series_description(work, series)
