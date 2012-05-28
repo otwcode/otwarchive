cfae72322020b41b8a5826e4c63e66410c54b942
Lalith Vipulananthan <lalithv@gmail.com>
2012-05-28 14:45:09 +0000

    Reverted change and added skip_before_filter instead

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
   before_filter :load_user, :except => [:activate, :create, :delete_confirmation, :index, :new]
   before_filter :check_ownership, :except => [:activate, :browse, :create, :delete_confirmation, :index, :new, :show]  
   before_filter :check_account_creation_status, :only => [:new, :create]
+  skip_before_filter :store_location, :only => [:end_first_login]
+
 
   # This is meant to rescue from race conditions that sometimes occur on user creation
   # The unique index on login (database level) prevents the duplicate user from being created,
