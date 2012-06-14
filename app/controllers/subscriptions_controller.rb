c7dbbb969b08cae7249bbcb9600cc0da4d2c8db9
Scott D. Sullivan <scottsds@gmail.com>
2012-06-14 09:56:56 +0000

    Issue 3112: Change confirmation message to point to your Subscriptions page

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -25,7 +25,7 @@ class SubscriptionsController < ApplicationController
     respond_to do |format|
       if @subscription.save
         format.html {
-          flash[:notice] = ts("You are now following %{name}. If you'd like to stop receiving email updates, you can return to this page and click 'Unsubscribe'.", :name => @subscription.name)
+          setflash; flash[:notice] = ts("You are now following %{name}. If you'd like to stop receiving email updates, you can unsubscribe from <a href=\"#{user_subscriptions_url}\">your Subscriptions page</a>.".html_safe, :name => @subscription.name)
           # redirect_back_or_default(@subscription.subscribable) # it always returns to subscriptions rather than the subscribable
           redirect_to(@subscription.subscribable)
         }
