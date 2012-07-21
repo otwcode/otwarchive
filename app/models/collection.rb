a91ef541f3ecba6822757fce2ec495684aefdead
Scott D. Sullivan <scottsds@gmail.com>
2012-07-21 06:42:48 +0000

    Issue 3171: verification needed on collection name creation to avoid commas

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -146,6 +146,9 @@ class Collection < ActiveRecord::Base
   def no_reserved_strings
     errors.add(:title, ts("^Sorry, we've had to reserve the ',,' string for behind-the-scenes usage!")) if
       title.match(/\,\,/)
+
+    errors.add(:title, ts("The ',' character cannot be in a Display Title.")) if
+      title.match(/\,/)
   end
 
   validates_length_of :description,
