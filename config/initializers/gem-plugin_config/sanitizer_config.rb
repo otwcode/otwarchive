27837d7d7839545d5c6c0139ebe81320f0c4d895
Sam Johnsson <samjohnsson@gmail.com>
2012-07-25 14:26:50 -0300

    Update config/initializers/gem-plugin_config/sanitizer_config.rb

--- config/initializers/gem-plugin_config/sanitizer_config.rb
+++ config/initializers/gem-plugin_config/sanitizer_config.rb
@@ -109,7 +109,7 @@ class Sanitize
       # if we don't know the source, sorry
       return if source.nil?           
 
-      allow_flashvars = ["ning", "vidders.net", "google", "criticalcommons"]
+      allow_flashvars = ["ning", "vidders.net", "google", "criticalcommons", "archiveofourown"]
 
       # We're now certain that this is an embed from a trusted source, but we still need to run
       # it through a special Sanitize step to ensure that no unwanted elements or
