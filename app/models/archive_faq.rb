3c67f4352d0f22689bd6cf798e99c50fd0d5703e
Scott D. Sullivan <scottsds@gmail.com>
2012-07-30 21:09:36 -0400

    Changed views, updated archive_faq controller and model

--- app/models/archive_faq.rb
+++ app/models/archive_faq.rb
@@ -1,15 +1,20 @@
 class ArchiveFaq < ActiveRecord::Base
   acts_as_list
 
+  has_many :questions, :dependent => :destroy
+  accepts_nested_attributes_for :questions, :reject_if => lambda { |a| a[:question].blank? }, :allow_destroy => true
+
   attr_protected :content_sanitizer_version
 
-  validates_presence_of :content
-  validates_length_of :content, :minimum => ArchiveConfig.CONTENT_MIN, 
-    :too_short => ts("must be at least %{min} letters long.", :min => ArchiveConfig.CONTENT_MIN)
+  # validates_presence_of :content
+  # CONTENT HAS BEEN REPLACED WITH 'ANSWER' in the Questions Model
+  # validates_length_of :content, :minimum => ArchiveConfig.CONTENT_MIN,
+  #   :too_short => ts("must be at least %{min} letters long.", :min => ArchiveConfig.CONTENT_MIN)
 
-  validates_length_of :content, :maximum => ArchiveConfig.CONTENT_MAX, 
-    :too_long => ts("cannot be more than %{max} characters long.", :max => ArchiveConfig.CONTENT_MAX)
-    
+  # validates_length_of :content, :maximum => ArchiveConfig.CONTENT_MAX,
+  #   :too_long => ts("cannot be more than %{max} characters long.", :max => ArchiveConfig.CONTENT_MAX)
+  #   :too_long => ts("cannot be more than %{max} characters long.", :max => ArchiveConfig.CONTENT_MAX)
+
   def self.reorder(positions)
     SortableList.new(self.find(:all, :order => 'position ASC')).reorder_list(positions)
   end
