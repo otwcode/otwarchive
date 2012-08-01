d689d84b8f414e78bb813e01035d48567f90a099
Scott D. Sullivan <scottsds@gmail.com>
2012-07-31 20:59:12 -0400

    Updates

--- db/migrate/20120730213945_doc_faq_rework.rb
+++ db/migrate/20120730213945_doc_faq_rework.rb
@@ -9,15 +9,14 @@ class DocFaqRework < ActiveRecord::Migration
       t.text    :screencast
       t.timestamps
     end
-    # Create new columns for content and screencast sanitizer
+    # Create new columns for content and screencast sanitizer and position for reordering
     add_column :questions, :content_sanitizer_version, :integer, :default => 0, :null => false, :limit => 2
     add_column :questions, :screencast_sanitizer_version, :integer, :default => 0, :null => false, :limit => 2
+    add_column :questions, :position, :integer, :default => 1
 
   end
 
   def self.down
     drop_table :questions
-    remove_column :archive_faqs, :anchor
-    remove_column :archive_faqs, :questions
   end
 end
