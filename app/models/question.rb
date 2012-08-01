884c804271736291f2a2b9bd7e424f84a6dbd473
Scott D. Sullivan <scottsds@gmail.com>
2012-07-31 20:58:26 -0400

    Revert to 41fbab1748

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,4 @@
 class Question < ActiveRecord::Base
-  acts_as_list
-
   belongs_to :archive_faq
   attr_protected :content_sanitizer_version
   attr_protected :screencast_sanitizer_version
@@ -14,8 +12,4 @@ class Question < ActiveRecord::Base
 
   validates_length_of :content, :maximum => ArchiveConfig.CONTENT_MAX,
                       :too_long => ts("cannot be more than %{max} characters long.", :max => ArchiveConfig.CONTENT_MAX)
-
-  def self.reorder(positions)
-    SortableList.new(self.find(:all, :order => 'position ASC')).reorder_list(positions)
-  end
 end
