09cf0b61e9a557e119c7f869dc350988f84d2139
Cristina <cristinar@gmail.com>
2012-08-19 21:57:35 +0100

    Adds back "align right" and "justify" options
    Adds back "align right" and "justify" options to the RTE config settings.

--- public/javascripts/mce_editor.js
+++ public/javascripts/mce_editor.js
@@ -13,7 +13,7 @@ tinyMCE.init({
 	inline_styles : false,
 
 	// Theme options - using the advanced theme for now and just limiting the buttons used - we may want to create a custom theme in future.
-	theme_advanced_buttons1 : "pasteword,|,bold,italic,underline,strikethrough,|,link,unlink,image,|,blockquote,|,hr,|,bullist,numlist,|,justifyleft,justifycenter,|,undo,redo",
+	theme_advanced_buttons1 : "pasteword,|,bold,italic,underline,strikethrough,|,link,unlink,image,|,blockquote,|,hr,|,bullist,numlist,|,justifyleft,justifycenter,justifyright,justifyfull,|,undo,redo",
 	theme_advanced_buttons2 : "",
 	theme_advanced_buttons3 : "",
 	theme_advanced_toolbar_location : "top",
@@ -96,7 +96,3 @@ function convertWord (type, content) {
     }
     return content;
 }
-
-
-
-
