f13d5bf9bd8a99a35eb18377d4fe7f1f93f32756
Scott D. Sullivan <scottsds@gmail.com>
2012-08-26 22:54:44 +0000

    Removing local config files

--- config/initializers/gem-plugin_config/resque.rb
+++ config/initializers/gem-plugin_config/resque.rb
@@ -6,5 +6,4 @@ redis_config = YAML.load_file(rails_root + '/config/redis.yml')
 Resque.redis = redis_config[rails_env]
 
 # in-process performing of jobs (for testing) doesn't require a redis server
-#Resque.inline = ENV['RAILS_ENV'] == "test"
-Resque.inline = true
+Resque.inline = ENV['RAILS_ENV'] == "test"
