76d2ab70a8fda643324c784130f0060018007845
elz <elzj78@gmail.com>
2012-09-08 01:18:24 +0000

    Fixing misplaced requirement

--- lib/word_counter.rb
+++ lib/word_counter.rb
@@ -1,5 +1,7 @@
 # encoding=utf-8
 
+require 'nokogiri'
+
 class WordCounter
 
   attr_accessor :text
--- spec/lib/word_counter_spec.rb
+++ spec/lib/word_counter_spec.rb
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-require 'nokogiri'
 require 'word_counter'
 
 describe WordCounter do
