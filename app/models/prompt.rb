046c313eb1e9dbaf21106a16e61668d77fe12df8
Scott D. Sullivan <scottsds@gmail.com>
2012-09-30 17:12:54 +0000

    Fixing all the things. Also the Flux Capacitor.

--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -135,7 +135,7 @@ class Prompt < ActiveRecord::Base
         else
           noncanonical_taglist = tag_set.send("#{tag_type}_taglist").reject {|t| t.canonical}
           unless noncanonical_taglist.empty?
-            errors.add(:base, ts("^These %{tag_type} tags in your %{prompt_type} are not canonical and can't be used unless the moderator adds them: %{taglist}. Please contact support to resolve this issue.",
+            errors.add(:base, ts("^These %{tag_type} tags in your %{prompt_type} are not canonical and can't be used unless the moderator requests them to be added: %{taglist}. Please contact support to resolve this issue.",
               :tag_type => tag_type,
               :prompt_type => self.class.name.downcase,
               :taglist => noncanonical_taglist.collect(&:name).join(ArchiveConfig.DELIMITER_FOR_OUTPUT)))
