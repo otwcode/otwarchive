046c3b7d999d6721bf734f143fa82ee31f1af445
elz <elzj78@gmail.com>
2012-10-05 04:03:48 +0100

    Update tire index name

--- config/initializers/gem-plugin_config/tire.rb
+++ config/initializers/gem-plugin_config/tire.rb
@@ -1,1 +1,1 @@
-Tire::Model::Search.index_prefix "#{Rails.application.class.parent_name.downcase}_#{Rails.env.to_s.downcase}seed"
+Tire::Model::Search.index_prefix "#{Rails.application.class.parent_name.downcase}_#{Rails.env.to_s.downcase}"
