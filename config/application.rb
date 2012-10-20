8e02c69d86dbeb2ba1d1980a9bdd9ad99cef4e21
elz <elzj78@gmail.com>
2012-10-20 10:10:33 -0400

    Nested load paths not working properly for challenge models - taking them out.

--- config/application.rb
+++ config/application.rb
@@ -15,9 +15,7 @@ module Otwarchive
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{Rails.root}/lib)
-    config.autoload_paths += %W(#{Rails.root}/app/models/challenge)
     config.autoload_paths += %W(#{Rails.root}/app/sweepers)
-    config.autoload_paths += %W(#{Rails.root}/app/challenge)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
