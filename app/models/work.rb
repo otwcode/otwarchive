6dc6e0a8fc7ac0918b047efe9cef4ca912ccea8e
elz <elzj78@gmail.com>
2012-10-20 14:01:21 -0400

    Fixing issue with anon works not being listed that way if posted without preview

--- app/models/work.rb
+++ app/models/work.rb
@@ -189,12 +189,13 @@ class Work < ActiveRecord::Base
   # These are methods that run before/after saves and updates to ensure
   # consistency and that associated variables are updated.
   ########################################################################
-  before_save :validate_authors, :set_author_sorting, :clean_and_validate_title, :validate_published_at, :ensure_revised_at
+  before_save :validate_authors, :clean_and_validate_title, :validate_published_at, :ensure_revised_at
 
   before_save :post_first_chapter, :set_word_count
 
   after_save :save_chapters, :save_parents
-  before_create :set_anon_unrevealed
+  before_create :set_anon_unrevealed, :set_author_sorting
+  before_update :set_author_sorting
 
   before_save :check_for_invalid_tags
   before_update :validate_tags
