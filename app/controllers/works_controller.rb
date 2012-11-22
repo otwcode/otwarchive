8df9899a01db167479bf8862655e3c733c1ae3ca
Scott D. Sullivan <scottsds@gmail.com>
2012-11-22 00:25:55 +0000

    DRYing up the milk I spilt

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -227,19 +227,14 @@ class WorksController < ApplicationController
         #hack for empty chapter authors in cucumber series tests
         @chapter.pseuds = @work.pseuds if @chapter.pseuds.blank?
         if params[:preview_button] || params[:cancel_coauthor_button]
-          if !@collection.nil? && @collection.moderated?
-            setflash; flash[:notice] = ts('Draft was successfully created. If you save this work, you will be submitting it to a moderated collection. It will show up in the collection once approved.')
-          else
-            setflash; flash[:notice] = ts('Draft was successfully created.')
-          end
+          setflash; flash[:notice] = ts('Draft was successfully created.')
+          in_moderated_collection
           redirect_to preview_work_path(@work)
         else
           # We check here to see if we are attempting to post to moderated collection
-          if !@collection.nil? && @collection.moderated?
-            redirect_to work_path(@work), :notice => ts('Work was submitted to a moderated collection. It will show up in the collection once approved.')
-          else
-            redirect_to work_path(@work), :notice => ts('Work was successfully posted.')
-          end
+          setflash; flash[:notice]= ts("Work was successfully posted.")
+          in_moderated_collection
+          redirect_to work_path(@work)
         end
       else
         if @work.errors.empty? && (!@work.invalid_pseuds.blank? || !@work.ambiguous_pseuds.blank?)
@@ -295,11 +290,8 @@ class WorksController < ApplicationController
     elsif params[:preview_button] || params[:cancel_coauthor_button]
       @preview_mode = true
       if @work.has_required_tags? && @work.invalid_tags.blank?
-        if !@collection.nil? && @collection.moderated?
-          setflash; flash[:notice] = ts('Draft was successfully created. If you save this work, you will be submitting it to a moderated collection. It will show up in the collection once approved.')
-        else
-          setflash; flash[:notice] = ts('Draft was successfully created.')
-        end
+        setflash; flash[:notice] = ts('Draft was successfully created.')
+        in_moderated_collection
         @chapter = @work.chapters.first unless @chapter
         render :preview
       else
@@ -370,17 +362,11 @@ class WorksController < ApplicationController
       end
       if saved
         if params[:post_button]
-          if !@collection.nil? && @collection.moderated?
-            setflash; flash[:notice] = ts('Work was submitted to a moderated collection. It will show up in the collection once approved.')
-            redirect_to(@work) and return
-          end
           setflash; flash[:notice] = ts('Work was successfully posted.')
+          in_moderated_collection
         elsif params[:update_button]
-          if !@collection.nil? && @collection.moderated?
-            setflash; flash[:notice] = ts('Work was submitted to a moderated collection. It will show up in the collection once approved. VIA PREVIEW')
-          else
-            setflash; flash[:notice] = ts('Work was successfully updated.')
-          end
+          setflash; flash[:notice] = ts('Work was successfully updated.')
+          in_moderated_collection
         end
         redirect_to(@work)
       else
@@ -610,6 +596,14 @@ protected
         message = " " + ts("We have notified the author(s) you imported stories for. If any were missed, you can also add co-authors manually.")
         setflash; flash[:notice] ? flash[:notice] += message : flash[:notice] = message
       end
+    end
+  end
+
+  # check to see if the work is being added / has been added to a moderated collection, then let user know that
+  def in_moderated_collection
+    if !@collection.nil? && @collection.moderated?
+      flash[:notice] ||= ""
+      flash[:notice] += ts(" Your work will only show up in the moderated collection you have submitted it to once it is approved by a moderator.")
     end
   end
 
