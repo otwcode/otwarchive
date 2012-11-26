bc3fb835976966bcc0ce05469d51f4329ab31f28
Scott D. Sullivan <scottsds@gmail.com>
2012-11-26 23:34:22 +0000

    Issue 3418: Multiple collections doesnt catch moderated status

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -660,9 +660,12 @@ protected
 
   # check to see if the work is being added / has been added to a moderated collection, then let user know that
   def in_moderated_collection
-    if !@collection.nil? && @collection.moderated?
-      flash[:notice] ||= ""
-      flash[:notice] += ts(" Your work will only show up in the moderated collection you have submitted it to once it is approved by a moderator.")
+    @work.collections.each do |collection|
+      if !collection.nil? && collection.moderated?
+        flash[:notice] ||= ""
+        flash[:notice] += ts(" Your work will only show up in the moderated collection you have submitted it to once it is approved by a moderator.")
+        return
+      end
     end
   end
 
