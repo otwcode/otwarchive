a9b40e1f6c132f81e9b322823a3e56bf5abb3d02
Scott D. Sullivan <scottsds@gmail.com>
2012-12-04 20:11:28 +0000

    Done Dec 4

--- app/models/archive_faq.rb
+++ app/models/archive_faq.rb
@@ -6,15 +6,6 @@ class ArchiveFaq < ActiveRecord::Base
 
   attr_protected :content_sanitizer_version
 
-  # validates_presence_of :content
-  # CONTENT HAS BEEN REPLACED WITH 'ANSWER' in the Questions Model
-  # validates_length_of :content, :minimum => ArchiveConfig.CONTENT_MIN,
-  #   :too_short => ts("must be at least %{min} letters long.", :min => ArchiveConfig.CONTENT_MIN)
-
-  # validates_length_of :content, :maximum => ArchiveConfig.CONTENT_MAX,
-  #   :too_long => ts("cannot be more than %{max} characters long.", :max => ArchiveConfig.CONTENT_MAX)
-  #   :too_long => ts("cannot be more than %{max} characters long.", :max => ArchiveConfig.CONTENT_MAX)
-
   def self.reorder(positions)
     SortableList.new(self.find(:all, :order => 'position ASC')).reorder_list(positions)
   end
