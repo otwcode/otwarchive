2c6d71b90a687c69626dfbdd6771db568144e6e3
Scott D. Sullivan <scottsds@gmail.com>
2012-12-05 22:55:40 +0000

    Issue 1331: Account creation options

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,20 +35,23 @@ class UsersController < ApplicationController
       flash[:error] = ts("You are already logged in!")
       redirect_to root_path and return
     end
-    token = params[:invitation_token]
-    if token.blank?
-      if !@admin_settings.account_creation_enabled?
-        flash[:error] = ts("You need an invitation to sign up.")
+    if !@admin_settings.account_creation_enabled?
+      flash[:error] = ts("Account creation is suspended at the moment. Please check back with us later.")
+      redirect_to root_path and return
+    elsif @admin_settings.creation_requires_invite?
+      token = params[:invitation_token]
+      if token.blank?
+        flash[:error] = ts("To create an account, you'll need an invitation. One option is to add your name to the automatic queue below.")
         redirect_to invite_requests_path and return
-      end
-    else
-      invitation = Invitation.find_by_token(token)
-      if !invitation
-        flash[:error] = ts("There was an error with your invitation token, please contact support")
-        redirect_to new_feedback_report_path and return
-      elsif invitation.redeemed_at && invitation.invitee
-        flash[:error] = ts("This invitation has already been used to create an account, sorry!")
-        redirect_to root_path and return
+      else
+        invitation = Invitation.find_by_token(token)
+        if !invitation
+          flash[:error] = ts("There was an error with your invitation token, please contact support")
+          redirect_to new_feedback_report_path and return
+        elsif invitation.redeemed_at && invitation.invitee
+          flash[:error] = ts("This invitation has already been used to create an account, sorry!")
+          redirect_to root_path and return
+        end
       end
     end
   end
