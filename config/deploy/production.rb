d8554dc8581e42de588a41bd9fbcecda4718852c
elz <elzj78@gmail.com>
2012-12-19 19:18:54 -0500

    Fixing deploy script for db server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -12,7 +12,7 @@ server "otw3.ao3.org", :web, :app
 server "otw4.ao3.org", :app, :worker, :db, :primary => true
 
 # otw5 is the db server
-server "otw5.ao3.org", :db, :no_release => true
+server "otw5.ao3.org", :db, :no_release => true, :primary => false
 
 # ORDER OF EVENTS
 # Calling "cap deploy" runs:
