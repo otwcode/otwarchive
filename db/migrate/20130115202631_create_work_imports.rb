0966bb28039e05651789a675cb376e5c4b036070
Stephanie <stephanie@thepotionsmaster.net>
2013-01-15 14:34:58 -0600

    added migration files

---
+++ db/migrate/20130115202631_create_work_imports.rb
@@ -0,0 +1,17 @@
+class CreateWorkImports < ActiveRecord::Migration
+  def self.up
+    create_table :work_imports do |t|
+      t.integer :work_id
+      t.integer :pseud_id
+      t.integer :source_archive_id
+      t.integer :source_work_id
+      t.integer :source_user_id
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :work_imports
+  end
+end
