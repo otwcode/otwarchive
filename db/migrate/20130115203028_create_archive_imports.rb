73e6b78a3e217f869b52440ce2e779aa7e9c50ff
Stephanie <stephanie@thepotionsmaster.net>
2013-01-15 14:49:06 -0600

    added migration files

---
+++ db/migrate/20130115203028_create_archive_imports.rb
@@ -0,0 +1,16 @@
+class CreateArchiveImports < ActiveRecord::Migration
+  def self.up
+  create_table :archive_imports do |t|
+    t.string :name
+    t.string :archive_type_id
+    t.string :old_base_url
+    t.string :notes
+    t.integer :associated_collection_id
+    t.timestamps
+  end
+
+
+  def self.down
+    drop_table :archive_imports
+  end
+end
