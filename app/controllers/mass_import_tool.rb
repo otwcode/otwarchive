4626fc38b56955d0c42e3b349a706d0bb0792741
Stephanie <stephanie@thepotionsmaster.net>
2013-01-16 12:56:20 -0600

    changed structs to classes

--- app/controllers/mass_import_tool.rb
+++ app/controllers/mass_import_tool.rb
@@ -449,7 +449,7 @@ class MassImportTool
             #user exists, but is being imported
             #insert the mapping value
             puts "---e"
-            update_record_target("insert into user_imports (user_id,source_archive_id,source_user_id) values (#{new_a.new_user_id},#{ns.old_user_id},#{ns.source_archive_id})")
+            update_record_target("insert into user_imports (user_id,source_archive_id,source_user_id) values (#{ns.new_user_id},#{ns.old_user_id},#{ns.source_archive_id})")
             ns.author = a.penname
             #check to see if penname exists as pseud for existing user
             temp_pseud_id = get_pseud_id_for_penname(temp_author_id,ns.penname)
