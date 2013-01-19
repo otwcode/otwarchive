9d3cd4324c91275e1b90241cc3a1cf5c7641d39f
Stephanie <stephanie@thepotionsmaster.net>
2013-01-19 16:17:57 -0600

    new

--- app/controllers/mass_import_tool.rb
+++ app/controllers/mass_import_tool.rb
@@ -596,8 +596,9 @@ class MassImportTool
 
           new_work.imported_from_url = "#{@import_archive_id}~~#{ns.old_work_id}"
           new_work = add_chapters(new_work,ns.old_work_id)
-          new_work.save!
           new_work.chapters.build
+          new_work.save!
+
 
           puts "new work created #{new_work.id}"
 
