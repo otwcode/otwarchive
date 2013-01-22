eb88c52fffc9b8dfe1506eb59897730b6ebfa72a
sarken <sarken@gmail.com>
2013-01-22 05:52:39 +0000

    Remove old JavaScript functions from very old versions of the pseud switcher and admin notification page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -32,15 +32,6 @@ $j(document).ready(function() {
     $j('#comments_placeholder .pagination a[data-remote], .actions.work .comments a').on('click.rails', function(e){ $j.scrollTo('#comments_placeholder'); });
 });
 
-// Shows expandable fields when clicked on
-function ShowExpandable() {
-  var expandable = document.getElementById('expandable');
-  if (expandable != null) expandable.style.display = 'inline';
-  var collapsible = document.getElementById('collapsible');
-  if (collapsible != null) collapsible.style.display = 'none';
-}
-
-
 ///////////////////////////////////////////////////////////////////
 // Autocomplete
 ///////////////////////////////////////////////////////////////////
@@ -236,13 +227,6 @@ function setupToggled(){
   });
 }
 
-
-// Hides expandable fields if Javascript is enabled
-function hideExpandable() {
-  var expandable = document.getElementById('expandable');
-  if (expandable != null) expandable.style.display = 'none';
-}
-
 function hideHideMe() {
     $j('.hideme').each(function() { $j(this).hide(); });
 }
@@ -300,14 +284,6 @@ function toggleFormField(element_id) {
         var item = document.getElementById('work_wip_length');
         if (item.value == 1 || item.value == '1') {item.value = '?';}
         else {item.value = 1;}
-    }
-}
-
-function showOptions(idToCheck, idToShow) {
-    var checkbox = document.getElementById(idToCheck);
-    var areaToShow = document.getElementById(idToShow);
-    if (checkbox.checked) {
-        Element.toggle(idToShow);
     }
 }
 
