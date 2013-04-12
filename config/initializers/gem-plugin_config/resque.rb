d56cf4c703de530bd2b567f628d6fd51fd2732be
Scott D. Sullivan <scottsds@gmail.com>
2013-04-12 05:00:36 +0100

    Issue 3580: Integrate with Travis-CI

--- config/initializers/gem-plugin_config/resque.rb
+++ config/initializers/gem-plugin_config/resque.rb
@@ -1,13 +1,19 @@
 require 'resque'
-rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../../..'
-rails_env = ENV['RAILS_ENV'] || 'development'
-
-redis_config = YAML.load_file(rails_root + '/config/redis.yml')
-Resque.redis = redis_config[rails_env]
-
-# in-process performing of jobs (for testing) doesn't require a redis server
-Resque.inline = ENV['RAILS_ENV'] == "test"
-
-Resque.after_fork do 
-  Resque.redis.client.reconnect
-end
+ 
+ if ENV['TRAVIS']
+   rails_root = ENV['TRAVIS_BUILD_DIR']
+   rails_env = 'test'
+ else
+   rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../../..'
+   rails_env = ENV['RAILS_ENV'] || 'development'
+ end
+ 
+ redis_config = YAML.load_file(rails_root + '/config/redis.yml')
+ Resque.redis = redis_config[rails_env]
+ 
+ # in-process performing of jobs (for testing) doesn't require a redis server
+ Resque.inline = ENV['RAILS_ENV'] == "test"
+ 
+ Resque.after_fork do
+   Resque.redis.client.reconnect
+ end
