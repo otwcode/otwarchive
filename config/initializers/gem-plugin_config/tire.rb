0eb4ce14ecdc7eb40965d1931b7b0feafab541e2
james_ <zz9pzza@gmail.com>
2013-05-26 07:46:19 +0100

    Add config for testing etal

--- config/initializers/gem-plugin_config/tire.rb
+++ config/initializers/gem-plugin_config/tire.rb
@@ -3,10 +3,11 @@ if Rails.env.production?
   Tire.configure do
     url "http://elasticsearch01.ao3.org:9200"
   end
-end
-if Rails.env.staging? 
+elsif Rails.env.staging? 
   Tire::Model::Search.index_prefix "#{Rails.application.class.parent_name.downcase}_staging"
   Tire.configure do
     url "http://test-db01.transformativeworks.org:9200"
   end
+else
+  Tire::Model::Search.index_prefix "#{Rails.application.class.parent_name.downcase}_#{Rails.env.to_s.downcase}"
 end
