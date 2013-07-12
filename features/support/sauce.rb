36dc76e8d1ec38a7ef5a36fefe65d7f369812bcc
Scott D. Sullivan <scottsds@gmail.com>
2013-07-11 20:00:51 -0700

    Specify a port

--- features/support/sauce.rb
+++ features/support/sauce.rb
@@ -2,6 +2,8 @@ require "sauce/cucumber"
 require "capybara/rails"
 require "sauce/connect"
 
+Capybara.default_driver = :sauce
+Capybara.server_port = 49221
 Sauce.config do |c|
   c[:browser] = "Firefox"
   c[:version] = "18"
