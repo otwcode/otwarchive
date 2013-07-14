b633351bc9415145765c642f26339beff2c9131c
hillary hueter <weimar1927@gmail.com>
2013-07-14 06:29:27 +0100

    updating factory syntax

---
+++ factories/abuse_reports.rb
@@ -0,0 +1,9 @@
+require 'faker'
+FactoryGirl.define do
+  factory :abuse_report do
+    email {Faker::Internet.email}
+    url "http://archiveofourown.org/tags/2000%20AD%20(Comics)/works"
+    comment {Faker::Lorem.paragraph(1)}
+    category 11469
+  end
+end
