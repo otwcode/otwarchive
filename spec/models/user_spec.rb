7a8a49cbfcdff1a542ed8c9afd8bc700d0030d60
hillary hueter <weimar1927@gmail.com>
2013-07-15 04:28:47 +0100

    updated the email veracity tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -61,10 +61,13 @@ describe User, :ready do
       end
 
       context "email veracity" do
-        let(:bad_email) {build(:user, email: "fakey@crazy-z3d9df-domain.com")}
-        it "does not save if email does not pass veracity check" do
-          bad_email.save.should be_false
-          bad_email.errors[:email].should_not be_empty
+        BAD_EMAILS.each do |email|
+          let(:bad_email) {build(:user, email: email)}
+          it "cannot be created if the email does not pass veracity check" do
+            bad_email.save.should be_false
+            bad_email.errors[:email].should include("should look like an email address.")
+            bad_email.errors[:email].should include("does not seem to be a valid address.")
+          end
         end
       end
 
