5e230b45f4b947fa1588f05a3c9920476670ef1e
Scott D. Sullivan <scottsds@gmail.com>
2013-09-08 21:27:24 -0700

    Final changes

--- public/javascripts/mce_editor.js
+++ public/javascripts/mce_editor.js
@@ -8,6 +8,7 @@ tinyMCE.init({
 	paste_insert_word_content_callback : "convertWord",
 	paste_auto_cleanup_on_paste : true,
     extended_valid_elements : "span[!class]",
+
     // TinyMCE default behaviour uses CSS styling for most things; this is disabled for now
 	// because we're stripping those tags out.
 	inline_styles : false,
