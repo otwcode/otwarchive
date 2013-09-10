aec38194ec7de766e628df9ac71903ca0f47f024
Stephanie <stephaine@thepotionsmaster.net>
2013-09-10 13:51:26 -0500

    Changes in work model

--- app/models/kudo.rb
+++ app/models/kudo.rb
@@ -2,6 +2,7 @@ class Kudo < ActiveRecord::Base
   belongs_to :pseud         
   belongs_to :commentable, :polymorphic => true
 
+
   validates_uniqueness_of :pseud_id, 
     :scope => [:commentable_id, :commentable_type], 
     :message => ts("^You have already left kudos here. :)"), 
@@ -15,6 +16,8 @@ class Kudo < ActiveRecord::Base
   scope :with_pseud, where("pseud_id IS NOT NULL")
   scope :by_guest, where("pseud_id IS NULL")
   
+
+
   # return either the name of the kudo-leaver or "guest"
   def name
     if self.pseud
