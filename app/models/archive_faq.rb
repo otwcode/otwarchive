7ead54e8d6b374bb851f4795f46543e9f0abb7dc
Scott D. Sullivan <scottsds@gmail.com>
2013-09-15 12:44:19 -0700

    Adding email functionality to FAQ Translations

--- app/models/archive_faq.rb
+++ app/models/archive_faq.rb
@@ -5,8 +5,14 @@ class ArchiveFaq < ActiveRecord::Base
   accepts_nested_attributes_for :questions, :reject_if => lambda { |a| a[:question].blank? }, :allow_destroy => true
 
   attr_protected :content_sanitizer_version
+  attr_accessor :notify_translations
 
   scope :non_translated, where('translated_faq_id IS NULL')
+
+
+  def email_translations?
+    notify_translations == "1"
+  end
 
   def self.reorder(positions)
     SortableList.new(self.find(:all, :order => 'position ASC')).reorder_list(positions)
