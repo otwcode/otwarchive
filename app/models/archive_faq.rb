683a3118822e236dffb7a2141230bbe7bafbf22a
Scott D. Sullivan <scottsds@gmail.com>
2013-09-30 13:25:38 -0700

    Added email funcationality after editing FAQ Category

--- app/models/archive_faq.rb
+++ app/models/archive_faq.rb
@@ -1,6 +1,7 @@
 class ArchiveFaq < ActiveRecord::Base
   acts_as_list
 
+
   has_many :questions, :dependent => :destroy
   accepts_nested_attributes_for :questions, :reject_if => lambda { |a| a[:question].blank? }, :allow_destroy => true
 
@@ -9,6 +10,22 @@ class ArchiveFaq < ActiveRecord::Base
 
   scope :non_translated, where('translated_faq_id IS NULL')
 
+  # When we modify either a FAQs Category name or one of the Questions, we send an email to Translations.
+  after_update :notify_translations_committee
+  def notify_translations_committee
+    # Check first to see if we are asked to send an email return if not
+    unless !self.email_translations?
+      self.questions.each do |question|
+        if question.changed?
+          (@changed_questions ||= []) << question
+        end
+      end
+      # A Question could have changed, or the Title of the FAQ Category
+      if @changed_questions.present? || self.title_changed?
+        AdminMailer.edited_faq(self.id, User.current_user.login).deliver
+      end
+    end
+  end
 
   def email_translations?
     notify_translations == "1"
