599ea76100de3807f17a9feb8a0e59045efbfee9
james_ <zz9pzza@gmail.com>
2013-10-04 21:50:15 +0100

    The conditional seemed to be missing

--- config/initializers/copycopter.rb
+++ config/initializers/copycopter.rb
@@ -1,5 +1,7 @@
-CopycopterClient.configure do |config|
-  config.api_key = 'bc9ac482bbb2dc056b55734ea306e5a3ef2cf9caec60c8be'
-  config.host = '10.10.11.126'
-  config.port = 3000
+if Rails.env.staging?
+  CopycopterClient.configure do |config|
+    config.api_key = 'bc9ac482bbb2dc056b55734ea306e5a3ef2cf9caec60c8be'
+    config.host = '10.10.11.126'
+    config.port = 3000
+  end
 end
