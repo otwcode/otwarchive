b213e1b39838ec0b41226b3d2cf3fa341506736e
Stephanie <stephaine@thepotionsmaster.net>
2013-10-29 11:20:10 -0500

    updated to remove additional duplicate

--- db/migrate/20130707160714_remove_duplicate_indexes.rb
+++ db/migrate/20130707160714_remove_duplicate_indexes.rb
@@ -5,7 +5,6 @@ class RemoveDuplicateIndexes < ActiveRecord::Migration
     remove_index(:challenge_claims , :name => 'index_challenge_claims_on_creation_id')
     remove_index(:creatorships, :name => 'index_creatorships_creation')
     remove_index(:kudos, :name => 'index_kudos_on_commentable_id_and_commentable_type')
-    remove_index(:translations, :name => 'index_translations_on_tr_key_and_locale_id')
     remove_index(:bookmarks, :name => 'index_bookmarkable')
   end
 
@@ -13,7 +12,6 @@ class RemoveDuplicateIndexes < ActiveRecord::Migration
     add_index(:challenge_claims, [:creation_id], :name => 'index_challenge_claims_on_creation_id')
     add_index(:creatorships, [:creation_id,:creation_type], :name => 'index_creatorships_creation')
     add_index(:kudos, [:commentable_id,:commentable_type], :name => 'index_kudos_on_commentable_id_and_commentable_type')
-    add_index(:translations, [:tr_key,:locale_id], :name => 'index_translations_on_tr_key_and_locale_id ')
     add_index(:bookmarks, [:bookmarkable_id,:bookmarkable_type], :name => 'index_bookmarkable')
   end
 end
