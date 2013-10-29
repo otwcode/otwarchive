d48636df29c8bcdc56a33ac1eee00efaed17ce66
Stephanie <stephanie@thepotionsmaster.net>
2013-10-29 00:34:16 -0500

    travis run me

--- db/migrate/20131007160814_add_redirect_work_id.rb
+++ db/migrate/20131007160814_add_redirect_work_id.rb
@@ -1,6 +1,6 @@
 class AddRedirectWorkId < ActiveRecord::Migration
   def self.up
-    add_column :works, :redirect_work_id, :integer
+    add_column :works, :redirect_work_id, :integer, :default => 0
   end
 
   def self.down
