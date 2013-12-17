34bb49d99ac2d0d1d327eb526141bf75fe71ad4d
elz <elzj78@gmail.com>
2013-12-16 21:40:35 -0500

    Split reindex queue to allow user changes to be prioritized

--- app/models/redis_search_index_queue.rb
+++ app/models/redis_search_index_queue.rb
@@ -7,15 +7,19 @@ class RedisSearchIndexQueue
   
   # Resque
   
-  @queue = :reindexer
   # This will be called by a worker when a job needs to be processed
   def self.perform(method, *args)
     self.send(method, *args)
   end
 
   # We can pass this any method that we want to run later.
-  def self.async(method, *args)
-    Resque.enqueue(RedisSearchIndexQueue, method, *args)
+  def self.async(method, id, options={})
+    if options[:priority] == :low
+      queue = :reindex_low
+    else
+      queue = :reindex_high
+    end
+    Resque::Job.create(queue, RedisSearchIndexQueue, method, id)
   end
 
   # Reindex an object
@@ -23,25 +27,25 @@ class RedisSearchIndexQueue
     if item.is_a?(Work)
       queue_work(item, options)
     elsif item.is_a?(Bookmark)
-      queue_bookmark(item)
+      queue_bookmark(item, options)
     end
   end
   
   #### WORKS
   
   def self.queue_works(work_ids, options={})
-    work_ids.each { |id| async(:run_work_reindex, id) }
+    work_ids.each { |id| async(:run_work_reindex, id, options) }
     unless options[:without_bookmarks].present? 
       # queue their bookmarks also
-      queue_bookmarks(Bookmark.where(:bookmarkable_type => "Work", :bookmarkable_id => work_ids).value_of(:id))
+      queue_bookmarks(Bookmark.where(:bookmarkable_type => "Work", :bookmarkable_id => work_ids).value_of(:id), options)
     end
   end    
   
   # queue a work to have its search index updated
   def self.queue_work(work, options={})
-    async(:run_work_reindex, work.id)
+    async(:run_work_reindex, work.id, options)
     unless options[:without_bookmarks].present?
-      queue_bookmarks(Bookmark.where(:bookmarkable_type => "Work", :bookmarkable_id => work.id).value_of(:id))
+      queue_bookmarks(Bookmark.where(:bookmarkable_type => "Work", :bookmarkable_id => work.id).value_of(:id), options)
     end
   end
   
@@ -51,12 +55,12 @@ class RedisSearchIndexQueue
 
   #### BOOKMARKS
 
-  def self.queue_bookmark(bookmark)
-    async(:run_bookmark_reindex, bookmark.id)
+  def self.queue_bookmark(bookmark, options={})
+    async(:run_bookmark_reindex, bookmark.id, options)
   end
   
-  def self.queue_bookmarks(ids)
-    ids.each { |id| async(:run_bookmark_reindex, id) }
+  def self.queue_bookmarks(ids, options={})
+    ids.each { |id| async(:run_bookmark_reindex, id, options) }
   end
 
   def self.run_bookmark_reindex(bookmark_id)
