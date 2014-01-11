7948439b37ac25a17be8408a67230b1008fc355e
elz <elzj78@gmail.com>
2014-01-10 22:41:34 -0500

    Fix join on related works posted scope

--- app/models/related_work.rb
+++ app/models/related_work.rb
@@ -2,7 +2,9 @@ class RelatedWork < ActiveRecord::Base
   belongs_to :work
   belongs_to :parent, :polymorphic => true
   
-  scope :posted, joins(:work).where(works: { posted: true })
+  scope :posted, 
+    joins("INNER JOIN `works` `child_works` ON `child_works`.`id` = `related_works`.`work_id`").
+    where("child_works.posted = 1")
 
   def notify_parent_owners
     if parent.respond_to?(:pseuds)
