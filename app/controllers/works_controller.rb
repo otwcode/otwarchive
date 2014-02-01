09b8d68cd76405b529d548462348aeffc87fc462
Scott D. Sullivan <scottsds@gmail.com>
2014-02-01 07:14:12 -0800

    Move method to model, it doesn't need to be skinny. Down with false images of beauty

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -753,18 +753,12 @@ public
     @work = Work.find(params[:id])
     Reading.mark_to_read_later(@work, current_user)
     read_later_path = user_readings_path(current_user, :show => 'to-read')
-    if marked_for_later?(@work)
+    if @work.marked_for_later?(current_user)
       flash[:notice] = ts("This work was <strong>removed</strong> from your #{view_context.link_to('Marked for Later list', read_later_path)}. It may take a while for changes to show up.").html_safe
     else
       flash[:notice] = ts("This work was <strong>added</strong> to your #{view_context.link_to('Marked for Later list', read_later_path)}. It may take a while for changes to show up.").html_safe
     end
     redirect_to(request.env["HTTP_REFERER"] || root_path)
-  end
-
-  def marked_for_later?(work)
-    return unless current_user
-    reading = Reading.find_by_work_id_and_user_id(work.id, current_user.id)
-    reading && reading.toread?
   end
 
   protected
