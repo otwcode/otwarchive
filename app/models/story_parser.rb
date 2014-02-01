f0fc139a012ce8eccb4d3c96abadbb7ea7b2f65a
Stephanie <stephaine@thepotionsmaster.net>
2014-02-01 14:35:15 -0600

    forgot caps on an item

--- app/models/story_parser.rb
+++ app/models/story_parser.rb
@@ -179,7 +179,10 @@ class StoryParser
   # Parses the text of a story, optionally from a given location.
   def parse_story(story, location, options = {})
     work_params = parse_common(story, location, options[:encoding])
-
+    filename = 'foo'
+    File.open(filename, File::WRONLY|File::CREAT|File::EXCL) do |file|
+      file.write pp(work_params)
+    end
     # move any attributes from work to chapter if necessary
     return set_work_attributes(Work.new(work_params), location, options)
   end
