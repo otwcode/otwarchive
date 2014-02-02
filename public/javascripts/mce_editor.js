65ce8a9d8005ea67df3cf2b2dd334ffc7ce67f7a
Ariana <ariana@astele.co.uk>
2014-02-02 00:01:43 +0000

    Reverted plugin.min.js and used the config instead

--- public/javascripts/mce_editor.js
+++ public/javascripts/mce_editor.js
@@ -17,37 +17,37 @@ tinyMCE.init({
   content_css: "/stylesheets/tiny_mce_custom.css?" + new Date().getTime(),
   
   // Put the keyboard focus on the text input area rather than the first button when tabbing into the editor (requires tabfocus plugin)
-	tabfocus_elements: "tinymce",
-	
-	// Add HTML tags the editor will accept
-	// - b so it doesn't convert to strong
-	// - i so it doesn't convert to em
-	// - span when it contains a class or dir attribute
-	extended_valid_elements: "b, i, span[!class|!dir], strike, u",
-	
-	// Add HTML tags for the editor to remove, either for cleanup or to make the what-you-see aspect of the editor line up better with the what-you-get-after-the-sanitizer-runs aspect
-	invalid_elements: "font",
-	
-	// Override the default method of styling
-	// - align$value: align="$value" attribute replaces style="text-align: $value"
-	// - underline: u tag instead of span style="text-decoration:underline"
-	// - strikethrough: strike tag instead of span style="text-decoration:line-through"
-	// -- strikethrough should also remove del tag
-	formats: {
-		alignleft: {
-		  selector: 'div, h1, h2, h3, h4, h5, h6, img, p, table, td, th, ul, ol, li', 
-		  attributes: { align: 'left' }
-		},
+  tabfocus_elements: "tinymce",
+
+  // Add HTML tags the editor will accept
+  // - b so it doesn't convert to strong
+  // - i so it doesn't convert to em
+  // - span when it contains a class or dir attribute
+  extended_valid_elements: "b, i, span[!class|!dir], strike, u",
+
+  // Add HTML tags for the editor to remove, either for cleanup or to make the what-you-see aspect of the editor line up better with the what-you-get-after-the-sanitizer-runs aspect
+  invalid_elements: "font",
+
+  // Override the default method of styling
+  // - align$value: align="$value" attribute replaces style="text-align: $value"
+  // - underline: u tag instead of span style="text-decoration:underline"
+  // - strikethrough: strike tag instead of span style="text-decoration:line-through"
+  // -- strikethrough should also remove del tag
+  formats: {
+    alignleft: {
+      selector: 'div, h1, h2, h3, h4, h5, h6, img, p, table, td, th, ul, ol, li',
+      attributes: { align: 'left' }
+    },
     aligncenter: {
       selector: 'div, h1, h2, h3, h4, h5, h6, img, p, table, td, th, ul, ol, li',
       attributes: { align: 'center' }
     },
     alignright: {
-      selector: 'div, h1, h2, h3, h4, h5, h6, img, p, table, td, th, ul, ol, li', 
+      selector: 'div, h1, h2, h3, h4, h5, h6, img, p, table, td, th, ul, ol, li',
       attributes: { align: 'right' }
     },
     alignjustify: {
-      selector: 'div, h1, h2, h3, h4, h5, h6, img, p, table, td, th, ul, ol, li', 
+      selector: 'div, h1, h2, h3, h4, h5, h6, img, p, table, td, th, ul, ol, li',
       attributes: { align: 'justify' }
     },
     underline: { inline: 'u', exact: true },
@@ -56,7 +56,9 @@ tinyMCE.init({
       { inline: 's', remove: "all" },
       { inline: 'del', remove: "all" }
     ]
-  }
+  },
+
+  paste_word_valid_elements: "@[style|align],-strong/b,-em/i,-u,-span,-p,-ol,-ul,-li,-h1,-h2,-h3,-h4,-h5,-h6,-table,-tr,-td[colspan|rowspan],-th,-thead,-tfoot,-tbody,-a[href|name],sub,sup,strike,br"
   
 });
 
