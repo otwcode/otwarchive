1f9dd94730e0009ae4d1b05e5f20b63621fe838c
sarken <sarken@gmail.com>
2014-03-08 01:08:01 +0000

    3895 Remove RTE's option for adding target=_blank to links

--- public/javascripts/mce_editor.js
+++ public/javascripts/mce_editor.js
@@ -58,8 +58,13 @@ tinyMCE.init({
     ]
   },
 
-  paste_word_valid_elements: "@[align],-strong/b,-em/i,-u,-span,-p,-ol,-ul,-li,-h1,-h2,-h3,-h4,-h5,-h6,-table,-tr,-td[colspan|rowspan],-th,-thead,-tfoot,-tbody,-a[href|name],sub,sup,strike,br"
+  // Override the list of elements that can be pasted from Word. It's necessary to copy the list from the plugin source.
+  // - allow align attribue
+  // - stop allowing style attribute
+  paste_word_valid_elements: "@[align],-strong/b,-em/i,-u,-span,-p,-ol,-ul,-li,-h1,-h2,-h3,-h4,-h5,-h6,-table,-tr,-td[colspan|rowspan],-th,-thead,-tfoot,-tbody,-a[href|name],sub,sup,strike,br",
   
+  // Override the list of targets provided in the link plugin. We do not allow the target attribute, so we want an empty list.
+  target_list: []
 });
 
 // Require the user to turn the RTE on instead of loading automatically using selector option 
@@ -95,4 +100,4 @@ $j(document).ready(function(){
     $j('.rtf-notes').addClass('hidden');
     event.preventDefault();
   });
-})      
+})
