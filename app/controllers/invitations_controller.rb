5c56570e429617334f1ee82ce75d65a5b97c755a
Scott D. Sullivan <scottsds@gmail.com>
2014-04-06 19:54:18 +0000

    Cleaning up logic

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -5,11 +5,7 @@ class InvitationsController < ApplicationController
 
   def check_permission
     @user = User.find_by_login(params[:user_id])
-    if @user.nil?
-      access_denied
-    else
-    access_denied unless logged_in_as_admin? || @user == current_user
-    end
+    access_denied unless logged_in_as_admin? || @user.present? && @user == current_user
   end
 
   def index
