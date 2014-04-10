1f560c61a6b64aa1e8a027566c9256f158faad1c
Ariana <ariana@astele.co.uk>
2014-04-10 13:14:57 +0100

    Increase timeout further.

--- features/support/env.rb
+++ features/support/env.rb
@@ -39,7 +39,7 @@ ActionController::Base.allow_rescue = false
 Capybara.configure do |config|
   config.match = :prefer_exact
   config.ignore_hidden_elements = false
-  config.default_wait_time = 10
+  config.default_wait_time = 15
 end
 
 
