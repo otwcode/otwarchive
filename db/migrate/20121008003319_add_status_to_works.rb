4abf27298d8a2b5f82dd05ac7daa88a1b88821c5
sarken <sarken@gmail.com>
2014-04-17 00:25:45 +0100

    2749 Tabs to spaces in migration file

--- db/migrate/20121008003319_add_status_to_works.rb
+++ db/migrate/20121008003319_add_status_to_works.rb
@@ -1,14 +1,14 @@
 class AddStatusToWorks < ActiveRecord::Migration
   def self.up
-	change_table :works do |t|
-		t.string :status
-	end
-	Work.reset_column_information
-	Work.where(:complete => true).update_all(:status => 'complete')
-	Work.where(:complete => [false, nil]).update_all(:status => 'wip')
+    change_table :works do |t|
+      t.string :status
+    end
+    Work.reset_column_information
+    Work.where(:complete => true).update_all(:status => 'complete')
+    Work.where(:complete => [false, nil]).update_all(:status => 'wip')
   end
 
   def self.down
-	remove_column :works, :status
+    remove_column :works, :status
   end
 end
