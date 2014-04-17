fe019612ae06998ade24ed72b6c9d1c22bfdea1b
sarken <sarken@gmail.com>
2014-04-17 02:02:07 +0100

    2749 Change logic for checking abandoned status for symbols block

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -268,7 +268,7 @@ module TagsHelper
     if [Work, Series].include?(item.class)
       if item.complete?
         symbol_block << get_symbol_link( "complete-yes iswip" , "Complete #{item.class.to_s}")
-      elsif item.class == Work && item.abandoned?
+      elsif item.respond_to?(:abandoned?) && item.abandoned?
         symbol_block << get_symbol_link( "complete-abandoned iswip", "Abandoned #{item.class.to_s}" )
       else
         symbol_block << get_symbol_link( "complete-no iswip", "#{item.class.to_s} in Progress" )
