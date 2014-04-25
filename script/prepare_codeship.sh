bc3f8c50ea61d4fc37de9011da1e2f59e20de808
James Beal <James.Beal@sanger.ac.uk>
2014-04-25 21:33:47 +0100

    Changes to allow codeship to work with split redis

--- script/prepare_codeship.sh
+++ script/prepare_codeship.sh
@@ -3,6 +3,8 @@ cp config/database.codeship.yml config/database.yml
 cp config/newrelic.example config/newrelic.yml
 cp config/redis-cucumber.conf.example config/redis-cucumber.conf
 cp config/redis.travis.example config/redis.yml
+/usr/bin/redis-server config/codeship/redis1.conf
+/usr/bin/redis-server config/codeship/redis2.conf
 bundle exec rake db:create:all --trace
 mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e  "ALTER DATABASE test$TEST_ENV_NUMBER CHARACTER SET utf8 COLLATE utf8_general_ci;"
 bundle exec rake db:schema:load --trace
