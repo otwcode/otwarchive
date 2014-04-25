c28b2d54aba3479e23a5a9677cc4958c603a3fff
James Beal <James.Beal@sanger.ac.uk>
2014-04-25 22:48:16 +0100

    Resolve conflict

--- script/prepare_codeship.sh
+++ script/prepare_codeship.sh
@@ -2,7 +2,7 @@
 cp config/database.codeship.yml config/database.yml
 cp config/newrelic.example config/newrelic.yml
 cp config/redis-cucumber.conf.example config/redis-cucumber.conf
-cp config/redis.travis.example config/redis.yml
+cp config/redis.codeship.example config/redis.yml
 /usr/bin/redis-server config/codeship/redis1.conf
 /usr/bin/redis-server config/codeship/redis2.conf
 bundle exec rake db:create:all --trace
