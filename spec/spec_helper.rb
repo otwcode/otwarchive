b6dec0d82523f18ab3718e80bc7d67e3e97bf869
Scott D. Sullivan <scottsds@gmail.com>
2014-05-25 03:44:47 +0000

    Added Coveralis code coverage for local test development

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,27 @@ require File.expand_path("../../config/environment", __FILE__)
 require File.expand_path('../../features/support/factories.rb', __FILE__)
 require 'rspec/rails'
 
+DatabaseCleaner.start
+
+DatabaseCleaner.clean
+
+
+# SimpleCov integration
+
+require 'simplecov'
+
+SimpleCov.start 'rails' do
+  add_filter '/features/'
+  add_filter '/spec/'
+  add_filter '/config/'
+  add_filter '/vendor/'
+  add_group 'Controllers', 'app/controllers'
+  add_group 'Models', 'app/models'
+  add_group 'Helpers', 'app/helpers'
+  add_group 'Mailers', 'app/mailers'
+  add_group 'Views', 'app/views'
+end
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
@@ -19,6 +40,19 @@ RSpec.configure do |config|
   config.mock_with :rspec
 
   config.include Capybara::DSL
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
