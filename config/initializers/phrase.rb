04b7a50249f1d5a57be94b5d1a633bc0eb995120
james_ <james_@catbus.co.uk>
2014-06-09 18:12:31 +0000

    Following Shalot's recomendations

--- config/initializers/phrase.rb
+++ config/initializers/phrase.rb
@@ -1,6 +1,6 @@
 Phrase.configure do |config|
   # Enable or disable the PhraseApp context editor in general
-  if ENV['AO3_PHRASEAPP'] == "1" then
+  if ArchiveConfig.PHRASEAPP_ENABLE == "true" then
     config.enabled = true 
   else 
     config.enabled = false 
