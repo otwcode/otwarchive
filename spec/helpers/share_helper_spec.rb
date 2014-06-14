6f41b7c5af8671ebc66ded4530e746a1e0ef505b
Scott D. Sullivan <scottsds@gmail.com>
2014-06-14 20:35:47 +0000

    Fix Share Helper

--- spec/helpers/share_helper_spec.rb
+++ spec/helpers/share_helper_spec.rb
@@ -3,19 +3,32 @@ require 'spec_helper'
 describe ShareHelper do
 
   describe "get_embed_link_title" do
-    let(:current_user) { FactoryGirl.create(:user) }
     context "for anonymous works" do
+
       it "should not link to a user's profile" do
-        @work = FactoryGirl.create(:work, :in_anon_collection => true)
-        get_embed_link_title(@work).should eq "Anonymous"
+        @collection = FactoryGirl.create(:collection)
+        @collection.collection_preference.send("anonymous=", true)
+        @collection.collection_preference.save
+        @work = FactoryGirl.create(:work, :collection_names => @collection.name)
+        get_embed_link_title(@work).should include("by Anonymous")
+      end
+    end
+  end
+
+  describe "get_embed_link_meta" do
+    context "a work in a series" do
+      it "should do something" do
+        @work
       end
     end
   end
 
   describe "get_tweet_text_for_bookmark" do
     context "bookmark is a work" do
-      @bookmark = FactoryGirl.create(:bookmark)
-      get_tweet_text_for_bookmark(@bookmark).should eq "Bookmark of #{@bookmark.bookmarkable.title} by #{@bookmark.bookmarkable.pseuds.map(&:name).join(', ')} - #{@bookmark.bookmarkable.fandoms.string}".truncate(83)
+      it "should return a formatted tweet" do
+        @bookmark = FactoryGirl.create(:bookmark)
+        get_tweet_text_for_bookmark(@bookmark).should eq "Bookmark of #{@bookmark.bookmarkable.title} by #{@bookmark.bookmarkable.pseuds.map(&:name).join(', ')} - #{@bookmark.bookmarkable.fandoms.string}".truncate(83)
+      end
     end
   end
 end
