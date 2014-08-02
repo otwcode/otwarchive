6938e9e34544e6e18614c25f31d5cf5af734921c
zz9pzza <zz9pzza@gmail.com>
2014-08-02 21:35:22 +0000

    Try this approach

--- spec/requests/series_spec.rb
+++ spec/requests/series_spec.rb
@@ -4,9 +4,9 @@ describe "Series" do
   subject { page }
 
   context "that have been deleted" do
-    it "should redirect and give a proper error message" do
+    it "should respond with a 404" do
       visit "/series/12345/"
-      should have_content("Couldn't find series '12345'")
+      response.response_code.should == 404
     end
   end
 end
