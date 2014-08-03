b2909d3afe382e3196c676b0a2e3c044355099fc
zz9pzza <zz9pzza@gmail.com>
2014-08-03 06:53:36 +0000

    I don't think testing something that raises Active Record not found is easy, eg https://www.ruby-forum.com/topic/214631

--- spec/requests/series_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe "Series" do
-  it 'response with 404 if page not found' do
-    get :show, { series: 'pages', id: '12345' }
-    expect(response.status).to eq(404)
-  end
-end
