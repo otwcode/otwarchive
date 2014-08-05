b540eae60f4645eff89a3ec718cf9e31f1675ea8
sarken <sarken@gmail.com>
2014-08-05 17:58:41 -0400

    Config setting for number of items to show on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -48,7 +48,7 @@ class HomeController < ApplicationController
     @user_count = User.count
     @work_count = Work.posted.count
     @fandom_count = Fandom.canonical.count
-    @admin_posts = AdminPost.non_translated.find(:all, :order => "created_at DESC", :limit => 3)
+    @admin_posts = AdminPost.non_translated.find(:all, :order => "created_at DESC", :limit => ArchiveConfig.NUMBER_OF_ITEMS_VISIBLE_ON_HOMEPAGE)
     @admin_post_show_more = AdminPost.count > 3
     
     @hide_dashboard = true
