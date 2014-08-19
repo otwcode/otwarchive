308bc6096510fad1b15803857d3f8e57f8c8f68b
sarken <sarken@gmail.com>
2014-08-19 19:05:20 -0400

    Ajax remove item from history: a first version

--- public/javascripts/favorite_tags.js
+++ public/javascripts/favorite_tags.js
@@ -9,12 +9,12 @@
 $j('.ajax-create-destroy').on("click", function(event) {
   event.preventDefault();
 
-  var flashContainer = $j('#ajax_flash');  
   var form = $j(this);
   var formAction = form.attr('action');
   var formSubmit = form.find('[type="submit"]');
   var createValue = form.data('create-value');
   var destroyValue = form.data('destroy-value');
+  var flashContainer = $j('#ajax_flash');  
   
   $j.ajax({
     type: 'POST',
@@ -42,5 +42,44 @@ $j('.ajax-create-destroy').on("click", function(event) {
         flashContainer.append(error + " ");
       });
     }
+  });
+});
+
+// For simple forms that update or destroy records and remove them from a listing
+// e.g. delete from history, mark as read
+// <form> needs ajax-remove class
+// controller needs item_success_message
+
+$j('.ajax-remove').on("click", function(event) {
+  event.preventDefault();
+
+  var form = $j(this);
+  var formAction = form.attr('action');
+  var formParent = form.closest('.blurb');
+  var parentContainer = formParent.closest('div');
+  var flashContainer = parentContainer.find('.flash');  
+  
+  $j.ajax({
+    type: 'POST',
+    url: formAction,
+    data: form.serialize(),
+    dataType: 'json',
+    success: function(data) {
+      flashContainer.removeClass('error').empty();
+      flashContainer.addClass('notice').text(data.item_success_message);
+    },
+    error: function(xhr, textStatus, errorThrown) {
+      flashContainer.empty();
+      flashContainer.addClass('error notice');
+      $j.each(jQuery.parseJSON(xhr.responseText).errors, function(index, error){
+        flashContainer.append(error + " ");
+      });
+    }
+  });
+  
+  $j(document).ajaxSuccess(function() {
+    formParent.slideUp(function() {
+      $j(this).remove();
+    });
   });
 });
