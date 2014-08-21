fc7bae5ca90261e67cce2d40c833bae63f578ad5
james_ <zz9pzza@gmail.com>
2014-08-21 18:46:54 +0000

    More 404 work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,8 +71,7 @@ class UsersController < ApplicationController
   # GET /users/1
   def show
     if @user.blank?
-      flash[:error] = ts("Sorry, could not find this user.")
-      redirect_to people_path and return
+      raise ActiveRecord::RecordNotFound, "Sorry, could not find user #{params[:id]}."
     end
     @page_subtitle = @user.login
 
