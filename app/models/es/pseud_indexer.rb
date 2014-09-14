0b65d71462d2461ca182775e0f17a8e6abc0bc86
elz <elzj78@gmail.com>
2014-09-13 21:50:10 -0400

    Pseud indexer basics

--- app/models/es/pseud_indexer.rb
+++ app/models/es/pseud_indexer.rb
@@ -6,9 +6,28 @@ module ES
     end
 
     def self.mapping
+      {
+        'pseud' => {
+          properties: {
+            name: {
+              type: 'string',
+              analyzer: 'simple'
+            },
+            user_login: {
+              type: 'string',
+              analyzer: 'simple'
+            }
+          }
+        }
+      }
     end
 
     def document(object)
+      object.as_json(
+        root: false,
+        only: [:id, :user_id, :name, :description],
+        methods: [:user_login]
+      )
     end
   end
 end
--- lib/tasks/search.rake
+++ lib/tasks/search.rake
@@ -3,4 +3,8 @@ namespace :search do
   task(:index_tags => :environment) do
     ES::TagIndexer.index_all
   end
+  desc "Reindex pseuds"
+  task(:index_pseuds => :environment) do
+    ES::PseudIndexer.index_all
+  end  
 end
