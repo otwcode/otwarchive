c10aaa5e26bc7a69e5489fad02ed15888b741107
elz <elzj78@gmail.com>
2014-09-22 07:22:05 -0400

    Removing uniqueness constraint from position indexes

--- db/migrate/20140922025054_add_indices_to_faqs.rb
+++ db/migrate/20140922025054_add_indices_to_faqs.rb
@@ -1,6 +1,6 @@
 class AddIndicesToFaqs < ActiveRecord::Migration
   def change
-    add_index :archive_faqs, :position, unique: true
-    add_index :questions, [:archive_faq_id, :position], unique: true
+    add_index :archive_faqs, :position
+    add_index :questions, [:archive_faq_id, :position]
   end
 end
