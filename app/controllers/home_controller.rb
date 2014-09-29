e5f934208050399de102818a8cd2dd37328d9021
sarken <sarken@gmail.com>
2014-09-29 17:21:26 -0400

    Spaces

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -54,13 +54,13 @@ class HomeController < ApplicationController
       # get and cache the user's favorite tags and some random works the user has marked for later
       if Rails.env.development?
         @favorite_tags = @current_user.favorite_tags.find(:all)
-        @readings = @current_user.readings.find(:all, :order => "RAND()", :limit => ArchiveConfig.NUMBER_OF_ITEMS_VISIBLE_ON_HOMEPAGE, :conditions => {:toread => true})   
+        @readings = @current_user.readings.find(:all, :order => "RAND()", :limit => ArchiveConfig.NUMBER_OF_ITEMS_VISIBLE_ON_HOMEPAGE, :conditions => { :toread => true })   
       else
-        @favorite_tags = Rails.cache.fetch("home/index/#{@current_user.id}/home_favorite_tags"){@current_user.favorite_tags.find(:all)}
-        @readings = Rails.cache.fetch("home/index/#{@current_user.id}/home_marked_for_later"){@current_user.readings.find(:all, :order => "RAND()", :limit => ArchiveConfig.NUMBER_OF_ITEMS_VISIBLE_ON_HOMEPAGE, :conditions => {:toread => true})}
+        @favorite_tags = Rails.cache.fetch("home/index/#{@current_user.id}/home_favorite_tags"){ @current_user.favorite_tags.find(:all) }
+        @readings = Rails.cache.fetch("home/index/#{@current_user.id}/home_marked_for_later"){@current_user.readings.find(:all, :order => "RAND()", :limit => ArchiveConfig.NUMBER_OF_ITEMS_VISIBLE_ON_HOMEPAGE, :conditions => { :toread => true })}
       end
       
-      @inbox_comments = @current_user.inbox_comments.find(:all, :order => "created_at DESC", :limit => ArchiveConfig.NUMBER_OF_ITEMS_VISIBLE_ON_HOMEPAGE, :conditions => {:read => false})
+      @inbox_comments = @current_user.inbox_comments.find(:all, :order => "created_at DESC", :limit => ArchiveConfig.NUMBER_OF_ITEMS_VISIBLE_ON_HOMEPAGE, :conditions => { :read => false })
     end
     
     @hide_dashboard = true
