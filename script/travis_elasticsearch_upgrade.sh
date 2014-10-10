8e788743134b0e5c841b1a3a8c58efadba949ff7
james_ <zz9pzza@gmail.com>
2014-10-10 16:48:55 -0400

    first attempt at script

--- script/travis_elasticsearch_upgrade.sh
+++ script/travis_elasticsearch_upgrade.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo service elasticsearch stop
-wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.5.deb
-sudo dpkg -i elasticsearch-0.90.5.deb
+wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.13.deb
+sudo dpkg --force-overwrite -i elasticsearch-0.90.13.deb
 sudo service elasticsearch start
