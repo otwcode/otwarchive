11d5205f179c542b03c994c674648456574983f0
james_ <zz9pzza@gmail.com>
2014-10-12 14:22:13 -0400

    Drier code

--- app/mailers/kudo_mailer.rb
+++ app/mailers/kudo_mailer.rb
@@ -14,15 +14,16 @@ class KudoMailer < ActionMailer::Base
     @kudo_givers = {}
     user = User.find(user_id)
     kudos_hash = JSON.parse(user_kudos)
-    kudos_hash.each_pair do |commentable_info, kudo_givers|
-      commentable_type, commentable_id, guest_count  = commentable_info.split('_')
-      commentable = commentable_type.constantize.find_by_id(commentable_id)
-      next unless commentable
-      @commentables << commentable
-      @kudo_givers[commentable_id] = kudo_givers
-      @guest_count[commentable_id] = guest_count
-    end
     I18n.with_locale(Locale.find(user.preference.prefered_locale).iso) do
+      kudos_hash.each_pair do |commentable_info, kudo_givers|
+        commentable_type, commentable_id, guest_count  = commentable_info.split('_')
+        commentable = commentable_type.constantize.find_by_id(commentable_id)
+        if guest_count.to_i == 1 then kudo_givers << "#{t 'mailer.kudos.guest'}" end 
+        if guest_count.to_i > 1 then kudo_givers << "#{guest_count} #{t 'mailer.kudos.guests'}" end 
+        next unless commentable
+        @commentables << commentable
+        @kudo_givers[commentable_id] = kudo_givers
+      end
       mail(
         :to => user.email,
         :subject => "[#{ArchiveConfig.APP_SHORT_NAME}] #{t 'mailer.kudos.youhave'}"
