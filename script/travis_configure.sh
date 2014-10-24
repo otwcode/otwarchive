1bd323761efbd5326cf8c10d6f3c554e4d75cab8
james_ <zz9pzza@gmail.com>
2014-10-24 18:18:37 +0000

    We can have bcrypt with a cost of 4 for travis

--- script/travis_configure.sh
+++ script/travis_configure.sh
@@ -5,5 +5,4 @@ cp config/database.travis.yml config/database.yml
 cp config/newrelic.example config/newrelic.yml
 cp config/redis-cucumber.conf.example config/redis-cucumber.conf
 cp config/redis.travis.example config/redis.yml
-#echo "BCRYPT: 'false'" >> config/config.yml
 echo "BCRYPT_COST: 4"  >> config/config.yml
