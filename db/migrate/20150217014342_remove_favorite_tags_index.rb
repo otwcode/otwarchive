b04e18b1ef0dc750ed62a43d639e687b5b111eb6
sarken <sarken@gmail.com>
2015-02-16 22:55:48 -0500

    4209 Make it one migration

--- db/migrate/20150217014342_remove_favorite_tags_index.rb
+++
@@ -1,5 +0,0 @@
-class RemoveFavoriteTagsIndex < ActiveRecord::Migration
-  def change
-    remove_index :favorite_tags, :user_id
-  end
-end
--- db/migrate/20150217014435_add_favorite_tags_index.rb
+++
@@ -1,5 +0,0 @@
-class AddFavoriteTagsIndex < ActiveRecord::Migration
-  def change
-    add_index :favorite_tags, [:user_id, :tag_id], unique: true
-  end
-end
