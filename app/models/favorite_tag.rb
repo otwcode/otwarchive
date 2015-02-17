6374b265d7164cfd6512f6e59f2c8972f4874ec9
sarken <sarken@gmail.com>
2015-02-16 21:04:20 -0500

    4209 I'm sorry but that was ugly as sin

--- app/models/favorite_tag.rb
+++ app/models/favorite_tag.rb
@@ -3,10 +3,10 @@ class FavoriteTag < ActiveRecord::Base
   belongs_to :tag
 
   validates :user_id, presence: true
-  validates :tag_id, presence: true, uniqueness: { scope: :user_id,
-                                                   message: ts("is already in
-                                                           your favorite tags.")
-                                                }
+  validates :tag_id, presence: true,
+                     uniqueness: { scope: :user_id,
+                                   message: ts("is already in your favorite tags.")
+                                 }
 
   validate :within_limit, on: :create
 
