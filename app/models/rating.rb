fdcf48a979aaae8f90f8797c431440366985cb34
sarken <sarken@gmail.com>
2015-03-04 22:13:03 -0500

    4261 Spacing

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -6,16 +6,15 @@ class Rating < Tag
   # Gives us the default ratings as Not Rated + low to high
   def self.defaults_by_severity
     ratings = [ArchiveConfig.RATING_DEFAULT_TAG_NAME,
-               ArchiveConfig.RATING_GENERAL_TAG_NAME, 
-               ArchiveConfig.RATING_TEEN_TAG_NAME, 
-               ArchiveConfig.RATING_MATURE_TAG_NAME, 
-               ArchiveConfig.RATING_EXPLICIT_TAG_NAME]   
+               ArchiveConfig.RATING_GENERAL_TAG_NAME,
+               ArchiveConfig.RATING_TEEN_TAG_NAME,
+               ArchiveConfig.RATING_MATURE_TAG_NAME,
+               ArchiveConfig.RATING_EXPLICIT_TAG_NAME]
     ratings_by_id = Rating.where(name: ratings).inject({}) do |result, rating| 
       result[rating.name] = rating
       result
     end
-    sorted_ratings = ratings.map {|id| ratings_by_id[id]}
+    ratings.map { |id| ratings_by_id[id] }
   end
 
 end
-
