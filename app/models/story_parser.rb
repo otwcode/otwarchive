c397876fd07ede2e220bc597f798787a44ec5d02
Ariana <ariana@astele.co.uk>
2015-03-15 10:43:54 +0000

    4265 - Convert accented characters in external authors to ASCII and strip out invalid punctuation

--- app/models/story_parser.rb
+++ app/models/story_parser.rb
@@ -398,8 +398,8 @@ class StoryParser
     end
 
     def parse_author_common(email, name)
-      # strip out invalid characters (everything except alphanumeric characters, _, @ and -)
-      name.gsub!(/[^\w[ \-@\.]]/u, "")
+      # convert to ASCII and strip out invalid characters (everything except alphanumeric characters, _, @ and -)
+      name = name.to_ascii.gsub!(/[^\w[ \-@\.]]/u, "")
       external_author = ExternalAuthor.find_or_create_by_email(email)
       unless name.blank?
         external_author_name = ExternalAuthorName.where(name: name, external_author_id: external_author.id).first ||
