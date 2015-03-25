fbcc5d386aad8e99f69ba8e6e122d8c2e66fe408
sarken <sarken@gmail.com>
2015-03-25 02:59:29 -0400

    4272 Align the parameters of the yada yada maybe?

--- app/models/external_work.rb
+++ app/models/external_work.rb
@@ -16,19 +16,19 @@ class ExternalWork < ActiveRecord::Base
   validates_presence_of :title
   validates_length_of :title, minimum: ArchiveConfig.TITLE_MIN,
                               too_short: ts("must be at least %{min} characters long.",
-                                         min: ArchiveConfig.TITLE_MIN)
+                                            min: ArchiveConfig.TITLE_MIN)
   validates_length_of :title, maximum: ArchiveConfig.TITLE_MAX,
                               too_long: ts("must be less than %{max} characters long.",
-                                        max: ArchiveConfig.TITLE_MAX)
+                                           max: ArchiveConfig.TITLE_MAX)
 
   validates_length_of :summary, allow_blank: true, maximum: ArchiveConfig.SUMMARY_MAX,
                                 too_long: ts("must be less than %{max} characters long.",
-                                          max: ArchiveConfig.SUMMARY_MAX)
+                                             max: ArchiveConfig.SUMMARY_MAX)
 
   validates_presence_of :author, message: ts('^Creator can\'t be blank')
   validates_length_of :author, maximum: AUTHOR_LENGTH_MAX,
                                too_long: ts('^Creator must be less than %{max} characters long.',
-                                         max: AUTHOR_LENGTH_MAX)
+                                            max: AUTHOR_LENGTH_MAX)
 
   # TODO: External works should have fandoms, but they currently don't get added through the
   # post new work form so we can't validate them
