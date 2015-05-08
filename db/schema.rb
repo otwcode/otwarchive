826804a6baec1f9c38dfee22615cbbddc15a7f22
james_ <zz9pzza@gmail.com>
2015-05-08 18:53:46 +0000

    We need to do the migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150217034225) do
+ActiveRecord::Schema.define(:version => 20121129192353) do
 
   create_table "abuse_reports", :force => true do |t|
     t.string   "email"
