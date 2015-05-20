4efa5af218bb09d9274c475bc3ae087577cad335
zz9pzza <zz9pzza@gmail.com>
2015-05-20 16:05:29 +0000

    Spelling

--- app/mailers/kudo_mailer.rb
+++ app/mailers/kudo_mailer.rb
@@ -37,7 +37,7 @@ class KudoMailer < ActionMailer::Base
         next if kudo_givers.empty?
 
         @commentables << commentable
-        @kudo_givers[commentable_info] = kudo_givers.to_sentance
+        @kudo_givers[commentable_info] = kudo_givers.to_sentence
       end
       mail(
         to: user.email,
