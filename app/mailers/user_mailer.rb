c7e6ca6546b8cdef22353953f864321fa8533ae8
zz9pzza <zz9pzza@gmail.com>
2015-05-28 21:17:24 +0000

    Lets see what travis thinks

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -58,7 +58,7 @@ class UserMailer < BulletproofMailer::Base
     I18n.with_locale(locale) do
       mail(
         to: creator.email,
-        subject: "[#{ArchiveConfig.APP_SHORT_NAME}] Works uploaded"
+        subject: t('user_mailer.claim_notification.subject', app_name: ArchiveConfig.APP_SHORT_NAME)
       )
     end
     ensure
