5d325a86ce4cf03e767820da6474dffe341a8a3d
zz9pzza <zz9pzza@gmail.com>
2015-11-15 17:08:26 +0000

    create an authlogic adapter

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Otwarchive::Application.initialize!
+
+# http://stackoverflow.com/questions/5270835/authlogic-activation-problems
+# After updatign authlogic
+Authlogic::Session::Base.controller = Authlogic::ControllerAdapters::RailsAdapter.new(self)
