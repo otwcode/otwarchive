8dcdc4d42ab7840f42348a9f6a8555dcd88b79ef
james_ <zz9pzza@gmail.com>
2015-11-21 22:14:44 +0000

    ao3-app08 should be back in service

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -14,7 +14,7 @@ server "ao3-app03.ao3.org",  :app , :workers , :schedulers
 server "ao3-app04.ao3.org",  :app
 server "ao3-app99.ao3.org",  :app , :workers , :schedulers
 server "ao3-app05.ao3.org",  :app
-#server "ao3-app08.ao3.org",  :app , :workers , :schedulers
+server "ao3-app08.ao3.org",  :app , :workers , :schedulers
 server "ao3-app06.ao3.org",  :app
 server "ao3-app09.ao3.org",  :app , :workers , :schedulers
 server "ao3-app07.ao3.org",  :app
