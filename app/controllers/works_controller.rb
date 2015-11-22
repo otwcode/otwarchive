4c5b5ed4324aa919ddcb38ab8c577be4f0288777
james_ <zz9pzza@gmail.com>
2015-11-22 10:37:32 +0000

    Reduce the number of calls on the work page

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -121,7 +121,7 @@ class WorksController < ApplicationController
 
     if @owner.present?
       if @admin_settings.disable_filtering?
-        @works = Work.list_without_filters(@owner, options)
+        @works = Work.includes(:tags).list_without_filters(@owner, options)
       else
         @search = WorkSearch.new(options.merge(faceted: true, works_parent: @owner))
 
@@ -134,7 +134,7 @@ class WorksController < ApplicationController
           subtag = (@tag.present? && @tag != @owner) ? @tag : nil
           user = current_user.present? ? "logged_in" : "logged_out"
           @works = Rails.cache.fetch("#{@owner.works_index_cache_key(subtag)}_#{user}_page#{params[:page]}", expires_in: 20.minutes) do
-            results = @search.search_results
+            results = @search.search_results.includes(:tags,:pseuds,:external_creatorships,:series,:language,:approved_collections)
             # calling this here to avoid frozen object errors
             results.items
             results.facets
@@ -148,10 +148,10 @@ class WorksController < ApplicationController
       end
     elsif use_caching?
       @works = Rails.cache.fetch("works/index/latest/v1", :expires_in => 10.minutes) do
-        Work.latest.to_a
+        Work.latest.includes(:tags,:pseuds,:external_creatorships,:series,:language,:approved_collections).to_a
       end
     else
-      @works = Work.latest.to_a
+      @works = Work.latest.includes(:tags,:pseuds,:external_creatorships,:series,:language,:approved_collections).to_a
     end
   end
 
