2c6f2597d4bc546467d82d86fca50dc5dadcc004
james_ <zz9pzza@gmail.com>
2015-11-29 22:28:45 +0000

    Clean out processed migrations

--- db/migrate/20141003205439_add_email_enabled_to_locale.rb
+++
@@ -1,5 +0,0 @@
-class AddEmailEnabledToLocale < ActiveRecord::Migration
-  def change
-    add_column :locales, :email_enabled, :boolean, default: false, null: false
-  end
-end
--- db/migrate/20150901024743_add_moderated_commenting_enabled_to_works.rb
+++
@@ -1,5 +0,0 @@
-class AddModeratedCommentingEnabledToWorks < ActiveRecord::Migration
-  def change
-    add_column :works, :moderated_commenting_enabled, :boolean, default: false, null: false
-  end
-end
