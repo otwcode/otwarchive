765e48b35ada92dcc98d1644a7f085cc6d4bc409
james_ <zz9pzza@gmail.com>
2015-11-29 16:01:04 +0000

    Repeat a command ( for codeship )

---
+++ script/try_command.sh
@@ -0,0 +1,10 @@
+#¡/bin/bash
+MAX_LOOP=${TRIES:-3}
+n=0
+until [ $n -ge $MAX_LOOP ]
+ do
+    echo "Attempt $n"
+    bash -c "$1" && break  # substitute your command here
+    n=$[$n+1]
+    sleep 5
+done
