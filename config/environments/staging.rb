b168919ace8930b41421af9e8505fab3e0895dae
Scott D. Sullivan <scottsds@gmail.com>
2016-01-02 20:07:54 -0800

    AO3-4413 Move Bullet notices to browser console instead of footer

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -67,7 +67,8 @@ Otwarchive::Application.configure do
   config.after_initialize do
     Bullet.enable = true
     Bullet.bullet_logger = true
-    Bullet.add_footer = false
+    Bullet.add_footer =
+    Bullet.console = true
     Bullet.rails_logger = true
     Bullet.counter_cache_enable = false
   end
