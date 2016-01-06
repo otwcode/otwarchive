44a3ed6d987db81042ac014bd175b8188ed12ffe
james_ <zz9pzza@gmail.com>
2016-01-06 22:52:59 +0000

    Changes to the key generation to halp writing tests if we need them

--- lib/cache_helper.rb
+++ lib/cache_helper.rb
@@ -1,8 +1,12 @@
 # note, if you modify this file you have to restart the server or console
 module CacheHelper
+  
+  def skin_cache_key(skin)
+   'skins_generation/'+(skin.type.nil? ? ("site_skin") : skin.id.to_s)
+  end
 
   def skin_cache(skin)
-    Rails.cache.increment('skins_generation/'+(skin.type.nil? ? ("site_skin") : skin.id.to_s) )
+    Rails.cache.increment(skin_cache_key(skin))
   end
 
 end
