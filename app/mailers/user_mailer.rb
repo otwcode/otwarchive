0d022561dcbce022589fd1a0dbbe74ab844c9f1a
Cesy Avon <cesy.avon@gmail.com>
2016-01-21 14:39:53 +0000

    Changing string interpolation to to_s for hound

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -220,7 +220,7 @@ class UserMailer < BulletproofMailer::Base
     @request = (assignment.request_signup || assignment.pinch_request_signup)
     mail(
       to: @assigned_user.email,
-      subject: "#{t 'user_mailer.challenge_assignment_notification.subject', app_name: ArchiveConfig.APP_SHORT_NAME, collection_title: @collection.title}"
+      subject: t('user_mailer.challenge_assignment_notification.subject', app_name: ArchiveConfig.APP_SHORT_NAME, collection_title: @collection.title).to_s
     )
   end
 
