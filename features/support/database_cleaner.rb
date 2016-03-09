b9cfba30eda8fb355464f69a1f8240be609d692c
zz9pzza <zz9pzza@gmail.com>
2016-03-09 21:25:15 +0000

    Put minimal info into the database each time

--- features/support/database_cleaner.rb
+++ features/support/database_cleaner.rb
@@ -11,6 +11,16 @@ end
 Before do
   DatabaseCleaner.strategy = :transaction
   DatabaseCleaner.start
+  if AdminSetting.table_exists? && !AdminSetting.first
+    settings = AdminSetting.new(:invite_from_queue_enabled => ArchiveConfig.INVITE_FROM_QUEUE_ENABLED,
+          :invite_from_queue_number => ArchiveConfig.INVITE_FROM_QUEUE_NUMBER,
+          :invite_from_queue_frequency => ArchiveConfig.INVITE_FROM_QUEUE_FREQUENCY,
+          :account_creation_enabled => ArchiveConfig.ACCOUNT_CREATION_ENABLED,
+          :days_to_purge_unactivated => ArchiveConfig.DAYS_TO_PURGE_UNACTIVATED)
+    settings.save(:validate => false)
+  end
+  Language.default
+  Locale.default
 end
 
 After do
