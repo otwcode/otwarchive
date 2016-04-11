0f4d0b6e6dfa48025f30f13c2731860d6347747e
Scott D. Sullivan <scottsds@gmail.com>
2016-04-11 07:16:35 -0700

    AO3-4386 Updating test syntax

--- spec/helpers/tag_sets_helper_spec.rb
+++ spec/helpers/tag_sets_helper_spec.rb
@@ -15,23 +15,23 @@ describe TagSetsHelper do
         @limit[:fandom] = 0
         @limit[:character] = 0
         @limit[:relationship] = 0
-        expect(helper.nomination_notes(@limit))
-          .to eq('You can nominate up to 3 additional tags.')
+        expect(helper.nomination_notes(@limit)).
+          to eq('You can nominate up to 3 additional tags.')
       end
     end
 
     context 'for nominations allowing relationships' do
       it 'should have relationships info listed last' do
-        expect(helper.nomination_notes(@limit))
-          .to match('characters and 3 relationships for each one.')
+        expect(helper.nomination_notes(@limit)).
+          to match('characters and 3 relationships for each one.')
       end
     end
 
     context 'for nominations allowing NO relationships' do
       it 'should not mention relationships' do
         @limit[:relationship] = 0
-        expect(helper.nomination_notes(@limit))
-          .to match('3 fandoms and up to 3 characters for each one.')
+        expect(helper.nomination_notes(@limit)).
+          to match('3 fandoms and up to 3 characters for each one.')
       end
     end
 
@@ -40,8 +40,8 @@ describe TagSetsHelper do
       it 'should mention only fandoms' do
         @limit[:relationship] = 0
         @limit[:character] = 0
-        expect(helper.nomination_notes(@limit))
-          .to match('You can nominate up to 3 fandoms.')
+        expect(helper.nomination_notes(@limit)).
+          to match('You can nominate up to 3 fandoms.')
       end
     end
 
@@ -49,16 +49,16 @@ describe TagSetsHelper do
         and NO characters' do
       it 'should mentions fandoms and relationships' do
         @limit[:character] = 0
-        expect(helper.nomination_notes(@limit))
-          .to match('3 fandoms and up to 3 relationships for each one.')
+        expect(helper.nomination_notes(@limit)).
+          to match('3 fandoms and up to 3 relationships for each one.')
       end
     end
 
     context 'for nominations allowing characters, relationships, NO fandoms' do
       it 'should mention characters and relationships' do
         @limit[:fandom] = 0
-        expect(helper.nomination_notes(@limit))
-          .to match('You can nominate up to 3 characters and 3 relationships.')
+        expect(helper.nomination_notes(@limit)).
+          to match('You can nominate up to 3 characters and 3 relationships.')
       end
     end
 
@@ -68,8 +68,8 @@ describe TagSetsHelper do
         @limit[:fandom] = 0
         @limit[:relationship] = 0
         @limit[:freeform] = 0
-        expect(helper.nomination_notes(@limit))
-          .to eq('You can nominate up to 3 characters.')
+        expect(helper.nomination_notes(@limit)).
+          to eq('You can nominate up to 3 characters.')
       end
     end
 
@@ -79,8 +79,8 @@ describe TagSetsHelper do
         @limit[:fandom] = 0
         @limit[:character] = 0
         @limit[:freeform] = 0
-        expect(helper.nomination_notes(@limit))
-          .to eq('You can nominate up to 3 relationships.')
+        expect(helper.nomination_notes(@limit)).
+          to eq('You can nominate up to 3 relationships.')
       end
     end
   end
@@ -92,18 +92,19 @@ describe TagSetsHelper do
 
     context 'for valid nominations' do
       it 'should show basic information' do
-        expect(helper.nomination_status(@fake_nomination))
-          .to include('unreviewed').and include('?!')
-          .and include('This nomination has not been reviewed yet.')
+        expect(helper.nomination_status(@fake_nomination)).
+          to include('unreviewed').and include('?!').
+          and include('This nomination has not been reviewed yet.')
       end
     end
 
     context 'for approved nominations' do
       it 'should show correct class information' do
         @nomination.approved = true
-        expect(helper.nomination_status(@nomination)).to include('approved')
-          .and include('This nomination has been approved!')
-          .and include('&#10004;')
+        expect(helper.nomination_status(@nomination)).
+          to include('approved').
+          and include('This nomination has been approved!').
+          and include('&#10004;')
       end
     end
 
@@ -111,9 +112,10 @@ describe TagSetsHelper do
       it 'should show correct class information' do
         @nomination.rejected = true
         @nomination.approved = false
-        expect(helper.nomination_status(@nomination)).to include('rejected')
-          .and include('This nomination was rejected')
-          .and include('&#10006;')
+        expect(helper.nomination_status(@nomination)).
+          to include('rejected').
+          and include('This nomination was rejected').
+          and include('&#10006;')
       end
     end
 
@@ -123,9 +125,10 @@ describe TagSetsHelper do
         @nomination.approved = ''
         @nomination.rejected = ''
         @tag_set = FactoryGirl.create(:owned_tag_set)
-        expect(helper.nomination_status(@nomination)).to include('unreviewed')
-          .and include('has not been reviewed yet and can still be changed.')
-          .and include('?!')
+        expect(helper.nomination_status(@nomination)).
+          to include('unreviewed').
+          and include('has not been reviewed yet and can still be changed.').
+          and include('?!')
       end
     end
   end
@@ -139,10 +142,10 @@ describe TagSetsHelper do
       it 'should show basic information' do
         @nominated_tag.canonical = false
         @nominated_tag.exists = false
-        expect(helper.nomination_tag_information(@nominated_tag))
-          .to include('nonexistent')
-          .and include('nonexistent tag')
-          .and include('This tag has never been used before.')
+        expect(helper.nomination_tag_information(@nominated_tag)).
+          to include('nonexistent').
+          and include('nonexistent tag').
+          and include('This tag has never been used before.')
       end
     end
 
@@ -150,18 +153,18 @@ describe TagSetsHelper do
       it 'should show correct tag information for parented tags' do
         @nominated_tag.canonical = true
         @nominated_tag.parented = true
-        expect(helper.nomination_tag_information(@nominated_tag))
-          .to include('This is a canonical tag.')
-          .and include("title=\"canonical tag\"")
-          .and include('canonical nomination')
+        expect(helper.nomination_tag_information(@nominated_tag)).
+          to include('This is a canonical tag.').
+          and include("title=\"canonical tag\"").
+          and include('canonical nomination')
       end
 
       it 'should show correct tag information for unparented tags' do
         @nominated_tag.canonical = true
         @nominated_tag.parented = false
-        expect(helper.nomination_tag_information(@nominated_tag))
-          .to include('but not associated with the')
-          .and include('tag without parent')
+        expect(helper.nomination_tag_information(@nominated_tag)).
+          to include('but not associated with the').
+          and include('tag without parent')
       end
     end
 
@@ -175,14 +178,14 @@ describe TagSetsHelper do
         @synonym.synonym = true
         @synonym.canonical = false
         @nominated_tag.canonical = true
-        @synonym.owned_tag_set
-          .tags.last.merger = @nominated_tag.owned_tag_set.tags.last
+        @synonym.owned_tag_set.
+          tags.last.merger = @nominated_tag.owned_tag_set.tags.last
         @synonym.tagname = @synonym.owned_tag_set.tags.last.name
 
-        expect(helper.nomination_tag_information(@synonym))
-          .to include("title=\"synonym\"")
-          .and include('This is a synonym of a canonical tag.')
-          .and include('non-canonical tag')
+        expect(helper.nomination_tag_information(@synonym)).
+          to include("title=\"synonym\"").
+          and include('This is a synonym of a canonical tag.').
+          and include('non-canonical tag')
       end
     end
 
@@ -191,10 +194,10 @@ describe TagSetsHelper do
         @nominated_tag.exists = true
         @nominated_tag.canonical = false
 
-        expect(helper.nomination_tag_information(@nominated_tag))
-          .to include("title=\"non-canonical tag\"")
-          .and include('This is not a canonical tag.')
-          .and include('unwrangled')
+        expect(helper.nomination_tag_information(@nominated_tag)).
+          to include("title=\"non-canonical tag\"").
+          and include('This is not a canonical tag.').
+          and include('unwrangled')
       end
     end
   end
@@ -203,8 +206,8 @@ describe TagSetsHelper do
     context 'for a single type of tag' do
       it 'should return a list of single-type tags' do
         @owned_tag_set = FactoryGirl.create(:owned_tag_set)
-        expect(helper.tag_relation_to_list(@owned_tag_set.with_type(Fandom)))
-          .to include("<li>#{@owned_tag_set.tags.last.name}</li>")
+        expect(helper.tag_relation_to_list(@owned_tag_set.with_type(Fandom))).
+          to include("<li>#{@owned_tag_set.tags.last.name}</li>")
       end
     end
   end
