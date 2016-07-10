5c13eba436a22efe233c3fca8eeb6e38a6d21c53
zz9pzza <zz9pzza@gmail.com>
2016-07-10 07:04:59 +0000

    Put ao3-app09 back in service

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -18,6 +18,7 @@ server "ao3-app08.ao3.org",  :app , :workers , :schedulers
 server "ao3-app06.ao3.org",  :app
 server "ao3-app07.ao3.org",  :app
 server "ao3-front01.ao3.org", :web
+server "ao3-app09.ao3.org",  :app 
 server "ao3-front02.ao3.org", :web
 
 # ORDER OF EVENTS
