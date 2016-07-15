453087e1f6a5a448fc6a92418e0a27941c71cbff
Leonardo Prado <leonardoprado@gmail.com>
2016-07-15 18:38:00 -0300

    AO3-2839 Remove letter_opener as it's incompatible with vagrant

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,6 @@ Otwarchive::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :letter_opener
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
