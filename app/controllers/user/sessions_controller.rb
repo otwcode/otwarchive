c4d582671e6fc819a480f3393f96609b373276f2
Leonardo Prado <leonardoprado@gmail.com>
2016-07-18 19:05:12 -0300

    AO3-2839 Fix user redirect after logout

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -4,5 +4,14 @@ class User
     layout 'session'
     before_filter :admin_logout_required, except: :destroy
     skip_before_filter :store_location
+
+    # Devise destroy the whole session, so we need to store the
+    # return path and pass if after the session is destroyed
+    def destroy
+      return_to = session[:return_to]
+      super do
+        session[:return_to] = return_to
+      end
+    end
   end
 end
