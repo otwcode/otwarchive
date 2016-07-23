a27ab5d176864e3ceb5b73c8a72d978ac2a3d0ac
sarken <sarken@gmail.com>
2016-07-23 04:41:43 -0400

    The pseud switcher specific JS is no longer needed and in fact breaks

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -399,19 +399,8 @@ function setupAccordion() {
         e.preventDefault();
       };
 
-      // We need to treat the pseud menu differently so it will be properly responsive
-      // The other accordions need to be converted to a similar system
-      // Otherwise we run into bugs if one @media uses inline display and another uses block
-      if (expander.attr('title') == 'Pseud Switcher') {
-        if (expander.hasClass('expanded')) {
-          expander.toggleClass("expanded").toggleClass("collapsed").next().removeAttr('style');
-        } else {
-          expander.toggleClass("expanded").toggleClass("collapsed").next().hide();
-        }
-      } else {
-        // change the classes upon clicking the expander
-        expander.toggleClass("collapsed").toggleClass("expanded").next().toggleClass("hidden");
-      }
+      // change the classes upon clicking the expander
+      expander.toggleClass("collapsed").toggleClass("expanded").next().toggleClass("hidden");
     });
   });
 }
