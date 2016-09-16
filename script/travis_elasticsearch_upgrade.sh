6d0d5a7b0b393f836497623685b61f16aecca1d1
sarken <sarken@gmail.com>
2016-09-16 06:14:42 +0000

    Try using same version of Elasticsearch as on our dev server

--- script/travis_elasticsearch_upgrade.sh
+++ script/travis_elasticsearch_upgrade.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo service elasticsearch stop
-wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.13.deb
-sudo dpkg --force-confold -i elasticsearch-0.90.13.deb
+wget https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.3.3/elasticsearch-2.3.3.deb
+sudo dpkg --force-confold -i elasticsearch-2.3.3.deb
 sudo service elasticsearch start
