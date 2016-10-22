1e9fe10a333c0942e500e2c272f846cb6a8795bd
Ariana <ariana@astele.co.uk>
2016-10-22 10:04:29 +0100

    AO3-4675 - Add tests for error messages in different situations

--- spec/api/api_base_controller_spec.rb
+++ spec/api/api_base_controller_spec.rb
@@ -1,59 +1,48 @@
-require 'spec_helper'
-require 'api/api_helper'
+require "spec_helper"
+require "api/api_helper"
 
 include ApiHelper
 
-describe 'Base API Controller' do
+describe "Base API Controller" do
+
+  before do
+    allow(User).to receive(:is_archivist?).and_return(true)
+  end
 
   before(:each) do
     @under_test = Api::V1::BaseController.new
   end
 
-  describe 'batch_errors with no archivist' do
-    it 'should return the "forbidden" status' do
-      status, _ = @under_test.instance_eval { batch_errors(nil, api_fields) }
-      assert_equal status, :forbidden
-    end
+  describe "batch_errors" do
 
-    it 'should return an error message' do
-      _, messages = @under_test.instance_eval { batch_errors(nil, api_fields) }
-      assert_equal "The 'archivist' field must specify the name of an Archive user with archivist privileges.", messages[0]
-    end
-  end
+    describe "with no archivist" do
+      it "should return the 'forbidden' status" do
+        status, _ = @under_test.instance_eval { batch_errors(nil, api_fields) }
+        assert_equal status, :forbidden
+      end
 
-  describe 'batch_errors with no items' do
-    # Override is_archivist so all users are archivists from this point on
-    class User < ActiveRecord::Base
-      def is_archivist?
-        true
+      it "should return an error message" do
+        _, messages = @under_test.instance_eval { batch_errors(nil, api_fields) }
+        assert_equal "The 'archivist' field must specify the name of an Archive user with archivist privileges.", messages[0]
       end
     end
 
-    it 'should return error messages with no items to import' do
-      user = create(:user)
-      _, messages = @under_test.instance_eval { batch_errors(user, nil) }
-      assert_equal 'No items to import were provided.', messages[0]
-    end
-  end
+    describe "with a user who is not an archivist" do
+      it "should return the 'forbidden' status" do
+        allow(User).to receive(:is_archivist?).and_return(false)
+        status, _ = @under_test.instance_eval { batch_errors(nil, api_fields) }
+        assert_equal status, :forbidden
+      end
 
-  describe 'batch_errors with too many items' do
-    # Override is_archivist so all users are archivists from this point on
-    class User < ActiveRecord::Base
-      def is_archivist?
-        true
+      it "should return an error message" do
+        allow(User).to receive(:is_archivist?).and_return(false)
+        _, messages = @under_test.instance_eval { batch_errors(nil, api_fields) }
+        assert_equal "The 'archivist' field must specify the name of an Archive user with archivist privileges.", messages[0]
       end
     end
-
-    it 'should return error messages with too many items to import' do
-      user = create(:user)
-      loads_of_items = Array.new(210) { |i| api_fields }
-      puts loads_of_items[0]
-      _, messages = @under_test.instance_eval { batch_errors(user, loads_of_items) }
-      assert_equal 'This request contains too many items to import. A maximum of 200 items can be imported at one time by an archivist.', messages[0]
-    end
   end
 
-  describe 'batch_errors with a valid pseud and items' do
+  describe "batch_errors with a valid pseud" do
     # Override is_archivist so all users are archivists from this point on
     class User < ActiveRecord::Base
       def is_archivist?
@@ -69,13 +58,26 @@ describe 'Base API Controller' do
       @user.destroy
     end
 
-    it 'should return OK status' do
+    it "should return error messages with no items to import" do
+      user = @user
+      _, messages = @under_test.instance_eval { batch_errors(user, nil) }
+      assert_equal "No items to import were provided.", messages[0]
+    end
+
+    it "should return error messages with too many items to import" do
+      user = @user
+      loads_of_items = Array.new(210) { |_| api_fields }
+      _, messages = @under_test.instance_eval { batch_errors(user, loads_of_items) }
+      assert_equal "This request contains too many items to import. A maximum of 200 items can be imported at one time by an archivist.", messages[0]
+    end
+
+    it "should return OK status" do
       user = @user
       status, _ = @under_test.instance_eval { batch_errors(user, api_fields) }
       assert_equal :ok, status
     end
 
-    it 'should return no error messages' do
+    it "should return no error messages" do
       user = @user
       _, messages = @under_test.instance_eval { batch_errors(user, api_fields) }
       assert_equal 0, messages.size
