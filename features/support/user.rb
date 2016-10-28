087f319cce24d75bb73231321cd108ae588b92df
Ariana <ariana@astele.co.uk>
2016-10-28 19:30:57 +0100

    AO3-4692 - Add hidden option to user login method

--- features/support/user.rb
+++ features/support/user.rb
@@ -1,5 +1,5 @@
 module UserHelpers
-  def find_or_create_user(login, password)
+  def find_or_create_user(login, password, hidden)
     user = User.find_by_login(login)
     if user.blank?
       user = FactoryGirl.create(:user, { login: login, password: password })
@@ -8,6 +8,11 @@ module UserHelpers
       user.password = password
       user.password_confirmation = password
       user.save
+    end
+    if hidden.present?
+      user.preference.hide_warnings = true
+      user.preference.hide_freeform = true
+      user.preference.save
     end
     user
   end
