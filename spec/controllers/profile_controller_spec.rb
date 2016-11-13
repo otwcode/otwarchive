fa1291a4ea3c95b7a83aa468c775574f954baf26
zz9pzza <zz9pzza@gmail.com>
2016-11-13 13:00:16 +0000

    listen to the hound

--- spec/controllers/profile_controller_spec.rb
+++ spec/controllers/profile_controller_spec.rb
@@ -1,21 +1,19 @@
 require 'spec_helper'
 
 describe ProfileController do
-
-  describe "show" do
-    it "should be an error for a non existent user" do
-      get :show, user: 999999999999 
+  describe 'show' do
+    it 'should be an error for a non existent user' do
+      get :show, user: 999_999_999_999
 
       expect(response).to redirect_to(root_path)
       expect(flash[:error]).to eq "Sorry, there's no user by that name."
     end
 
-    it "should create a new profile if one does not exist" do
+    it 'should create a new profile if one does not exist' do
       @user = FactoryGirl.create(:user)
       @user.profile.destroy
       get :show, user: @user.id
       expect(@user.profile).to be
     end
   end
-
 end
