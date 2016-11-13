34ea300d3dbb6f7ce6d6ea61c4b3f72314e814df
zz9pzza <zz9pzza@gmail.com>
2016-11-13 20:02:59 +0000

    Add rspec tests for the comments controller

---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe CommentsController do
+  include LoginMacros
+
+  describe 'comment reviews' do
+  
+    it "checks that the comment is reviewed and asks that you login" do
+      comment = FactoryGirl.create(:comment)
+      comment.unreviewed = true
+      comment.save
+      get :add_comment_reply, comment_id: comment.id
+      expect(response).to redirect_to(login_path)
+      expect(flash[:error]).to eq "Sorry, you cannot reply to an unapproved comment."
+    end
+
+    it "checks that the comment is reviewed" do
+      fake_login
+      comment = FactoryGirl.create(:comment)
+      comment.unreviewed = true
+      comment.save
+      get :add_comment_reply, comment_id: comment.id
+      expect(response).to redirect_to(root_path)
+      expect(flash[:error]).to eq "Sorry, you cannot reply to an unapproved comment."
+    end
+
+    it "checks that there is something to comment on" do
+      post :new, tag_id: "Non existant tag"
+      expect(flash[:error]).to eq "What did you want to comment on?"
+    end
+
+    it "create a comment on an Admin post" do
+      admin_post = FactoryGirl.create(:admin_post)
+      post :new, admin_post_id: admin_post.id
+      expect(response).to render_template("new")
+    end
+
+    it "create a comment on an fandom" do
+      fandom = FactoryGirl.create(:fandom)
+      post :new, tag_id: fandom.name
+      expect(response).to render_template("new")
+    end
+
+    it "create a new comment on an comment" do
+      comment = FactoryGirl.create(:comment)
+      post :new, comment_id: comment.id
+      expect(response).to render_template("new")
+    end
+  
+  end
+
+end
