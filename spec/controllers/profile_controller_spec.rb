3c0b0ad3f60962fc4995e34981b2e8c480f3ff21
zz9pzza <zz9pzza@gmail.com>
2016-11-13 12:57:22 +0000

    Simple rspec test for profile controller

---
+++ spec/controllers/profile_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe ProfileController do
+
+  describe "show" do
+    it "should be an error for a non existent user" do
+      get :show, user: 999999999999 
+
+      expect(response).to redirect_to(root_path)
+      expect(flash[:error]).to eq "Sorry, there's no user by that name."
+    end
+
+    it "should create a new profile if one does not exist" do
+      @user = FactoryGirl.create(:user)
+      @user.profile.destroy
+      get :show, user: @user.id
+      expect(@user.profile).to be
+    end
+  end
+
+end
