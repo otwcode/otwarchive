5bfe80e14c45f70a0f32decdb2a526e5f27fec91
zz9pzza <zz9pzza@gmail.com>
2016-12-03 15:43:43 +0000

    Get rid of infected file

--- spec/controllers/comments_controller_spec.rb
+++
@@ -1,6 +0,0 @@
---2016-11-19 22:30:54--  https://raw.githubusercontent.com/otwcode/otwarchive/master/spec/controllers/comments_controller_spec.rb
-Resolving raw.githubusercontent.com (raw.githubusercontent.com)... 151.101.16.133
-Connecting to raw.githubusercontent.com (raw.githubusercontent.com)|151.101.16.133|:443... connected.
-HTTP request sent, awaiting response... 404 Not Found
-2016-11-19 22:30:54 ERROR 404: Not Found.
-
