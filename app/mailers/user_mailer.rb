16711cd1914ee14d898cb6d98554a232cf48d393
zz9pzza <zz9pzza@gmail.com>
2016-12-19 15:52:42 +0000

    Change subject

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -275,7 +275,7 @@ class UserMailer < BulletproofMailer::Base
     I18n.with_locale(Locale.find(@user.preference.preferred_locale).iso) do
       mail(
         to: @user.email,
-        subject: "#{t 'user_mailer.coauthor_notification.subject', app_name: ArchiveConfig.APP_SHORT_NAME}"
+        subject: t('user_mailer.coauthor_notification.subject', app_name: ArchiveConfig.APP_SHORT_NAME})
       )
     end
     ensure
