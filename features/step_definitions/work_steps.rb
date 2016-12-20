deeffada9163db4e6dc02485732f47344056fa40
pixelred <orihar4@gmail.com>
2016-12-19 22:36:00 -0800

    AO3-4768 regex in work_steps.rb should be updated and working now

--- features/step_definitions/work_steps.rb
+++ features/step_definitions/work_steps.rb
@@ -80,8 +80,8 @@ end
 # To test posting after preview, use: Given the draft "Foo"
 # Then use: When I post the work "Foo"
 # and the above step
-Given /^the draft "([^"]*)"(?: with fandom "([^"]*)")?(?: with freeform "([^"]*)")?(?: with category "([^"]*)")?(?: in (?:the )?collection "([^"]*)")?(?: as a gift (?:for|to) "([^"]*)")?$/ do |title, fandom, freeform, category, collection, recipient|
-  step %{I set up the draft "#{title}" with fandom "#{fandom}" with freeform "#{freeform}" with category "#{category}" in collection "#{collection}" as a gift to "#{recipient}"}
+Given /^the draft "([^"]*)"(?: with fandom "([^"]*)")?(?: with character "([^"]*)")?(?: with second character "([^"]*)")?(?: with freeform "([^"]*)")?(?: with second freeform "([^"]*)")?(?: with category "([^"]*)")?(?: (?:in|to) (?:the )?collection "([^"]*)")?(?: as a gift (?:for|to) "([^"]*)")?(?: as part of a series "([^"]*)")?(?: with relationship "([^"]*)")?(?: using the pseud "([^"]*)")?$/ do |title, fandom, character, character2, freeform, freeform2, category, collection, recipient, series, relationship, pseud|
+  step %{I set up the draft "#{title}" with fandom "#{fandom}" with character "#{character}" with second character "#{character2}" with freeform "#{freeform}" with second freeform "#{freeform2}" with category "#{category}" in collection "#{collection}" as a gift to "#{recipient}" as part of a series "#{series}" with relationship "#{relationship}" using the pseud "#{pseud}"}
   click_button("Preview")
 end
 
