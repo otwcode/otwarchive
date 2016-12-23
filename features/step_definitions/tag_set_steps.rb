cd5421d91bf11c386db417d34382ba848357e62a
pixelred <orihar4@gmail.com>
2016-12-22 17:50:46 -0800

    IT'S GONNA WORK

--- features/step_definitions/tag_set_steps.rb
+++ features/step_definitions/tag_set_steps.rb
@@ -89,7 +89,7 @@ end
 When /^there are (\d+) unreviewed nominations$/ do |n|
   (1..n.to_i).each do |i|
     step %{I am logged in as \"nominator#{i}\"}
-    step %{And I nominate 6 fandoms and 6 characters in the "Nominated Tags" tag set as \"nominator#{i}\"}
+    step %{I nominate 6 fandoms and 6 characters in the "Nominated Tags" tag set as \"nominator#{i}\"}
   end
 end
 
