fa545c4f8aebdd0950b4d929b2684b5beb89c2ed
pixelred <orihar4@gmail.com>
2016-12-23 17:53:24 -0800

    shadowed variable

--- features/step_definitions/tag_set_steps.rb
+++ features/step_definitions/tag_set_steps.rb
@@ -27,8 +27,8 @@ end
 # Takes things like When I add the fandom tags "Bandom" to the tag set "MoreJoyDay". Don't forget the extra s, even if it's singular.
 When /^I add (.*) to the tag ?set "([^\"]*)"$/ do |tags, title|
   step %{I go to the "#{title}" tag set edit page}
-    tags.scan(/the (\w+) tags "([^\"]*)"/).each do |type, tags|
-      fill_in("owned_tag_set_tag_set_attributes_#{type}_tagnames_to_add", :with => tags)
+    tags.scan(/the (\w+) tags "([^\"]*)"/).each do |type, scannedtags|
+      fill_in("owned_tag_set_tag_set_attributes_#{type}_tagnames_to_add", :with => scannedtags)
     end
     step %{I submit}
   step %{I should see an update confirmation message}
