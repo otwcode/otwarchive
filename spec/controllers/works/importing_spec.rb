d68e9021a46b4a9a9401e6ac492af56acd5cacbc
Ariana <ariana@astele.co.uk>
2016-12-28 19:32:53 +0000

    AO3-4797 - Cover import_multiple error message

--- spec/controllers/works/importing_spec.rb
+++ spec/controllers/works/importing_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 describe WorksController do
   include LoginMacros
 
+  puts "RUNNING IMPORTING_SPEC"
+
   describe "import" do
     context "should return the right error messages" do
       let(:user) { create(:user) }
@@ -73,23 +75,33 @@ describe WorksController do
     end
   end
 
+  # note: this non-action method renders a template before returning which Rspec can't process,
+  # so calling the method directly requires a rescue to swallow the Rspec error
   describe "import_single" do
     it "should display the correct error when a timeout occurs" do
       allow_any_instance_of(StoryParser).to receive(:download_and_parse_story).and_raise(Timeout::Error)
-      expect(controller.send(:import_single, ["url1"], {})).to rescue(:new_import)
+      expect(controller.send(:import_single, ["url1"], {})).to rescue # stop it whingeing about the response
       expect(flash[:error]).to start_with "Import has timed out"
       allow_any_instance_of(StoryParser).to receive(:download_and_parse_story).and_call_original
     end
 
     it "should display the correct error when a StoryParser error occurs" do
       allow_any_instance_of(StoryParser).to receive(:download_and_parse_story).and_raise(StoryParser::Error.new("message"))
-      expect(controller.send(:import_single, ["url1"], {})).to rescue(:new_import)
+      expect(controller.send(:import_single, ["url1"], {})).to rescue # stop it whingeing about the response
       expect(flash[:error]).to start_with "We couldn't successfully import that work, sorry: message"
       allow_any_instance_of(StoryParser).to receive(:download_and_parse_story).and_call_original
     end
   end
 
+  # note: this non-action method renders a template before returning which Rspec can't process,
+  # so calling the method directly requires a rescue to swallow the Rspec error
   describe "import_multiple" do
-
+    it "should display the correct error when a timeout occurs" do
+      works_failed_errors = [[], ["url1"], ["error 1", "error 2"]]
+      allow_any_instance_of(StoryParser).to receive(:import_from_urls).and_return(works_failed_errors)
+      expect(controller.send(:import_multiple, ["url1"], {})).to rescue # stop it whingeing about the response
+      expect(flash[:error]).to eq "<h3>Failed Imports</h3><dl><dt>url1</dt><dd>error 1</dd>\n<dt></dt><dd>error 2</dd></dl>"
+      allow_any_instance_of(StoryParser).to receive(:import_from_urls).and_call_original
+    end
   end
 end
