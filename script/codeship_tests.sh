39f5ec7a5b4bac313cf9480d41352388c5a86dea
zz9pzza <zz9pzza@gmail.com>
2017-01-05 23:20:29 +0000

    Try multiple tests again

--- script/codeship_tests.sh
+++ script/codeship_tests.sh
@@ -9,35 +9,11 @@ bash ./script/prepare_codeship.sh
 # This rune forces something to succeed. "|| :"
 if [ -n "${BROWSERSTACK_USERNAME}" ] ; then
   export TRIES = 1
-  CFG_NAME="_windows_10_edge" bash ./script/try_command.sh "" \
-  "bundle exec cucumber --tags @browserstack -f progress -r features features/\$TEST_RUN"
-
-  CFG_NAME="_windows_10_ie" bash ./script/try_command.sh "" \
-  "bundle exec cucumber --tags @browserstack -f progress -r features features/\$TEST_RUN"
-
-  CFG_NAME="_windows_10_firefox" bash ./script/try_command.sh "" \
-  "bundle exec cucumber --tags @browserstack -f progress -r features features/\$TEST_RUN"
-
-  CFG_NAME="_windows_8_opera" bash ./script/try_command.sh "" \
-  "bundle exec cucumber --tags @browserstack -f progress -r features features/\$TEST_RUN"
-
-  CFG_NAME="_windows_10_chrome" bash ./script/try_command.sh "" \
-  "bundle exec cucumber --tags @browserstack -f progress -r features features/\$TEST_RUN"
-
-  CFG_NAME="_osx_yosemite_safari" bash ./script/try_command.sh "" \
-  "bundle exec cucumber --tags @browserstack -f progress -r features features/\$TEST_RUN"
-
-  CFG_NAME="_nexus_5" TRIES=1 bash ./script/try_command.sh "" \
-  "bundle exec cucumber --tags @browserstack -f progress -r features features/\$TEST_RUN"
-
-  CFG_NAME="_kindle_fire_2" bash ./script/try_command.sh "" \
-  "bundle exec cucumber --tags @browserstack -f progress -r features features/\$TEST_RUN"
-
-  CFG_NAME="_galaxy_tab4_101" bash ./script/try_command.sh "" \
-  "bundle exec cucumber --tags @browserstack -f progress -r features features/\$TEST_RUN"
-
-  CFG_NAME="_ipad_mini_4" bash ./script/try_command.sh "" \
-  "bundle exec cucumber --tags @browserstack -f progress -r features features/\$TEST_RUN"
+  for i in _windows_10_edge _windows_10_ie _windows_10_firefox _windows_8_opera _windows_10_chrome \
+        osx_yosemite_safari _nexus_5 _kindle_fire_2 _ipad_mini_4 do
+    export CFG_NAME="$i"
+    bash ./script/try_command.sh "" "bundle exec cucumber --tags @browserstack -f progress -r features features/\$TEST_RUN"
+  done
   export TRIES = 3
 fi
 bash ./script/try_command.sh admins             "bundle exec cucumber --tags ~@browserstack -f progress -r features features/\$TEST_RUN"
