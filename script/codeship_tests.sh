c5ffc16680195fb06d1d2b766eeb3ca7bd683d38
zz9pzza <zz9pzza@gmail.com>
2017-01-05 23:27:57 +0000

    Try multiple tests again again

--- script/codeship_tests.sh
+++ script/codeship_tests.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 export RAILS_ENV=test
 bash ./script/prepare_codeship.sh
 TRIES=1 bash ./script/try_command.sh "" "bundle exec cucumber --tags @browserstack -f progress -r features features/\$TEST_RUN"
@@ -11,8 +11,7 @@ if [ -n "${BROWSERSTACK_USERNAME}" ] ; then
   export TRIES = 1
   for i in _windows_10_edge _windows_10_ie _windows_10_firefox _windows_8_opera _windows_10_chrome \
         osx_yosemite_safari _nexus_5 _kindle_fire_2 _ipad_mini_4 do
-    export CFG_NAME="$i"
-    bash ./script/try_command.sh "" "bundle exec cucumber --tags @browserstack -f progress -r features features/\$TEST_RUN"
+    bash ./script/try_command.sh "" "bundle exec cucumber --tags @browserstack -f progress -r features features/\$TEST_RUN" "$i"
   done
   export TRIES = 3
 fi
