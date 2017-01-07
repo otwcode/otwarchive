e5fd9d26f208f9892d28c67a3ecb3ab50a38cdea
zz9pzza <zz9pzza@gmail.com>
2017-01-07 16:29:58 +0000

    Have demo test and all the thing tests

--- script/codeship_tests.sh
+++ script/codeship_tests.sh
@@ -10,10 +10,11 @@ if [ -n "${BROWSERSTACK_USERNAME}" ] ; then
   for i in _windows_10_edge _windows_10_ie _windows_10_firefox _windows_8_opera _windows_10_chrome \
         _osx_yosemite_safari _nexus_5 _kindle_fire_2 _ipad_mini_4 ; do
     export CFG_NAME="$i"
-    bash ./script/try_command.sh "" "bundle exec cucumber --tags @browserstack -f progress -r features features/\$TEST_RUN"
+    bash ./script/try_command.sh "other_b/browserstack_demo.feature" "bundle exec cucumber --tags @browserstack -f progress -r features features/\$TEST_RUN"
   done
   export TRIES=3
 fi
+export CFG_NAME="_windows_10_chrome" bash ./script/try_command.sh "" "bundle exec cucumber --tags @browserstack -f progress -r features features/\$TEST_RUN"
 bash ./script/try_command.sh admins             "bundle exec cucumber --tags ~@browserstack -f progress -r features features/\$TEST_RUN"
 bash ./script/try_command.sh bookmarks          "bundle exec cucumber --tags ~@browserstack -f progress -r features features/\$TEST_RUN"
 bash ./script/try_command.sh collections        "bundle exec cucumber --tags ~@browserstack -f progress -r features features/\$TEST_RUN"
