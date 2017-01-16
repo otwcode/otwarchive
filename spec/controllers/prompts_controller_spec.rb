42b750bcfdefd7c758cb730cf7aeda6d86c2c02f
zz9pzza <zz9pzza@gmail.com>
2017-01-16 21:47:11 +0000

    Fix spacing

--- spec/controllers/prompts_controller_spec.rb
+++ spec/controllers/prompts_controller_spec.rb
@@ -8,12 +8,11 @@ describe PromptsController do
   end
 
   describe 'no_prompt' do
-   it 'should show an error and redirect' do
+    it 'should show an error and redirect' do
       signups = create(:challenge_signup)
-      post :edit,  collection_id: signups.collection.name
+      post :edit, collection_id: signups.collection.name
       expect(response).to redirect_to(collection_path(signups.collection))
       expect(flash[:error]).to eq "What prompt did you want to work on?"
     end
   end
-
 end
