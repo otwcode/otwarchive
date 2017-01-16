c3df282ba6cf7d99cd16a4050d5cb55be0142907
zz9pzza <zz9pzza@gmail.com>
2017-01-16 01:46:45 +0000

    AO3-4301 I18n the password reset email (#2116)
    * Reset password email
    
    * .html_safe is needed
    
    * Tidy subject up
    
    * Fix typo
    
    * Short locale keys and a bit of a test

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -244,7 +244,7 @@ class UserMailer < BulletproofMailer::Base
     I18n.with_locale(Locale.find(@user.preference.preferred_locale).iso) do
       mail(
         to: @user.email,
-        subject: "[#{ArchiveConfig.APP_SHORT_NAME}] Generated password"
+        subject: t('user_mailer.reset_password.subject', app_name: ArchiveConfig.APP_SHORT_NAME)
       )
     end
     ensure
