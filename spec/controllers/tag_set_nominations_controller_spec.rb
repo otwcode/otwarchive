f1f7c4782c2d5fd7fe063b9b9977e31c9bd71107
cyrilcee <crcortland+otw@gmail.com>
2017-01-18 13:23:17 -0800

    Create TagSetNominationsController spec file

---
+++ spec/controllers/tag_set_nominations_controller_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe TagSetNominationsController do
+  describe 'GET index' do
+
+  end
+
+  describe 'GET show' do
+
+  end
+
+  describe 'POST create' do
+
+  end
+
+  describe 'PUT update' do
+
+  end
+
+  describe 'DELETE destroy' do
+
+  end
+
+  describe 'PUT edit' do
+
+  end
+
+  describe 'GET confirm_destroy_multiple' do
+
+  end
+
+  describe 'DELETE destroy_multiple' do
+
+  end
+
+  describe 'PUT update_multiple' do
+
+  end
+
+  describe 'check_pseud_ownership'
+  describe 'load_tag_set'
+  describe 'load_nomination'
+  describe 'set_limit'
+  describe 'build_nominations'
+  describe 'request_noncanonical_info'
+  describe 'base_nom_query'
+  describe 'setup_for_review'
+end
