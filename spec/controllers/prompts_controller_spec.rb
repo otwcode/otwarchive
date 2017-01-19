34278fb6e2b404e0416facad74fe19de97d11c95
zz9pzza <zz9pzza@gmail.com>
2017-01-19 00:28:39 +0000

    AO3-4811 Allow error to be reached in prompt controller, removing error 500 (#2698)
    * Remove unused methods
    
    * Rspec test for prompts_controller
    
    * Reset tidy up
    
    * Reset tidy up
    
    * Fix chance of getting to error
    
    * Fix spacing

---
+++ spec/controllers/prompts_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe PromptsController do
+  include LoginMacros
+
+  before do
+    fake_login
+  end
+
+  describe 'no_prompt' do
+    it 'should show an error and redirect' do
+      signups = create(:challenge_signup)
+      post :edit, collection_id: signups.collection.name
+      expect(response).to redirect_to(collection_path(signups.collection))
+      expect(flash[:error]).to eq "What prompt did you want to work on?"
+    end
+  end
+end
