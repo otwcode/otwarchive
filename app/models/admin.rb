99f17e6e12655e5742af10be811b513a288b9efc
zz9pzza <zz9pzza@gmail.com>
2017-01-22 23:38:34 +0000

    Get backwards compatability working

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -6,6 +6,29 @@ class Admin < ActiveRecord::Base
          :validatable,
          password_length: ArchiveConfig.PASSWORD_LENGTH_MIN..ArchiveConfig.PASSWORD_LENGTH_MAX
 
+  # http://stackoverflow.com/questions/6113375/converting-existing-password-hash-to-devise
+
+  alias :devise_valid_password? :valid_password?
+  def valid_password?(password)
+    begin
+      result = super(password)
+      # Now the common form is that we are using an authlogic method so lets
+      # test that on failure
+      return true if result
+      if Authlogic::CryptoProviders::BCrypt.matches?(self.encrypted_password,[password, self.password_salt].compact)
+        self.password = password
+        self.save
+        return true
+      end
+      return false
+    rescue BCrypt::Errors::InvalidHash
+      # Now a really old password hash
+      return false unless Digest::SHA1.hexdigest(password) == encrypted_password
+      self.password = password
+      true
+    end
+  end
+
   has_many :log_items
   has_many :invitations, as: :creator
   has_many :wrangled_tags, class_name: 'Tag', as: :last_wrangler
