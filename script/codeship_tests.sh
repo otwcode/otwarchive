99f77191a65d703339d08de95d681dbfcbc8a4a8
zz9pzza <zz9pzza@gmail.com>
2017-01-22 10:46:03 +0000

    Run tests

--- script/codeship_tests.sh
+++ script/codeship_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # The expectation is that this script is copied in to the test area
-# on codeship rather than run as a single script as having timings for 
+# on codeship rather than run as a single script as having timings for
 # each stage is useful.
 export RAILS_ENV=test
 bash ./script/prepare_codeship.sh
@@ -8,14 +8,7 @@ bash ./script/try_command.sh rspec "bundle exec rspec spec "
 bundle exec rake db:drop
 bash ./script/prepare_codeship.sh
 echo 'Skin.load_site_css; Skin.where(cached: true).each{|skin| skin.cache!}' | bundle exec rails c  > /dev/null
-if [ -n "${BROWSERSTACK_USERNAME}" ] ; then
-  export TRIES=1
-  for i in config/browserstack/browserstack*.config.yml ; do
-    export CFG_NAME="../${i}"
-    bash ./script/try_command.sh "other_b/browserstack_demo.feature" "bundle exec cucumber --tags @browserstack -f progress -r features features/\$TEST_RUN"
-  done
-  export TRIES=3
-fi
+if [ -n "${BROWSERSTACK_USERNAME}" ] ; then export TRIES=1 ;for i in config/browserstack/browserstack*.config.yml ; do export CFG_NAME="../${i}" ; bash ./script/try_command.sh "other_b/browserstack_demo.feature" "bundle exec cucumber --tags @browserstack -f progress -r features features/\$TEST_RUN" ;done ;export TRIES=3 ; fi
 CFG_NAME="browserstack/browserstack_windows_10_chrome.config.yml" bash ./script/try_command.sh "" "bundle exec cucumber --tags @browserstack -f progress -r features features/\$TEST_RUN"
 bash ./script/try_command.sh admins             "bundle exec cucumber --tags ~@browserstack -f progress -r features features/\$TEST_RUN"
 bash ./script/try_command.sh bookmarks          "bundle exec cucumber --tags ~@browserstack -f progress -r features features/\$TEST_RUN"
