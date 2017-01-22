d72b4f4bac3d9f74ff375ee6fcb2178d20fac6bf
zz9pzza <zz9pzza@gmail.com>
2017-01-22 10:07:31 +0000

    Tidy up whitespace

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,8 +1,8 @@
 # Namespaced Admin class
 class Admin
  # Handle admin session authentication
- class SessionsController < Devise::SessionsController
-   before_filter :user_logout_required, except: :destroy
-   skip_after_filter :store_location
- end
+  class SessionsController < Devise::SessionsController
+    before_filter :user_logout_required, except: :destroy
+    skip_after_filter :store_location
+  end
 end
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,18 +1,18 @@
 class Admin < ActiveRecord::Base
   include ActiveModel::ForbiddenAttributesProtection
 
-  devise :database_authenticatable, \
-         :registerable, \
-         :encryptable, \
-         :validatable, \
+  devise :database_authenticatable,
+         :registerable,
+         :encryptable,
+         :validatable,
          password_length: ArchiveConfig.PASSWORD_LENGTH_MIN..ArchiveConfig.PASSWORD_LENGTH_MAX
-  
+
   has_many :log_items
-  has_many :invitations, :as => :creator
-  has_many :wrangled_tags, :class_name => 'Tag', :as => :last_wrangler 
+  has_many :invitations, as: :creator
+  has_many :wrangled_tags, class_name: 'Tag', as: :last_wrangler
 
   validates :login, presence: true, uniqueness: true, length: { in: ArchiveConfig.LOGIN_LENGTH_MIN..ArchiveConfig.LOGIN_LENGTH_MAX }
-  validates :email,  uniqueness: true
+  validates :email, uniqueness: true
   validates_presence_of :password_confirmation, if: :new_record?
   validates_confirmation_of :password, if: :new_record?
 end
