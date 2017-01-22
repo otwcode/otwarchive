015176046e4f6a6c9a9cbfce5859953006713591
zz9pzza <zz9pzza@gmail.com>
2017-01-22 23:47:02 +0000

    Backwards compatability \o/

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -7,6 +7,9 @@ class Admin < ActiveRecord::Base
          password_length: ArchiveConfig.PASSWORD_LENGTH_MIN..ArchiveConfig.PASSWORD_LENGTH_MAX
 
   # http://stackoverflow.com/questions/6113375/converting-existing-password-hash-to-devise
+  # https://github.com/binarylogic/authlogic/blob/master/lib/authlogic/acts_as_authentic/password.rb#L361
+  # https://www.ruby-forum.com/topic/217465
+  # Not useful but https://github.com/plataformatec/devise/issues/511
 
   alias :devise_valid_password? :valid_password?
   def valid_password?(password)
@@ -17,7 +20,6 @@ class Admin < ActiveRecord::Base
       return true if result
       if Authlogic::CryptoProviders::BCrypt.matches?(self.encrypted_password,[password, self.password_salt].compact)
         self.password = password
-        self.save
         return true
       end
       return false
