ab0feb512ad184583a384ac24ded2e3119cef8c9
zz9pzza <zz9pzza@gmail.com>
2017-01-24 21:43:35 +0000

    Listen to sarken

--- spec/controllers/collection_profile_controller_spec.rb
+++ spec/controllers/collection_profile_controller_spec.rb
@@ -1,10 +1,13 @@
 require 'spec_helper'
 
 describe CollectionProfileController do
-
-  it "checks to see if the collection exists" do
-    get :show, collection_id: "A non existent collection"
-    expect(response).to redirect_to(collections_path)
-    expect(flash[:error]).to eq "What collection did you want to look at?"
-  end 
+  describe "GET #show" do
+    context "collection does not exist" do
+      it "redirects and provides an error message" do
+        get :show, collection_id: "A non existent collection"
+        expect(response).to redirect_to(collections_path)
+        expect(flash[:error]).to eq "What collection did you want to look at?"
+      end
+    end
+  end
 end
