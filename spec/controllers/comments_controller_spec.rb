9534b05dc88f2c8d917d51d3e495f4786f444bfe
sarken <sarken@gmail.com>
2017-01-30 18:39:19 -0500

    AO3-4726 Rewrite descriptions for most examples

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -43,7 +43,7 @@ describe CommentsController do
   end
 
   describe "GET #unreviewed" do
-    it "checks that the comment right user is reviewing" do
+    it "redirects to root path with an error if logged in user does not own the comment or commentable" do
       comment = create(:unreviewed_comment)
       fake_login
       get :unreviewed, comment_id: comment.id
@@ -53,24 +53,24 @@ describe CommentsController do
   end
 
   describe "POST #new" do
-    it "checks that there is something to comment on" do
-      post :new, tag_id: "Non existant tag"
+    it "errors if the commentable is not a valid tag" do
+      post :new, tag_id: "Non existent tag"
       expect(flash[:error]).to eq "What did you want to comment on?"
     end
 
-    it "create a comment on an Admin post" do
+    it "renders the :new template if commentable is a valid admin post" do
       admin_post = create(:admin_post)
       post :new, admin_post_id: admin_post.id
       expect(response).to render_template("new")
     end
 
-    it "create a comment on an fandom" do
+    it "renders the :new template if commentable is a valid tag" do
       fandom = create(:fandom)
       post :new, tag_id: fandom.name
       expect(response).to render_template("new")
     end
 
-    it "create a new comment on an comment" do
+    it "renders the :new template if commentable is a valid comment" do
       comment = create(:comment)
       post :new, comment_id: comment.id
       expect(response).to render_template("new")
@@ -87,7 +87,7 @@ describe CommentsController do
   end
 
   describe "PUT #approve" do
-    it "can approve a comment" do
+    it "redirects to the comment on the commentable without an error" do
       comment = create(:unreviewed_comment)
       put :approve, id: comment.id
       expect(flash[:error]).to be_nil
@@ -96,7 +96,7 @@ describe CommentsController do
   end
 
   describe "GET #hide_comments" do
-    it "can hide a comment" do
+    it "redirects to the comment path without an error" do
       comment = create(:unreviewed_comment)
       get :hide_comments, comment_id: comment.id
       expect(flash[:error]).to be_nil
@@ -105,7 +105,7 @@ describe CommentsController do
   end
 
   describe "GET #add_comment" do
-    it "can add a comment" do
+    it "redirects to the comment path with add_comment params and without an error" do
       comment = create(:unreviewed_comment)
       get :add_comment, comment_id: comment.id
       expect(flash[:error]).to be_nil
@@ -138,7 +138,7 @@ describe CommentsController do
   end
 
   describe "GET #cancel_comment_delete" do
-    it "can cancel a request to delete a comment ?" do
+    it "redirects to the comment on the commentable without an error" do
       comment = create(:comment)
       get :cancel_comment_delete, id: comment.id
       expect(flash[:error]).to be_nil
@@ -147,7 +147,7 @@ describe CommentsController do
   end
 
   describe "GET #cancel_comment_edit" do
-    it "can cancel an edit to a comment" do
+    it "redirects to the comment on the commentable without an error" do
       comment = create(:comment)
       get :cancel_comment_edit, id: comment.id
       expect(flash[:error]).to be_nil
@@ -156,12 +156,14 @@ describe CommentsController do
   end
 
   describe "GET #destroy" do
-    it "checks that the owner can delete an unreviewed comment" do
-      fake_login
-      comment = create(:unreviewed_comment, pseud_id: @current_user.default_pseud.id)
-      get :destroy, id: comment.id
-      expect(response).to redirect_to("/where_i_came_from")
-      expect(flash[:notice]).to eq "Comment deleted."
+    context "when logged in as the owner of the comment" do
+      it "redirects to referrer with a success message" do
+        fake_login
+        comment = create(:unreviewed_comment, pseud_id: @current_user.default_pseud.id)
+        get :destroy, id: comment.id
+        expect(response).to redirect_to("/where_i_came_from")
+        expect(flash[:notice]).to eq "Comment deleted."
+      end
     end
   end
 end
