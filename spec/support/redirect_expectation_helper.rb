f0f9401ff78f31090dabd18d8f47fee894ba5376
zz9pzza <zz9pzza@gmail.com>
2017-02-04 14:21:17 +0000

    Be more assertive

--- spec/support/redirect_expectation_helper.rb
+++ spec/support/redirect_expectation_helper.rb
@@ -1,17 +1,31 @@
 # frozen_string_literal: true
 module RedirectExpectationHelper
   def it_redirects_to_with_notice(path, notice)
-    it_redirects_to(path)
+    it_redirects_to_internal(path)
     expect(flash[:notice]).to eq notice
+    expect(flash[:error]).blank?
   end
 
   def it_redirects_to_with_error(path, error)
-    it_redirects_to(path)
+    it_redirects_to_internal(path)
     expect(flash[:error]).to eq error
+    expect(flash[:notice]).blank?
+  end
+
+  def it_redirects_to_with_error_and_notice(path, error,notice)
+    it_redirects_to_internal(path)
+    expect(flash[:error]).to eq error
+    expect(flash[:notice]).to eq notice
+  end
+
+  def it_redirects_to_internal(path)
+    expect(response).to have_http_status :redirect
+    expect(response).to redirect_to path
   end
 
   def it_redirects_to(path)
-    expect(response).to have_http_status :redirect
-    expect(response).to redirect_to path
+    it_redirects_to_internal(path)
+    expect(flash[:notice]).blank?
+    expect(flash[:error]).blank?
   end
 end
