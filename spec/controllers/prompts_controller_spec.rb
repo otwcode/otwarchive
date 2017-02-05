aa3be2dab209bea9d1a5ae9630470e1d3eda7829
zz9pzza <zz9pzza@gmail.com>
2017-02-05 08:26:15 +0000

    Fix test

--- spec/controllers/prompts_controller_spec.rb
+++ spec/controllers/prompts_controller_spec.rb
@@ -12,7 +12,7 @@ describe PromptsController do
     it 'should show an error and redirect' do
       signups = create(:challenge_signup)
       post :edit, collection_id: signups.collection.name
-      it_redirects_to_with_error(collection_path(signup.collection), "What prompt did you want to work on?")
+      it_redirects_to_with_error(collection_path(signups.collection), "What prompt did you want to work on?")
     end
   end
 end
