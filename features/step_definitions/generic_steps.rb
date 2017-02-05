c1cae5b03924568017db75911e09c3914f0b119b
zz9pzza <zz9pzza@gmail.com>
2017-02-05 13:06:27 +0000

    Use as a Then not a Given

--- features/step_definitions/generic_steps.rb
+++ features/step_definitions/generic_steps.rb
@@ -42,6 +42,10 @@ Then /^show me the (\d+)(?:st|nd|rd|th) form$/ do |index|
   puts "\n" + page.all("#main form")[(index.to_i-1)].native.inner_html
 end
 
+Then /^I wait (\d+) seconds?$/ do |number|
+  step "I wait #{number} seconds?"
+end
+
 Given /^I wait (\d+) seconds?$/ do |number|
   Kernel::sleep number.to_i
 end
