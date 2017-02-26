517eb8237ff928a1d97579dd06d774a1f48140cb
zz9pzza <zz9pzza@gmail.com>
2017-02-26 08:57:44 +0000

    Use let

--- spec/controllers/external_authors_controller_spec.rb
+++ spec/controllers/external_authors_controller_spec.rb
@@ -5,6 +5,8 @@ describe ExternalAuthorsController do
   include LoginMacros
   include RedirectExpectationHelper
   let(:user) { FactoryGirl.create(:user) }
+  let(:invitation) { FactoryGirl.create(:invitation, external_author: external_author) }
+  let(:external_author) { FactoryGirl.create(:external_author) }
 
   describe "GET #get_external_author_from_invitation" do
     it "needs a valid invitaiton" do
@@ -13,18 +15,15 @@ describe ExternalAuthorsController do
     end
    
     it "needs to have a story assocated with the invitation" do
-      invitation = FactoryGirl.create(:invitation)
-      get :get_external_author_from_invitation, invitation_token: invitation.token
-      it_redirects_to_with_error(signup_path(invitation.token), "There are no stories to claim on this invitation. Did you want to sign up instead?")
+      no_story_invitation = FactoryGirl.create(:invitation)
+      get :get_external_author_from_invitation, invitation_token: no_story_invitation.token
+      it_redirects_to_with_error(signup_path(no_story_invitation.token), "There are no stories to claim on this invitation. Did you want to sign up instead?")
     end
   end
 
   describe "GET #complete_claim" do 
     it "should give the user the works" do
-      external_author = FactoryGirl.create(:external_author)
-      user = FactoryGirl.create(:user)
       fake_login_known_user(user)
-      invitation = FactoryGirl.create(:invitation, external_author: external_author)
       get :complete_claim, invitation_token: invitation.token
       it_redirects_to_with_notice(user_external_authors_path(user), "We have added the stories imported under #{external_author.email} to your account.")
     end
@@ -32,38 +31,27 @@ describe ExternalAuthorsController do
 
   describe "GET #update" do
     it "needs to be done by the right user" do
-      external_author = FactoryGirl.create(:external_author)
       wrong_external_author = FactoryGirl.create(:external_author)
-      user = FactoryGirl.create(:user)
       fake_login_known_user(user)
-      invitation = FactoryGirl.create(:invitation, external_author: wrong_external_author)
-      get :update, invitation_token: invitation.token, id: external_author.id
+      someone_elses_invitation = FactoryGirl.create(:invitation, external_author: wrong_external_author)
+      get :update, invitation_token: someone_elses_invitation.token, id: external_author.id
       it_redirects_to_with_error(root_path, "You don't have permission to do that.")
     end
 
     it "can do nothing" do
-      external_author = FactoryGirl.create(:external_author)
-      user = FactoryGirl.create(:user)
       fake_login_known_user(user)
-      invitation = FactoryGirl.create(:invitation, external_author: external_author)
       get :update, invitation_token: invitation.token, id: external_author.id, imported_stories: "nothing"
       it_redirects_to_with_notice(root_path, "Okay, we'll leave things the way they are! You can use the email link any time if you change your mind.")
     end
 
     it "can be orphaned" do
-      external_author = FactoryGirl.create(:external_author)
-      user = FactoryGirl.create(:user)
       fake_login_known_user(user)
-      invitation = FactoryGirl.create(:invitation, external_author: external_author)
       get :update, invitation_token: invitation.token, id: external_author.id, imported_stories: "orphan"
       it_redirects_to_with_notice(root_path, "Your imported stories have been orphaned. Thank you for leaving them in the archive! Your preferences have been saved.")
     end
 
     xit "errors if the preferences can't be saved" do
-      external_author = FactoryGirl.create(:external_author)
-      user = FactoryGirl.create(:user)
       fake_login_known_user(user)
-      invitation = FactoryGirl.create(:invitation, external_author: external_author)
       allow_any_instance_of(ExternalAuthor).to receive(:update_attributes).and_return(false)
       get :update, invitation_token: invitation.token, id: external_author.id, imported_stories: "orphan"
       allow_any_instance_of(ExternalAuthor).to receive(:update_attributes).and_call_original
@@ -75,8 +63,6 @@ describe ExternalAuthorsController do
 
   describe "GET #edit" do
     it "assigns external_author" do
-      external_author = FactoryGirl.create(:external_author)
-      user = FactoryGirl.create(:user)
       user.save
       fake_login_known_user(user)
       get :edit, id: external_author.id, user_id: user.login
@@ -91,8 +77,6 @@ describe ExternalAuthorsController do
     end
 
     it "assigns @external_authors" do
-      external_author = FactoryGirl.create(:external_author)
-      user = FactoryGirl.create(:user)
       external_author.claim!(user)     
       fake_login_known_user(user)
       get :index, user_id: user.login
@@ -100,14 +84,12 @@ describe ExternalAuthorsController do
     end
 
     it "redirects when you are logged in" do
-      user = FactoryGirl.create(:user)
       fake_login_known_user(user)
       get :index
       it_redirects_to(user_external_authors_path(user))
     end
 
     it "archivist are special" do
-      user = FactoryGirl.create(:user)
       fake_login_known_user(user)
       allow_any_instance_of(User).to receive(:is_archivist?).and_return(true)
       get :index
