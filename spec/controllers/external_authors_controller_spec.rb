e72b5373d27ae3633ed43c45b2bf720400050a65
zz9pzza <zz9pzza@gmail.com>
2017-02-26 12:15:05 +0000

    Follow context suggestions

--- spec/controllers/external_authors_controller_spec.rb
+++ spec/controllers/external_authors_controller_spec.rb
@@ -13,50 +13,72 @@ describe ExternalAuthorsController do
   end
 
   describe "GET #get_external_author_from_invitation" do
-    it "needs a valid invitaiton" do
-      get :get_external_author_from_invitation, invitation_token: "None existent"
-      it_redirects_to_with_error(root_path, "You need an invitation to do that.")
+    context "with invalid invitation token" do
+      it "redirects with an error" do
+        get :get_external_author_from_invitation, invitation_token: "None existent"
+        it_redirects_to_with_error(root_path, "You need an invitation to do that.")
+      end
+    end
+
+    context "with valid invitation token" do
+      xit "assigns invitation" do
+        # ActionView::MissingTemplate:
+        # Missing template external_authors/get_external_author_from_invitation, application/get_external_author_from_invitation with {:locale=>[:en], :formats=>[:html], :handlers=>[:erb, :builder]}. Searched in:
+        get :get_external_author_from_invitation, invitation_token: invitation.token
+        expect(assigns(:invitation)).to eq(invitation)
+        assert_equal response.status, 200
+      end
     end
    
-    it "needs to have a story assocated with the invitation" do
-      no_story_invitation = FactoryGirl.create(:invitation)
-      get :get_external_author_from_invitation, invitation_token: no_story_invitation.token
-      it_redirects_to_with_error(signup_path(no_story_invitation.token), "There are no stories to claim on this invitation. Did you want to sign up instead?")
+    context "without works to claim" do
+      it "redirects with an error" do
+        no_story_invitation = FactoryGirl.create(:invitation)
+        get :get_external_author_from_invitation, invitation_token: no_story_invitation.token
+        it_redirects_to_with_error(signup_path(no_story_invitation.token), "There are no stories to claim on this invitation. Did you want to sign up instead?")
+      end
     end
   end
 
   describe "GET #complete_claim" do 
-    it "should give the user the works" do
+    it "redirects with a success message" do
       get :complete_claim, invitation_token: invitation.token
       it_redirects_to_with_notice(user_external_authors_path(user), "We have added the stories imported under #{external_author.email} to your account.")
     end
   end 
 
   describe "GET #update" do
-    it "needs to be done by the right user" do
+    it "redirects with an error if the user does not have permission" do
       wrong_external_author = FactoryGirl.create(:external_author)
       someone_elses_invitation = FactoryGirl.create(:invitation, external_author: wrong_external_author)
       get :update, invitation_token: someone_elses_invitation.token, id: external_author.id
       it_redirects_to_with_error(root_path, "You don't have permission to do that.")
     end
 
-    it "can do nothing" do
-      get :update, invitation_token: invitation.token, id: external_author.id, imported_stories: "nothing"
-      it_redirects_to_with_notice(root_path, "Okay, we'll leave things the way they are! You can use the email link any time if you change your mind.")
-    end
+    context "When the user has permission" do
+      context "when doing nothing with imported works" do
+        it "redirects with a success message" do
+          get :update, invitation_token: invitation.token, id: external_author.id, imported_stories: "nothing"
+          it_redirects_to_with_notice(root_path, "Okay, we'll leave things the way they are! You can use the email link any time if you change your mind.")
+        end
+      end
 
-    it "can be orphaned" do
-      get :update, invitation_token: invitation.token, id: external_author.id, imported_stories: "orphan"
-      it_redirects_to_with_notice(root_path, "Your imported stories have been orphaned. Thank you for leaving them in the archive! Your preferences have been saved.")
-    end
-
-    xit "errors if the preferences can't be saved" do
-      allow_any_instance_of(ExternalAuthor).to receive(:update_attributes).and_return(false)
-      get :update, invitation_token: invitation.token, id: external_author.id, imported_stories: "orphan"
-      allow_any_instance_of(ExternalAuthor).to receive(:update_attributes).and_call_original
-      expect(response).to render_template :edit
-      expect(flash[:notice]).to eq "Your imported stories have been orphaned. Thank you for leaving them in the archive! "
-      expect(flash[:error]).to eq "There were problems saving your preferences."
+      context "when orphaning imported works" do
+        it "redirects with a success message" do
+          get :update, invitation_token: invitation.token, id: external_author.id, imported_stories: "orphan"
+          it_redirects_to_with_notice(root_path, "Your imported stories have been orphaned. Thank you for leaving them in the archive! Your preferences have been saved.")
+        end
+ 
+        context "when updating preferences" do
+          xit "renders edit template with a success message for orphaning and an error for preferences" do
+            allow_any_instance_of(ExternalAuthor).to receive(:update_attributes).and_return(false)
+            get :update, invitation_token: invitation.token, id: external_author.id, imported_stories: "orphan"
+            allow_any_instance_of(ExternalAuthor).to receive(:update_attributes).and_call_original
+            expect(response).to render_template :edit
+            expect(flash[:notice]).to eq "Your imported stories have been orphaned. Thank you for leaving them in the archive! "
+            expect(flash[:error]).to eq "There were problems saving your preferences."
+          end
+        end
+      end
     end
   end
 
