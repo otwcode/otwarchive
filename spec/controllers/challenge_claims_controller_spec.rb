13418bee159d22b91004a54bd480be0704594e46
zz9pzza <zz9pzza@gmail.com>
2017-03-05 09:09:03 +0000

    Add test for save failing on create

--- spec/controllers/challenge_claims_controller_spec.rb
+++ spec/controllers/challenge_claims_controller_spec.rb
@@ -8,12 +8,20 @@ RSpec.describe ChallengeClaimsController, type: :controller do
   let(:collection) { signups.collection }
   let(:claim) { create(:challenge_claim) }
 
-      describe 'create' do
-    it 'on an exception gives an error and redirects' do
+  describe 'create' do
+    it 'sets a notice and redirects' do
       fake_login_known_user(@user)
       post :create, collection_id: collection.name
       it_redirects_to_with_notice(collection_claims_path(collection, for_user: true), \
                                   "New claim made.")
+    end
+
+    it 'on an exception gives an error and redirects' do
+      fake_login_known_user(@user)
+      allow_any_instance_of(ChallengeClaim).to receive(:save) { false}
+      post :create, collection_id: collection.name
+      it_redirects_to_with_error(collection_claims_path(collection, for_user: true), \
+                                 "We couldn't save the new claim.")
     end
   end
 
