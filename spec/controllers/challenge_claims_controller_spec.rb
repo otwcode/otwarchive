9be6f1816a88b5e93c139a4c58e133a0a2c737c4
zz9pzza <zz9pzza@gmail.com>
2017-03-05 00:35:51 +0000

    A second test

--- spec/controllers/challenge_claims_controller_spec.rb
+++ spec/controllers/challenge_claims_controller_spec.rb
@@ -3,22 +3,27 @@ require 'spec_helper'
 RSpec.describe ChallengeClaimsController, type: :controller do
   include LoginMacros
   include RedirectExpectationHelper
-  begin
-    @user = FactoryGirl.create(:user)
+  let(:user) { create(:user) }
+  let(:signups) { create(:challenge_signup) }
+  let(:collection) { signups.collection }
+  let(:claim) { create(:challenge_claim) }
+
+      describe 'create' do
+    it 'on an exception gives an error and redirects' do
+      fake_login_known_user(@user)
+      post :create, collection_id: collection.name
+      it_redirects_to_with_notice(collection_claims_path(collection, for_user: true), \
+                                  "New claim made.")
+    end
   end
 
   describe 'destory' do
     context 'with a claim' do
-      before do
-        @signups = create(:challenge_signup)
-        @collection = @signups.collection
-        @claim = create(:challenge_claim)
-      end
       it 'on an exception gives an error and redirects' do
         fake_login_known_user(@user)
         allow_any_instance_of(ChallengeClaim).to receive(:destroy) { raise ActiveRecord::RecordNotDestroyed }
-        delete :destroy, id: @claim.id, collection_id: @collection.name
-        it_redirects_to_with_error(collection_claims_path(@collection), \
+        delete :destroy, id: claim.id, collection_id: collection.name
+        it_redirects_to_with_error(collection_claims_path(collection), \
                                    "We couldn't delete that right now, sorry! Please try again later.")
       end
     end
