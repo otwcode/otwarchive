1038c20dd03351d09af7a7301ccd4d4568df67e5
zz9pzza <zz9pzza@gmail.com>
2017-03-05 12:58:42 +0000

    Add tests which can't be reached

--- factories/challenge_claims.rb
+++ factories/challenge_claims.rb
@@ -1,7 +1,5 @@
 require 'faker'
 FactoryGirl.define do
   factory :challenge_claim do
-    after(:build) do |assignment|
-    end
   end
 end
--- spec/controllers/challenge_claims_controller_spec.rb
+++ spec/controllers/challenge_claims_controller_spec.rb
@@ -6,7 +6,39 @@ RSpec.describe ChallengeClaimsController, type: :controller do
   let(:user) { create(:user) }
   let(:signups) { create(:challenge_signup) }
   let(:collection) { signups.collection }
-  let(:claim) { create(:challenge_claim) }
+  let(:claim) { create(:challenge_claim, collection: collection) }
+
+  describe 'index' do
+    it 'gives a notice if the collection is closed' do
+      fake_login_known_user(user)
+      allow_any_instance_of(Collection).to receive(:closed?) { true }
+      get :index, id: claim.id, collection_id: collection.name, for_user: true
+      expect(flash[:notice]).to include("This challenge is currently closed to new posts.")
+      expect(assigns(:claims))
+    end
+
+    xit 'will not allow you to see someone elses claims' do
+      second_user = create(:user)
+      fake_login_known_user(user)
+      get :index, id: claim.id, user_id: second_user.id
+      it_redirects_to_with_error(root_path, \
+                                 "You aren't allowed to see that user's claims")
+    end
+  end
+
+  describe 'show' do
+    xit 'redirects' do
+      get :show, id: claim.id, collection_id: collection.name
+      it_redirects_to(collection_prompt_path(collection, claim.request_prompt))
+    end
+
+    xit 'needs a collection' do
+      fake_login_known_user(@user)
+      get :show
+      it_redirects_to_with_error(root_path, \
+                                 "What challenge did you want to work with?")
+    end
+  end
 
   describe 'create' do
     it 'sets a notice and redirects' do
@@ -18,7 +50,7 @@ RSpec.describe ChallengeClaimsController, type: :controller do
 
     it 'on an exception gives an error and redirects' do
       fake_login_known_user(@user)
-      allow_any_instance_of(ChallengeClaim).to receive(:save) { false}
+      allow_any_instance_of(ChallengeClaim).to receive(:save) { false }
       post :create, collection_id: collection.name
       it_redirects_to_with_error(collection_claims_path(collection, for_user: true), \
                                  "We couldn't save the new claim.")
