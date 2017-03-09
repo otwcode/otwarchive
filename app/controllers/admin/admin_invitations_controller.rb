bdb19f9ed0141ca6e931be65100ab1afcfb9b1d9
Matt Sears <matt@mattsears.com>
2017-03-09 11:30:36 -0800

    Use's Ruby 1.9 syntax.

--- app/controllers/admin/admin_invitations_controller.rb
+++ app/controllers/admin/admin_invitations_controller.rb
@@ -8,22 +8,22 @@ class Admin::AdminInvitationsController < ApplicationController
   def create
     @invitation = current_admin.invitations.new(invitation_params)
     if @invitation.invitee_email.blank?
-      flash[:error] = t('no_email', :default => "Please enter an email address.")
-      render :action => 'index'
+      flash[:error] = t('no_email', default: "Please enter an email address.")
+      render action: 'index'
     elsif @invitation.save
-      flash[:notice] = t('sent', :default => "An invitation was sent to %{email_address}", :email_address => @invitation.invitee_email)
+      flash[:notice] = t('sent', default: "An invitation was sent to %{email_address}", email_address: @invitation.invitee_email)
       redirect_to admin_invitations_url
     else
-      render :action => 'index'
+      render action: 'index'
     end
   end
 
   def invite_from_queue
-    InviteRequest.find(:all, :order => :position, :limit => invitation_params[:invite_from_queue].to_i).each do |request|
+    InviteRequest.find(:all, order: :position, limit: invitation_params[:invite_from_queue].to_i).each do |request|
       request.invite_and_remove(current_admin)
     end
     InviteRequest.reset_order
-    flash[:notice] = t('invited_from_queue', :default => "%{count} people from the invite queue were invited.", :count => invitation_params[:invite_from_queue].to_i)
+    flash[:notice] = t('invited_from_queue', default: "%{count} people from the invite queue were invited.", count: invitation_params[:invite_from_queue].to_i)
     redirect_to admin_invitations_url
   end
 
@@ -33,7 +33,7 @@ class Admin::AdminInvitationsController < ApplicationController
     else
       Invitation.grant_empty(invitation_params[:number_of_invites].to_i)
     end
-    flash[:notice] = t('invites_created', :default => 'Invitations successfully created.')
+    flash[:notice] = t('invites_created', default: 'Invitations successfully created.')
     redirect_to admin_invitations_url
   end
 
@@ -46,11 +46,11 @@ class Admin::AdminInvitationsController < ApplicationController
     if !invitation_params[:token].blank?
       @invitation = Invitation.find_by_token(invitation_params[:token])
     elsif !invitation_params[:invitee_email].blank?
-      @invitations = Invitation.find(:all, :conditions => ['invitee_email LIKE ?', '%' + invitation_params[:invitee_email] + '%'])
+      @invitations = Invitation.find(:all, conditions: ['invitee_email LIKE ?', '%' + invitation_params[:invitee_email] + '%'])
       @invitation = @invitations.first if @invitations.length == 1
     end
     unless @user || @invitation || @invitations
-      flash.now[:error] = t('user_not_found', :default => "No results were found. Try another search.")
+      flash.now[:error] = t('user_not_found', default: "No results were found. Try another search.")
     end
   end
 
