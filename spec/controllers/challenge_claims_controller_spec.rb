b68bfa701553cae1bc15a2c6e987961f8ec936be
zz9pzza <zz9pzza@gmail.com>
2017-03-11 15:16:44 +0000

    Fixes from sarken

--- spec/controllers/challenge_claims_controller_spec.rb
+++ spec/controllers/challenge_claims_controller_spec.rb
@@ -1,15 +1,16 @@
+# frozen_string_literal: true
 require 'spec_helper'
 
 RSpec.describe ChallengeClaimsController, type: :controller do
   include LoginMacros
   include RedirectExpectationHelper
   let(:user) { create(:user) }
-  let(:signups) { create(:challenge_signup) }
-  let(:collection) { signups.collection }
+  let(:signup) { create(:challenge_signup) }
+  let(:collection) { signup.collection }
   let(:claim) { create(:challenge_claim, collection: collection) }
 
   describe 'index' do
-    it 'gives a notice if the collection is closed' do
+    it 'assigns claims and gives a notice if the collection is closed and the user is not the maintainer' do
       fake_login_known_user(user)
       allow_any_instance_of(Collection).to receive(:closed?) { true }
       get :index, id: claim.id, collection_id: collection.name, for_user: true
@@ -17,23 +18,26 @@ RSpec.describe ChallengeClaimsController, type: :controller do
       expect(assigns(:claims))
     end
 
-    xit 'will not allow you to see someone elses claims' do
+    it 'will not allow you to see someone elses claims' do
       second_user = create(:user)
       fake_login_known_user(user)
-      get :index, id: claim.id, user_id: second_user.id
+      get :index, user_id: second_user.login
       it_redirects_to_with_error(root_path, \
-                                 "You aren't allowed to see that user's claims")
+                                 "You aren't allowed to see that user's claims.")
     end
   end
 
   describe 'show' do
-    xit 'redirects' do
-      get :show, id: claim.id, collection_id: collection.name
-      it_redirects_to(collection_prompt_path(collection, claim.request_prompt))
+    it 'redirects logged in user to the prompt' do
+      request_prompt = create(:prompt, collection_id: collection.id, challenge_signup_id: signup.id)
+      claim_with_prompt = create(:challenge_claim, collection: collection, request_prompt_id: request_prompt.id)
+      fake_login_known_user(user)
+      get :show, id: claim_with_prompt.id, collection_id: collection.name
+      it_redirects_to(collection_prompt_path(collection, claim_with_prompt.request_prompt))
     end
 
     xit 'needs a collection' do
-      fake_login_known_user(@user)
+      fake_login_known_user(user)
       get :show
       it_redirects_to_with_error(root_path, \
                                  "What challenge did you want to work with?")
