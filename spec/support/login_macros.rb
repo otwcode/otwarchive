0b216b66ca8b01d204c4fea3b3a4b1cf524062f4
zz9pzza <zz9pzza@gmail.com>
2017-03-11 16:53:47 +0000

    Consistency

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -1,12 +1,5 @@
 module LoginMacros
 
-  def fake_logout
-    # Stub out the current_user method
-    @current_user = nil
-    allow(controller).to receive(:logged_in?).and_return(false)
-    allow(controller).to receive(:current_user).and_return(nil)
-  end
-
   def fake_login
     # Stub out the current_user method
     @current_user = FactoryGirl.create(:user)
@@ -28,5 +21,14 @@ module LoginMacros
     allow(controller).to receive(:logged_in_as_admin?).and_return(true)
     allow(controller).to receive(:current_admin).and_return(@current_admin)
     sign_in :admin, admin 
+  end
+
+  def fake_logout
+    @current_admin = nil
+    @current_user = nil
+    allow(controller).to receive(:logged_in_as_admin?).and_return(false)
+    allow(controller).to receive(:current_admin).and_return(@current_admin)
+    allow(controller).to receive(:logged_in?).and_return(false)
+    allow(controller).to receive(:current_user).and_return(@current_user)
   end
 end
