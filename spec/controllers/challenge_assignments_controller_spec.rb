abaf9b4f4f008cd382316332029f4015a9896594
Cesy Avon <cesy.avon@gmail.com>
2017-03-12 08:35:06 +0000

    AO3-4887 simplification

--- spec/controllers/challenge_assignments_controller_spec.rb
+++ spec/controllers/challenge_assignments_controller_spec.rb
@@ -6,25 +6,19 @@ describe ChallengeAssignmentsController do
 
   context "when not logged in" do
     describe 'no_challenge' do
-      before(:each) do
-        @collection = FactoryGirl.create(:collection)
-      end
-      
-      it 'should error to login page' do
-        get :no_challenge, collection_id: @collection.name
+      it 'error to login page' do
+        let(:collection) { create(:collection) }
+        get :no_challenge, collection_id: collection.name
         it_redirects_to_with_error(new_user_session_path, "Sorry, you don't have permission to access the page you were trying to reach. Please log in.")
       end
     end
   end
 
   context "when logged in" do
-    before(:each) do
-      @collection = FactoryGirl.create(:collection)
-      fake_login_known_user(@collection.owners.first.user)
-    end
-    
     describe 'no_challenge' do
-      it 'should show an error, redirect and return false' do
+      it 'show an error, redirect and return false' do
+        let(:collection) { create(:collection) }
+        fake_login_known_user(@collection.owners.first.user)
         get :no_challenge, collection_id: @collection.name
         it_redirects_to_with_error(collection_path(@collection), "What challenge did you want to work with?")
       end
