f48840b3e04427e283edf6c183b077fc5a1f4c79
Cesy Avon <cesy.avon@gmail.com>
2017-03-12 09:10:42 +0000

    AO3-4887 too much simplification

--- spec/controllers/challenge_assignments_controller_spec.rb
+++ spec/controllers/challenge_assignments_controller_spec.rb
@@ -3,11 +3,14 @@ require 'spec_helper'
 describe ChallengeAssignmentsController do
   include LoginMacros
   include RedirectExpectationHelper
+  
+  before(:each) do
+    let(:collection) { create(:collection) }
+  end
 
   context "when not logged in" do
     describe 'no_challenge' do
       it 'error to login page' do
-        let(:collection) { create(:collection) }
         get :no_challenge, collection_id: collection.name
         it_redirects_to_with_error(new_user_session_path, "Sorry, you don't have permission to access the page you were trying to reach. Please log in.")
       end
@@ -17,7 +20,6 @@ describe ChallengeAssignmentsController do
   context "when logged in" do
     describe 'no_challenge' do
       it 'show an error, redirect and return false' do
-        let(:collection) { create(:collection) }
         fake_login_known_user(@collection.owners.first.user)
         get :no_challenge, collection_id: @collection.name
         it_redirects_to_with_error(collection_path(@collection), "What challenge did you want to work with?")
