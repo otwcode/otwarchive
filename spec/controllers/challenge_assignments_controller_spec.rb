08f227bff73e7a2ae14d7e76d2eb94574a5e5536
Cesy Avon <cesy.avon@gmail.com>
2017-03-15 09:16:05 +0000

    AO3-4887 test failure

--- spec/controllers/challenge_assignments_controller_spec.rb
+++ spec/controllers/challenge_assignments_controller_spec.rb
@@ -17,9 +17,9 @@ describe ChallengeAssignmentsController do
   context "when logged in" do
     describe 'no_challenge' do
       it 'show an error, redirect and return false' do
-        fake_login_known_user(@collection.owners.first.user)
-        get :no_challenge, collection_id: @collection.name
-        it_redirects_to_with_error(collection_path(@collection), "What challenge did you want to work with?")
+        fake_login_known_user(collection.owners.first.user)
+        get :no_challenge, collection_id: collection.name
+        it_redirects_to_with_error(collection_path(collection), "What challenge did you want to work with?")
       end
     end
   end
