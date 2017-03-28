4d0304fd62c7a74b2f26f1651a9222429fd1d666
zz9pzza <zz9pzza@gmail.com>
2017-03-28 23:07:28 +0000

    Listen to the hound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,7 @@ class ApplicationController < ActionController::Base
   # redirect to the logout page
   before_filter :logout_if_not_user_credentials
   def logout_if_not_user_credentials
-    return unless logged_in? && cookies[:user_credentials] == nil && controller_name != "user_sessions"
+    return unless logged_in? && cookies[:user_credentials].nil? && controller_name != "user_sessions"
     path = request.fullpath
     logger.error "Forcing logout #{path}"
     @user_session = UserSession.find
