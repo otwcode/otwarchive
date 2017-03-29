3ce77447f16ac9a9f8f4c2b4b0afa647a43e55dd
cresenne <katieby@users.noreply.github.com>
2017-03-28 23:03:41 -0700

    AO3-4924 add tests for index and manage

--- spec/controllers/chapters_controller_spec.rb
+++ spec/controllers/chapters_controller_spec.rb
@@ -2,14 +2,69 @@ require 'spec_helper'
 
 describe ChaptersController do
   include LoginMacros
-  
+  include RedirectExpectationHelper
+
+  before do
+    @user = FactoryGirl.create(:user)
+    @work = FactoryGirl.create(:work, posted: true, authors: [@user.pseuds.first])
+  end
+
+  describe "index" do
+    it "should redirect to work" do
+      get :index, work_id: @work.id
+      it_redirects_to work_path(@work.id)
+    end
+  end
+
+  describe "manage" do
+    context "when user is logged out" do
+      it "should error and redirect to login" do
+        get :manage, work_id: @work.id
+        it_redirects_to_with_error(new_user_session_path, "Sorry, you don't have permission to access the page you were trying to reach. Please log in.")
+      end
+    end
+
+    context "when work owner is logged in" do
+      before do
+        fake_login_known_user(@user)
+      end
+
+      it "should error and redirect to root path if work does not exist" do
+        get :manage, work_id: nil
+        it_redirects_to_with_error(root_path, "Sorry, we couldn't find the work you were looking for.")
+      end
+
+      it "should render manage template" do
+        get :manage, work_id: @work.id
+        expect(response).to render_template(:manage)
+      end
+
+      it "should assign @chapters to only posted chapters" do
+        @chapter = FactoryGirl.create(:chapter, work: @work, authors: [@user.pseuds.first], posted: false)
+        get :manage, work_id: @work.id
+        expect(assigns[:chapters]).to eq([@work.chapters.first])
+      end
+
+      it "should assign @chapters to chapters in order" do
+        @chapter = FactoryGirl.create(:chapter, work: @work, authors: [@user.pseuds.first], position: 2, posted: true)
+        get :manage, work_id: @work.id
+        expect(assigns[:chapters]).to eq([@work.chapters.first, @chapter])
+      end
+    end
+
+    context "when other user is logged in" do
+      it "should error and redirect to work" do
+        fake_login
+        get :manage, work_id: @work.id
+        it_redirects_to_with_error(work_path(@work.id), "Sorry, you don't have permission to access the page you were trying to reach.")
+      end
+    end
+  end 
+
   describe "create" do
     before do
-      @user = FactoryGirl.create(:user)
       fake_login_known_user(@user)
-      @work = FactoryGirl.create(:work, authors: [@user.pseuds.first])
       @chapter_attributes = { content: "This doesn't matter" }
-
     end
     
     it "should add a new chapter" do
