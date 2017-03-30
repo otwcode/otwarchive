6581ccd83522a17aef64e0960a7c1683cc84df47
David Stump <david@davidstump.net>
2017-03-30 15:35:14 -0400

    AO3-4873 Implements strong parameters for Tag Set Nominations (#2763)
    * AO3-4873 Implements strong parameters for Tag Set Nominations
    
    * Adds empty param under tag_set_nomination to avoid being stripped
    
    With the addition of strong params, the tag_set_nomination param is
    required. The spec was passing an empty hash which gets stripped and
    results in the param not getting passed to strong params and resulting
    in a 400 error. Adding an empty nested param (such as pseud_id) keeps
    the tag_set_nomination param in the request while avoiding passing any
    actual modifying data to the test.
    
    For referehce, see issue: https://github.com/rails/rails/issues/26569
    
    * Remove leftover spec focus
    
    * AO3-4873 Adds updated params based on @tickinginstant's QA

--- spec/controllers/tag_set_nominations_controller_spec.rb
+++ spec/controllers/tag_set_nominations_controller_spec.rb
@@ -974,7 +974,7 @@ describe TagSetNominationsController do
           end
 
           it 'redirects and returns an error message' do
-            put :update, id: tag_set_nomination.id, tag_set_id: owned_tag_set.id, tag_set_nomination: {}
+            put :update, id: tag_set_nomination.id, tag_set_id: owned_tag_set.id, tag_set_nomination: {pseud_id: nil}
             it_redirects_to_with_notice(tag_set_path(owned_tag_set),
                                         'You can only see your own nominations or nominations for a set you moderate.')
           end
