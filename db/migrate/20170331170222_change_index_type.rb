aa226549f70432bd1db6df5f0c25e93ee975b5eb
zz9pzza <zz9pzza@gmail.com>
2017-03-31 22:12:00 +0000

    Baby steps

--- db/migrate/20170331170222_change_index_type.rb
+++ db/migrate/20170331170222_change_index_type.rb
@@ -1,8 +1,6 @@
 class ChangeIndexType < ActiveRecord::Migration
   def up
     change_column :readings, :id, "bigint NOT NULL AUTO_INCREMENT"
-    change_column :taggings, :id, "bigint NOT NULL AUTO_INCREMENT"
-    change_column :kudos, :id, "bigint NOT NULL AUTO_INCREMENT"
   end
 
   def down
