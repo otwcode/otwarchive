05b4fb26c0c223852c77deee2ba6c50698894754
zz9pzza <zz9pzza@gmail.com>
2017-04-02 10:49:49 +0000

    Rename the class

--- db/migrate/20170331170222_change_readings_id_type.rb
+++ db/migrate/20170331170222_change_readings_id_type.rb
@@ -1,4 +1,4 @@
-class ChangeIndexType < ActiveRecord::Migration
+class ChangeReadingsIdType < ActiveRecord::Migration
   def up
     change_column :readings, :id, "bigint NOT NULL AUTO_INCREMENT"
   end
