e709a13455876c45bdbf5c7c9f0f414b98be7ebe
redsummernight <redsummernight@users.noreply.github.com>
2017-04-02 00:20:19 -0400

    AO3-4926 Extend tests for inbox_controller.rb
    - Add specs for the controller.
    - Add cukes for replying to inbox comments from the home page,
      both with and without JavaScript.
    - Remove the route cancel_reply which has no action.

--- spec/support/redirect_expectation_helper.rb
+++ spec/support/redirect_expectation_helper.rb
@@ -5,6 +5,11 @@ module RedirectExpectationHelper
     expect(flash[:notice]).to eq notice
   end
 
+  def it_redirects_to_with_caution(path, caution)
+    it_redirects_to(path)
+    expect(flash[:caution]).to eq caution
+  end
+
   def it_redirects_to_with_error(path, error)
     it_redirects_to(path)
     expect(flash[:error]).to eq error
