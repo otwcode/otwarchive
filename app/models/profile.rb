b3db4e2aa01f83535b34997d55ce80e4c30e8b1c
David Stump <david@davidstump.net>
2017-04-03 17:46:48 -0400

    AO3-4784 Profile strong params (#2810)
    * AO3-4784 Implements strong params on Profile
    
    * AO3-4784 Adds standard rails params to users_controller

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,16 +1,17 @@
 class Profile < ActiveRecord::Base
-  
+  include ActiveModel::ForbiddenAttributesProtection
+
   PROFILE_TITLE_MAX = 255
   LOCATION_MAX = 255
   ABOUT_ME_MAX = 2000
-  
+
   belongs_to :user
-  
-  validates_length_of :location, :allow_blank => true, :maximum => LOCATION_MAX, 
+
+  validates_length_of :location, :allow_blank => true, :maximum => LOCATION_MAX,
     :too_long => ts("must be less than %{max} characters long.", :max => LOCATION_MAX)
-  validates_length_of :title, :allow_blank => true, :maximum => PROFILE_TITLE_MAX, 
+  validates_length_of :title, :allow_blank => true, :maximum => PROFILE_TITLE_MAX,
     :too_long => ts("must be less than %{max} characters long.", :max => PROFILE_TITLE_MAX)
-  validates_length_of :about_me, :allow_blank => true, :maximum => ABOUT_ME_MAX, 
+  validates_length_of :about_me, :allow_blank => true, :maximum => ABOUT_ME_MAX,
     :too_long => ts("must be less than %{max} characters long.", :max => ABOUT_ME_MAX)
 
   before_update :validate_date_of_birth
