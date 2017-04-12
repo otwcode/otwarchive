3e75ef6c43a2ed7351e4f28040c47fa27f5957db
zz9pzza <zz9pzza@gmail.com>
2017-04-12 23:03:33 +0000

    Add ambiguous_pseuds to the strong parameters

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1070,7 +1070,7 @@ class WorksController < ApplicationController
       challenge_claim_ids: [],
       category_string: [],
       warning_strings: [],
-      author_attributes: [:byline, ids: [], coauthors: []],
+      author_attributes: [:byline, ids: [], coauthors: [], ambiguous_pseuds:[]],
       series_attributes: [:id, :title],
       parent_attributes: [:url, :title, :author, :language_id, :translation],
       chapter_attributes: [
