7b81e69de6de72a2990b10e02175bbff42abe9b1
zz9pzza <zz9pzza@gmail.com>
2017-04-17 22:10:06 +0000

    Listen to the hound

--- spec/models/spam_report_spec.rb
+++ spec/models/spam_report_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 describe SpamReport do
-  let (:first_spam) { create(:work, spam: true, posted: true) }
-  let (:second_spam) { create(:work, spam: true, posted: true, authors: first_spam.authors) }
-  let (:third_spam) { create(:work, spam: true, posted: true, authors: second_spam.authors) }
+  let(:first_spam) { create(:work, spam: true, posted: true) }
+  let(:second_spam) { create(:work, spam: true, posted: true, authors: first_spam.authors) }
+  let(:third_spam) { create(:work, spam: true, posted: true, authors: second_spam.authors) }
 
   before(:example) do
     ArchiveConfig.SPAM_THRESHOLD = 10
-    firs_work = create(:work, spam: false, posted: true)
+    create(:work, spam: false, posted: true)
   end
 
   it 'has a recent date after the new date' do
@@ -23,30 +23,29 @@ describe SpamReport do
   end
 
   it 'sends email if there is significant spam' do
-    expect(AdminMailer).to receive(:send_spam_alert).with({
-        third_spam.pseuds.first.user_id => { score: 13, \
-                                             work_ids: [first_spam.id, second_spam.id, third_spam.id]}
-      }).and_return(double("AdminMailer", deliver: true))
+    expect(AdminMailer).to receive(:send_spam_alert).with(
+      third_spam.pseuds.first.user_id => { score: 13, \
+                                           work_ids: [first_spam.id, second_spam.id, third_spam.id] }
+    ).and_return(double("AdminMailer", deliver: true))
     SpamReport.run
   end
 
   it 'Using more than one ip address increases the score' do
     third_spam.ip_address = "192.168.11.1"
     third_spam.save
-    expect(AdminMailer).to receive(:send_spam_alert).with({
-        third_spam.pseuds.first.user_id => { score: 14, \
-                                             work_ids: [first_spam.id, second_spam.id, third_spam.id]}
-      }).and_return(double("AdminMailer", deliver: true))
-     SpamReport.run
-  end
-
-  it 'Posting in the past decreases the score' do
-    second_work = create(:work, spam: false, posted: true, authors: second_spam.authors, created_at: 3.days.ago)
-    expect(AdminMailer).to receive(:send_spam_alert).with({
-        third_spam.pseuds.first.user_id => { score: 11, \
-                                             work_ids: [first_spam.id, second_spam.id, third_spam.id]}
-      }).and_return(double("AdminMailer", deliver: true))
+    expect(AdminMailer).to receive(:send_spam_alert).with(
+      third_spam.pseuds.first.user_id => { score: 14, \
+                                           work_ids: [first_spam.id, second_spam.id, third_spam.id] }
+    ).and_return(double("AdminMailer", deliver: true))
     SpamReport.run
   end
 
+  it 'Posting in the past decreases the score' do
+    create(:work, spam: false, posted: true, authors: second_spam.authors, created_at: 3.days.ago)
+    expect(AdminMailer).to receive(:send_spam_alert).with(
+      third_spam.pseuds.first.user_id => { score: 11, \
+                                           work_ids: [first_spam.id, second_spam.id, third_spam.id] }
+    ).and_return(double("AdminMailer", deliver: true))
+    SpamReport.run
+  end
 end
