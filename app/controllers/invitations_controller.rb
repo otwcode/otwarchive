8d83d26a74401fd3fa1b9b4ea78da42a861abff7
Sara Olive <ssheehan@thinkthroughmath.com>
2017-04-17 20:55:40 -0400

    Add translations and update rails syntax

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -12,58 +12,58 @@ class InvitationsController < ApplicationController
   def index
     @unsent_invitations = @user.invitations.unsent.limit(5)
   end
-  
+
   def manage
     if params[:status].blank? || !['unsent', 'unredeemed', 'redeemed'].include?(params[:status])
       @invitations = @user.invitations
     else
       @invitations = @user.invitations.send(params[:status])
-    end  
+    end
   end
-  
+
   def show
     @invitation = Invitation.find(params[:id])
   end
-  
+
   def invite_friend
     @invitation = @user.invitations.find(params[:id])
     if !params[:invitee_email].blank?
       @invitation.invitee_email = params[:invitee_email]
       if @invitation.save
-        flash[:notice] = 'Invitation was successfully sent.'
-        redirect_to([@user, @invitation]) 
+        flash[:notice] = ts('Invitation was successfully sent.')
+        redirect_to([@user, @invitation])
       else
-        render :action => "show"
+        render action: "show"
       end
     else
-      flash[:error] = "Please enter an email address."
-      render :action => "show" 
-    end    
+      flash[:error] = ts("Please enter an email address.")
+      render action: "show"
+    end
   end
-  
+
   def create
     if params[:number_of_invites].to_i > 0
       params[:number_of_invites].to_i.times do
         @user.invitations.create
       end
     end
-    flash[:notice] = "Invitations were successfully created."
+    flash[:notice] = ts("Invitations were successfully created.")
     redirect_to user_invitations_url(@user)
   end
-  
+
   def update
     @invitation = Invitation.find(params[:id])
     @invitation.attributes = params[:invitation]
     if @invitation.invitee_email_changed? && @invitation.update_attributes(params[:invitation])
-      flash[:notice] = 'Invitation was successfully sent.'
+      flash[:notice] = ts('Invitation was successfully sent.')
       if logged_in_as_admin?
         redirect_to find_admin_invitations_url(:token => @invitation.token)
       else
-        redirect_to([@user, @invitation])        
+        redirect_to([@user, @invitation])
       end
     else
-      flash[:error] = "Please enter an email address." if @invitation.invitee_email.blank?
-      render :action => "show"
+      flash[:error] = ts("Please enter an email address.") if @invitation.invitee_email.blank?
+      render action: "show"
     end
   end
 
@@ -71,15 +71,14 @@ class InvitationsController < ApplicationController
     @invitation = Invitation.find(params[:id])
     @user = @invitation.creator
     if @invitation.destroy
-      flash[:notice] = "Invitation successfully destroyed"
+      flash[:notice] = ts("Invitation successfully destroyed")
     else
-      flash[:error] = "Invitation was not destroyed."
+      flash[:error] = ts("Invitation was not destroyed.")
     end
     if @user.is_a?(User)
       redirect_to user_invitations_url(@user)
     else
       redirect_to admin_invitations_url
-    end      
+    end
   end
-
 end
