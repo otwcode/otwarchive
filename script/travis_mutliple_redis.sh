853e798aa439ef6724ea0c8db1158abe8953edf6
zz9pzza <zz9pzza@gmail.com>
2017-04-23 15:40:08 +0000

    Generate more config

--- script/travis_mutliple_redis.sh
+++ script/travis_mutliple_redis.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-sudo cat /etc/redis/redis.conf
-sudo cp /etc/redis/redis2.conf
-sudo cp /etc/redis/redis3.conf
-sudo cp /etc/redis/redis4.conf
-sudo cp /etc/redis/redis5.conf
-sudo cp /etc/redis/redis6.conf
+echo "cat /etc/redis/redis.conf | sed -e s'#pidfile  /var/run/redis/redis.pid# /var/run/redis/redis.pid2#' -e 's/port 6379/port 6380/' -e 's/dbfilename redis.rdb/dbfilename redis2.rdb/' >  /etc/redis/redis2.conf" | sudo sh
+echo "cat /etc/redis/redis.conf | sed -e s'#pidfile  /var/run/redis/redis.pid# /var/run/redis/redis.pid3#' -e 's/port 6379/port 6381/' -e 's/dbfilename redis.rdb/dbfilename redis3.rdb/' >  /etc/redis/redis3.conf" | sudo sh
+echo "cat /etc/redis/redis.conf | sed -e s'#pidfile  /var/run/redis/redis.pid# /var/run/redis/redis.pid4#' -e 's/port 6379/port 6382/' -e 's/dbfilename redis.rdb/dbfilename redis4.rdb/' >  /etc/redis/redis4.conf" | sudo sh
+echo "cat /etc/redis/redis.conf | sed -e s'#pidfile  /var/run/redis/redis.pid# /var/run/redis/redis.pid6#' -e 's/port 6379/port 6383/' -e 's/dbfilename redis.rdb/dbfilename redis5.rdb/' >  /etc/redis/redis5.conf" | sudo sh
+echo "cat /etc/redis/redis.conf | sed -e s'#pidfile  /var/run/redis/redis.pid# /var/run/redis/redis.pid7#' -e 's/port 6379/port 6384/' -e 's/dbfilename redis.rdb/dbfilename redis6.rdb/' >  /etc/redis/redis6.conf" | sudo sh
