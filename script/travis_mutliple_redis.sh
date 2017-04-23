aeab51511307cc54fd9fafa0d44f04902eb104a2
zz9pzza <zz9pzza@gmail.com>
2017-04-23 15:49:55 +0000

    try and run servers

--- script/travis_mutliple_redis.sh
+++ script/travis_mutliple_redis.sh
@@ -4,11 +4,11 @@ echo "cat /etc/redis/redis.conf | sed -e s'#pidfile  /var/run/redis/redis.pid# /
 echo "cat /etc/redis/redis.conf | sed -e s'#pidfile  /var/run/redis/redis.pid# /var/run/redis/redis.pid4#' -e 's/port 6379/port 6382/' -e 's/dbfilename redis.rdb/dbfilename redis4.rdb/' >  /etc/redis/redis4.conf" | sudo sh
 echo "cat /etc/redis/redis.conf | sed -e s'#pidfile  /var/run/redis/redis.pid# /var/run/redis/redis.pid6#' -e 's/port 6379/port 6383/' -e 's/dbfilename redis.rdb/dbfilename redis5.rdb/' >  /etc/redis/redis5.conf" | sudo sh
 echo "cat /etc/redis/redis.conf | sed -e s'#pidfile  /var/run/redis/redis.pid# /var/run/redis/redis.pid7#' -e 's/port 6379/port 6384/' -e 's/dbfilename redis.rdb/dbfilename redis6.rdb/' >  /etc/redis/redis6.conf" | sudo sh
-sudo "redis-server  /etc/redis/redis2.conf" 
-sudo "redis-server  /etc/redis/redis3.conf"
-sudo "redis-server  /etc/redis/redis4.conf"
-sudo "redis-server  /etc/redis/redis5.conf"
-sudo "redis-server  /etc/redis/redis6.conf"
+sudo "/usr/bin/redis-server  /etc/redis/redis2.conf" 
+sudo "/usr/bin/redis-server  /etc/redis/redis3.conf"
+sudo "/usr/bin/redis-server  /etc/redis/redis4.conf"
+sudo "/usr/bin/redis-server  /etc/redis/redis5.conf"
+sudo "/usr/bin/redis-server  /etc/redis/redis6.conf"
 sleep 10
 ps -ef |grep redis
 
