20fc04211d68df8380248c9cd013a3919fa1d0ae
zz9pzza <zz9pzza@gmail.com>
2017-04-23 15:28:05 +0000

    Start on travis config

---
+++ script/travis_mutliple_redis.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo cat /etc/redis/redis.conf
+sudo cp /etc/redis/redis2.conf
+sudo cp /etc/redis/redis3.conf
+sudo cp /etc/redis/redis4.conf
+sudo cp /etc/redis/redis5.conf
+sudo cp /etc/redis/redis6.conf
