3787e4d4357f07b09d57158c70fe7c2149612448
zz9pzza <zz9pzza@gmail.com>
2017-04-26 21:14:19 +0000

    quotes, continuation lines and English

--- spec/models/spam_report_spec.rb
+++ spec/models/spam_report_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe SpamReport do
   let(:first_spam) { create(:work, spam: true, posted: true) }
@@ -10,19 +10,19 @@ describe SpamReport do
     create(:work, spam: false, posted: true)
   end
 
-  it 'has a recent date after the new date' do
+  it "has a recent date after the new date" do
     spam_report = SpamReport.new
     expect(spam_report.new_date).to be > spam_report.recent_date
   end
 
-  it 'does not send email if the spam score is lower than the spam threshold' do
+  it "does not send email if the spam score is lower than the spam threshold" do
     third_spam
     ArchiveConfig.SPAM_THRESHOLD = 70
     expect(AdminMailer).not_to receive(:send_spam_alert)
     SpamReport.run
   end
 
-  it 'sends email if the spam score is higher than the spam threshold' do
+  it "sends email if the spam score is higher than the spam threshold" do
     expect(AdminMailer).to receive(:send_spam_alert).with(
       third_spam.pseuds.first.user_id => { score: 13,
                                            work_ids: [first_spam.id, second_spam.id, third_spam.id] }
@@ -30,25 +30,25 @@ describe SpamReport do
     SpamReport.run
   end
 
-  it 'increases the spam score if the work creator has used more than one IP address"' do
+  it "increases the spam score if the work creator has used more than one IP address" do
     third_spam.ip_address = "192.168.11.1"
     third_spam.save
     expect(AdminMailer).to receive(:send_spam_alert).with(
-      third_spam.pseuds.first.user_id => { score: 14, \
+      third_spam.pseuds.first.user_id => { score: 14,
                                            work_ids: [first_spam.id, second_spam.id, third_spam.id] }
     ).and_return(double("AdminMailer", deliver: true))
     SpamReport.run
   end
 
-  it 'Posting works in the recent ( but not too recent ) past reduces the score' do
+  it "reduces the spam score if works are posted in the recent past" do
     expect(AdminMailer).to receive(:send_spam_alert).with(
-      third_spam.pseuds.first.user_id => { score: 13, \
+      third_spam.pseuds.first.user_id => { score: 13,
                                            work_ids: [first_spam.id, second_spam.id, third_spam.id] }
     ).and_return(double("AdminMailer", deliver: true))
     SpamReport.run
     create(:work, spam: false, posted: true, authors: second_spam.authors, created_at: 3.days.ago)
     expect(AdminMailer).to receive(:send_spam_alert).with(
-      third_spam.pseuds.first.user_id => { score: 11, \
+      third_spam.pseuds.first.user_id => { score: 11,
                                            work_ids: [first_spam.id, second_spam.id, third_spam.id] }
     ).and_return(double("AdminMailer", deliver: true))
     SpamReport.run
