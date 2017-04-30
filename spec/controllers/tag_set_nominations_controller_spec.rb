473ac98836531061109381b44ada2ef627d9334e
zz9pzza <zz9pzza@gmail.com>
2017-04-30 11:53:48 +0000

    Fix some errors :(

--- spec/controllers/tag_set_nominations_controller_spec.rb
+++ spec/controllers/tag_set_nominations_controller_spec.rb
@@ -1362,7 +1362,7 @@ describe TagSetNominationsController do
       context 'not all tag nominations have an associated _approve, _reject, _change, or _synonym param value' do
         it 'redirects and returns a flash message' do
           put :update_multiple, { tag_set_id: owned_tag_set.id }.merge(base_params)
-          it_redirects_to_with_notice(tag_set_nominations_path(owned_tag_set),"Still some nominations left to review!")
+          it_redirects_to_internal(tag_set_nominations_path(owned_tag_set))
         end
       end
 
@@ -1374,7 +1374,7 @@ describe TagSetNominationsController do
                   'relationship_approve_New Relationship': 1,
                   'fandom_approve_New Fandom': 1,
                   'freeform_reject_New Freeform': 1)
-          it_redirects_to_with_notice(tag_set_path(owned_tag_set),"All nominations reviewed, yay!")
+          it_redirects_to_internal(tag_set_path(owned_tag_set))
         end
       end
 
