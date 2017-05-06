bf4319f208994887f7a045ddef348e69fefa3beb
cresenne <katieby@users.noreply.github.com>
2017-05-05 18:18:41 -0700

    AO3-5008 Validate presence of subscribable rather than id and type.

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,7 +4,7 @@ class Subscription < ActiveRecord::Base
   belongs_to :user
   belongs_to :subscribable, :polymorphic => true
   
-  validates_presence_of :user, :subscribable_id, :subscribable_type
+  validates_presence_of :user, :subscribable
   
   # Get the subscriptions associated with this work
   # currently: users subscribed to work, users subscribed to creator of work
