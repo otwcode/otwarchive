5f6c60dbee247533d96a648c2d98abf0eb93ac89
sarken <sarken@gmail.com>
2017-05-09 18:57:48 -0400

    AO3-3176 Don't add more unnecssary interger conversion for pseud.id, and fix a line break in a code comment

--- app/helpers/pseuds_helper.rb
+++ app/helpers/pseuds_helper.rb
@@ -22,12 +22,13 @@ module PseudsHelper
   # Controls which of current_user's pseuds is selected when posting or editing 
   # an item. It used to be handled with just @selected_pseuds from the item 
   # type's controller (chapter, series, work), but needs a special case when 
-  # current_user is editing a chapter they didn't co-author. When that happens, # we want to select all pseuds current_user has listed on the work itself.
+  # current_user is editing a chapter they didn't co-author. When that happens,
+  # we want to select all pseuds current_user has listed on the work itself.
   def selected_pseuds(item_type)
     if item_type == "chapter" && @chapter.posted?
       to_select = @to_select.select { |pseud| pseud.user.id == current_user.id }
       pseuds = to_select.empty? ? @work.pseuds : to_select
-      pseuds.collect { |pseud| pseud.id.to_i }
+      pseuds.collect { |pseud| pseud.id }
     else
       @selected_pseuds
     end
