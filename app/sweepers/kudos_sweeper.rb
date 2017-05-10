0c1e4a967c96a0f1c6aef1337d59c12f0c1ad47f
WendyBeth <wendybeth010@gmail.com>
2017-05-10 10:00:28 -0400

    Uses expire_fragment with skip_digest: true for kudos sweeper
    Instead of dealing with an invisible digest that doesn't matter and just causes
    confusion.

--- app/sweepers/kudos_sweeper.rb
+++ app/sweepers/kudos_sweeper.rb
@@ -11,6 +11,7 @@ class KudosSweeper < ActionController::Caching::Sweeper
 
     # expire the cache for the kudos section in the view
     # and for the full-page, expanded list
-    kudo.commentable.touch
+    expire_fragment "#{kudo.commentable.cache_key}/kudos-v1"
+    expire_fragment "#{kudo.commentable.cache_key}/kudos/full"
   end
 end
