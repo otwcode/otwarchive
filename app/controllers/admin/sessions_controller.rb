aeb6e670777deaed797ca0d1329c0552e925b301
WendyBeth <wendybeth010@gmail.com>
2017-06-12 09:57:02 -0400

    Fixes :store_location has not been defined error
    skip_* actions in Rails 5 will now raise if the provided method is not defined.
    
    https://github.com/plataformatec/devise/issues/4207

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -3,6 +3,6 @@ class Admin
  # Handle admin session authentication
   class SessionsController < Devise::SessionsController
     before_action :user_logout_required, except: :destroy
-    skip_after_action :store_location
+    skip_after_action :store_location, raise: false
   end
 end
