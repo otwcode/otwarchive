62adc0a913e646ce673c2d03bebebe74530538df
Katie Bylander <katieb@swbell.net>
2017-07-18 12:54:31 -0700

    AO3-5008 Validate that subscribable exists before saving (#2904)
    * AO3-5008 Validate presence of subscribable rather than id and type.
    
    * AO3-5008 listen to the hound.
    
    * Add test for invalid type name.
    
    * Hound fixed
    
    * Listen to the hound

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,57 +1,80 @@
 require 'spec_helper'
 
 describe Subscription do
-  let(:subscription) { FactoryGirl.build(:subscription) }
+  let(:subscription) { build(:subscription) }
 
   context "to a work" do
-    before(:each) do
-      subscription.subscribable = FactoryGirl.create(:work)
+    before do
+      subscription.subscribable = create(:work)
       subscription.save!
     end
 
     describe "when the work is destroyed" do
-      before(:each) do
+      before do
         subscription.subscribable.destroy
       end
 
       it "should be destroyed" do
-        expect { subscription.reload }.to raise_error
+        expect { subscription.reload }.to raise_error ActiveRecord::RecordNotFound
       end
     end
   end
 
   context "to a series" do
-    before(:each) do
-      subscription.subscribable = FactoryGirl.create(:series)
+    before do
+      subscription.subscribable = create(:series)
       subscription.save!
     end
 
     describe "when the series is destroyed" do
-      before(:each) do
+      before do
         subscription.subscribable.destroy
       end
 
       it "should be destroyed" do
-        expect { subscription.reload }.to raise_error
+        expect { subscription.reload }.to raise_error ActiveRecord::RecordNotFound
       end
     end
   end
 
   context "to a user" do
-    before(:each) do
-      subscription.subscribable = FactoryGirl.create(:user)
+    before do
+      subscription.subscribable = create(:user)
       subscription.save!
     end
 
     describe "when the user is destroyed" do
-      before(:each) do
+      before do
         subscription.subscribable.destroy
       end
 
       it "should be destroyed" do
-        expect { subscription.reload }.to raise_error
+        expect { subscription.reload }.to raise_error ActiveRecord::RecordNotFound
       end
     end
   end
 
+  context "when subscribable does not exist" do
+    before do
+      work = create(:work)
+      subscription.subscribable_id = work.id
+      subscription.subscribable_type = "Work"
+      work.destroy
+    end
+
+    it "should not save" do
+      expect(subscription.save).to be_falsey
+    end
+  end
+
+  context "when subscribable_type is not a valid type" do
+    before do
+      subscription.subscribable_id = 1
+      subscription.subscribable_type = "Серия"
+    end
+
+    it "should raise error" do
+      expect { subscription.save }.to raise_error NameError
+    end
+  end
 end
