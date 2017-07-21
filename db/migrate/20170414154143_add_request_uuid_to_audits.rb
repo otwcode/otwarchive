42cbfbe299ab9cc0a7b0b5c0f8f7f8dd271af2da
WendyBeth <wendybeth010@gmail.com>
2017-07-21 09:26:06 -0400

    Specifies version of all migrations
    Not entirely certain how specific this has to be, but because all of these
    migrations *worked* in 4.2, it seems reasonable they can inherit from
    ActiveRecord::Migration[4.2].

--- db/migrate/20170414154143_add_request_uuid_to_audits.rb
+++ db/migrate/20170414154143_add_request_uuid_to_audits.rb
@@ -1,4 +1,4 @@
-class AddRequestUuidToAudits < ActiveRecord::Migration
+class AddRequestUuidToAudits < ActiveRecord::Migration[4.2]
   def self.up
     add_column :audits, :request_uuid, :string
     add_index :audits, :request_uuid
