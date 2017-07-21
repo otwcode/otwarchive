d79c12fd078039970870e61e0fe6a6a1d24d1130
WendyBeth <wendybeth010@gmail.com>
2017-07-21 09:43:18 -0400

    Adds note about deprecated :controller and :action segments

--- config/routes.rb
+++ config/routes.rb
@@ -582,5 +582,8 @@ Otwarchive::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
+  #
+  # TODO: Replace or get rid of this by Rails 5.2, as both :controller and
+  #         :action are deprecated
   get ':controller(/:action(/:id(.:format)))'
 end
