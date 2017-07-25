45daa8183659c889d9eef9c03e496b9dbe673036
Elz <elzj78@gmail.com>
2017-07-25 19:29:04 -0400

    AO3-5008: prevent subscriptions to nonsubscribable models (#2974)
    * AO3 5008: Tweak subscription validation to prevent subscriptions to nonsubscribable models
    
    * AO3 5008: Update spec
    
    * AO3 5008: Format tweaks
    
    * AO3 5008: Fix spec syntax and speed up a little
    
    * AO3 5008: Update spec text to match changes

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -62,19 +62,30 @@ describe Subscription do
       work.destroy
     end
 
-    it "should not save" do
-      expect(subscription.save).to be_falsey
+    it "should be invalid" do
+      expect(subscription.valid?).to be_falsey
     end
   end
 
-  context "when subscribable_type is not a valid type" do
+  context "when subscribable is not a valid object to subscribe to" do
+    before do
+      subscription.subscribable_id = 1
+      subscription.subscribable_type = "Pseud"
+    end
+
+    it "should be invalid" do
+      expect(subscription.valid?).to be_falsey
+    end
+  end
+
+  context "when subscribable_type is not a real model name" do
     before do
       subscription.subscribable_id = 1
       subscription.subscribable_type = "Серия"
     end
 
-    it "should raise error" do
-      expect { subscription.save }.to raise_error NameError
+    it "should be invalid" do
+      expect(subscription.valid?).to be_falsey
     end
   end
 end
