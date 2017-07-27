1e3c04657395285cebc749363c9b338b530cf4c6
WendyBeth <wendybeth010@gmail.com>
2017-07-27 11:18:58 -0400

    Fixes before_save callback

--- config/initializers/monkeypatches/update_sanitizer_version_before_save.rb
+++ config/initializers/monkeypatches/update_sanitizer_version_before_save.rb
@@ -3,7 +3,7 @@ module ActiveRecord
     before_save :update_sanitizer_version
     def update_sanitizer_version
       ArchiveConfig.FIELDS_ALLOWING_HTML.each do |field|
-        if self.respond_to?("saved_change_to_#{field}?") && self.send("saved_change_to_#{field}?")
+        if self.respond_to?("#{field}_changed?") && self.send("#{field}_changed?")
           self.send("#{field}_sanitizer_version=", ArchiveConfig.SANITIZER_VERSION)
         end
       end
