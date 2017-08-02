4d157701feff861a9ece05ad8b5e018139981af3
WendyBeth <wendybeth010@gmail.com>
2017-08-02 10:22:46 -0400

    Specifies Rails version in migration file

--- db/migrate/20170615090455_remove_admin_emails_off_from_preferences.rb
+++ db/migrate/20170615090455_remove_admin_emails_off_from_preferences.rb
@@ -1,4 +1,4 @@
-class RemoveAdminEmailsOffFromPreferences < ActiveRecord::Migration
+class RemoveAdminEmailsOffFromPreferences < ActiveRecord::Migration[4.2]
   def change
     remove_column :preferences, :admin_emails_off, :boolean
   end
