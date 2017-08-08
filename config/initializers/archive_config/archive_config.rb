25a4ffe209a500bf7f7d2ef2fa4f13ef23719263
elz <elzj78@gmail.com>
2017-08-07 20:12:13 -0400

    AO3-5033: Remove stray reference to deleted file

--- config/initializers/archive_config/archive_config.rb
+++ config/initializers/archive_config/archive_config.rb
@@ -10,9 +10,5 @@ end
 
 ### more items here preserved from Rails 2 environment.rb that might not belong here
 ActionController::AbstractRequest.relative_url_root = ArchiveConfig.PRODUCTION_URL_ROOT if ArchiveConfig.PRODUCTION_URL_ROOT && ENV['RAILS_ENV'] == 'production'
-
-class ActiveRecord::Base
-  include FindRandom
-end
 ### end of preservation section
 
