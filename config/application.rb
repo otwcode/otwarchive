e71d9d43db439b83a8bdb41b7ca8e1f9f7eaa863
Elz <elzj78@gmail.com>
2017-08-07 21:20:00 -0400

    AO3-5033: Fix lib directory loading for Rails 5 (#2990)
    * AO3-5033: Fix lib directory loading for Rails 5 and remove unused/broken files
    
    * AO3-5033: Remove stray reference to deleted file

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Otwarchive
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-    config.autoload_paths += %W(#{Rails.root}/lib)
+    config.eager_load_paths += %W(#{Rails.root}/lib)
     config.autoload_paths += %W(#{Rails.root}/app/sweepers)
     %w(
       challenge_models
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,6 +5,7 @@ Otwarchive::Application.configure do
   # every request.  This slows down response time but is perfect for development
   # since you don't have to restart the webserver when you make code changes.
   config.cache_classes = false
+  config.eager_load = false
   config.cache_store = :dalli_store, '127.0.0.1:11211',
                           { namespace:  'ao3-v1', expires_in:  0, compress: true , pool_size: 10 }
 
