3d91a2df53e852560882e5397ebb4ba18fcef840
elz <elzj78@gmail.com>
2017-08-07 20:03:13 -0400

    AO3-5033: Fix lib directory loading for Rails 5 and remove unused/broken files

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -6,6 +6,7 @@ Otwarchive::Application.configure do
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs.  Don't rely on the data there!
   config.cache_classes = true
+  config.eager_load = true
 
   # Log error messages when you accidentally call methods on nil.
   # config.whiny_nils = true
