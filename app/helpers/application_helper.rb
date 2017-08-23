20a0e3f77e8cac94be3c5e67279b40ce4204fbc0
sarken <sarken@gmail.com>
2017-08-22 20:17:34 -0400

    AO3-5107 Address syntax issues with pagination renderer override

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -554,11 +554,12 @@ module ApplicationHelper
   # change the default link renderer for will_paginate
   def will_paginate(collection_or_options = nil, options = {})
     if collection_or_options.is_a? Hash
-      options, collection_or_options = collection_or_options, nil
+      options = collection_or_options
+      collection_or_options = nil
     end
     unless options[:renderer]
-      options = options.merge :renderer => PaginationListLinkRenderer
+      options = options.merge renderer: PaginationListLinkRenderer
     end
-    super *[collection_or_options, options].compact
+    super(*[collection_or_options, options].compact)
   end
 end # end of ApplicationHelper
