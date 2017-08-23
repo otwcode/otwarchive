d953fa37f0a4500b0a6e44c6352812d08b0818c8
sarken <sarken@gmail.com>
2017-08-22 21:46:49 -0400

    AO3-5107 Remove the old pagination code from the will_paginate config

--- config/initializers/gem-plugin_config/will_paginate_config.rb
+++ config/initializers/gem-plugin_config/will_paginate_config.rb
@@ -2,15 +2,17 @@ require 'will_paginate/array'
 WillPaginate.per_page = ArchiveConfig.ITEMS_PER_PAGE
 
 module WillPaginate
-  module ActionView
-    def will_paginate(collection = nil, options = {})
-      options, collection = collection, nil if collection.is_a? Hash
-      collection ||= infer_collection_from_controller
-
-      options = options.symbolize_keys
-      options[:renderer] = PaginationListLinkRenderer
-
-      super(collection, options).try(:html_safe)
+  module ActionView::Helpers::ApplicationHelper
+    # change the default link renderer for will_paginate
+    def will_paginate(collection_or_options = nil, options = {})
+      if collection_or_options.is_a? Hash
+        options = collection_or_options
+        collection_or_options = nil
+      end
+      unless options[:renderer]
+        options = options.merge renderer: PaginationListLinkRenderer
+      end
+      super(*[collection_or_options, options].compact)
     end
   end
 end
