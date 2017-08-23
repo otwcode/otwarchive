ed4468845dde0a043cabd4de30247a2ed7356aae
sarken <sarken@gmail.com>
2017-08-22 21:48:00 -0400

    AO3-5107 ACTUALLY remove the old pagination method from the config

--- config/initializers/gem-plugin_config/will_paginate_config.rb
+++ config/initializers/gem-plugin_config/will_paginate_config.rb
@@ -1,18 +1,2 @@
 require 'will_paginate/array'
 WillPaginate.per_page = ArchiveConfig.ITEMS_PER_PAGE
-
-module WillPaginate
-  module ActionView::Helpers::ApplicationHelper
-    # change the default link renderer for will_paginate
-    def will_paginate(collection_or_options = nil, options = {})
-      if collection_or_options.is_a? Hash
-        options = collection_or_options
-        collection_or_options = nil
-      end
-      unless options[:renderer]
-        options = options.merge renderer: PaginationListLinkRenderer
-      end
-      super(*[collection_or_options, options].compact)
-    end
-  end
-end
