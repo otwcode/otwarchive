4799cd90787f20780896f83d16dc8b2b6f844b55
elz <elzj78@gmail.com>
2017-08-24 11:03:05 -0400

    AO3-5111: Fix attribute_changed deprecation message

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -6,7 +6,7 @@ class ApplicationRecord < ActiveRecord::Base
 
   def update_sanitizer_version
     ArchiveConfig.FIELDS_ALLOWING_HTML.each do |field|
-      if self.respond_to?("#{field}_changed?") && self.send("#{field}_changed?")
+      if self.will_save_change_to_attribute?(field)
         self.send("#{field}_sanitizer_version=", ArchiveConfig.SANITIZER_VERSION)
       end
     end
