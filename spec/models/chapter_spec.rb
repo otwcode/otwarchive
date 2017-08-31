1fe246930a3731bce34998ecdf4acfd333c8ac78
sarken <sarken@gmail.com>
2017-08-30 21:46:43 -0400

    AO3-5133 Add some chapter specs to make sure chapters are created correctly, and add work specs to make sure the posted_work factory is producing a posted chapter

--- spec/models/chapter_spec.rb
+++ spec/models/chapter_spec.rb
@@ -16,12 +16,6 @@ describe Chapter do
     chapter.posted.should == false
   end
 
-  it "is posted on a posted work" do
-    work = create(:posted_work)
-    chapter = work.first_chapter
-    chapter.posted.should == true
-  end
-
   describe "save" do
 
     before(:each) do
