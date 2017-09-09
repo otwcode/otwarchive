786a92dce2323c876d48629291f78e9aa20f0c17
elz <elzj78@gmail.com>
2017-09-09 18:41:39 -0400

    AO3-5155: Flip the condition the right way around

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -466,7 +466,7 @@ public
                       :set_redirects,
                       :set_media,
                       :store_location,
-                      unless: proc { %w(js json).include?(request.format) }
+                      if: proc { %w(js json).include?(request.format) }
 
   #### -- AUTHORIZATION -- ####
 
