7ab31557a5b74e5039d5eb4183ac97a365f0256e
WendyBeth <wendybeth010@gmail.com>
2017-09-20 15:59:51 -0400

    Attempt with ES 6.0 alpha

--- script/travis_elasticsearch_upgrade.sh
+++ script/travis_elasticsearch_upgrade.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo service elasticsearch stop
 wget
-https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-5.5.1.deb
-sudo dpkg --force-confold -i elasticsearch-5.5.1.deb
+https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-6.6.0-alpha1
+sudo dpkg --force-confold -i elasticsearch-6.0.0-alpha1.deb
 sudo service elasticsearch start
