80515a9bb049d0cb510f294b00aeb3dd72845e2e
Elz <elzj78@gmail.com>
2017-09-20 00:56:28 -0400

    AO3-5178: Fix slow tag set callback (#3067)
    * AO3-5178: Fix slow tag set callback
    
    * AO3-5178: Avoid uniqueness errors

--- app/models/tagset_models/tag_set.rb
+++ app/models/tagset_models/tag_set.rb
@@ -87,10 +87,10 @@ class TagSet < ApplicationRecord
         tags_to_remove += (old_tags - new_tags)
       else
         #
-        if !self.instance_variable_get("@#{type}_tagnames_to_add").blank?
+        unless self.instance_variable_get("@#{type}_tagnames_to_add").blank?
           tags_to_add += tagnames_to_list(self.instance_variable_get("@#{type}_tagnames_to_add"), type)
         end
-        if !self.instance_variable_get("@#{type}_tags_to_remove").blank?
+        unless self.instance_variable_get("@#{type}_tags_to_remove").blank?
           tagclass = type.classify.constantize # Safe constantize itterating over TAG_TYPES
           tags_to_remove += (self.instance_variable_get("@#{type}_tags_to_remove").map {|tag_id| tag_id.blank? ? nil : tagclass.find(tag_id)}.compact)
         end
@@ -98,7 +98,7 @@ class TagSet < ApplicationRecord
     end
 
     # This overrides the type-specific
-    if !@tagnames_to_remove.blank?
+    if @tagnames_to_remove.present?
       tags_to_remove = @tagnames_to_remove.split(ArchiveConfig.DELIMITER_FOR_INPUT).map {|tname| Tag.find_by_name(tname.squish)}.compact
     end
 
@@ -109,14 +109,24 @@ class TagSet < ApplicationRecord
       tags_to_remove = (self.tags - new_tags)
     end
 
-    tags_to_remove.uniq!
-    tags_to_add.uniq!
-
     # actually remove and add the tags, and update autocomplete
-    self.tags -= tags_to_remove
-    remove_tags_from_autocomplete(tags_to_remove)
+    remove_from_set(tags_to_remove.uniq)
+    add_to_set(tags_to_add.uniq)
+  end
 
-    self.tags += tags_to_add
+  def remove_from_set(tags_to_remove)
+    return unless tags_to_remove.present?
+    self.set_taggings.where(tag_id: tags_to_remove.map(&:id)).delete_all
+    remove_tags_from_autocomplete(tags_to_remove)
+  end
+
+  def add_to_set(tags_to_add)
+    return unless tags_to_add.present?
+    existing_ids = self.set_taggings.where(tag_id: tags_to_add.map(&:id)).pluck(:tag_id)
+    tags_to_add.each do |tag|
+      next if existing_ids.include?(tag.id)
+      self.set_taggings.create(tag_id: tag.id)
+    end
     add_tags_to_autocomplete(tags_to_add)
   end
 
