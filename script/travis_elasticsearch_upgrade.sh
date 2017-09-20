6488da3c3ae59197def20d070b3e70c41e74f5ac
WendyBeth <wendybeth010@gmail.com>
2017-09-20 14:58:06 -0400

    Sets Travis to dl new version of ES to see how build does

--- script/travis_elasticsearch_upgrade.sh
+++ script/travis_elasticsearch_upgrade.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo service elasticsearch stop
-wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.13.deb
-sudo dpkg --force-confold -i elasticsearch-0.90.13.deb
+wget
+https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-5.5.1.deb
+sudo dpkg --force-confold -i elasticsearch-5.5.1.deb
 sudo service elasticsearch start
