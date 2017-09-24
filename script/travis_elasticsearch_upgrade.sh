15f24717be2996b1c9d148256a3e1fac2a3b6d00
zz9pzza <zz9pzza@gmail.com>
2017-09-24 21:34:19 +0000

    Try starting both

--- script/travis_elasticsearch_upgrade.sh
+++ script/travis_elasticsearch_upgrade.sh
@@ -3,11 +3,14 @@ sudo service elasticsearch stop
 cd /tmp
 wget https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-0.90.13.tar.gz
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.0.0-beta2.tar.gz
-mkdir 0.90.13
-cd 0.90.13
 tar xvfz /tmp/elasticsearch-0.90.13.tar.gz
-cd /tmp
-mkdir 6.0.0-beta2
-cd 6.0.0-beta2
-tar xvfz /tmp/lasticsearch-6.0.0-beta2.tar.gz
+tar xvfz /tmp/elasticsearch-6.0.0-beta2.tar.gz
+sed -i elasticsearch-6.0.0-beta2/config/elasticsearch.yml  -e 's/#http.port: 9200/http.port: 9400/'
+sed -i elasticsearch-0.90.13/config/elasticsearch.yml  -e 's/# http.port: 9200/http.port: 9500/' 
+nohup ./elasticsearch-6.0.0-beta2/bin/elasticsearch &
+sleep 5
+nohup ./elasticsearch-0.90.13/bin/elasticsearch &
+sleep 5
+wget -q --waitretry=1 --retry-connrefused -T 10 -O - http://127.0.0.1:9400
+wget -q --waitretry=1 --retry-connrefused -T 10 -O - http://127.0.0.1:9500
 
