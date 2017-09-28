b541821642f2897772f59c628d30f892dd24f79b
WendyBeth <wendybeth010@gmail.com>
2017-09-28 10:05:33 -0400

    Updates set_rollout script to use global  variable

--- script/set_rollout.rb
+++ script/set_rollout.rb
@@ -3,11 +3,11 @@ require 'redis'
 require 'rollout'
 require_relative '../config/environment'
 
-rollout = Rollout.new(REDIS_ROLLOUT)
+# $rollout = Rollout.new(REDIS_ROLLOUT)
 value = nil
 parser = OptionParser.new do |options|
   options.on '-d', '--display rollout_name', 'Display the value of a rollout variable.' do |arg|
-    puts rollout.get(arg.to_sym).percentage
+    puts $rollout.get(arg.to_sym).percentage
   end
   options.on '-v', '--value value', 'The value to set the variable to' do |arg|
     value = arg.to_i
@@ -17,7 +17,7 @@ parser = OptionParser.new do |options|
     if value.nil?
       puts "Supply a value with -v"
     else
-      rollout.activate_percentage(arg.to_sym, value)
+      $rollout.activate_percentage(arg.to_sym, value)
     end
   end
 end
