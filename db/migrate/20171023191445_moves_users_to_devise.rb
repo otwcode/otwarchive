5d00ba134baee63904348291fc80e4262d7644f8
WendyBeth <wendybeth010@gmail.com>
2017-10-23 15:31:18 -0400

    Moves user table to devise

--- db/migrate/20171023191445_moves_users_to_devise.rb
+++ db/migrate/20171023191445_moves_users_to_devise.rb
@@ -1,13 +1,15 @@
-class MoveUsersToDevise < ActiveRecord::Migration[5.1]
+class MovesUsersToDevise < ActiveRecord::Migration[5.1]
   def change
-    change_table "users" do |t|
+    change_table :users do |t|
+      # Remove old authlogic field
+      t.remove :persistence_token
+
       # Database authenticatable
       t.rename :crypted_password, :encrypted_password
       t.rename :salt, :password_salt
 
       # Confirmable
       t.rename :activation_code, :confirmation_token
-      t.index :confirmation_token
       t.rename :activated_at, :confirmed_at
       t.datetime :confirmation_sent_at
 
@@ -20,17 +22,16 @@ class MoveUsersToDevise < ActiveRecord::Migration[5.1]
       t.datetime :remember_created_at
 
       # Trackable
-      t.integer :sign_in_count, default: 0, null: false
+      t.integer :sign_in_count, default: 0
       t.datetime :current_sign_in_at
       t.datetime :last_sign_in_at
       t.string :current_sign_in_ip
       t.string :last_sign_in_ip
 
       # Lockable
-      t.integer :failed_attempts, default: 0, null: false
-      t.remove :failed_login_count, :integer
+      t.rename :failed_login_count, :failed_attempts
+      t.change :failed_attempts, :integer, default: 0
       t.string :unlock_token
-      t.index :unlock_token
       t.datetime :locked_at
     end
   end
