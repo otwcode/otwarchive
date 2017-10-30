222e451828da16b15b5fe37543f7b55e85d2a523
elz <elzj78@gmail.com>
2017-10-30 18:08:36 -0400

    AO3-5205: Prevent sign ups with duplicate emails in the invite queue

--- app/models/invite_request.rb
+++ app/models/invite_request.rb
@@ -3,7 +3,9 @@ class InviteRequest < ApplicationRecord
   acts_as_list
   validates :email, presence: true, email_veracity: true
   validates_uniqueness_of :email, message: "is already part of our queue."
+  validate :simplified_email_uniqueness
   before_validation :compare_with_users, on: :create
+  before_validation :set_simplified_email, on: :save
 
   # Realign positions if they're incorrect
   def self.reset_order
@@ -11,6 +13,20 @@ class InviteRequest < ApplicationRecord
     unless first_request && first_request.position == 1
       requests = order(:position)
       requests.each_with_index {|request, index| request.update_attribute(:position, index + 1)}
+    end
+  end
+
+  # Borrow the blacklist cleaner but just strip out all the periods for all domains
+  def set_simplified_email
+    return if email.blank?
+    simplified = AdminBlacklistedEmail.canonical_email(email).split('@')
+    self.simplified_email = simplified.first.gsub(".", "") + "@#{simplified.last}"
+  end
+
+  # Doing this with a method so the error message makes more sense
+  def simplified_email_uniqueness
+    if InviteRequest.where(simplified_email: simplified_email).exists?
+      errors.add(:email, "is already part of our queue.")
     end
   end
 
