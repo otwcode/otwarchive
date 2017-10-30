60fb0afcf9334f72dedcaf6140051cd814c55263
elz <elzj78@gmail.com>
2017-10-30 19:29:02 -0400

    AO3-5205: Add condition to validation to avoid false positives on update

--- app/models/invite_request.rb
+++ app/models/invite_request.rb
@@ -25,7 +25,7 @@ class InviteRequest < ApplicationRecord
 
   # Doing this with a method so the error message makes more sense
   def simplified_email_uniqueness
-    if InviteRequest.where(simplified_email: simplified_email).exists?
+    if InviteRequest.where(simplified_email: simplified_email).where.not(id: id).exists?
       errors.add(:email, "is already part of our queue.")
     end
   end
