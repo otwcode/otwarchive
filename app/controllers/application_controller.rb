2e9684ace8d1ec926cfb462200b4b2a5b72c6e16
WendyBeth <wendybeth010@gmail.com>
2017-11-09 15:22:59 -0500

    Removes unnecessary helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,24 +21,6 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  # Devise helper methods allow header login form to work
-  helper_method :resource_name, :resource, :devise_mapping, :resource_class
-  def resource_name
-    :user
-  end
-
-  def resource
-    @resource ||= User.new
-  end
-
-  def resource_class
-    User
-  end
-
-  def devise_mapping
-    @devise_mapping ||= Devise.mappings[:user]
-  end
-
   def display_auth_error
     respond_to do |format|
       format.html do
