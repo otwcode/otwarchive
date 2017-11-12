84a1fd1d7023f549e16942d458b776aac514ce2a
Tal Hayon <talhayon1@gmail.com>
2017-11-12 17:39:27 +0200

    AO3-4928 Fix hound problems

--- spec/controllers/gifts_controller_spec.rb
+++ spec/controllers/gifts_controller_spec.rb
@@ -6,11 +6,12 @@ describe GiftsController do
   include LoginMacros
   include RedirectExpectationHelper
 
-  describe 'toggle_rejected'
+  describe 'toggle_rejected' do
     let (:gift) { create(:gift) }
 
     it 'should reject if no user is logged on' do
       post :toggle_rejected, params: { id: gift.id }
       expect(flash[:error]).to include("Sorry, you don't have permission to access the page you were trying to reach. Please log in.")
     end
+  end
 end
