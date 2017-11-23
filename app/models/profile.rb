63aa64189d8ce800b94845ce228257603f9a5ab0
sarken <sarken@gmail.com>
2017-11-23 03:16:48 -0500

    AO3-3952 Can't flex text inputs

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,10 +2,8 @@ class Profile < ApplicationRecord
   include ActiveModel::ForbiddenAttributesProtection
 
   # Max is 1.02 times displayed
-  PROFILE_TITLE_MAX = 261
-  PROFILE_TITLE_MAX_DISPLAYED = 255
-  LOCATION_MAX = 261
-  LOCATION_MAX_DISPLAYED = 255
+  PROFILE_TITLE_MAX = 255
+  LOCATION_MAX = 255
   ABOUT_ME_MAX = 2040
   ABOUT_ME_MAX_DISPLAYED = 2000
 
