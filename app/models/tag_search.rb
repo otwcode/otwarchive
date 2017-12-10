c6de10077fff71e3573a1ae25eed4e95c29a6526
tickinginstant <tickinginstant@users.noreply.github.com>
2017-12-10 17:21:41 -0500

    AO3-5284 Fix old tag indexing/search by type.

--- app/models/tag_search.rb
+++ app/models/tag_search.rb
@@ -8,8 +8,18 @@ class TagSearch < Search
       query do
         boolean do
           must { string query_string, default_operator: "AND" } if query_string.present?
-          must { term '_type', options[:type].downcase } if options[:type].present?
           must { term :canonical, 'T' } if options[:canonical].present?
+
+          if options[:type].present?
+            # To support the tags indexed prior to IndexSubqueue, we want to
+            # find the type either in the tag_type field or the _type field:
+            should { term '_type', options[:type].downcase }
+            should { term :tag_type, options[:type].downcase }
+
+            # The tire gem doesn't natively support :minimum_should_match,
+            # but elasticsearch 0.90 does, so we hack it in.
+            @value[:minimum_should_match] = 1
+          end
         end
       end
     end
