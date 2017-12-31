87d00eabb563f372aeea2901559f56f66795b1fb
sarken <sarken@gmail.com>
2017-12-30 23:09:30 -0500

    AO3-5276 Remove 'shoulds' for brevity

--- spec/models/pseud_decorator_spec.rb
+++ spec/models/pseud_decorator_spec.rb
@@ -36,14 +36,14 @@ describe PseudDecorator do
     end
 
     describe "#works_count" do
-      it "should return the public count if there's no current user" do
+      it "returns the public count if there's no current user" do
         expect(@decorator.works_count).to eq(7)
       end
-      it "should return the general count if there is a current user" do
+      it "returns the general count if there is a current user" do
         User.current_user = User.new
         expect(@decorator.works_count).to eq(10)
       end
-      it "should return 0 if a count is nil" do
+      it "returns 0 if a count is nil" do
         data = @search_results.first.dup
         data["_source"]["public_works_count"] = nil
         dec = PseudDecorator.decorate_from_search([@pseud], [data]).first
@@ -52,74 +52,74 @@ describe PseudDecorator do
     end
 
     describe "#bookmarks_count" do
-      it "should return the public bookmarks count" do
+      it "returns the public bookmarks count" do
         expect(@decorator.bookmarks_count).to eq(5)
       end
     end
 
     describe "#byline" do
-      it "should match the pseud byline" do
+      it "matches the pseud byline" do
         expect(@decorator.byline).to eq(@pseud.byline)
       end
     end
 
     describe "#user_login" do
-      it "should match the user login" do
+      it "matches the user login" do
         expect(@decorator.user_login).to eq(@pseud.user.login)
       end
     end
 
     describe "#pseud_path" do
-      it "should be the path to the pseud" do
+      it "is the path to the pseud" do
         expect(@decorator.pseud_path).to eq("/users/#{@pseud.user.to_param}/pseuds/#{@pseud.to_param}")
       end
     end
 
     describe "#works_path" do
-      it "should be the path to the pseud works page" do
+      it "is the path to the pseud works page" do
         expect(@decorator.works_path).to eq("/users/#{@pseud.user.to_param}/pseuds/#{@pseud.to_param}/works")
       end
     end
 
     describe "#works_link" do
-      it "should be an html link to the pseud works page" do
+      it "is an html link to the pseud works page" do
         expect(@decorator.works_link).to eq("<a href='/users/#{@pseud.user.to_param}/pseuds/#{@pseud.to_param}/works'>7 works</a>")
       end
     end
 
     describe "#bookmarks_path" do
-      it "should be the path to the pseud bookmarks page" do
+      it "is the path to the pseud bookmarks page" do
         expect(@decorator.bookmarks_path).to eq("/users/#{@pseud.user.to_param}/pseuds/#{@pseud.to_param}/bookmarks")
       end
     end
 
     describe "#bookmarks_link" do
-      it "should be an html link to the pseud bookmarks page" do
+      it "is an html link to the pseud bookmarks page" do
         expect(@decorator.bookmarks_link).to eq("<a href='/users/#{@pseud.user.to_param}/pseuds/#{@pseud.to_param}/bookmarks'>5 bookmarks</a>")
       end
     end
 
     describe "#fandom_path" do
-      it "should be the path to the pseud works page with the fandom id" do
+      it "is the path to the pseud works page with the fandom id" do
         expect(@decorator.fandom_path(13)).to eq("/users/#{@pseud.user.to_param}/pseuds/#{@pseud.to_param}/works?fandom_id=13")
       end
     end
 
     describe "#fandom_link" do
-      it "should be an html link to the pseud works page with the fandom id" do
+      it "is an html link to the pseud works page with the fandom id" do
         expect(@decorator.fandom_link(13)).to eq("<a href='/users/#{@pseud.user.to_param}/pseuds/#{@pseud.to_param}/works?fandom_id=13'>7 works in Stargate SG-1</a>")
       end
     end
 
     describe "#authored_items_links" do
-      it "should combine the work and bookmark links" do
+      it "combines the work and bookmark links" do
         str = "<a href='/users/#{@pseud.user.to_param}/pseuds/#{@pseud.to_param}/works'>7 works</a>, <a href='/users/#{@pseud.user.to_param}/pseuds/#{@pseud.to_param}/bookmarks'>5 bookmarks</a>"
         expect(@decorator.authored_items_links).to eq(str)
       end
     end
 
     describe "#constructed_byline" do
-      it "should match the pseud byline" do
+      it "matches the pseud byline" do
         expect(@decorator.constructed_byline).to eq(@pseud.byline)
       end
     end
