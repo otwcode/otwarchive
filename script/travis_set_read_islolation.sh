ea542a90ca2c5a971aaca5cff254ef855b831c15
zz9pzza <zz9pzza@gmail.com>
2018-05-16 19:23:38 +0100

    debug

--- script/travis_set_read_islolation.sh
+++ script/travis_set_read_islolation.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sed -i  /etc/mysql/my.cnf  -e 's/\[mysqld\]/[mysqld]\ninnodb_lock_wait_timeout=15\ntransaction-isolation = READ-COMMITTED\n/'
-#/etc/init.d/mysql restart
+/etc/init.d/mysql restart
 cat  /etc/mysql/my.cnf
