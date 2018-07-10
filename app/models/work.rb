e24eca974f126291f0b2077d95718fd49c2cc9fa
elz <elzj78@gmail.com>
2018-07-10 14:14:53 -0400

    Restore method that's still needed

--- app/models/work.rb
+++ app/models/work.rb
@@ -1456,6 +1456,22 @@ class Work < ApplicationRecord
     WorkIndexer.new({}).document(self)
   end
 
+  def bookmarkable_json
+    as_json(
+      root: false,
+      only: [:title, :summary, :hidden_by_admin, :restricted, :posted,
+        :created_at, :revised_at, :language_id, :word_count, :complete],
+      methods: [:tag, :filter_ids, :rating_ids, :warning_ids, :category_ids,
+        :fandom_ids, :character_ids, :relationship_ids, :freeform_ids,
+        :pseud_ids, :creators, :collection_ids, :work_types]
+    ).merge(
+      anonymous: anonymous?,
+      unrevealed: unrevealed?,
+      bookmarkable_type: 'Work',
+      bookmarkable_join: { name: "bookmarkable" }
+    )
+  end
+
   def pseud_ids
     creatorships.pluck :pseud_id
   end
