942b1a1821d125f68b37030e3ed37d6f24bb2abc
Ariana <ariana@astele.co.uk>
2018-08-24 20:43:27 +0100

    AO3-4625 Remove redundant variable assignment

--- app/controllers/api/v2/bookmarks_controller.rb
+++ app/controllers/api/v2/bookmarks_controller.rb
@@ -56,8 +56,7 @@ class Api::V2::BookmarksController < Api::V2::BaseController
   private
 
   # Find bookmarks for this archivist
-  def check_archivist_bookmark(archivist, original_url, archivist_bookmarks)
-    current_bookmark_url = original_url
+  def check_archivist_bookmark(archivist, current_bookmark_url, archivist_bookmarks)
     archivist_bookmarks = archivist_bookmarks
                             .select { |b| b&.bookmarkable&.url == current_bookmark_url }
                             .map    { |b| [b, b.bookmarkable] }
