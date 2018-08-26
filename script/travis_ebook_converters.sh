c4bcbd81541301f088fc71dc07f363e8b6fa563d
redsummernight <redsummernight@users.noreply.github.com>
2018-08-26 15:58:35 -0400

    AO3-3880 Install download dependencies in Travis before_script

--- script/travis_ebook_converters.sh
+++ script/travis_ebook_converters.sh
@@ -5,4 +5,9 @@ set -e
 wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
 sudo tar --directory=/opt -xvf wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
 sudo ln -s /opt/wkhtmltox/bin/wkhtmltopdf /usr/bin/wkhtmltopdf
-wkhtmltopdf -h
+wkhtmltopdf --version
+
+# Calibre
+sudo apt-get update -qq
+sudo apt-get install -qq calibre
+ebook-convert --version
