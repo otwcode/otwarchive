a67f91243960d5775487c85b791659eafb9d164f
redsummernight <redsummernight@users.noreply.github.com>
2018-09-17 19:52:36 -0400

    Ignore vulnerability in ruby-ffi 1.9.18
    This only affects Windows, and the gem will be removed when we
    switch from Authlogic to Devise.

--- script/gem_security.sh
+++ script/gem_security.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 TMP=/tmp/audit.$$
 bundle-audit update
-bundle-audit check  > $TMP
+bundle-audit check --ignore CVE-2018-1000201 > $TMP
 if [ "`cat $TMP |wc -l`" != "1" ]; then
    cat $TMP
    echo "Please either update gem or if that is not possible update ignore list in"
