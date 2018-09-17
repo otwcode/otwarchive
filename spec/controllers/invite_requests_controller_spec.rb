b9acb1b4783e736e2202755e7f8e5d690c9d7614
sarken <sarken@gmail.com>
2018-09-17 19:40:08 -0400

    AO3-5310 Actually fix the test

--- spec/controllers/invite_requests_controller_spec.rb
+++ spec/controllers/invite_requests_controller_spec.rb
@@ -70,7 +70,7 @@ describe InviteRequestsController do
       it "redirects to index with notice" do
         email = generate(:email)
         post :create, params: { invite_request: { email: email } }
-        invite_request = InviteRequest.find_by!(email)
+        invite_request = InviteRequest.find_by!(email: email)
         it_redirects_to_with_notice(invite_requests_path, "You've been added to our queue! Yay! We estimate that you'll receive an invitation around #{invite_request.proposed_fill_date}. We strongly recommend that you add do-not-reply@archiveofourown.org to your address book to prevent the invitation email from getting blocked as spam by your email provider.")
       end
 
