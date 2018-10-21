135e3067216e8a8487534fa4926c3e29d59ec573
zz9pzza <zz9pzza@gmail.com>
2018-10-21 20:35:27 +0000

    Retry wait on Travis

--- script/travis_elasticsearch_upgrade.sh
+++ script/travis_elasticsearch_upgrade.sh
@@ -5,3 +5,5 @@ tar xvfz /tmp/elasticsearch-6.4.2.tar.gz
 sed -i elasticsearch-6.4.2/config/elasticsearch.yml  -e 's/#http.port: 9200/http.port: 9400/'
 nohup ./elasticsearch-6.4.2/bin/elasticsearch &
 wget -q --waitretry=1 --retry-connrefused -T 20 -O - "http://127.0.0.1:9400/_cluster/health?wait_for_status=yellow"
+cat nohup.out
+wget -q --waitretry=1 --retry-connrefused -T 20 -O - "http://127.0.0.1:9400/_cluster/health?wait_for_status=yellow"
