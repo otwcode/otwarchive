09f9b23cff7a498faf5147744d9594a2995c9913
sarken <sarken@gmail.com>
2018-10-22 19:48:55 -0400

    Make the Elasticsearch version and port configurable in Travis script

--- script/travis_elasticsearch_upgrade.sh
+++ script/travis_elasticsearch_upgrade.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+ES_VERSION="6.4.2"
+ES_PORT="9400"
 cd /tmp
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.4.2.tar.gz
-tar xvfz /tmp/elasticsearch-6.4.2.tar.gz
-sed -i elasticsearch-6.4.2/config/elasticsearch.yml  -e 's/#http.port: 9200/http.port: 9400/'
-nohup ./elasticsearch-6.4.2/bin/elasticsearch &
-wget -q --waitretry=1 --retry-connrefused -T 20 -O - "http://127.0.0.1:9400/_cluster/health?wait_for_status=yellow"
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ES_VERSION}.tar.gz
+tar xvfz /tmp/elasticsearch-${ES_VERSION}.tar.gz
+sed -i elasticsearch-${ES_VERSION}/config/elasticsearch.yml  -e "s/#http.port: 9200/http.port: ${ES_PORT}/"
+nohup ./elasticsearch-${ES_VERSION}/bin/elasticsearch &
+wget -q --waitretry=1 --retry-connrefused -T 20 -O - "http://127.0.0.1:${ES_PORT}/_cluster/health?wait_for_status=yellow"
 cat nohup.out
-wget -q --waitretry=1 --retry-connrefused -T 20 -O - "http://127.0.0.1:9400/_cluster/health?wait_for_status=yellow"
+wget -q --waitretry=1 --retry-connrefused -T 20 -O - "http://127.0.0.1:${ES_PORT}/_cluster/health?wait_for_status=yellow"
