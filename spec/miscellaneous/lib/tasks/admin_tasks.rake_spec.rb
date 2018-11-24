317ebf14ab1234cd9861e8f12214c593a0e44dfe
redsummernight <redsummernight@users.noreply.github.com>
2018-11-23 21:55:38 -0500

    AO3-2378 Add task to reset word count for works of a language (#3473)
    * AO3-2378 Add task to reset word count for works of a language
    
    Add new test type for Rake tasks.
    
    * AO3-2378 Remove unnecessary word_count_will_change! call on works
    
    Unlike chapters, works will always have word count reset on save.
    
    * AO3-2378 Move new task to namespace After
    
    * AO3-2378 Update task comment

--- spec/miscellaneous/lib/tasks/admin_tasks.rake_spec.rb
+++ spec/miscellaneous/lib/tasks/admin_tasks.rake_spec.rb
@@ -1,61 +1,46 @@
-require 'spec_helper'
-require 'rake'
+require "spec_helper"
 
-describe "admin rake tasks" do
-  before do
-    # Code for activating rake, adapted from:
-    # spec/miscellaneous/lib/tasks/resque.rake_spec.rb
-    @rake = Rake.application
-    @rake.init
-    @rake.load_rakefile
-  end
-
-  after do
-    @rake.clear
-  end
-
-  describe "purging unconfirmed users" do
-    context "when the unconfirmed account is younger than two weeks" do
-      it "doesn't delete the account" do
-        unconfirmed = Delorean.time_travel_to 13.days.ago do
-          create(:user)
-        end
-
-        @rake["admin:purge_unvalidated_users"].execute
-
-        expect { unconfirmed.reload }.not_to raise_exception
+describe "rake admin:purge_unvalidated_users" do
+  context "when the unconfirmed account is younger than two weeks" do
+    it "doesn't delete the account" do
+      unconfirmed = Delorean.time_travel_to 13.days.ago do
+        create(:user)
       end
+
+      subject.invoke
+
+      expect { unconfirmed.reload }.not_to raise_exception
+    end
+  end
+
+  context "when the unconfirmed account is older than two weeks" do
+    it "deletes the account" do
+      unconfirmed = Delorean.time_travel_to 15.days.ago do
+        create(:user)
+      end
+
+      subject.invoke
+
+      expect { unconfirmed.reload }.to \
+        raise_exception(ActiveRecord::RecordNotFound)
     end
 
-    context "when the unconfirmed account is older than two weeks" do
-      it "deletes the account" do
-        unconfirmed = Delorean.time_travel_to 15.days.ago do
-          create(:user)
-        end
+    it "resets the account's invitation" do
+      invitation = create(:invitation)
 
-        @rake["admin:purge_unvalidated_users"].execute
-
-        expect { unconfirmed.reload }.to \
-          raise_exception(ActiveRecord::RecordNotFound)
+      unconfirmed = Delorean.time_travel_to 15.days.ago do
+        create(:user, invitation_token: invitation.token)
       end
 
-      it "resets the account's invitation" do
-        invitation = create(:invitation)
+      invitation.reload
+      expect(invitation.redeemed_at).not_to be_nil
+      expect(invitation.invitee).to eq(unconfirmed)
 
-        unconfirmed = Delorean.time_travel_to 15.days.ago do
-          create(:user, invitation_token: invitation.token)
-        end
+      subject.invoke
 
-        invitation.reload
-        expect(invitation.redeemed_at).not_to be_nil
-        expect(invitation.invitee).to eq(unconfirmed)
-
-        @rake["admin:purge_unvalidated_users"].execute
-
-        invitation.reload
-        expect(invitation.redeemed_at).to be_nil
-        expect(invitation.invitee).to be_nil
-      end
+      invitation.reload
+      expect(invitation.redeemed_at).to be_nil
+      expect(invitation.invitee).to be_nil
     end
   end
 end
