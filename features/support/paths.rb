035fdb432233755b6351f4a396fbfa1908df03ab
sarken <sarken@gmail.com>
2018-11-24 16:32:18 -0500

    AO3-5493 Try reindexing tags after editing

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -233,6 +233,7 @@ module NavigationHelpers
     when /^the wranglers page$/i
       tag_wranglers_path
     when /^my wrangling page$/i
+      step %{the tag indexing job has been run}
       tag_wrangler_path(User.current_user)
     when /^the unassigned fandoms page $/i
       unassigned_fandoms_path
