edc2405cea2cf4ec6c50176486684106601842b7
redsummernight <redsummernight@users.noreply.github.com>
2018-11-29 19:58:59 -0500

    AO3-2839 Allow reset password token to remain valid for 48 hours

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -195,7 +195,7 @@ Devise.setup do |config|
   # Time interval you can reset your password with a reset password key.
   # Don't put a too small interval or your users won't have the time to
   # change their passwords.
-  config.reset_password_within = 6.hours
+  config.reset_password_within = 48.hours
 
   # When set to false, does not sign a user in automatically after their password is
   # reset. Defaults to true, so a user is signed in automatically after a reset.
