606473a66e849fd6377e3c5d50933aac06f33c8e
tickinginstant <tickinginstant@users.noreply.github.com>
2019-01-11 19:41:42 -0500

    AO3-5593 Make includes() use approved_collections.

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -93,7 +93,7 @@ class WorksController < ApplicationController
 
     if @owner.present?
       if @admin_settings.disable_filtering?
-        @works = Work.includes(:tags, :external_creatorships, :series, :language, collections: [:collection_items], pseuds: [:user]).list_without_filters(@owner, options)
+        @works = Work.includes(:tags, :external_creatorships, :series, :language, :approved_collections, pseuds: [:user]).list_without_filters(@owner, options)
       else
         @search = WorkSearchForm.new(options.merge(faceted: true, works_parent: @owner))
         # If we're using caching we'll try to get the results from cache
@@ -128,10 +128,10 @@ class WorksController < ApplicationController
       end
     elsif use_caching?
       @works = Rails.cache.fetch('works/index/latest/v1', expires_in: 10.minutes) do
-        Work.latest.includes(:tags, :external_creatorships, :series, :language, collections: [:collection_items], pseuds: [:user]).to_a
+        Work.latest.includes(:tags, :external_creatorships, :series, :language, :approved_collections, pseuds: [:user]).to_a
       end
     else
-      @works = Work.latest.includes(:tags, :external_creatorships, :series, :language, collections: [:collection_items], pseuds: [:user]).to_a
+      @works = Work.latest.includes(:tags, :external_creatorships, :series, :language, :approved_collections, pseuds: [:user]).to_a
     end
     set_own_works
   end
