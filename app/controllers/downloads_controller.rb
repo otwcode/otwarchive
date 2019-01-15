7407aeefeaeb201ce3f4d912ad10a8de38827f3a
sarken <sarken@gmail.com>
2019-01-14 23:05:19 -0500

    AO3-3880 Check if downloads are enabled before loading the work; clean up comments in downloads_controller

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -9,14 +9,15 @@ class DownloadsController < ApplicationController
     respond_to :html, :pdf, :mobi, :epub
     @download = Download.generate(@work, mime_type: request.format)
 
-    # Make sure we were able to generate the download
+    # Make sure we were able to generate the download.
     unless @download.present? && @download.exists?
       flash[:error] = ts("We were not able to render this work. Please try again in a little while or try another format.")
       redirect_to work_path(@work)
       return
     end
 
-    # send file synchronously so we don't delete it before we have finished sending it
+    # Send file synchronously so we don't delete it before we have finished
+    # sending it
     File.open(@download.file_path, 'r') do |f|
       send_data f.read, filename: "#{@download.file_name}.#{@download.file_type}", type: @download.mime_type
     end
@@ -25,12 +26,18 @@ class DownloadsController < ApplicationController
 protected
 
   # Set up the work and check revealed status
-  # once a format has been created, we want nginx to be able to serve
-  # it directly, without going through rails again (until the work changes).
-  # This means no processing per user. consider this the "published" version
-  # It can't contain unposted chapters, nor unrevealed authors, even
-  # if the author is the one requesting the download
+  # Once a format has been created, we want nginx to be able to serve
+  # it directly, without going through Rails again (until the work changes).
+  # This means no processing per user. Consider this the "published" version.
+  # It can't contain unposted chapters, nor unrevealed creators, even
+  # if the creator is the one requesting the download.
   def load_work
+    unless @admin_settings.downloads_enabled?
+      flash[:error] = ts("Sorry, downloads are currently disabled.")
+      redirect_back_or_default works_path
+      return
+    end
+
     @work = Work.find(params[:id])
 
     if @work.in_unrevealed_collection?
@@ -39,18 +46,12 @@ protected
       return
     end
 
-    unless @admin_settings.downloads_enabled?
-      flash[:error] = ts("Sorry, downloads are currently disabled.")
-      redirect_back_or_default works_path
-      return
-    end
-
-    # set this for checking visibility
+    # Set this for checking visibility.
     @check_visibility_of = @work
   end
 
-  # We're currently just writing everything to tmp and feeding them through nginx
-  # so we don't want to keep the files around
+  # We're currently just writing everything to tmp and feeding them through
+  # nginx so we don't want to keep the files around.
   def remove_downloads
     @download&.remove unless Rails.env.test?
   end
