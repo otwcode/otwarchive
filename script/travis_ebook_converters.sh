3a2cdec0ec38783858d4b81638b4adf7eaa841fa
sarken <sarken@gmail.com>
2019-01-31 19:30:05 -0500

    AO3-3880 Leave wkhtmltopdf version alone in CI for now

--- script/travis_ebook_converters.sh
+++ script/travis_ebook_converters.sh
@@ -2,8 +2,8 @@
 set -e
 
 # PDF
-wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.5/wkhtmltox-0.12.5_linux-generic-amd64.tar.xz
-sudo tar --directory=/opt -xvf wkhtmltox-0.12.5_linux-generic-amd64.tar.xz
+wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
+sudo tar --directory=/opt -xvf wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
 sudo ln -s /opt/wkhtmltox/bin/wkhtmltopdf /usr/bin/wkhtmltopdf
 wkhtmltopdf --version
 
