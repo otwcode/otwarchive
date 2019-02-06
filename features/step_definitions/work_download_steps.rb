8ee740f8c2d993285a19cd145e70eb5a07857174
sarken <sarken@gmail.com>
2019-02-05 19:19:56 -0500

    AO3-5614 Add AZW3 downloads (#3515)
    * AO3-5614 Add AZW3 downloads
    
    * AO3-5614 Fix file_type_from_mime(mime) method
    
    * AO3-5614 Fix step definition

--- features/step_definitions/work_download_steps.rb
+++ features/step_definitions/work_download_steps.rb
@@ -1,6 +1,7 @@
 Then /^I should receive a file of type "([^"]*)"$/ do |filetype|
+  mime_type = filetype == "azw3" ? "application/x-mobi8-ebook" : MIME::Types.type_for("foo.#{filetype}").first
   page.driver.response.headers['Content-Disposition'].should =~ /filename=.+\.#{filetype}/
-  page.response_headers['Content-Type'].should == MIME::Types.type_for("foo.#{filetype}").first
+  page.response_headers['Content-Type'].should == mime_type
 end
 
 Then /^I should be able to download all versions of "(.*)"$/ do |title|
@@ -15,9 +16,10 @@ Then /^I (?:should be able to )?download the (\w+) version of "(.*)"$/ do |filet
   visit work_url(work)
   step %{I follow "#{filetype.upcase}"}
   filename = download.file_path # the full path of the download file we expect to exist
+  mime_type = filetype == "azw3" ? "application/x-mobi8-ebook" : MIME::Types.type_for(filename).first
   assert File.exist?(filename), "#{filename} does not exist"
   page.driver.response.headers['Content-Disposition'].should =~ /filename=\"#{File.basename(filename)}\"/
-  page.response_headers['Content-Type'].should == MIME::Types.type_for(filename).first
+  page.response_headers['Content-Type'].should == mime_type
 end
 
 Then /^I should not be able to download the (\w+) version of "(.*)"$/ do |filetype, title|
@@ -26,8 +28,9 @@ Then /^I should not be able to download the (\w+) version of "(.*)"$/ do |filety
   visit work_url(work)
   step %{I follow "#{filetype.upcase}"}
   filename = download.file_path # the full path of the download file we expect to exist
+  mime_type = filetype == "azw3" ? "application/x-mobi8-ebook" : MIME::Types.type_for(filename).first
   page.driver.response.headers['Content-Disposition'].should_not =~ /filename=\"#{File.basename(filename)}\"/
-  page.response_headers['Content-Type'].should_not == MIME::Types.type_for(filename).first
+  page.response_headers['Content-Type'].should_not == mime_type
 end
 
 Then /^I should not be able to manually download the (\w+) version of "(.*)"$/ do |filetype, title|
@@ -35,9 +38,10 @@ Then /^I should not be able to manually download the (\w+) version of "(.*)"$/ d
   download = Download.new(work, format: filetype)
   download_url = "#{ArchiveConfig.APP_URL}#{download.public_path}"
   filename = download.file_path # the full path of the download file we expect to exist
+  mime_type = filetype == "azw3" ? "application/x-mobi8-ebook" : MIME::Types.type_for(filename).first
   visit download_url
   page.driver.response.headers['Content-Disposition'].should_not =~ /filename=\"#{File.basename(filename)}\"/
-  page.response_headers['Content-Type'].should_not == MIME::Types.type_for(filename).first
+  page.response_headers['Content-Type'].should_not == mime_type
 end
 
 Then /^the (.*) version of "([^"]*)" should exist$/ do |filetype, title|
