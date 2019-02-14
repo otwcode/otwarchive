de236fecbbfb429fb9b6a686f1d615d9ebf8e543
sarken <sarken@gmail.com>
2019-02-13 19:13:44 -0500

    AO3-3808 Make check_download_visibility less complex by splitting out posted check

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -2,6 +2,7 @@ class DownloadsController < ApplicationController
 
   skip_before_action :store_location, only: :show
   before_action :load_work, only: :show
+  before_action :check_download_posted_status, only: :show
   before_action :check_download_visibility, only: :show
   after_action :remove_downloads, only: :show
 
@@ -50,15 +51,19 @@ protected
   # We can't use check_visibility because this controller doesn't have access to
   # cookies on production or staging.
   def check_download_visibility
-    return unless !@work.posted? || @work.hidden_by_admin || @work.in_unrevealed_collection?
-    message = if !@work.posted?
-                ts("Sorry, you can't download a draft.")
-              elsif @work.hidden_by_admin
+    return unless @work.hidden_by_admin || @work.in_unrevealed_collection?
+    message = if @work.hidden_by_admin
                 ts("Sorry, you can't download a work that has been hidden by an admin.")
               else
                 ts("Sorry, you can't download an unrevealed work.")
               end
     flash[:error] = message
+    redirect_to work_path(@work)
+  end
+
+  def check_download_posted_status
+    return if @work.posted
+    flash[:error] = ts("Sorry, you can't download a draft.")
     redirect_to work_path(@work)
   end
 end
