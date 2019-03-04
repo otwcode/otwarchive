c8c97707e2ff7dbf520480825e31da5b7d7bb241
zz9pzza <zz9pzza@gmail.com>
2019-03-04 22:48:09 +0000

    Reduce log messages from elasticsearch in travis

--- script/travis_elasticsearch_upgrade.sh
+++ script/travis_elasticsearch_upgrade.sh
@@ -5,7 +5,6 @@ cd /tmp
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ES_VERSION}.tar.gz
 tar xvfz /tmp/elasticsearch-${ES_VERSION}.tar.gz
 sed -i elasticsearch-${ES_VERSION}/config/elasticsearch.yml  -e "s/#http.port: 9200/http.port: ${ES_PORT}/"
-nohup ./elasticsearch-${ES_VERSION}/bin/elasticsearch &
-wget -q --waitretry=1 --retry-connrefused -T 20 -O - "http://127.0.0.1:${ES_PORT}/_cluster/health?wait_for_status=yellow"
-cat nohup.out
-wget -q --waitretry=1 --retry-connrefused -T 20 -O - "http://127.0.0.1:${ES_PORT}/_cluster/health?wait_for_status=yellow"
+nohup ./elasticsearch-${ES_VERSION}/bin/elasticsearch > elastic.out 2>&1 &
+wget -q --waitretry=1 --retry-connrefused -T 40 -O - "http://127.0.0.1:${ES_PORT}/_cluster/health?wait_for_status=yellow"
+
