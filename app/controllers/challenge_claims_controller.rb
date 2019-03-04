5ba326aab0d95ee0c74187490d38cec289933954
james_ <zz9pzza@gmail.com>
2019-03-04 22:18:44 +0000

    Try and update

--- app/controllers/challenge_claims_controller.rb
+++ app/controllers/challenge_claims_controller.rb
@@ -1,10 +1,12 @@
 class ChallengeClaimsController < ApplicationController
-  before_filter :users_only
-  before_filter :load_collection, except: [:index]
-  before_filter :collection_owners_only, except: [:index, :show, :create, :destroy]
-  before_filter :load_claim_from_id, only: [:show, :destroy]
-  before_filter :load_challenge, except: [:index]
-  before_filter :allowed_to_destroy, only: [:destroy]
+
+  before_action :users_only
+  before_action :load_collection, except: [:index]
+  before_action :collection_owners_only, except: [:index, :show, :create, :destroy]
+  before_action :load_claim_from_id, only: [:show, :destroy]
+  before_action :load_challenge, except: [:index]
+  before_action :allowed_to_destroy, only: [:destroy]
+
 
   # PERMISSIONS AND STATUS CHECKING
 
@@ -12,10 +14,9 @@ class ChallengeClaimsController < ApplicationController
     if @collection
       @challenge = @collection.challenge
     elsif @challenge_claim
-      # TODO: Replace with &. operator after upgrading to Ruby 2.3
-      @challenge = @challenge_claim.collection.challenge if @challenge_claim.collection
+      @challenge = @challenge_claim&.collection&.challenge
     end
-    return unless @challenge.nil?
+    return unless @challenge
     no_challenge
   end
 
@@ -48,17 +49,20 @@ class ChallengeClaimsController < ApplicationController
   # ACTIONS
 
   def index
-    flash[:notice] = ts("This challenge is currently closed to new posts.") if !(@collection = Collection.find_by_name(params[:collection_id])).nil? && @collection.closed? && !@collection.user_is_maintainer?(current_user)
+    if !(@collection = Collection.find_by(name: params[:collection_id])).nil? && @collection.closed? && !@collection.user_is_maintainer?(current_user)
+      flash[:notice] = ts("This challenge is currently closed to new posts.")
+    end
     if params[:collection_id]
       return unless load_collection
       @challenge = @collection.challenge if @collection
       @claims = ChallengeClaim.unposted_in_collection(@collection)
       @claims = @claims.where(claiming_user_id: current_user.id) if params[:for_user] || !@challenge.user_allowed_to_see_claims?(current_user)
+
+
       # sorting
       set_sort_order
-      @claims = params[:sort] == "claimer" ? @claims.order_by_offering_pseud(@sort_direction) : @claims.order(@sort_order)
-    elsif params[:user_id]
-      @user = User.find_by_login(params[:user_id])
+      @claims = params[:sort] == "claimer" ? @claims.order_by_offering_pseud(@sort_direction) : @claims.order(@sort_order
+    elsif params[:user_id] && (@user = User.find_by(login: params[:user_id]))
       if current_user == @user
         @claims = @user.request_claims.order_by_date.unposted
         @claims = @user.request_claims.order_by_date.posted if params[:posted]
@@ -79,11 +83,11 @@ class ChallengeClaimsController < ApplicationController
 
   def create
     # create a new claim
-    claim = ChallengeClaim.new(params[:challenge_claim])
+    claim = ChallengeClaim.new(challenge_claim_params)
     if claim.save
-      flash[:notice] = ts("New claim made.")
+      flash[:notice] = "New claim made."
     else
-      flash[:error] = ts("We couldn't save the new claim.")
+      flash[:error] = "We couldn't save the new claim."
     end
     redirect_to collection_claims_path(@collection, for_user: true)
   end
@@ -97,5 +101,13 @@ class ChallengeClaimsController < ApplicationController
       flash[:error] = ts("We couldn't delete that right now, sorry! Please try again later.")
     end
     redirect_to collection_claims_path(@collection)
+  end
+
+  private
+
+  def challenge_claim_params
+    params.require(:challenge_claim).permit(
+      :collection_id, :request_signup_id, :request_prompt_id, :claiming_user_id
+    )
   end
 end
--- spec/controllers/challenge_claims_controller_spec.rb
+++ spec/controllers/challenge_claims_controller_spec.rb
@@ -16,16 +16,15 @@ RSpec.describe ChallengeClaimsController, type: :controller do
   describe 'index' do
     it 'assigns claims and gives a notice if the collection is closed and the user is not the maintainer' do
       allow_any_instance_of(Collection).to receive(:closed?) { true }
-      get :index, id: claim.id, collection_id: collection.name, for_user: true
+      get :index, params: { id: claim.id, collection_id: collection.name, for_user: true }
       expect(flash[:notice]).to include("This challenge is currently closed to new posts.")
       expect(assigns(:claims))
     end
 
     it 'will not allow you to see someone elses claims' do
       second_user = create(:user)
-      get :index, user_id: second_user.login
-      it_redirects_to_with_error(root_path, \
-                                 "You aren't allowed to see that user's claims.")
+      get :index, params: { user_id: second_user.login }
+      it_redirects_to_with_error(root_path,  "You aren't allowed to see that user's claims.")
     end
   end
 
@@ -37,47 +36,41 @@ RSpec.describe ChallengeClaimsController, type: :controller do
       it_redirects_to(collection_prompt_path(collection, claim_with_prompt.request_prompt))
     end
 
-    it 'redirects logged in users if no collection is given' do
-      request_prompt = create(:prompt, collection_id: collection.id, challenge_signup_id: signup.id)
-      create(:challenge_claim, collection: collection, request_prompt_id: request_prompt.id)
-      get :show, id: 999_999, collection_id: collection.name
-      it_redirects_to_with_error(collection_path(collection), \
-                                 "What claim did you want to work on?")
+    xit 'needs a collection' do
+      get :show
+      it_redirects_to_with_error(root_path, "What challenge did you want to work with?")
     end
   end
 
   describe 'create' do
     it 'sets a notice and redirects' do
-      post :create, collection_id: collection.name
+      post :create, params: { collection_id: collection.name, challenge_claim: {collection_id: collection.id} }
       it_redirects_to_with_notice(collection_claims_path(collection, for_user: true), \
                                   "New claim made.")
     end
 
     it 'on an exception gives an error and redirects' do
       allow_any_instance_of(ChallengeClaim).to receive(:save) { false }
-      post :create, collection_id: collection.name
-      it_redirects_to_with_error(collection_claims_path(collection, for_user: true), \
-                                 "We couldn't save the new claim.")
+      post :create, params: { collection_id: collection.name, challenge_claim: {collection_id: collection.id} }
+      it_redirects_to_with_error(collection_claims_path(collection, for_user: true),"We couldn't save the new claim.")
     end
   end
 
-  describe 'destory' do
+  describe 'destroy' do
     context 'with a claim' do
       it 'on an exception gives an error and redirects' do
         fake_login_known_user(User.find(collection.all_owners.first.user_id))
         collection.challenge = nil
         collection.save
         delete :destroy, id: claim.id, collection_id: collection.name
-        it_redirects_to_with_error(collection_path(collection), \
-                                   "What challenge did you want to work with?")
+        it_redirects_to_with_error(collection_path(collection),"What challenge did you want to work with?")
       end
 
-      it 'on an exception gives an error and redirects' do
+      it 'gives and error and redirects if record can not be destroyed' do
         allow_any_instance_of(ChallengeClaim).to receive(:destroy) { raise ActiveRecord::RecordNotDestroyed }
         fake_login_known_user(User.find(collection.all_owners.first.user_id))
         delete :destroy, id: claim.id, collection_id: collection.name
-        it_redirects_to_with_error(collection_claims_path(collection), \
-                                   "We couldn't delete that right now, sorry! Please try again later.")
+        it_redirects_to_with_error(collection_claims_path(collection), "We couldn't delete that right now, sorry! Please try again later.")
       end
     end
   end
