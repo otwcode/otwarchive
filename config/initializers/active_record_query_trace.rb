dc1700f9607cd33bd92a0a31930692badee0e1bb
redsummernight <redsummernight@users.noreply.github.com>
2019-03-10 20:59:13 -0400

    Update active_record_query_trace.rb

--- config/initializers/active_record_query_trace.rb
+++ config/initializers/active_record_query_trace.rb
@@ -1,6 +1,5 @@
 if Rails.env.development?
   ActiveRecordQueryTrace.enabled = true
-  # Optional: other gem config options go here
-  ActiveRecordQueryTrace.ignore_cached_queries = true # Default is false.
-  ActiveRecordQueryTrace.colorize = :light_purple            # Colorize in default color
+  ActiveRecordQueryTrace.ignore_cached_queries = true
+  ActiveRecordQueryTrace.colorize = :light_purple
 end
