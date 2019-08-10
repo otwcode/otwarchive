08d428f330af927b2f7e6a82c3a2dea247f14900
tickinginstant <tickinginstant@users.noreply.github.com>
2019-08-10 11:42:43 -0400

    Cache the access token.

--- lib/zoho_auth_client.rb
+++ lib/zoho_auth_client.rb
@@ -1,16 +1,28 @@
 class ZohoAuthClient
   ACCESS_TOKEN_REQUEST_ENDPOINT = "https://accounts.zoho.com/oauth/v2/token".freeze
+  ACCESS_TOKEN_CACHE_KEY = "/v1/zoho_access_token".freeze
 
   # TODO: Can we reduce this?
   SCOPE = "Desk.tickets.ALL,Desk.contacts.READ,Desk.contacts.WRITE,Desk.contacts.CREATE,Desk.basic.READ,Desk.search.READ".freeze
 
-  def initialize
-    @access_token = access_token
-  end
-
   def access_token
+    if (cached_token = Rails.cache.read(ACCESS_TOKEN_CACHE_KEY)).present?
+      return cached_token
+    end
+
     response_raw = HTTParty.post(ACCESS_TOKEN_REQUEST_ENDPOINT, query: access_token_params)
-    JSON.parse(response_raw.body)["access_token"]
+    response_json = JSON.parse(response_raw.body)
+    access_token = response_json["access_token"]
+
+    if (expires_in = response_json["expires_in_sec"]).present?
+      # We don't want the token to expire while we're in the middle of a sequence
+      # of requests, so we take the stated expiration time and subtract a little.
+      Rails.cache.write(ACCESS_TOKEN_CACHE_KEY, access_token,
+                        expires_in: expires_in - 1.minute)
+    end
+
+    # Return the access token:
+    access_token
   end
 
   private
--- spec/miscellaneous/lib/zoho_auth_client_spec.rb
+++ spec/miscellaneous/lib/zoho_auth_client_spec.rb
@@ -5,7 +5,9 @@ describe ZohoAuthClient do
     allow(HTTParty).to receive(:post).and_return(response)
   end
 
-  let(:response) { double(:response, body: '{"access_token":"1a2b3c"}') }
+  let(:response) do
+    double(:response, body: '{"access_token":"1a2b3c","expires_in_sec":3600}')
+  end
 
   before do
     stub_const("ArchiveConfig", OpenStruct.new(ArchiveConfig))
@@ -18,30 +20,37 @@ describe ZohoAuthClient do
     stub_access_token_request
   end
 
-  describe "#new" do
-    context "when it cannot find the environment variables" do
-      xit "raises an exception?"
-    end
-
-    context "with the right environment variables" do
-      it "makes a well formed request" do
-        ZohoAuthClient.new
-
-        expect(HTTParty).to have_received(:post).
-          with("https://accounts.zoho.com/oauth/v2/token",
-               query: { client_id: "111",
-                        client_secret: "a1b2c3",
-                        redirect_uri: "https://archiveofourown.org/support",
-                        scope: "Desk.tickets.ALL,Desk.contacts.READ,Desk.contacts.WRITE,Desk.contacts.CREATE,Desk.basic.READ,Desk.search.READ",
-                        grant_type: "refresh_token",
-                        refresh_token: "x1y2z3" })
-      end
-    end
-  end
-
   describe "#access_token" do
+    it "makes a well formed request" do
+      ZohoAuthClient.new.access_token
+
+      expect(HTTParty).to have_received(:post).
+        with("https://accounts.zoho.com/oauth/v2/token",
+             query: { client_id: "111",
+                      client_secret: "a1b2c3",
+                      redirect_uri: "https://archiveofourown.org/support",
+                      scope: "Desk.tickets.ALL,Desk.contacts.READ,Desk.contacts.WRITE,Desk.contacts.CREATE,Desk.basic.READ,Desk.search.READ",
+                      grant_type: "refresh_token",
+                      refresh_token: "x1y2z3" })
+    end
+
     it "returns the access token from the response" do
       expect(ZohoAuthClient.new.access_token).to eq("1a2b3c")
+    end
+
+    it "caches the access token from the response" do
+      ZohoAuthClient.new.access_token
+
+      expect(Rails.cache.read(ZohoAuthClient::ACCESS_TOKEN_CACHE_KEY)).to \
+        eq("1a2b3c")
+    end
+
+    it "returns the cached token if it's available" do
+      Rails.cache.write(ZohoAuthClient::ACCESS_TOKEN_CACHE_KEY,
+                        "1a2b3c-cached")
+
+      expect(ZohoAuthClient.new.access_token).to eq("1a2b3c-cached")
+      expect(HTTParty).not_to have_received(:post)
     end
   end
 end
