ae2aea32726247e7c7d6bb9419e7f0c38907f5ed
elz <elzj78@gmail.com>
2019-08-26 12:03:22 -0400

    Third time's the charm

--- script/travis_ebook_converters.sh
+++ script/travis_ebook_converters.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-sudo apt-get update -qq
+# sudo apt-get update -qq
 
 # PDF
 sudo apt-get install -qq xfonts-75dpi xfonts-base
-# wget https://downloads.wkhtmltopdf.org/0.12/0.12.5/wkhtmltox_0.12.5-1.xenial_amd64.deb
-# sudo dpkg -i ./wkhtmltox_0.12.5-1.xenial_amd64.deb
-# wkhtmltopdf --version
+wget https://downloads.wkhtmltopdf.org/0.12/0.12.5/wkhtmltox_0.12.5-1.xenial_amd64.deb
+sudo dpkg -i ./wkhtmltox_0.12.5-1.xenial_amd64.deb
+wkhtmltopdf --version
 
 # Calibre
 sudo apt-get install -qq calibre
