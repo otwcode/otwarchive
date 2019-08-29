fb81b9237bde4b63a18de455ebc2acce26781b58
sarken <sarken@gmail.com>
2019-08-28 21:10:06 -0400

    AO3-5733 Update syntax before red has to tell me to

--- features/step_definitions/challege_gift_exchange_steps.rb
+++ features/step_definitions/challege_gift_exchange_steps.rb
@@ -104,7 +104,7 @@ Given /^the gift exchange "([^\"]*)" is ready for signups$/ do |title|
   step %{I open signups for "#{title}"}
 end
 
-Given /^"([^\"]*)" has two pinchhit assignments in the gift exchange "([^\"]*)"$/ do |user, collection_title|
+Given /^"(.*?)" has two pinchhit assignments in the gift exchange "(.*?)"$/ do |user, collection_title|
   collection = Collection.find_by(title: collection_title)
   user = User.find_by(login: user)
   assignments = ChallengeAssignment.where(collection_id: collection.id).limit(2)
