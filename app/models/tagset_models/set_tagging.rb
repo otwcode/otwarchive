146cb5b95ffa004644c790d900014fcde76a915a
sarken <sarken@gmail.com>
2019-09-11 23:21:28 -0400

    AO3-4353 Touch the tag set when the set taggings change

--- app/models/tagset_models/set_tagging.rb
+++ app/models/tagset_models/set_tagging.rb
@@ -1,6 +1,6 @@
 class SetTagging < ApplicationRecord
   belongs_to :tag
-  belongs_to :tag_set
+  belongs_to :tag_set, touch: true
   
   validates_uniqueness_of :tag_id, scope: [:tag_set_id], message: ts("^That tag already seems to be in this set.")
 end
