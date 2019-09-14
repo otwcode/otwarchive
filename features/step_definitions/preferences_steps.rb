e6d07f0c2cb90dc4d26dfd5f194352e4bceee68b
sarken <sarken@gmail.com>
2019-09-14 01:29:57 -0400

    AO3-5613 Update tests to better cover the hide warnings and freeforms preferences

--- features/step_definitions/preferences_steps.rb
+++ features/step_definitions/preferences_steps.rb
@@ -23,15 +23,15 @@ When /^I set my preferences to turn off notification emails for gifts$/ do
 end
 
 When /^I set my preferences to hide warnings$/ do
-  user = User.current_user
-  user.preference.hide_warnings = true
-  user.preference.save
+  step %{I follow "My Preferences"}
+  check("preference_hide_warnings")
+  click_button("Update")
 end
 
 When /^I set my preferences to hide freeform$/ do
-  user = User.current_user
-  user.preference.hide_freeform = true
-  user.preference.save
+  step %{I follow "My Preferences"}
+  check("preference_hide_freeform")
+  click_button("Update")
 end
 
 When /^I set my preferences to hide all hit counts$/ do
@@ -94,20 +94,6 @@ When /^I set my time zone to "([^"]*)"$/ do |time_zone|
   user.preference.save
 end
 
-When /^I set my preferences to hide warnings by browser$/ do
-  step %{I follow "My Preferences"}
-  check("preference[hide_warnings]")
-  click_button("Update")
-  step %{I should see "Your preferences were successfully updated"}
-end
-
-When /^I set my preferences to hide freeform by browser$/ do
-  step %{I follow "My Preferences"}
-  check("preference[hide_freeform]")
-  click_button("Update")
-  step %{I should see "Your preferences were successfully updated"}
-end
-
 When /^I set my preferences to automatically agree to my work being collected$/ do
   user = User.current_user
   user.preference.automatically_approve_collections = true
@@ -118,6 +104,13 @@ When /^I set my preferences to require my approval for my work to be collected$/
   user = User.current_user
   user.preference.automatically_approve_collections = false
   user.preference.save
+end
+
+When /^I set my preferences to hide both warnings and freeforms$/ do
+  step %{I follow "My Preferences"}
+  check("preference_hide_warnings")
+  check("preference_hide_freeform")
+  click_button("Update")
 end
 
 Given(/^the user "(.*?)" disallows co-creators$/) do |login|
