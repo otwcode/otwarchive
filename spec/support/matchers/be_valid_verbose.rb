18f0f56b9abaca0cb5f53d66be5a2cf964fa7188
elz <elzj78@gmail.com>
2019-09-21 20:26:46 -0400

    Fix deprecated spec methods

--- spec/support/matchers/be_valid_verbose.rb
+++ spec/support/matchers/be_valid_verbose.rb
@@ -1,11 +1,11 @@
 RSpec::Matchers.define :be_valid_verbose do
   match(&:valid?)
 
-  failure_message_for_should do |model|
+  failure_message do |model|
     "#{model.class} expected to be valid but had errors:n #{model.errors.full_messages.join('n ')}"
   end
 
-  failure_message_for_should_not do |model|
+  failure_message_when_negated do |model|
     "#{model.class} expected to have errors, but it did not"
   end
 
