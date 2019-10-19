fb25cf2cfd4bd1a12924f1ead3428544f873036d
tlee <thomasgylee@gmail.com>
2019-10-19 15:07:34 -0400

    Easy startup script

---
+++ docker-init.sh
@@ -0,0 +1,14 @@
+docker-compose build
+
+docker-compose run web rake db:create
+docker-compose run web rake db:schema:load
+docker-compose run web rake db:migrate
+docker-compose run web rake db:otwseed
+
+docker-compose run web rake work:missing_stat_counters
+docker-compose run web rake skins:load_site_skins
+
+docker-compose run web rake search:index_tags
+docker-compose run web rake search:index_works
+docker-compose run web rake search:index_pseuds
+docker-compose run web rake search:index_bookmarks
