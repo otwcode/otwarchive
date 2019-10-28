4ff299593e032d2836df82548ff5c5dbf9bbec7f
sarken <sarken@gmail.com>
2019-10-28 03:54:07 -0400

    AO3-2240 Use simpler check to see if item is in other unrevealed collections

--- app/models/collection_item.rb
+++ app/models/collection_item.rb
@@ -191,13 +191,8 @@ class CollectionItem < ApplicationRecord
 
   after_update :notify_of_status_change
   def notify_of_status_change
-    unrevealed_collection_ids = item
-      .collection_items
-      .unrevealed
-      .approved_by_user
-      .pluck(:collection_id)
     # If the item is also in other unrevealed collections, skip notifications.
-    return if (unrevealed_collection_ids - [collection_id]).any?
+    return if item.user_approved_collection_items.unrevealed.any?
 
     notify_of_reveal if saved_change_to_unrevealed?
   end
