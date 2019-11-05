ea189dbe9cd6e07cf1d540e0542b71e31e54c476
sarken <sarken@gmail.com>
2019-11-04 21:17:01 -0500

    Update config to look for locale files in nested folders
    lol, imagine if I had committed this when I wrote it

--- config/application.rb
+++ config/application.rb
@@ -44,8 +44,8 @@ module Otwarchive
     # config.time_zone = 'Central Time (US & Canada)'
     config.time_zone = 'Eastern Time (US & Canada)'
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # The default locale is :en and all translations from config/locales/**/*.rb,yml are auto loaded.
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
