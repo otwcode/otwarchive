8aa7e234fbbb9342a16a52a1f6190dea4df47632
AO3 Unicorn service account <ao3app@template>
2019-11-09 08:26:18 +0000

    Skip over ao3-front03

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -14,7 +14,6 @@ server "ao3-front01.ao3.org", :web
 server "ao3-app15.ao3.org", :web
 server "ao3-app05.ao3.org", :app
 server "ao3-app08.ao3.org", :app, :workers, :schedulers
-server "ao3-front03.ao3.org", :web
 server "ao3-app06.ao3.org", :app
 server "ao3-app12.ao3.org", :app
 server "ao3-app07.ao3.org", :app
