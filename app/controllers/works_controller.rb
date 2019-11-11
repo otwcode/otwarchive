1a807b5e71c217e10ece23df5567a66533bce238
tickinginstant <tickinginstant@users.noreply.github.com>
2019-11-10 21:23:31 -0500

    AO3-5593 Rewrite definition of approved_collections. (#3502)
    * AO3-5593 Fix approved_collections definition.
    
    * AO3-5593 Make includes() use approved_collections.
    
    * AO3-5593 Fix up two more includes().

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -123,10 +123,10 @@ class WorksController < ApplicationController
       end
     elsif use_caching?
       @works = Rails.cache.fetch('works/index/latest/v1', expires_in: 10.minutes) do
-        Work.latest.includes(:tags, :external_creatorships, :series, :language, collections: [:collection_items], pseuds: [:user]).to_a
+        Work.latest.includes(:tags, :external_creatorships, :series, :language, :approved_collections, pseuds: [:user]).to_a
       end
     else
-      @works = Work.latest.includes(:tags, :external_creatorships, :series, :language, collections: [:collection_items], pseuds: [:user]).to_a
+      @works = Work.latest.includes(:tags, :external_creatorships, :series, :language, :approved_collections, pseuds: [:user]).to_a
     end
     set_own_works
   end
