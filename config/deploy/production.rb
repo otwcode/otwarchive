67c16af9e33662ee0fba88ac637ef71e3b8e71cf
AO3 Unicorn service account <ao3app@template>
2019-11-19 17:23:16 +0000

    It was ao3-app14 that went to elastic

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -20,7 +20,7 @@ server "ao3-app07.ao3.org", :app
 server "ao3-front02.ao3.org", :web
 server "ao3-app09.ao3.org", :app
 server "ao3-app11.ao3.org", :app, :workers, :schedulers
-server "ao3-app14.ao3.org", :app
+server "ao3-app15.ao3.org", :app
 server "ao3-front04.ao3.org", :web
 server "ao3-app04.ao3.org", :app
 
