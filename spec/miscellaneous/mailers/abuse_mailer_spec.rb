7fe0deaa7201f46b643fc05a07fd1d2892f58be9
sarken <sarken@gmail.com>
2019-11-27 00:00:31 -0500

    AO3-5817 Update abuse_mailer_spec
    Use method name.
    Eliminate redundancy.
    Divide into HTML and text email versions.
    Check multipart email behavior.
    Don't use have_body_text.

--- spec/miscellaneous/mailers/abuse_mailer_spec.rb
+++ spec/miscellaneous/mailers/abuse_mailer_spec.rb
@@ -6,66 +6,55 @@ describe AdminMailer, type: :mailer do
     allow(Akismetor).to receive(:spam?).and_return(false)
   end
 
-  context "abuse_reports with email" do
+  describe "abuse_report" do
     let(:report) { create(:abuse_report) }
-    let(:mail) { AdminMailer.abuse_report(report.id) }
+    let(:email) { AdminMailer.abuse_report(report.id) }
+    let(:email2) { UserMailer.abuse_report(report.id) }
 
     it "has the correct subject" do
-      expect(mail).to have_subject "[#{ArchiveConfig.APP_SHORT_NAME}] Admin Abuse Report"
+      expect(email).to have_subject "[#{ArchiveConfig.APP_SHORT_NAME}] Admin Abuse Report"
     end
 
     it "delivers to the correct address" do
-      expect(mail).to deliver_to ArchiveConfig.ABUSE_ADDRESS
+      expect(email).to deliver_to ArchiveConfig.ABUSE_ADDRESS
     end
 
     it "delivers from the correct address" do
-      expect(mail).to deliver_from("Archive of Our Own <#{ArchiveConfig.RETURN_ADDRESS}>")
+      expect(email).to deliver_from("Archive of Our Own <#{ArchiveConfig.RETURN_ADDRESS}>")
     end
 
-    it "body text contains the comment" do
-      expect(mail).to have_body_text(/#{report.comment}/)
+    it "ccs the user who filed the report" do
+      expect(email2).to deliver_to(report.email)
     end
 
-    it "body text contains the email" do
-      expect(mail).to have_body_text(/#{report.email}/)
+    it_behaves_like "a multipart email"
+
+    describe "HTML version" do
+      it "contains the comment" do
+        expect(email).to have_html_part_content(report.comment)
+      end
+
+      it "contains the email address" do
+        expect(email).to have_html_part_content(report.email)
+      end
+
+      it "contains the url of the page with abuse" do
+        expect(email).to have_html_part_content(report.url)
+      end
     end
 
-    it "has the url of the page with abuse" do
-      expect(mail.html_part).to have_body_text(report.url)
-    end
+    describe "text version" do
+      it "contains the comment" do
+        expect(email).to have_text_part_content(report.comment)
+      end
 
+      it "contains the email address" do
+        expect(email).to have_text_part_content(report.email)
+      end
+
+      it "contains the url of the page with abuse" do
+        expect(email).to have_text_part_content(report.url)
+      end
+    end
   end
-
-  context "abuse_reports sends copy to submitter" do
-    let(:report) { create(:abuse_report, email: "cc_me@email.com") }
-    let(:mail) { AdminMailer.abuse_report(report.id) }
-    let(:mail2) { UserMailer.abuse_report(report.id) }
-
-
-   it "has the correct subject" do
-     expect(mail).to have_subject "[#{ArchiveConfig.APP_SHORT_NAME}] Admin Abuse Report"
-   end
-
-   it "delivers to the correct address" do
-     expect(mail).to deliver_to ArchiveConfig.ABUSE_ADDRESS
-   end
-
-   it "ccs the user who filed the report" do
-     expect(mail2).to deliver_to("cc_me@email.com")
-   end
-
-   it "delivers from the correct address" do
-     expect(mail).to deliver_from("Archive of Our Own <#{ArchiveConfig.RETURN_ADDRESS}>")
-   end
-
-   it "body text contains the comment" do
-     expect(mail).to have_body_text(/#{report.comment}/)
-   end
-
-   it "has the url of the page with abuse" do
-     expect(mail.html_part).to have_body_text(report.url)
-   end
-  end
-
-
 end
