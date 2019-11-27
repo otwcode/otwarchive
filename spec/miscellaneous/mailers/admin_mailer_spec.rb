30abfd94720367d216a3cdb959f1f2f8ccc19b71
sarken <sarken@gmail.com>
2019-11-27 01:34:58 -0500

    AO3-5817 Test all admin mailer methods in admin_mailer_spec

--- spec/miscellaneous/mailers/admin_mailer_spec.rb
+++ spec/miscellaneous/mailers/admin_mailer_spec.rb
@@ -135,4 +135,98 @@ describe AdminMailer, type: :mailer do
       end
     end
   end
+
+  describe "feedback" do
+    let(:feedback) { create(:feedback) }
+    let(:email) { AdminMailer.feedback(feedback.id) }
+
+    it "has the correct subject" do
+      expect(email).to have_subject("[#{ArchiveConfig.APP_SHORT_NAME}] Support - #{feedback.summary}")
+    end
+
+    it "delivers to the correct address" do
+      expect(email).to deliver_to(ArchiveConfig.FEEDBACK_ADDRESS)
+    end
+
+    it "delivers from the correct address" do
+      expect(email).to deliver_from(feedback.email)
+    end
+
+    it_behaves_like "a multipart email"
+
+    describe "HTML email" do
+      it "contains the comment" do
+        expect(email).to have_html_part_content(feedback.comment)
+      end
+
+      it "contains the summary" do
+        expect(email).to have_html_part_content(feedback.summary)
+      end
+    end
+
+    describe "text email" do
+      it "contains the comment" do
+        expect(email).to have_text_part_content(feedback.comment)
+      end
+
+      it "contains the summary" do
+        expect(email).to have_text_part_content(feedback.summary)
+      end
+    end
+  end
+
+  describe "abuse_report" do
+    let(:report) { create(:abuse_report) }
+    let(:email) { AdminMailer.abuse_report(report.id) }
+    let(:email2) { UserMailer.abuse_report(report.id) }
+
+    # Assume all of these reports pass the spam check
+    before(:each) do
+      allow(Akismetor).to receive(:spam?).and_return(false)
+    end
+
+    it "has the correct subject" do
+      expect(email).to have_subject "[#{ArchiveConfig.APP_SHORT_NAME}] Admin Abuse Report"
+    end
+
+    it "delivers to the correct address" do
+      expect(email).to deliver_to ArchiveConfig.ABUSE_ADDRESS
+    end
+
+    it "ccs the user who filed the report" do
+      expect(email2).to deliver_to(report.email)
+    end
+
+    it_behaves_like "an email with a valid sender"
+
+    it_behaves_like "a multipart email"
+
+    describe "HTML version" do
+      it "contains the comment" do
+        expect(email).to have_html_part_content(report.comment)
+      end
+
+      it "contains the email address" do
+        expect(email).to have_html_part_content(report.email)
+      end
+
+      it "contains the url of the page with abuse" do
+        expect(email).to have_html_part_content(report.url)
+      end
+    end
+
+    describe "text version" do
+      it "contains the comment" do
+        expect(email).to have_text_part_content(report.comment)
+      end
+
+      it "contains the email address" do
+        expect(email).to have_text_part_content(report.email)
+      end
+
+      it "contains the url of the page with abuse" do
+        expect(email).to have_text_part_content(report.url)
+      end
+    end
+  end
 end
