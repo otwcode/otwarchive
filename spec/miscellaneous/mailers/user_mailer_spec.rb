d662a4de66e9d0ae4a3429d422526cc62ba674b9
Morrgani <Morrgani@users.noreply.github.com>
2019-12-01 21:20:12 -0500

    AO3-5723 Update sign-up confirmation email

--- spec/miscellaneous/mailers/user_mailer_spec.rb
+++ spec/miscellaneous/mailers/user_mailer_spec.rb
@@ -285,4 +285,37 @@ describe UserMailer, type: :mailer do
       end
     end
   end
+
+  describe "signup_notification" do
+    let(:user) { create(:user) }
+
+    subject(:email) { UserMailer.signup_notification(user.id).deliver }
+
+    # Test the headers
+    it_behaves_like "an email with a valid sender"
+
+    it "has the correct subject line" do
+      subject = "[#{ArchiveConfig.APP_SHORT_NAME}] Confirmation"
+      expect(email).to have_subject(subject)
+    end
+
+    # Test both body contents
+    it_behaves_like "a multipart email"
+
+    it_behaves_like "a translated email"
+
+    describe "HTML version" do
+      it "has the correct content" do
+        expect(email).to have_html_part_content("Once your account is up and running, you can post your fanworks, set up email")
+        expect(email).to have_html_part_content("follow this link to activate your account")
+      end
+    end
+
+    describe "text version" do
+      it "has the correct content" do
+        expect(email).to have_html_part_content("Once your account is up and running, you can post your fanworks, set up email")
+        expect(email).to have_text_part_content("follow this link to activate your account")
+      end
+    end
+  end
 end
