2135c5d40c0217068e862921f386b25f908b1539
sarken <sarken@gmail.com>
2019-12-08 22:27:36 -0500

    Add coverage for bookmark index when use_caching? is true

--- spec/controllers/bookmarks_controller_spec.rb
+++ spec/controllers/bookmarks_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe BookmarksController do
+describe BookmarksController, bookmark_search: true do
   include LoginMacros
   include RedirectExpectationHelper
 
@@ -39,6 +39,84 @@ describe BookmarksController do
         fake_login_known_user(bookmark.pseud.user)
         get :edit, params: { id: bookmark.id, format: :js }, xhr: true
         expect(response).to render_template('bookmark_form_dynamic')
+      end
+    end
+  end
+
+  describe "index" do
+    let!(:external_work_bookmark) { create(:external_work_bookmark) }
+    let!(:series_bookmark) { create(:series_bookmark) }
+    let!(:work_bookmark) { create(:bookmark) }
+
+    it "returns search results when given bookmark_search parameters" do
+      params = { :bookmark_search => { bookmarkable_query: "restricted: false" } }
+      get :index, params: params
+      expect(assigns(:bookmarks)).to include(work_bookmark)
+    end
+
+    describe "without caching" do
+      before do
+        AdminSetting.first.update_attribute(:enable_test_caching, false)
+      end
+
+      it "returns work bookmarks" do
+        get :index
+        expect(assigns(:bookmarks)).to include(work_bookmark)
+      end
+
+      it "does not return external work bookmarks" do
+        get :index
+        expect(assigns(:bookmarks)).not_to include(external_work_bookmark)
+      end
+
+      it "does not return series bookmarks" do
+        get :index
+        expect(assigns(:bookmarks)).not_to include(series_bookmark)
+      end
+
+      it "returns the result with new bookmarks the second time" do
+        get :index
+        expect(assigns(:bookmarks)).to include(work_bookmark)
+        work_bookmark2 = create(:bookmark)
+        get :index
+        expect(assigns(:bookmarks)).to include(work_bookmark)
+        expect(assigns(:bookmarks)).to include(work_bookmark2)
+      end
+    end
+
+    describe "with caching" do
+      before do
+        AdminSetting.first.update_attribute(:enable_test_caching, true)
+        run_all_indexing_jobs
+      end
+
+      it "returns work bookmarks" do
+        get :index
+        expect(assigns(:bookmarks)).to include(work_bookmark)
+      end
+
+      it "returns external work bookmarks" do
+        get :index
+        expect(assigns(:bookmarks)).to include(external_work_bookmark)
+      end
+
+      it "returns series bookmarks" do
+        get :index
+        expect(assigns(:bookmarks)).to include(series_bookmark)
+      end
+
+      it "returns the same result the second time when a new bookmark is created within the expiration time" do
+        get :index
+        expect(assigns(:bookmarks)).to include(external_work_bookmark)
+        expect(assigns(:bookmarks)).to include(series_bookmark)
+        expect(assigns(:bookmarks)).to include(work_bookmark)
+        work_bookmark2 = create(:bookmark)
+        run_all_indexing_jobs
+        get :index
+        expect(assigns(:bookmarks)).to include(external_work_bookmark)
+        expect(assigns(:bookmarks)).to include(series_bookmark)
+        expect(assigns(:bookmarks)).to include(work_bookmark)
+        expect(assigns(:bookmarks)).not_to include(work_bookmark2)
       end
     end
   end
