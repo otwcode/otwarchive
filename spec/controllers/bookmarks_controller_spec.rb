a500ff13731370bb7a977aec6086b1d77982e766
sarken <sarken@gmail.com>
2019-12-08 23:35:16 -0500

    Better organization with contexts

--- spec/controllers/bookmarks_controller_spec.rb
+++ spec/controllers/bookmarks_controller_spec.rb
@@ -54,7 +54,7 @@ describe BookmarksController, bookmark_search: true do
       expect(assigns(:bookmarks)).to include(work_bookmark)
     end
 
-    context "when given chapter_id parameters" do
+    context "with chapter_id parameters" do
       it "loads the work as the bookmarkable" do
         params = { chapter_id: work_bookmark.bookmarkable.chapters.first.id }
         get :index, params: params
@@ -62,7 +62,7 @@ describe BookmarksController, bookmark_search: true do
       end
     end
 
-    context "when given external_work_id parameters" do
+    context "with external_work_id parameters" do
       it "loads the external work as the bookmarkable" do
         params = { external_work_id: external_work_bookmark.bookmarkable.id }
         get :index, params: params
@@ -71,52 +71,64 @@ describe BookmarksController, bookmark_search: true do
     end
 
     context "with user_id parameters" do
-      it "raises an error if user_id cannot be found" do
-        params = { user_id: "nobody" }
-        expect { get :index, params: params }.to raise_error(
-          ActiveRecord::RecordNotFound,
-          "Couldn't find user named 'nobody'"
-        )
-      end
-      
-      context "with valid user_id and invalid pseud_id parameters" do
-        it "raises an error if pseud_id cannot be found" do
-          params = { user_id: work_bookmark.pseud.user, pseud_id: "nobody" }
+      context "when user_id does not exist" do
+        it "raises a RecordNotFound error" do
+          params = { user_id: "nobody" }
           expect { get :index, params: params }.to raise_error(
             ActiveRecord::RecordNotFound,
-            "Couldn't find pseud named 'nobody'"
+            "Couldn't find user named 'nobody'"
           )
+        end
+      end
+
+      context "when user_id exists" do
+        context "when pseud_id does not exist" do
+          it "raises a RecordNotFound error for the pseud" do
+            params = { user_id: work_bookmark.pseud.user, pseud_id: "nobody" }
+            expect { get :index, params: params }.to raise_error(
+              ActiveRecord::RecordNotFound,
+              "Couldn't find pseud named 'nobody'"
+            )
+          end
         end
       end
     end
 
-    context "when given tag_id parameters" do
+    context "with tag_id parameters" do
       let(:tag) { create(:fandom) }
       let(:merger) { create(:fandom, merger_id: canonical.id) }
       let(:canonical) { create(:canonical_fandom) } 
 
-      it "raises an error if tag_id cannot be found" do
-        params = { tag_id: "nothingness" }
-        expect { get :index, params: params }.to raise_error(
-          ActiveRecord::RecordNotFound,
-          "Couldn't find tag named 'nothingness'"
-        )
+      context "when tag_id does not exist" do
+        it "raises a RecordNotFound error" do
+          params = { tag_id: "nothingness" }
+          expect { get :index, params: params }.to raise_error(
+            ActiveRecord::RecordNotFound,
+            "Couldn't find tag named 'nothingness'"
+          )
+        end
       end
 
-      it "redirects to canonical tag's bookmarks if tag_id is a synonym" do
-        params = { tag_id: merger }
-        get :index, params: params
-        expect(response).to redirect_to(tag_bookmarks_path(canonical))
-      end
+      context "when tag_id is not canonical" do
+        context "when tag_id is a synonym" do
+          it "redirects to the canonical's tag_bookmarks_path" do
+            params = { tag_id: merger }
+            get :index, params: params
+            expect(response).to redirect_to(tag_bookmarks_path(canonical))
+          end
+        end
 
-      it "redirects to tag page if tag_id is neither a canonical nor a synonym" do
-        params = { tag_id: tag }
-        get :index, params: params
-        expect(response).to redirect_to(tag_path(tag))
+        context "when tag_id is not a synonym" do
+          it "redirects to tag_path" do
+            params = { tag_id: tag }
+            get :index, params: params
+            expect(response).to redirect_to(tag_path(tag))
+          end
+        end
       end
     end
 
-    describe "without caching" do
+    context "without caching" do
       before do
         AdminSetting.first.update_attribute(:enable_test_caching, false)
       end
@@ -146,7 +158,7 @@ describe BookmarksController, bookmark_search: true do
       end
     end
 
-    describe "with caching" do
+    context "with caching" do
       before do
         AdminSetting.first.update_attribute(:enable_test_caching, true)
         run_all_indexing_jobs
