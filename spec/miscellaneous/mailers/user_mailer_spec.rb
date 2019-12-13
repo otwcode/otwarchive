15967e918fd5444a10c006c1d8466f32cbbc5f56
sarken <sarken@gmail.com>
2019-12-12 21:52:12 -0500

    AO3-5813 Remove HTML for mailers from locale file (#3709)
    * AO3-5813 Remove html sent_by key from locale file
    
    This key is only used by a mailer that is no longer used. Ergo
    the mailer and its code should go bye-bye too. Admin notification
    feature was removed in AO3-5043.
    
    * AO3-5813 Remove HTML from footer mailer text in locale
    
    * AO3-5813 Don't include italics in kudo mailer
    
    We reuse the italic bold link style a lot for titles in emails.
    Let's make it a helper.
    
    * AO3-5813 Remove HTML for invite_increase_notification from mailer locale
    
    * AO3-5813 Improve handling of different numbers in invite_increase_notification
    
    Also capitalize Archive because it made Priscilla and me twitch
    
    * AO3-5813 Update specs to be consistent with other reworked mailer specs
    
    * AO3-5813 Spacing fixes in spec files
    
    * AO3-5813 Prefer double quotes
    
    * AO3-5813 Update the text we check for in the invite_increase_notification tests
    
    We don't want any false successes, so let's check for 'invitations, which'
    when there is >1 invitation, and 'invitation, which' when there is only 1.
    
    * AO3-5813 Use new_feedback_report_url in text footer too
    
    * AO3-5813 Make translatable app_short_name for signatures
    
    * AO3-5813 Prefer double quotes in invite_request_declined mailer

--- spec/miscellaneous/mailers/user_mailer_spec.rb
+++ spec/miscellaneous/mailers/user_mailer_spec.rb
@@ -285,4 +285,68 @@ describe UserMailer, type: :mailer do
       end
     end
   end
+
+  describe "invite_increase_notification" do
+    let!(:user) { create(:user) }
+
+    context "when 1 invitation is issued" do
+      let(:count) { 1 }
+
+      subject(:email) { UserMailer.invite_increase_notification(user.id, count).deliver }
+
+      # Test the headers
+      it_behaves_like "an email with a valid sender"
+
+      it "has the correct subject line" do
+        expect(email.subject).to eq("[#{ArchiveConfig.APP_SHORT_NAME}] New Invitations")
+      end
+
+      # Test both body contents
+      it_behaves_like "a multipart email"
+
+      it_behaves_like "a translated email"
+
+      describe "HTML version" do
+        it "has the correct content" do
+          expect(email).to have_html_part_content("you have #{count} new invitation, which")
+        end
+      end
+
+      describe "text version" do
+        it "has the correct content" do
+          expect(email).to have_text_part_content("you have #{count} new invitation, which")
+        end
+      end
+    end
+
+    context "when multiple invitations are issued" do
+      let(:count) { 5 }
+
+      subject(:email) { UserMailer.invite_increase_notification(user.id, count).deliver }
+
+      # Test the headers
+      it_behaves_like "an email with a valid sender"
+
+      it "has the correct subject line" do
+        expect(email.subject).to eq("[#{ArchiveConfig.APP_SHORT_NAME}] New Invitations")
+      end
+
+      # Test both body contents
+      it_behaves_like "a multipart email"
+
+      it_behaves_like "a translated email"
+
+      describe "HTML version" do
+        it "has the correct content" do
+          expect(email).to have_html_part_content("you have #{count} new invitations, which")
+        end
+      end
+
+      describe "text version" do
+        it "has the correct content" do
+          expect(email).to have_text_part_content("you have #{count} new invitations, which")
+        end
+      end
+    end
+  end
 end
