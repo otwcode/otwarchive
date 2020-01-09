8506d487cfd11d33d78f5c276c5da5d81f9ae6e9
sarken <sarken@gmail.com>
2020-01-08 19:47:04 -0500

    Revert "AO3-5759 Add Lockup to password protect staging (#3710)" (#3717)
    This reverts commit 1e63946d26dabb52084fcda4d811c4f9ba0fc40c.

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 # If you have a Gemfile, require the gems listed there, including any gems
-# you've limited to :test, :development, :staging, or :production.
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Otwarchive
