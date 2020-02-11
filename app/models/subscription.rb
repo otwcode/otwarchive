81d228713c5ff90ceaf64ff262fa9d01e6113799
sarken <sarken@gmail.com>
2020-02-10 22:53:18 -0500

    AO3-5040 Use &: syntax when mapping pseud bylines in Subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -41,7 +41,7 @@ class Subscription < ApplicationRecord
     authors = if creation.is_a?(Work) && creation.anonymous? || creation.is_a?(Chapter) && creation.work.anonymous?
                 "Anonymous"
               else
-                creation.pseuds.map{ |p| p.byline }.to_sentence
+                creation.pseuds.map(&:byline).to_sentence
               end
     chapter_text = creation.is_a?(Chapter) ? "#{creation.chapter_header} of " : ""
     work_title = creation.is_a?(Chapter) ? creation.work.title : creation.title
