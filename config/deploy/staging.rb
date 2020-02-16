7569c1e76951762191494b110a406e91a859d9a4
zz9pzza <zz9pzza@gmail.com>
2020-02-16 16:39:59 +0000

    Actually get the right names this time

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -8,10 +8,10 @@
 # Calling "cap deploy:migrations" inserts the task "deploy:migrate" before deploy:symlink
 require 'capistrano/gitflow_version'
 
-server "test-app10.transformativeworks.org", :app, :db, :schedulers
-server "test-app11.transformativeworks.org", :app, :schedulers
-server "test-app12.transformativeworks.org", :app, :workers, :schedulers, primary: true
-server "test-front10.transformativeworks.org", :web
+server "test-app13", :app, :db, :schedulers
+server "test-app14", :app, :schedulers
+server "test-app15", :app, :workers, :schedulers, primary: true
+server "test-front11", :web
 
 set :rails_env, 'staging'
 
