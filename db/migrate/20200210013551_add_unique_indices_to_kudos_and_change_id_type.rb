92e838a422e68580ecbde9255cc704b939f693c3
tickinginstant <tickinginstant@users.noreply.github.com>
2020-02-21 19:38:26 -0500

    AO3-5869 Clearer indentation.

--- db/migrate/20200210013551_add_unique_indices_to_kudos_and_change_id_type.rb
+++ db/migrate/20200210013551_add_unique_indices_to_kudos_and_change_id_type.rb
@@ -3,30 +3,30 @@ class AddUniqueIndicesToKudosAndChangeIdType < ActiveRecord::Migration[5.1]
     if Rails.env.staging? || Rails.env.production?
       database = Kudo.connection.current_database
 
-      puts <<-PTOSC
-Schema Change Command:
+      puts <<~PTOSC
+        Schema Change Command:
 
-pt-online-schema-change D=#{database},t=kudos \\
-  --alter "ADD UNIQUE INDEX index_kudos_on_commentable_and_user (commentable_id, commentable_type, user_id),
-           ADD UNIQUE INDEX index_kudos_on_commentable_and_ip_address (commentable_id, commentable_type, ip_address),
-           CHANGE COLUMN id id bigint NOT NULL AUTO_INCREMENT" \\
-  --no-swap-tables --no-drop-new-table --no-drop-triggers --no-check-unique-key-change \\
-  -uroot --chunk-size=10k --max-flow-ctl 0 --pause-file /tmp/pauseme \\
-  --max-load Threads_running=25 --critical-load Threads_running=400 \\
-  --set-vars innodb_lock_wait_timeout=2 --alter-foreign-keys-method=auto \\
-  --execute
+        pt-online-schema-change D=#{database},t=kudos \\
+          --alter "ADD UNIQUE INDEX index_kudos_on_commentable_and_user (commentable_id, commentable_type, user_id),
+                   ADD UNIQUE INDEX index_kudos_on_commentable_and_ip_address (commentable_id, commentable_type, ip_address),
+                   CHANGE COLUMN id id bigint NOT NULL AUTO_INCREMENT" \\
+          --no-swap-tables --no-drop-new-table --no-drop-triggers --no-check-unique-key-change \\
+          -uroot --chunk-size=10k --max-flow-ctl 0 --pause-file /tmp/pauseme \\
+          --max-load Threads_running=25 --critical-load Threads_running=400 \\
+          --set-vars innodb_lock_wait_timeout=2 --alter-foreign-keys-method=auto \\
+          --execute
 
-Table Swap and Cleanup Commands:
+        Table Swap and Cleanup Commands:
 
-RENAME TABLE
-  `#{database}`.`kudos` TO `#{database}`.`_kudos_old`,
-  `#{database}`.`_kudos_new` TO `#{database}`.`kudos`;
+        RENAME TABLE
+          `#{database}`.`kudos` TO `#{database}`.`_kudos_old`,
+          `#{database}`.`_kudos_new` TO `#{database}`.`kudos`;
 
-DROP TRIGGER IF EXISTS `#{database}`.`pt_osc_#{database}_kudos_del`;
-DROP TRIGGER IF EXISTS `#{database}`.`pt_osc_#{database}_kudos_ins`;
-DROP TRIGGER IF EXISTS `#{database}`.`pt_osc_#{database}_kudos_upd`;
+        DROP TRIGGER IF EXISTS `#{database}`.`pt_osc_#{database}_kudos_del`;
+        DROP TRIGGER IF EXISTS `#{database}`.`pt_osc_#{database}_kudos_ins`;
+        DROP TRIGGER IF EXISTS `#{database}`.`pt_osc_#{database}_kudos_upd`;
 
-DROP TABLE IF EXISTS `#{database}`.`_kudos_old`;
+        DROP TABLE IF EXISTS `#{database}`.`_kudos_old`;
       PTOSC
     else
       add_index :kudos, [:commentable_id, :commentable_type, :user_id],
@@ -42,30 +42,30 @@ DROP TABLE IF EXISTS `#{database}`.`_kudos_old`;
     if Rails.env.staging? || Rails.env.production?
       database = Kudo.connection.current_database
 
-      puts <<-PTOSC
-Schema Change Command:
+      puts <<~PTOSC
+        Schema Change Command:
 
-pt-online-schema-change D=#{database},t=kudos \\
-  --alter "DROP INDEX index_kudos_on_commentable_and_user,
-           DROP INDEX index_kudos_on_commentable_and_ip_address,
-           CHANGE COLUMN id id int NOT NULL AUTO_INCREMENT" \\
-  --no-swap-tables --no-drop-new-table --no-drop-triggers \\
-  -uroot --chunk-size=10k --max-flow-ctl 0 --pause-file /tmp/pauseme \\
-  --max-load Threads_running=25 --critical-load Threads_running=400 \\
-  --set-vars innodb_lock_wait_timeout=2 --alter-foreign-keys-method=auto \\
-  --execute
+        pt-online-schema-change D=#{database},t=kudos \\
+          --alter "DROP INDEX index_kudos_on_commentable_and_user,
+                   DROP INDEX index_kudos_on_commentable_and_ip_address,
+                   CHANGE COLUMN id id int NOT NULL AUTO_INCREMENT" \\
+          --no-swap-tables --no-drop-new-table --no-drop-triggers \\
+          -uroot --chunk-size=10k --max-flow-ctl 0 --pause-file /tmp/pauseme \\
+          --max-load Threads_running=25 --critical-load Threads_running=400 \\
+          --set-vars innodb_lock_wait_timeout=2 --alter-foreign-keys-method=auto \\
+          --execute
 
-Table Swap and Cleanup Commands:
+        Table Swap and Cleanup Commands:
 
-RENAME TABLE
-  `#{database}`.`kudos` TO `#{database}`.`_kudos_old`,
-  `#{database}`.`_kudos_new` TO `#{database}`.`kudos`;
+        RENAME TABLE
+          `#{database}`.`kudos` TO `#{database}`.`_kudos_old`,
+          `#{database}`.`_kudos_new` TO `#{database}`.`kudos`;
 
-DROP TRIGGER IF EXISTS `#{database}`.`pt_osc_#{database}_kudos_del`;
-DROP TRIGGER IF EXISTS `#{database}`.`pt_osc_#{database}_kudos_ins`;
-DROP TRIGGER IF EXISTS `#{database}`.`pt_osc_#{database}_kudos_upd`;
+        DROP TRIGGER IF EXISTS `#{database}`.`pt_osc_#{database}_kudos_del`;
+        DROP TRIGGER IF EXISTS `#{database}`.`pt_osc_#{database}_kudos_ins`;
+        DROP TRIGGER IF EXISTS `#{database}`.`pt_osc_#{database}_kudos_upd`;
 
-DROP TABLE IF EXISTS `#{database}`.`_kudos_old`;
+        DROP TABLE IF EXISTS `#{database}`.`_kudos_old`;
       PTOSC
     else
       remove_index :kudos, name: "index_kudos_on_commentable_and_user"
