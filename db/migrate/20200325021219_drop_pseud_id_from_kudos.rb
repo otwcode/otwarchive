d6fa1044859d6bab4817c9410ec4a5887b248649
sarken <sarken@gmail.com>
2020-03-24 22:30:59 -0400

    AO3-5887 Add migration to drop pseud_id from kudos table

---
+++ db/migrate/20200325021219_drop_pseud_id_from_kudos.rb
@@ -0,0 +1,65 @@
+class DropPseudIdFromKudos < ActiveRecord::Migration[5.1]
+  def up
+    if Rails.env.staging? || Rails.env.production?
+      database = Kudo.connection.current_database
+
+      puts <<~PTOSC
+        Schema Change Command:
+
+        pt-online-schema-change D=#{database},t=kudos \\
+          --alter "DROP COLUMN pseud_id" \\
+          --no-swap-tables --no-drop-new-table --no-drop-triggers \\
+          -uroot --ask-pass --chunk-size=10k --max-flow-ctl 0 --pause-file /tmp/pauseme \\
+          --max-load Threads_running=25 --critical-load Threads_running=400 \\
+          --set-vars innodb_lock_wait_timeout=2 --alter-foreign-keys-method=auto \\
+          --execute
+
+        Table Swap and Cleanup Commands:
+
+        RENAME TABLE
+          `#{database}`.`kudos` TO `#{database}`.`_kudos_old`,
+          `#{database}`.`_kudos_new` TO `#{database}`.`kudos`;
+
+        DROP TRIGGER IF EXISTS `#{database}`.`pt_osc_#{database}_kudos_del`;
+        DROP TRIGGER IF EXISTS `#{database}`.`pt_osc_#{database}_kudos_ins`;
+        DROP TRIGGER IF EXISTS `#{database}`.`pt_osc_#{database}_kudos_upd`;
+
+        DROP TABLE IF EXISTS `#{database}`.`_kudos_old`;
+      PTOSC
+    else
+      remove_column :kudos, :pseud_id
+    end
+  end
+
+  def down
+    if Rails.env.staging? || Rails.env.production?
+      database = Kudo.connection.current_database
+
+      puts <<~PTOSC
+        Schema Change Command:
+
+        pt-online-schema-change D=#{database},t=kudos \\
+          --alter "ADD COLUMN pseud_id INT" \\
+          --no-swap-tables --no-drop-new-table --no-drop-triggers \\
+          -uroot --ask-pass --chunk-size=10k --max-flow-ctl 0 --pause-file /tmp/pauseme \\
+          --max-load Threads_running=25 --critical-load Threads_running=400 \\
+          --set-vars innodb_lock_wait_timeout=2 --alter-foreign-keys-method=auto \\
+          --execute
+
+        Table Swap and Cleanup Commands:
+
+        RENAME TABLE
+          `#{database}`.`kudos` TO `#{database}`.`_kudos_old`,
+          `#{database}`.`_kudos_new` TO `#{database}`.`kudos`;
+
+        DROP TRIGGER IF EXISTS `#{database}`.`pt_osc_#{database}_kudos_del`;
+        DROP TRIGGER IF EXISTS `#{database}`.`pt_osc_#{database}_kudos_ins`;
+        DROP TRIGGER IF EXISTS `#{database}`.`pt_osc_#{database}_kudos_upd`;
+
+        DROP TABLE IF EXISTS `#{database}`.`_kudos_old`;
+      PTOSC
+    else
+      add_column :kudos, :pseud_id, :integer
+    end
+  end
+end
