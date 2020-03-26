d289dd4ac62e43d1426566d69d7afe0ebb85b2d9
redsummernight <redsummernight@users.noreply.github.com>
2020-03-25 20:59:27 -0400

    AO3-5757 Regenerate env.rb and move custom code
    - Run rails generate cucumber:install and reset env.rb to its auto-generated
      form.
    
    - Simplify DatabaseCleaner settings.
    
      Non-JS tests use :transaction, this hasn't changed.
    
      JS tests (Cucumber::Rails::Database.javascript_strategy) previously used
      :transaction for no documented reason. Switch to :truncation as recommended
      by cucumber-rails.
    
    - Move custom code out of env.rb: Before/After hooks and requires to hooks.rb,
      Capybara settings to capybara.rb. The coverage setup stays in env.rb, because
      it needs to happen before everything else.

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,4 +1,21 @@
+require 'cucumber/rspec/doubles'
+require 'cucumber/timecop'
+require 'email_spec/cucumber'
+
 Before do
+  settings = AdminSetting.new(invite_from_queue_enabled: ArchiveConfig.INVITE_FROM_QUEUE_ENABLED,
+          invite_from_queue_number: ArchiveConfig.INVITE_FROM_QUEUE_NUMBER,
+          invite_from_queue_frequency: ArchiveConfig.INVITE_FROM_QUEUE_FREQUENCY,
+          account_creation_enabled: ArchiveConfig.ACCOUNT_CREATION_ENABLED,
+          days_to_purge_unactivated: ArchiveConfig.DAYS_TO_PURGE_UNACTIVATED)
+  settings.save(validate: false)
+
+  language = Language.find_or_create_by(short: "en", name: "English")
+  Locale.set_base_locale(iso: "en", name: "English (US)", language_id: language.id)
+
+  # Assume all spam checks pass by default.
+  allow(Akismetor).to receive(:spam?).and_return(false)
+
   # Reset the current user:
   User.current_user = nil
 
@@ -17,4 +34,17 @@ end
 
 After do
   Indexer.all.map(&:delete_index)
+end
+
+@javascript = false
+Before "@javascript" do
+  @javascript = true
+end
+
+Before "@disable_caching" do
+  ActionController::Base.perform_caching = false
+end
+
+After "@disable_caching" do
+  ActionController::Base.perform_caching = true
 end
