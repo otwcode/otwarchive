e8300ed4f82b957a131b46dde33e016a6809a8c7
tickinginstant <tickinginstant@users.noreply.github.com>
2020-04-05 16:11:56 -0400

    AO3-5905 Use JSON for requests.

--- app/controllers/hit_count_controller.rb
+++ app/controllers/hit_count_controller.rb
@@ -1,5 +1,7 @@
 class HitCountController < ApplicationController
   def create
+    raise NotFound unless request.format == "json"
+
     if ENV['REQUEST_FROM_BOT']
       render json: "failure"
     else
