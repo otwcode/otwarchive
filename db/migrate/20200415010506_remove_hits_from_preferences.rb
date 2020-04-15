37d61f39cbb96b24c8aa7ecbee2a1d94d70d3691
sarken <sarken@gmail.com>
2020-04-14 22:46:11 -0400

    AO3-4622 Make migration reversible

--- db/migrate/20200415010506_remove_hits_from_preferences.rb
+++ db/migrate/20200415010506_remove_hits_from_preferences.rb
@@ -1,7 +1,7 @@
 class RemoveHitsFromPreferences < ActiveRecord::Migration[5.1]
   def change
-    remove_column :preferences, :hide_all_hit_counts
-    remove_column :preferences, :hide_private_hit_count
-    remove_column :preferences, :hide_public_hit_count
+    remove_column :preferences, :hide_all_hit_counts, :boolean, default: false, null: false
+    remove_column :preferences, :hide_private_hit_count, :boolean, default: false, null: false
+    remove_column :preferences, :hide_public_hit_count, :boolean, default: false, null: false
   end
 end
