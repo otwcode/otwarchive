d595b61a7f6cc4b1d841179314b8af3400d1b2cd
J <kuzmanovajana@gmail.com>
2020-04-21 11:57:24 +0200

    AO3-5266 added ning.com and vidders.net to https list and code to ensure https in flashvars and updated tests.

--- spec/lib/html_cleaner_spec.rb
+++ spec/lib/html_cleaner_spec.rb
@@ -154,8 +154,8 @@ describe HtmlCleaner do
         end
 
         %w{youtube.com youtube-nocookie.com vimeo.com player.vimeo.com
-           archiveofourown.org archive.org dailymotion.com 8tracks.com podfic.com
-           open.spotify.com spotify.com w.soundcloud.com soundcloud.com viddertube.com}.each do |source|
+           archiveofourown.org archive.org dailymotion.com 8tracks.com static.ning.com ning.com podfic.com
+           open.spotify.com spotify.com w.soundcloud.com soundcloud.com vidders.net viddertube.com}.each do |source|
 
           it "converts src to https for #{source}" do
             html = '<iframe width="560" height="315" src="http://' + source + '/embed/123" frameborder="0"></iframe>'
@@ -182,7 +182,7 @@ describe HtmlCleaner do
           expect(result).to be_empty
         end
 
-        %w(metacafe.com vidders.net criticalcommons.org static.ning.com ning.com).each do |source|
+        %w(metacafe.com criticalcommons.org).each do |source|
           it "doesn't convert src to https for #{source}" do
             html = '<iframe width="560" height="315" src="http://' + source + '/embed/123" frameborder="0"></iframe>'
             result = sanitize_value(field, html)
