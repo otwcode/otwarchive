e38220c356a6473a6f5e958f69180b9c99e058d0
jeff <jeffreyckingswood@gmail.com>
2020-05-13 10:38:47 -0400

    AO3-5884 Adds invitation test
    AO3-5884 Adds invitation tests for all controller methods
    
    AO3-5884 Adding tests for features
    
    AO3-5884 Adds tests for invitations find method

--- spec/controllers/invitations_controller_spec.rb
+++ spec/controllers/invitations_controller_spec.rb
@@ -13,7 +13,7 @@ describe InvitationsController do
         admin.update(roles: [])
         fake_login_admin(admin)
         get :index, params: { user_id: user.login }
-        it_redirects_to_with_error(root_url, "Sorry, only an authorized admin can access the page you were trying to reach.")
+        it_redirects_to_with_error(new_user_session_path, "Sorry, you don't have permission to access the page you were trying to reach. Please log in.")
       end
     end
 
@@ -33,7 +33,7 @@ describe InvitationsController do
         admin.update(roles: [])
         fake_login_admin(admin)
         get :manage, params: { user_id: user.login }
-        it_redirects_to_with_error(root_url, "Sorry, only an authorized admin can access the page you were trying to reach.")
+        it_redirects_to_with_error(new_user_session_path, "Sorry, you don't have permission to access the page you were trying to reach. Please log in.")
       end
     end
 
@@ -51,6 +51,7 @@ describe InvitationsController do
     let(:invitation) { create(:invitation) }
 
     it "allows admins to access show page" do
+      admin.update(roles: ['policy_and_abuse'])
       fake_login_admin(admin)
       get :show, params: { id: invitation.id }
       expect(response).to render_template("show")
@@ -60,25 +61,78 @@ describe InvitationsController do
   describe "GET #invite_friend" do
     let(:invitation) { create(:invitation) }
 
-    # it "does not send invite if email is missing" do
-    #   fake_login_admin(admin)
-    #   # invite_friend_user_invitations
-    #   post :invite_friend, params: { id: invitation.id, invitation: { invitee_email: nil, number_of_invites: 1 }}
-    #   it_redirects_to_with_error(invitation_path(invitation.id), "Please enter an email address.")
-    # end
+    it "does not send invite if email is missing" do
+      admin.update(roles: ['policy_and_abuse'])
+      fake_login_admin(admin)
+      post :invite_friend, params: { user_id: user.id, id: invitation.id, invitation: { invitee_email: nil, number_of_invites: 1 }}
+      expect(response).to render_template("show")
+      expect(flash[:error]).to match('Please enter an email address.')
+    end
 
-    # it "sends invite" do
-    #   fake_login_admin(admin)
-    #   # invite_friend_user_invitations
-    #   post :invite_friend, params: { id: invitation.id, invitation: { invitee_email: user.email, number_of_invites: 1 }}
-    #   expect(response).to render_template("show")
-    #   it_redirects_to_with_notice(user_invitation_path(user, invitation), "Invitation was successfully sent.")
-    # end
+    it "sends invite" do
+      admin.update(roles: ['policy_and_abuse'])
+      fake_login_admin(admin)
+      post :invite_friend, params: { user_id: user.id, id: invitation.id, invitation: { invitee_email: user.email, number_of_invites: 1 }}
+      it_redirects_to_with_notice(invitation_path(invitation), "Invitation was successfully sent.")
+    end
   end
 
-  # describe "GET #create" do
+  describe "POST #create" do
+    let(:invitee) { create(:user) }
 
-  # describe "GET #update" do
+    it "does not allow non-admins to create" do
+      fake_login
+      post :create, params: { user_id: invitee.login, invitation: { invitee_email: invitee.email, number_of_invites: 1 }}
 
-  # describe "GET #destroy" do
+      it_redirects_to_with_error(
+        user_path(@current_user),
+        "Sorry, you don't have permission to access the page you were trying to reach."
+      )
+    end
+
+    it "creates invitations" do
+      admin.update(roles: ['policy_and_abuse'])
+      fake_login_admin(admin)
+
+      post :create, params: { user_id: invitee.login, invitation: { invitee_email: invitee.email, number_of_invites: 1 }}
+      it_redirects_to_with_notice(user_invitations_path(invitee), "Invitations were successfully created.")
+    end
+  end
+
+  describe "PUT #update" do
+    it "updates and resends invitations" do
+      invitee = create(:user)
+      invitation = create(:invitation)
+      admin.update(roles: ['policy_and_abuse'])
+      fake_login_admin(admin)
+
+      put :update, params: { id: invitation.id, invitation: { invitee_email: invitee.email }}
+      it_redirects_to_with_notice(
+        find_admin_invitations_path("invitation[token]" => invitation.token),
+        'Invitation was successfully sent.'
+      )
+    end
+  end
+
+  describe "DELETE #destroy" do
+    it "does not allow non-admins to destroy" do
+      invitation = create(:invitation)
+      fake_login
+      post :destroy, params: { id: invitation.id }
+
+      it_redirects_to_with_error(
+        user_path(@current_user),
+        "Sorry, you don't have permission to access the page you were trying to reach."
+      )
+    end
+
+    it "deletes invitations" do
+      invitation = create(:invitation)
+      admin.update(roles: ['policy_and_abuse'])
+      fake_login_admin(admin)
+
+      post :destroy, params: { id: invitation.id }
+      it_redirects_to_with_notice(admin_invitations_path, "Invitation successfully destroyed")
+    end
+  end
 end
