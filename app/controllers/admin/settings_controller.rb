b348bfa22188521137cee3e649fe1d584b26a197
jeff <jeffreyckingswood@gmail.com>
2020-05-13 15:16:36 -0400

    AO3-5883 Updates settings page to fit requirements
    AO3-5883 Adds tests for settings controller
    
    AO3-5883 changes the way errors are rendered in the setting update method
    
    AO3-5883 ixes settings controller tests

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -1,5 +1,4 @@
 class Admin::SettingsController < Admin::BaseController
-
   def index
     @admin_setting = AdminSetting.first || AdminSetting.create(last_updated_by: Admin.first)
     authorize @admin_setting
@@ -10,18 +9,27 @@ class Admin::SettingsController < Admin::BaseController
   def update
     @admin_setting = AdminSetting.first || AdminSetting.create(last_updated_by: Admin.first)
     authorize @admin_setting
-    if @admin_setting.update(admin_setting_params)
+    authorized_params = policy(@admin_setting).verify_permitted_params(admin_setting_params)
+
+    if authorized_params == true && @admin_setting.update(admin_setting_params)
       flash[:notice] = ts("Archive settings were successfully updated.")
       redirect_to admin_settings_path
     else
+      flash[:error] = authorized_params unless authorized_params == true 
       render action: "index"
     end
   end
 
   private
+
   def admin_setting_params
     params.require(:admin_setting).permit(
-      policy(@admin_setting).permitted_attributes
+      :account_creation_enabled, :invite_from_queue_enabled, :invite_from_queue_number,
+      :invite_from_queue_frequency, :days_to_purge_unactivated,
+      :invite_from_queue_at, :suspend_filter_counts, :suspend_filter_counts_at,
+      :enable_test_caching, :cache_expiration, :tag_wrangling_off,
+      :request_invite_enabled, :creation_requires_invite, :downloads_enabled,
+      :hide_spam, :disable_support_form, :disabled_support_form_text
     ).merge(last_updated_by: current_admin.id)
   end
 end
--- app/policies/admin_setting_policy.rb
+++ app/policies/admin_setting_policy.rb
@@ -1,17 +1,26 @@
 class AdminSettingPolicy < ApplicationPolicy
-  SETTINGS_ROLES = %w(superadmin tag_wrangling).freeze
+  SETTINGS_ROLES = %w(superadmin support tag_wrangling policy_and_abuse).freeze
 
-  def index?
-    update?
+  def self.user_has_correct_role?(user, role)
+    self.new(user, nil).user_has_correct_role?(role)
   end
 
-  def update?
+  def user_has_correct_role?(role)
+    user_has_roles?([role]) || user.roles.include?('superadmin')
+  end
+
+  def self.can_update_settings?(user)
+    self.new(user, nil).can_update_settings?
+  end
+
+  def can_update_settings?
     user_has_roles?(SETTINGS_ROLES)
   end
 
   def permitted_attributes
+    permitted = []
     if user.roles.include?('superadmin')
-      [
+      permitted = [
         :account_creation_enabled, :invite_from_queue_enabled, :invite_from_queue_number,
         :invite_from_queue_frequency, :days_to_purge_unactivated,
         :invite_from_queue_at, :suspend_filter_counts, :suspend_filter_counts_at,
@@ -19,8 +28,27 @@ class AdminSettingPolicy < ApplicationPolicy
         :request_invite_enabled, :creation_requires_invite, :downloads_enabled,
         :hide_spam, :disable_support_form, :disabled_support_form_text
       ]
-    elsif user.roles.include?('tag_wrangling')
-      [:tag_wrangling_off]
+    else
+      permitted = permitted + [:tag_wrangling_off] if user.roles.include?('tag_wrangling')
+      permitted = permitted + [:disable_support_form, :disabled_support_form_text] if user.roles.include?('support')
+      permitted = permitted + [:hide_spam] if user.roles.include?('policy_and_abuse')
+    end
+
+    permitted
+  end
+
+  def verify_permitted_params(setting_params)
+    setting_params = setting_params.keys.map(&:to_sym) - [:last_updated_by]
+    unauthorized_params = setting_params - permitted_attributes
+
+    if unauthorized_params.any?
+      extra_params = unauthorized_params.map{ |p| p.to_s.humanize }.join(", ")
+      "You are not permitted to change the following settings: #{extra_params}"
+    else
+      true
     end
   end
+
+  alias_method :index?, :can_update_settings?
+  alias_method :update?, :can_update_settings?
 end
