aa9c3ed0fbf3e2b17bce22c866c636b36f8df703
Stephen Burrows <stephen.r.burrows@gmail.com>
2020-06-03 17:46:23 -0700

    Switched to custom step for detecting twitter share button

--- features/step_definitions/work_steps.rb
+++ features/step_definitions/work_steps.rb
@@ -706,3 +706,12 @@ Then /^the share modal should contain ("|')(.*?)\1$/ do |quote, text|
   )
   step %{I should see #{quote}#{text}#{quote} within "#share"}
 end
+
+Then /^the share modal should contain a Twitter share button$/ do
+  with_scope('#share') do
+    iframe = find('li.twitter #twitter-widget-0')
+    within_frame(iframe) do
+      page.should have_content("Tweet")
+    end
+  end
+end
