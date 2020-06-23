ace5c341e7c894f21b1789406ae811f0b7a885cd
jeff <jeffreyckingswood@gmail.com>
2020-06-23 09:27:36 -0400

    Review change requests

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -1,23 +1,23 @@
 class Admin::SettingsController < Admin::BaseController
+  before_action :find_admin_setting
+
   def index
-    @admin_setting = AdminSetting.first || AdminSetting.create(last_updated_by: Admin.first)
     authorize @admin_setting
   end
 
   # PUT /admin_settings/1
   # PUT /admin_settings/1.xml
   def update
-    @admin_setting = AdminSetting.first || AdminSetting.create(last_updated_by: Admin.first)
     authorize @admin_setting
     authorized_params = policy(@admin_setting).verify_permitted_params(admin_setting_params)
 
     if authorized_params == true && @admin_setting.update(admin_setting_params)
       flash[:notice] = ts("Archive settings were successfully updated.")
-      redirect_to admin_settings_path
     else
-      flash[:error] = authorized_params unless authorized_params == true 
-      render action: "index"
+      flash[:error] = authorized_params unless authorized_params == true
     end
+
+    redirect_to admin_settings_path
   end
 
   private
@@ -31,5 +31,9 @@ class Admin::SettingsController < Admin::BaseController
       :request_invite_enabled, :creation_requires_invite, :downloads_enabled,
       :hide_spam, :disable_support_form, :disabled_support_form_text
     ).merge(last_updated_by: current_admin.id)
+  end
+
+  def find_admin_setting
+    @admin_setting = AdminSetting.first || AdminSetting.create(last_updated_by: Admin.first)
   end
 end
