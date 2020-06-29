b005ca475d7e444d7d54148ceeb185a17b038a6b
jeff <jeffreyckingswood@gmail.com>
2020-06-29 10:27:55 -0400

    Switch out single quotes for doubles

--- spec/controllers/admin/user_creations_controller_spec.rb
+++ spec/controllers/admin/user_creations_controller_spec.rb
@@ -10,20 +10,20 @@ describe Admin::UserCreationsController do
     let(:admin) { create(:admin) }
     let(:work) { create(:work) }
 
-    context 'when admin does not have correct authorization' do
+    context "when admin does not have correct authorization" do
       it "denies random admin access" do
         admin.update(roles: [])
         fake_login_admin(admin)
-        get :hide, params: { id: work.id, creation_type: 'Work' }
+        get :hide, params: { id: work.id, creation_type: "Work" }
         it_redirects_to_with_error(root_url, "Sorry, only an authorized admin can access the page you were trying to reach.")
       end
     end
 
-    context 'when admin does have correct authorization' do
+    context "when admin does have correct authorization" do
       it "allows admin with authorization to hide user_creation" do
-        admin.update(roles: ['policy_and_abuse'])
+        admin.update(roles: ["policy_and_abuse"])
         fake_login_admin(admin)
-        get :hide, params: { id: work.id, creation_type: 'Work', hidden: true }
+        get :hide, params: { id: work.id, creation_type: "Work", hidden: true }
 
         it_redirects_to_with_notice(work_path(work), "Item has been hidden.")
         work.reload
@@ -32,9 +32,9 @@ describe Admin::UserCreationsController do
 
       it "allows admin with authorization to make user_creation visible" do
         work.update(hidden_by_admin: true)
-        admin.update(roles: ['policy_and_abuse'])
+        admin.update(roles: ["policy_and_abuse"])
         fake_login_admin(admin)
-        get :hide, params: { id: work.id, creation_type: 'Work', hidden: false }
+        get :hide, params: { id: work.id, creation_type: "Work", hidden: false }
 
         it_redirects_to_with_notice(work_path(work), "Item is no longer hidden.")
         work.reload
@@ -47,20 +47,20 @@ describe Admin::UserCreationsController do
     let(:admin) { create(:admin) }
     let(:work) { create(:work) }
 
-    context 'when admin does not have correct authorization' do
+    context "when admin does not have correct authorization" do
       it "denies random admin access" do
         admin.update(roles: [])
         fake_login_admin(admin)
-        get :set_spam, params: { id: work.id, creation_type: 'Work', spam: true }
+        get :set_spam, params: { id: work.id, creation_type: "Work", spam: true }
         it_redirects_to_with_error(root_url, "Sorry, only an authorized admin can access the page you were trying to reach.")
       end
     end
 
-    context 'when admin does have correct authorization' do
+    context "when admin does have correct authorization" do
       it "allows admin with authorization to mark user_creation as spam" do
-        admin.update(roles: ['policy_and_abuse'])
+        admin.update(roles: ["policy_and_abuse"])
         fake_login_admin(admin)
-        get :set_spam, params: { id: work.id, creation_type: 'Work', spam: true }
+        get :set_spam, params: { id: work.id, creation_type: "Work", spam: true }
 
         it_redirects_to_with_notice(work_path(work), "Work was marked as spam and hidden.")
         work.reload
@@ -68,10 +68,10 @@ describe Admin::UserCreationsController do
       end
 
       it "allows admin with authorization to mark user_creation as NOT spam" do
-        admin.update(roles: ['policy_and_abuse'])
+        admin.update(roles: ["policy_and_abuse"])
         work.update(spam: true)
         fake_login_admin(admin)
-        get :set_spam, params: { id: work.id, creation_type: 'Work', spam: false }
+        get :set_spam, params: { id: work.id, creation_type: "Work", spam: false }
 
         it_redirects_to_with_notice(work_path(work), "Work was marked not spam and unhidden.")
         work.reload
@@ -84,20 +84,20 @@ describe Admin::UserCreationsController do
     let(:admin) { create(:admin) }
     let(:work) { create(:work) }
 
-    context 'when admin does not have correct authorization' do
+    context "when admin does not have correct authorization" do
       it "denies random admin access" do
         admin.update(roles: [])
         fake_login_admin(admin)
-        get :destroy, params: { id: work.id, creation_type: 'Work' }
+        get :destroy, params: { id: work.id, creation_type: "Work" }
         it_redirects_to_with_error(root_url, "Sorry, only an authorized admin can access the page you were trying to reach.")
       end
     end
 
-    context 'when admin does have correct authorization' do
+    context "when admin does have correct authorization" do
       it "allows admin with authorization to delete user_creation" do
-        admin.update(roles: ['policy_and_abuse'])
+        admin.update(roles: ["policy_and_abuse"])
         fake_login_admin(admin)
-        get :destroy, params: { id: work.id, creation_type: 'Work' }
+        get :destroy, params: { id: work.id, creation_type: "Work" }
 
         it_redirects_to_with_notice(works_path, "Item was successfully deleted.")
       end
