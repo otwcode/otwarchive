718782518c2cb33aae6ffb2891f2d3e572dbef31
redsummernight <redsummernight@users.noreply.github.com>
2020-07-10 01:07:03 -0400

    AO3-5980 AO3-5623 Update admin creation script to prompt for roles
    Also:
    
    - Print the admin login HTTPS URL for the right environment
    - Remove spaces in admin logins
    - Output i18n role names in wiki output

--- script/create_admin.rb
+++ script/create_admin.rb
@@ -1,24 +1,34 @@
 #!script/rails runner
-# 
-# 1. Run this script on production as follows:
-#      RAILS_ENV=production bundle exec rails r script/create_admin.rb
-# 2. When prompted, type or paste in the admins, one per line, in the format USERNAME, EMAIL 
-#    (USERNAME is Org name without the admin- prefix or any spaces)
-# 3. Enter a final blank line to finish inputting admins
-# 4. Follow the onscreen instructions
+#
+# Run this script and follow the onscreen instructions:
+#   bundle exec rails r script/create_admin.rb
 
-require 'csv'
+require "csv"
 
-def multi_gets(all_text = '')
+include Rails.application.routes.url_helpers
+
+# Base URL
+default_url_options[:host] = ArchiveConfig.APP_URL
+
+def multi_gets(all_text = "")
   until (text = gets) == "\n"
     all_text << text
   end
   all_text.chomp
 end
 
-print "Paste or enter admins, one per line, in the format\n\tUSERNAME, EMAIL\n
-(where USERNAME is their Org name without spaces and without the admin- prefix)\n
-then two line breaks to end:\n"
+print "Paste or enter admins, one per line, in the format
+
+\tUSERNAME, EMAIL, ROLE
+or
+\tUSERNAME, EMAIL, ROLE, ROLE, ROLE
+
+where USERNAME is their Org name without spaces and without the admin- prefix
+and ROLE is one of:
+
+#{Admin::VALID_ROLES.sort.map { |r| "\t#{r}" }.join("\n")}
+
+then two line breaks to end:\n\n"
 input = multi_gets
 
 list = CSV.parse(input)
@@ -27,17 +37,24 @@ puts "\nCopy and paste each section into a separate file and upload to the user'
 
 admins = []
 list.each do |user|
-  name = user[0].strip
+  name = user[0].gsub(/\s+/, "")
   email = user[1].strip
   password = `pwgen 8 1`.strip
+  roles = user.drop(2).compact.map(&:strip)
 
-  a = Admin.new(email: email, login: "admin-#{name}", password: password, password_confirmation: password)
+  a = Admin.new(
+    email: email,
+    login: "admin-#{name}",
+    password: password,
+    password_confirmation: password,
+    roles: roles
+  )
 
   if a.save
     puts
-    puts "username: admin-#{name}"
+    puts "username: #{a.login}"
     puts "password: #{password}"
-    puts "http://archiveofourown.org/admin/login"
+    puts new_admin_session_url
     puts
     admins << a
   else
@@ -48,5 +65,7 @@ end
 puts "\nCopy and paste into the wiki at https://wiki.transformativeworks.org/mediawiki/AO3_Admins:\n"
 
 admins.each do |admin|
-  puts "|-\n| #{Date.today.to_formatted_s("YYYY-MM-dd")} || #{admin.login} || UPDATE WITH USER COMMITTEE"
+  role_description = admin.roles.map { |r| I18n.t("activerecord.attributes.admin/role.#{r}") }.sort.join(", ")
+  role_description = "UPDATE WITH USER COMMITTEE" if role_description.blank?
+  puts "|-\n| #{Time.zone.today.to_formatted_s('YYYY-MM-dd')} || #{admin.login} || #{role_description}"
 end
