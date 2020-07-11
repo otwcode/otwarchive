a5d38b8d64fba4103dafab8d073621eab4829d6e
sarken <sarken@gmail.com>
2020-07-10 23:36:50 -0400

    AO3-5885 Use helper when determining whether to show admin delete option on user creations

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -10,10 +10,16 @@ module AdminHelper
   def can_access_admin_options?(current_admin)
     return unless logged_in_as_admin?
 
-    UserCreationPolicy.can_destroy_creations?(current_admin) ||
+    admin_can_destroy_creations?(current_admin) ||
       admin_can_edit_creations?(current_admin) ||
       admin_can_hide_creations?(current_admin) ||
       admin_can_mark_creations_spam?(current_admin)
+  end
+
+  def admin_can_destroy_creations?(current_admin)
+    return unless logged_in_as_admin?
+
+    UserCreationPolicy.can_destroy_creations?(current_admin)
   end
 
   # Currently applies to editing ExternalWorks and the tags or laguage of Works.
