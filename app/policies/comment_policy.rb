d0960f1815e4c347bbf2468a6b1e39ab96c7cdf6
sarken <sarken@gmail.com>
2020-07-13 18:24:02 -0400

    AO3-5886 Remove unused methods from CommentPolicy

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -2,14 +2,6 @@ class CommentPolicy < ApplicationPolicy
   DESTROY_ROLES = %w(superadmin policy_and_abuse communications support).freeze
   SPAM_ROLES = %w(superadmin policy_and_abuse communications support).freeze
 
-  def self.can_destroy_comment?(user)
-    self.new(user, nil).can_destroy_comment?
-  end
-    
-  def self.can_mark_comment_spam?(user)
-    self.new(user, nil).can_mark_comment_spam?
-  end
-
   def can_destroy_comment?
     user_has_roles?(DESTROY_ROLES)
   end
