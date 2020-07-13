d15e98cbbde3610a5d667b82919a909bc30ab62a
Enigel <enigel@gmail.com>
2020-07-13 04:38:09 +0100

    Dalli store doesn't support recyclable keys

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -16,6 +16,8 @@ Otwarchive::Application.configure do
   config.action_controller.perform_caching = true
   config.action_controller.page_cache_directory = Rails.root.join("public", "test_cache")
 
+  config.active_record.cache_versioning = false
+
   config.action_mailer.perform_caching = true
   config.cache_store = :dalli_store, '127.0.0.1:11211',
                           { namespace:  'ao3-v1', expires_in:  0, compress: true , pool_size: 10 , raise_errors: true}
