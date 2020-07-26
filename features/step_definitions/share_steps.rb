0f3171d6c9ef091692328972652b70df6aeb63f9
Tom Milligan <tom@reinfer.io>
2020-07-26 23:23:22 +0100

    hound: fix style violations

--- features/step_definitions/share_steps.rb
+++ features/step_definitions/share_steps.rb
@@ -1,6 +1,6 @@
 Then /^the share modal should contain a Twitter share button$/ do
-  with_scope('#share') do
-    iframe = find('li.twitter #twitter-widget-0')
+  with_scope("#share") do
+    iframe = find("li.twitter #twitter-widget-0")
     within_frame(iframe) do
       page.should have_content("Tweet")
     end
