b0f548fed5de86851de70da8b5b17e1fc3d945b4
zz9pzza <zz9pzza@gmail.com>
2020-08-12 23:50:11 +0100

    Remove unwanted commit

--- script/travis/mysql.sh
+++ script/travis/mysql.sh
@@ -4,10 +4,10 @@ set -e
 # Use READ-COMMITTED transaction isolation level
 # https://railsmachine.com/articles/2017/05/19/converting-a-rails-database-to-utf8mb4.html
 sudo sed -i /etc/mysql/mariadb.conf.d/50-server.cnf  -e "s/\[mysqld\]/[mysqld]\n\
-	innodb_lock_wait_timeout=15\n\
-	transaction-isolation=READ-COMMITTED\n\
-	innodb_file_per_table=1\n\
-	/"
+innodb_lock_wait_timeout=15\n\
+transaction-isolation=READ-COMMITTED\n\
+innodb_file_per_table=1\n\
+/"
 
 # The conf change requires a restart
 sudo systemctl restart mariadb
@@ -16,4 +16,3 @@ sudo mysql -e "
 CREATE USER 'travis'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON *.* TO 'travis'@'%';
 flush privileges;"
-
