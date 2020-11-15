be029cf837d67283a90c99236382c83fedc9e7ed
sarken <sarken@gmail.com>
2020-11-14 23:59:53 -0500

    AO3-6071 Set sortable_name when manually configuring environment locale
    The sortable_name field is required and usually set by code, but
    that code doesn't run in this context

--- config/initializers/archive_config/locale.rb
+++ config/initializers/archive_config/locale.rb
@@ -10,7 +10,7 @@ rescue ActiveRecord::ConnectionNotEstablished
 rescue
   # ArchiveConfig didn't work, try to set it manually
   if Language.table_exists? && Locale.table_exists?
-    language = Language.find_or_create_by(short: 'en', name: 'English')
+    language = Language.find_or_create_by(short: "en", name: "English", sortable_name: "English")
     Locale.set_base_locale(iso: "en", name: "English (US)", language_id: language.id)
   end
 end
