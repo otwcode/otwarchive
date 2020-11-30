48f43a21c7d4f887d6ee85d7d0b4549cb05c655e
tickinginstant <tickinginstant@users.noreply.github.com>
2020-11-29 19:37:04 -0500

    AO3-6079 Review comments.

--- spec/controllers/api/v2/api_bookmarks_spec.rb
+++ spec/controllers/api/v2/api_bookmarks_spec.rb
@@ -1,9 +1,9 @@
 require "spec_helper"
 require "controllers/api/api_helper"
 
-include ApiHelper
-
 describe "API v2 BookmarksController", type: :request, bookmark_search: true do
+  include ApiHelper
+
   bookmark = { id: "123",
                url: "http://example.com",
                author: "Thing",
@@ -21,7 +21,7 @@ describe "API v2 BookmarksController", type: :request, bookmark_search: true do
                rec: "0" }
 
   before do
-    ApiHelper.mock_external
+    mock_external
   end
 
   after do
