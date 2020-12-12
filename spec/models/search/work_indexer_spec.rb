bedbbef8c43585d97b5a56062355921e4c8f5fa7
tickinginstant <tickinginstant@users.noreply.github.com>
2020-12-12 16:23:38 -0500

    AO3-6066 Avoid errors when indexing batch is empty. (#3935)
    * AO3-6066 Don't error when indexing batch is empty.
    
    * AO3-6066 Make indexers return the stored batch.

--- spec/models/search/work_indexer_spec.rb
+++ spec/models/search/work_indexer_spec.rb
@@ -3,13 +3,22 @@ require "spec_helper"
 describe WorkIndexer, work_search: true do
   describe "#index_documents" do
     let(:work) { create(:work) }
-    let(:indexer) { WorkIndexer.new([work.id]) }
 
     context "when a work in the batch has no stat_counter" do
+      let(:indexer) { WorkIndexer.new([work.id]) }
+
       before { work.stat_counter.delete }
 
       it "doesn't error" do
         expect { indexer.index_documents }.not_to raise_exception
+      end
+    end
+
+    context "when there are no IDs in the batch" do
+      let(:indexer) { WorkIndexer.new([]) }
+
+      it "returns nil" do
+        expect(indexer.index_documents).to be_nil
       end
     end
   end
