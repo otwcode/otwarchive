ce44c0fd3710d11919bb5b856c0de8729129562d
tickinginstant <tickinginstant@users.noreply.github.com>
2021-01-19 19:54:19 -0500

    AO3-6111 Remove unused by_date scope.

--- app/models/kudo.rb
+++ app/models/kudo.rb
@@ -21,8 +21,6 @@ class Kudo < ApplicationRecord
   scope :with_user, -> { where("user_id IS NOT NULL") }
   scope :by_guest, -> { where("user_id IS NULL") }
 
-  scope :by_date, -> { order("created_at DESC") }
-
   after_destroy :update_work_stats
   after_create :after_create, :update_work_stats
   def after_create
