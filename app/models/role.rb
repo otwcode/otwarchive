321964e3a70786fd17e00b162b3315ed586b217c
sarken <sarken@gmail.com>
2021-02-24 22:44:47 -0500

    AO3-6137 Remove unnecessary change to role model

--- app/models/role.rb
+++ app/models/role.rb
@@ -3,7 +3,7 @@
 # "moderator" for an instance of a model (i.e., an object), a model class,
 # or without any specification at all.
 class Role < ApplicationRecord
-  has_many :roles_users, dependent: :destroy
+  has_many :roles_users
   has_many :users, through: :roles_users
   belongs_to :authorizable, polymorphic: true
 
