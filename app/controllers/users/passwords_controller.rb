b777045e229f601944ae01df58bbf5de3d57eaf4
sarken <sarken@gmail.com>
2021-02-27 22:47:16 -0500

    AO3-5525 && return instead of and return

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -8,7 +8,7 @@ class Users::PasswordsController < Devise::PasswordsController
   def create
     if User.find_for_authentication(resource_params.permit(:login))&.protected_user
       flash[:error] = t(".reset_blocked", contact_abuse_link: view_context.link_to(t(".contact_abuse"), new_abuse_report_path)).html_safe
-      redirect_to root_path and return
+      redirect_to root_path && return
     end
     super do |user|
       flash.now[:notice] = ts("We couldn't find an account with that email address or username. Please try again?") if user.nil? || user.new_record?
