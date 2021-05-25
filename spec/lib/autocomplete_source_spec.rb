be1495ccf2b12085097a4980e035e7ea9fecba19
tlee <thomasgylee@gmail.com>
2021-05-25 16:46:18 -0400

    AO3-2380 Adding spec tests

--- spec/lib/autocomplete_source_spec.rb
+++ spec/lib/autocomplete_source_spec.rb
@@ -1,64 +1,94 @@
-require 'spec_helper'
-require 'autocomplete_source'
+require "spec_helper"
+require "autocomplete_source"
 
 shared_examples_for "an autocompleting tag" do
   context "without diacritics" do
     let(:auto) { described_class.new(name: "Autobot") }
 
-    describe '#autocomplete_prefixes' do
-      it "should return an array that includes the class name" do
+    describe "#autocomplete_prefixes" do
+      it "returns an array that includes the class name" do
         expect(auto.autocomplete_prefixes).to include("autocomplete_tag_all")
       end
     end
 
-    describe '#autocomplete_search_string' do
-      it "should be equal to its name" do
+    describe "#autocomplete_search_string" do
+      it "is equal to its name" do
         expect(auto.autocomplete_search_string).to eq(auto.name)
       end
     end
 
-    describe '#autocomplete_value' do
-      it "should include id and name" do
+    describe "#autocomplete_value" do
+      it "includes id and name" do
         expect(auto.autocomplete_value).to eq("#{auto.id}: #{auto.name}")
       end
     end
 
-    describe '#autocomplete_score' do
-      it "should return zero" do
+    describe "#autocomplete_score" do
+      it "returns zero" do
         expect(auto.autocomplete_score).to eq(0)
       end
     end
 
-    describe '#add_to_autocomplete' do
-      it "should add itself to the autocomplete"
+    describe "#add_to_autocomplete" do
+      it "adds itself to the autocomplete" do
+        auto.add_to_autocomplete
+        ac = REDIS_AUTOCOMPLETE.zrange("autocomplete_tag_fandom_completion", 0, -1)
+
+        (1..ac.length).each do |i|
+          search_string = auto.name.downcase.slice(0, i)
+          expect(ac).to include(search_string.to_s)
+        end
+
+        expect(ac).to include("#{auto.name.downcase},,")
+      end
     end
 
-    describe '#remove_from_autocomplete' do
-      it "should remove itself from the autocomplete"
+    describe "#remove_from_autocomplete" do
+      it "removes itself from the autocomplete" do
+        auto.add_to_autocomplete
+        auto.remove_from_autocomplete
+        ac = REDIS_AUTOCOMPLETE.zrange("autocomplete_tag_fandom_completion", 0, -1)
+        expect(ac).not_to include("#{auto.name.downcase},,")
+      end
     end
   end
 
   context "with diacritics" do
     let(:auto) { described_class.new(name: "Âutobot2") }
 
-    describe '#autocomplete_search_string' do
-      it "should be equal to its name" do
+    describe "#autocomplete_search_string" do
+      it "is equal to its name" do
         expect(auto.autocomplete_search_string).to eq(ActiveSupport::Inflector.transliterate(auto.name))
       end
     end
 
-    describe '#autocomplete_value' do
-      it "should include id and name" do
+    describe "#autocomplete_value" do
+      it "includes id and name" do
         expect(auto.autocomplete_value).to eq(ActiveSupport::Inflector.transliterate("#{auto.id}: #{auto.name}"))
       end
     end
 
-    describe '#add_to_autocomplete' do
-      it "should add itself to the autocomplete"
+    describe "#add_to_autocomplete" do
+      it "adds itself to the autocomplete" do
+        auto.add_to_autocomplete
+        ac = REDIS_AUTOCOMPLETE.zrange("autocomplete_tag_fandom_completion", 0, -1)
+
+        (1..ac.length).each do |i|
+          search_string = ActiveSupport::Inflector.transliterate(auto.name.downcase.slice(0, i))
+          expect(ac).to include(search_string.to_s)
+        end
+
+        expect(ac).to include("#{ActiveSupport::Inflector.transliterate(auto.name.downcase)},,")
+      end
     end
 
-    describe '#remove_from_autocomplete' do
-      it "should remove itself from the autocomplete"
+    describe "#remove_from_autocomplete" do
+      it "removes itself from the autocomplete" do
+        auto.add_to_autocomplete
+        auto.remove_from_autocomplete
+        ac = REDIS_AUTOCOMPLETE.zrange("autocomplete_tag_fandom_completion", 0, -1)
+        expect(ac).not_to include("#{ActiveSupport::Inflector.transliterate(auto.name.downcase)},,")
+      end
     end
   end
 end
@@ -66,10 +96,9 @@ end
 shared_examples_for "an autocompletable class with a title" do
   let(:auto) { described_class.new(name: "Autobot", title: "Transformer") }
 
-  it "should have a search string composed of its name and title" do
+  it "has a search string composed of its name and title" do
     expect(auto.autocomplete_search_string).to eq("Autobot Transformer")
   end
-
 end
 
 describe Fandom do
