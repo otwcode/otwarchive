acf77297cd1b9ec1b0fc63c2b48dfc8fde1afb96
Cesy <cesy.avon@gmail.com>
2021-06-16 08:14:54 +0100

    default_i18n_subject change for archive_devise_mailer

--- app/mailers/archive_devise_mailer.rb
+++ app/mailers/archive_devise_mailer.rb
@@ -12,8 +12,7 @@ class ArchiveDeviseMailer < Devise::Mailer
     @user = record
     @token = token
     I18n.with_locale(Locale.find(@user.preference.preferred_locale).iso) do
-      subject = t('users.mailer.reset_password_instructions.subject',
-                  app_name: ArchiveConfig.APP_SHORT_NAME)
+      subject = default_i18n_subject(app_name: ArchiveConfig.APP_SHORT_NAME)
       devise_mail(record, :reset_password_instructions,
                   options.merge(subject: subject))
     end
