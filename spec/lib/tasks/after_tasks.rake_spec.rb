e48466193cc292fc232b6f3315277b22be922c85
tlee <thomasgylee@gmail.com>
2021-06-23 23:16:56 -0400

    AO3-5988 Fix tests after #4011

--- spec/lib/tasks/after_tasks.rake_spec.rb
+++ spec/lib/tasks/after_tasks.rake_spec.rb
@@ -155,11 +155,15 @@ describe "rake After:replace_dewplayer_embeds" do
 end
 
 describe "rake After:fix_teen_and_up_imported_rating" do
-  let!(:noncanonical_teen_rating) { Rating.create(name: "Teen & Up Audiences") }
+  let!(:noncanonical_teen_rating) { Rating.create(name: "Teen & Up Audiences", canonical: true) }
   let!(:canonical_gen_rating) { Rating.find_or_create_by!(name: ArchiveConfig.RATING_GENERAL_TAG_NAME, canonical: true) }
   let!(:canonical_teen_rating) { Rating.find_or_create_by!(name: ArchiveConfig.RATING_TEEN_TAG_NAME, canonical: true) }
-  let!(:work_with_noncanonical_rating) { create(:work, ratings: [noncanonical_teen_rating]) }
-  let!(:work_with_canonical_and_noncanonical_ratings) { create(:work, ratings: [noncanonical_teen_rating, canonical_gen_rating]) }
+  let!(:work_with_noncanonical_rating) { create(:work, rating_string: noncanonical_teen_rating.name) }
+  let!(:work_with_canonical_and_noncanonical_ratings) { create(:work, rating_string: [noncanonical_teen_rating.name, ArchiveConfig.RATING_GENERAL_TAG_NAME].join(",")) }
+
+  before do
+    noncanonical_teen_rating.update_attribute(:canonical, false)
+  end
 
   it "updates the works' ratings to the canonical teen rating" do
     subject.invoke
@@ -169,10 +173,14 @@ describe "rake After:fix_teen_and_up_imported_rating" do
 end
 
 describe "rake After:clean_up_noncanonical_categories" do
-  let!(:noncanonical_category_tag) { Category.create(name: "Borked category tag", canonical: false) }
+  let!(:noncanonical_category_tag) { Category.create(name: "Borked category tag", canonical: true) }
   let!(:canonical_category_tag) { Category.find_or_create_by(name: ArchiveConfig.CATEGORY_GEN_TAG_NAME, canonical: true) }
   let!(:work_with_noncanonical_categ) { create(:work, categories: [noncanonical_category_tag]) }
   let!(:work_with_canonical_and_noncanonical_categs) { create(:work, categories: [noncanonical_category_tag, canonical_category_tag]) }
+
+  before do
+    noncanonical_category_tag.update_attribute(:canonical, false)
+  end
 
   it "changes and replaces the noncanonical category tags" do
     subject.invoke
