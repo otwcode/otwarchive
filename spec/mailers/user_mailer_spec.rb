6235628d0fa35786c362c2732bf4ef5395d5d12e
Sarken <sarken@gmail.com>
2021-07-31 02:01:57 -0400

    Prefer 'invitation' to 'invite'

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -266,7 +266,7 @@ describe UserMailer do
     it_behaves_like "an email with a valid sender"
 
     it "has the correct subject line" do
-      subject = "[#{ArchiveConfig.APP_SHORT_NAME}] Additional invite code request declined"
+      subject = "[#{ArchiveConfig.APP_SHORT_NAME}] Additional invitation request declined"
       expect(email).to have_subject(subject)
     end
 
