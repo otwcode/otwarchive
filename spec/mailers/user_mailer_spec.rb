2781e4fee1affd0e1df14487c741919d7977b299
sarken <sarken@gmail.com>
2021-09-01 05:37:40 -0400

    Initial pass at putting more work info into recipient email

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -675,7 +675,7 @@ describe UserMailer do
       subject(:email) { UserMailer.recipient_notification(user.id, work.id, collection.id) }
 
       let(:user) { create(:user) }
-      let(:work) { create(:work) }
+      let(:work) { create(:work, fandom_string: "Fandom 1, Fandom 2", character_string: "A, B") }
       let(:collection) { create(:collection) }
 
       # Test the headers
@@ -689,16 +689,20 @@ describe UserMailer do
       # Test both body contents
       it_behaves_like "a multipart email"
 
+      it_behaves_like "a translated email"
+
       describe "HTML version" do
         it "has the correct content" do
           expect(email).to have_html_part_content("Hi, <b")
           expect(email).to have_html_part_content("#{user.login}</b>")
+          expect(email).to have_html_part_content("FAIL")
         end
       end
 
       describe "text version" do
         it "has the correct content" do
           expect(email).to have_text_part_content("Hi, #{user.login}!")
+          expect(email).to have_text_part_content("FAIL")
         end
       end
     end
@@ -719,6 +723,8 @@ describe UserMailer do
 
       # Test both body contents
       it_behaves_like "a multipart email"
+
+      it_behaves_like "a translated email"
 
       describe "HTML version" do
         it "has the correct content" do
