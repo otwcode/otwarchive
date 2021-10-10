5148f66a5eafab3764c726be3e4b63ada9062000
sarken <sarken@gmail.com>
2021-10-10 17:59:11 -0400

    AO3-6109 Update list of locales to pull from Phrase (#4090)
    * AO3-6109 Update list of locales to pull from Phrase
    
    * AO3-6109 Fix Dutch and add Kyrgyz
    
    * AO3-6109 Update i18n available_locales
    
    * AO3-6109 Another pass at correcting locales
    
    * AO3-6109 Remove ka (Georgian) defaults
    
    * AO3-6109 Rename mr-IN (Marathi, India) defaults to mr (Marathi)
    
    * AO3-6109 Rename tl (Tagalog) defaults to fil (Filipino)
    
    * AO3-6109 Update README
    
    * AO3-6109 Update Phrase config to reflect languages being renamed on Phrase

--- config/application.rb
+++ config/application.rb
@@ -37,10 +37,10 @@ module Otwarchive
 
     I18n.config.enforce_available_locales = false
     I18n.config.available_locales = [
-      :en, :af, :ar, :bg, :bn, :ca, :cs, :cy, :da, :de, :el, :es, :fa, :fi, :fr,
-      :he, :hi, :hr, :hu, :id, :it, :ja, :ka, :ko, :lt, :lv, :mk, :"mr-IN", :ms,
-      :nb, :nl, :pl, :"pt-BR", :"pt-PT", :ro, :ru, :sk, :sl, :sr, :sv, :th, :tl,
-      :tr, :uk, :vi, :"zh-CN"
+      :en, :af, :ar, :bg, :bn, :ca, :cs, :cy, :da, :de, :el, :es, :fa, :fi,
+      :fil, :fr, :he, :hi, :hr, :hu, :id, :it, :ja, :ko, :ky, :lt, :lv, :mk,
+      :mr, :ms, :nb, :nl, :pl, :"pt-BR", :"pt-PT", :ro, :ru, :sk, :sl, :sr, :sv,
+      :th, :tr, :uk, :vi, :"zh-CN"
     ]
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
