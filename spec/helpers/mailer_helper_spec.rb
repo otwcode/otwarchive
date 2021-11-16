4c4dda036d68201e2fa6c6b1d44e1724bc705519
Daroc Alden <setupminimal@gmail.com>
2021-11-16 08:47:00 -0500

    AO3-4509: Updating mailer test

--- spec/helpers/mailer_helper_spec.rb
+++ spec/helpers/mailer_helper_spec.rb
@@ -4,7 +4,7 @@ describe MailerHelper do
   describe "style_creation_link" do
     it "nests red link inside bold inside italics" do
       work = create(:work)
-      expect(style_creation_link(work.title, work_url(work))).to eq("<i><b><a style=\"color:#990000\" href=\"#{work_url(work)}\">#{work.title}</a></b></i>")
+      expect(style_creation_link(work.title, work_url(work))).to eq("<i><b><a style=\"color:#990000\" href=\"#{work_url(work)}\"><bdi>#{work.title}</bdi></a></b></i>")
     end
   end
 end
