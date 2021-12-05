72b975cf5ed90ce28cf212e84420aaccea871372
redsummernight <redsummernight@users.noreply.github.com>
2021-12-05 18:56:30 -0500

    AO3-6262 Install rails-i18n for pluralization rules (#4106)
    - Remove most locale files in config/locales/defaults, as we should have
      them from the rails-i18n dependency now.
    - Rename config/locales/defaults to config/locales/rails-i18n for custom
      locales.
    - Fix locale keys for the renamed fil.yml and mr.yml; add corresponding
      pluralization rules.
    - Remove unused TranslationNote model.
    - Remove unused Copycopter gem and configuration.
    - Add missing push for app/controllers i18n keys.

--- config/initializers/copycopter.rb
+++
@@ -1,8 +0,0 @@
-# Disabled until translation work is underway
-# if Rails.env.staging?
-#   CopycopterClient.configure do |config|
-#     config.api_key = 'bc9ac482bbb2dc056b55734ea306e5a3ef2cf9caec60c8be'
-#     config.host = 'test-copycopter'
-#     config.port = 3000
-#   end
-# end
