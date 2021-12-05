e03b56d11f1d81f583a0e1fe9499c3b6037a04e1
Sarken <sarken@gmail.com>
2021-12-04 23:29:15 -0500

    AO3-6257 Fix migration

--- db/migrate/20211204001151_add_allow_gifts_to_preference.rb
+++ db/migrate/20211204001151_add_allow_gifts_to_preference.rb
@@ -1,4 +1,4 @@
-class AddAllowGiftsToPreferences < ActiveRecord::Migration[5.2]
+class AddAllowGiftsToPreference < ActiveRecord::Migration[5.2]
   def change
     add_column :preferences, :allow_gifts, :boolean, default: true, null: false
   end
