7a35b86bbdafbd92b8d3b9f20bc01be110d16f5a
tickinginstant <tickinginstant@users.noreply.github.com>
2021-12-05 18:38:24 -0500

    AO3-6189 Don't set Time.zone in challenge controllers. (#4030)

--- app/controllers/challenge/prompt_meme_controller.rb
+++ app/controllers/challenge/prompt_meme_controller.rb
@@ -4,18 +4,8 @@ class Challenge::PromptMemeController < ChallengesController
   before_action :load_collection
   before_action :load_challenge, except: [:new, :create]
   before_action :collection_owners_only, only: [:new, :create, :edit, :update, :destroy]
-  before_action :set_time_zone, only: [:create, :edit, :update]
 
   # ACTIONS
-
-  # we use this to make the times get set in the moderator's specified timezone
-  def set_time_zone
-    if params[:prompt_meme] && prompt_meme_params[:time_zone]
-      Time.zone = prompt_meme_params[:time_zone]
-    elsif @challenge && @challenge.time_zone
-      Time.zone = @challenge.time_zone
-    end
-  end
 
   # is actually a blank page - should it be redirected to collection profile?
   def show
