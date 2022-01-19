e32a0d8364acb58c417c3832d4e178c0cf3b41e0
Sarken <sarken@gmail.com>
2022-01-18 21:06:50 -0500

    Simplify test for failing to destroy invitation

--- spec/controllers/invitations_controller_spec.rb
+++ spec/controllers/invitations_controller_spec.rb
@@ -236,13 +236,9 @@ describe InvitationsController do
       admin.update(roles: ["policy_and_abuse"])
       fake_login_admin(admin)
 
-      allow(Invitation).to receive(:find).with(invite.id.to_s).and_return(invite)
-      allow(invite).to receive(:destroy).and_return(false)
+      allow_any_instance_of(Invitation).to receive(:destroy).and_return(false)
 
       delete :destroy, params: { id: invite.id }
-
-      expect(Invitation).to have_received(:find)
-      expect(invite).to have_received(:destroy)
       expect(flash[:error]).to eq("Invitation was not destroyed.")
     end
   end
