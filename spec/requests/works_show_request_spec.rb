75f5c6385eb02b94b4101210e4e9a755baa7f7d6
Sarken <sarken@gmail.com>
2022-02-01 19:19:57 -0500

    AO3-4313 Expand specs

--- spec/requests/works_show_request_spec.rb
+++ spec/requests/works_show_request_spec.rb
@@ -1,6 +1,7 @@
 require "spec_helper"
 
-describe "Works#show", :type => :request do
+
+describe "Works#show" do
   let(:work) { create(:work) }
   let(:chapter) { work.chapters.first }
 
@@ -10,11 +11,71 @@ describe "Works#show", :type => :request do
       chapter.update(position: 2)
     end
 
-    it "displays the first posted chapter" do
-      get "/works/#{work.id}"
-      expect(response).to render_template(:show)
-      expect(response.body).to include(chapter.content)
-      expect(response.body).not_to include("Draft content")
+    context "when logged in as an admin" do
+      before { sign_in create(:admin), scope: :admin }
+
+      it "displays the unposted chapter" do
+        get work_url(work)
+        expect(response).to render_template(:show)
+        expect(response.body).not_to include(chapter.content)
+        expect(response.body).to include("Draft content")
+      end
+    end
+
+    context "when logged in as a random user" do
+      before do
+        allow_any_instance_of(ApplicationController).to receive(:logout_if_not_user_credentials).and_return(false)
+        sign_in create(:user), scope: :user
+      end
+
+      it "displays the first posted chapter" do
+        get work_url(work)
+        expect(response).to render_template(:show)
+        expect(response.body).to include(chapter.content)
+        expect(response.body).not_to include("Draft content")
+      end
+    end
+
+    context "when logged in as the work creator" do
+      let(:user) { work.creatorships.first.pseud.user }
+
+      before do
+        allow_any_instance_of(ApplicationController).to receive(:logout_if_not_user_credentials).and_return(false)
+        sign_in user, scope: :user
+      end
+
+      it "displays the unposted chapter" do
+        get work_url(work)
+        expect(response).to render_template(:show)
+        expect(response.body).not_to include(chapter.content)
+        expect(response.body).to include("Draft content")
+      end
+    end
+
+    context "when logged in as a user who is an invited co-creator" do
+      let(:user) { create(:user) }
+
+      before do
+        work.creatorships.find_or_create_by(pseud: user.default_pseud, approved: false)
+        allow_any_instance_of(ApplicationController).to receive(:logout_if_not_user_credentials).and_return(false)
+        sign_in user, scope: :user
+      end
+
+      it "displays the unposted chapter" do
+        get work_url(work)
+        expect(response).to render_template(:show)
+        expect(response.body).not_to include(chapter.content)
+        expect(response.body).to include("Draft content")
+      end
+    end
+
+    context "when logged out" do
+      it "displays the first posted chapter" do
+        get work_url(work)
+        expect(response).to render_template(:show)
+        expect(response.body).to include(chapter.content)
+        expect(response.body).not_to include("Draft content")
+      end
     end
   end
 end
