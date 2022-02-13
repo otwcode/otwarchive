b10cff21682ae5a401a5f08488f6c448886a4070
tickinginstant <tickinginstant@users.noreply.github.com>
2022-02-13 17:01:38 -0500

    AO3-4488 Add VCR back to some tests, and cache the cassettes in the automated tests. (#4160)
    * AO3-4488 Fix VCR usage, and add cassette caching.
    
    * AO3-4488 Use more double-quotes, for Hound.

--- features/support/vcr.rb
+++ features/support/vcr.rb
@@ -6,45 +6,43 @@ VCR.configure do |c|
   c.hook_into                :webmock
   c.allow_http_connections_when_no_cassette = true
 
-  #use this after setup...
   # Cassettes are now deleted and re-recorded after 30 days. This will ensure
   # that LJ/DW/DA don't update their HTML and break our story parser without us
   # knowing about it.
-  c.default_cassette_options = { record: :none, re_record_interval: 30.days }
-  #use this for setup...
-  #c.default_cassette_options = { record: :new_episodes, re_record_interval: 30.days }
+  c.default_cassette_options = {
+    record: :once,
+    re_record_interval: 30.days
+  }
 end
 
 VCR.cucumber_tags do |t|
-  t.tags '@archivist_import'
+  t.tags "@archivist_import", use_scenario_name: true
 
-  t.tags '@work_import_minimal_valid'
-  t.tags '@work_import_tags'
-  t.tags '@work_import_multi_tags_backdate'
-  t.tags '@work_import_special_characters_auto_utf'
-  t.tags '@work_import_special_characters_auto_latin', record: :all
-  t.tags '@work_import_special_characters_man_latin', record: :all
-  t.tags '@work_import_special_characters_man_cp', record: :all
-  t.tags '@work_import_special_characters_man_utf'
+  t.tags "@work_import_multi_work_backdate"
+  t.tags "@work_import_multi_chapter_backdate"
+  t.tags "@work_import_special_characters_auto_utf"
+  t.tags "@work_import_special_characters_auto_latin"
+  t.tags "@work_import_special_characters_man_latin"
+  t.tags "@work_import_special_characters_man_cp"
+  t.tags "@work_import_special_characters_man_utf"
 
-  # need to run this every time for the devart features, because the recorded responses run into an encoding error I don't have time to investigate
-  t.tags '@import_da_title_link', record: :all
-  # t.tags '@import_da_gallery_link', record: :all # TODO: uncomment if/when implementing this feature
-  t.tags '@import_da_fic', record: :all
+  t.tags "@import_da_title_link"
+  t.tags "@import_da_gallery_link"
+  t.tags "@import_da_fic"
 
-  t.tags '@import_dw'
-  t.tags '@import_dw_tables'
-  t.tags '@import_dw_tables_no_backdate'
-  t.tags '@import_dw_comm'
-  t.tags '@import_dw_multi_chapter'
+  t.tags "@import_dw"
+  t.tags "@import_dw_tables"
+  t.tags "@import_dw_tables_no_backdate"
+  t.tags "@import_dw_comm"
+  t.tags "@import_dw_multi_chapter"
 
-  t.tags '@import_ffn'
-  t.tags '@import_ffn_multi_chapter'
+  t.tags "@import_ffn"
+  t.tags "@import_ffn_multi_chapter"
 
-  t.tags '@import_lj'
-  t.tags '@import_lj_tables'
-  t.tags '@import_lj_no_backdate'
-  t.tags '@import_lj_comm', record: :all
-  t.tags '@import_lj_multi_chapter'
-  t.tags '@import_lj_underscores'
+  t.tags "@import_lj"
+  t.tags "@import_lj_tables"
+  t.tags "@import_lj_no_backdate"
+  t.tags "@import_lj_comm"
+  t.tags "@import_lj_multi_chapter"
+  t.tags "@import_lj_underscores"
 end
