a6a29a4f1e07f4ddb5a09fd56302cf0c88bbc59e
Stephen Lewis <stephen.r.burrows@gmail.com>
2022-02-12 21:13:51 -0800

    AO3-5628 Added empty line after guard clause for rubocop

--- app/models/work.rb
+++ app/models/work.rb
@@ -576,6 +576,7 @@ class Work < ApplicationRecord
     # Invalidate chapter count cache
     self.invalidate_work_chapter_count(self)
     return if self.posted? && !chapter.posted?
+
     if (self.new_record? || chapter.posted_changed?) && chapter.published_at == Date.current
       self.set_revised_at(Time.current) # a new chapter is being posted, so most recent update is now
     else
