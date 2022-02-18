c846418a0a1bab8cb3cbcf83c7c7ec9af015b632
Sarken <sarken@gmail.com>
2022-02-18 18:54:22 -0500

    AO3-6300 Use default Rails time_zone (UTC)

--- config/application.rb
+++ config/application.rb
@@ -50,7 +50,6 @@ module Otwarchive
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
-    config.time_zone = "Eastern Time (US & Canada)"
 
     # The default locale is :en and all translations from config/locales/**/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join("config/locales/**/*.{rb,yml}")]
