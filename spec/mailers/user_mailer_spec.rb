c95ec50f7d26ba98e4d0200aacfd6388c8a3adaf
tickinginstant <tickinginstant@users.noreply.github.com>
2022-03-17 19:15:50 -0400

    AO3-5955 AO3-5956 AO3-6294 Retry on more errors.

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,13 +1,33 @@
 require "spec_helper"
 
 describe UserMailer do
+  shared_examples "it retries and fails on" do |error|
+    it "retries 3 times and ultimately fails with a #{error}" do
+      assert_performed_jobs 3, only: ApplicationMailerJob do
+        expect { subject.deliver_later }.to raise_exception(error)
+      end
+    end
+  end
+
   describe "creatorship_request" do
-    subject(:email) { UserMailer.creatorship_request(work_creatorship.id, author.id).deliver }
+    subject(:email) { UserMailer.creatorship_request(work_creatorship.id, author.id) }
 
     let(:author) { create(:user) }
     let(:second_author) { create(:user) }
     let(:work) { create(:work, authors: [author.default_pseud, second_author.default_pseud]) }
     let(:work_creatorship) { Creatorship.find_by(creation_id: work.id, pseud_id: second_author.default_pseud.id) }
+
+    context "when the creation is unavailable" do
+      before { work_creatorship.creation.delete }
+
+      include_examples "it retries and fails on", ActionView::Template::Error
+    end
+
+    context "when the pseud being invited is unavailable" do
+      before { work_creatorship.pseud.delete }
+
+      include_examples "it retries and fails on", NoMethodError
+    end
 
     # Test the headers
     it_behaves_like "an email with a valid sender"
@@ -38,12 +58,24 @@ describe UserMailer do
   end
 
   describe "creatorship_notification" do
-    subject(:email) { UserMailer.creatorship_notification(work_creatorship.id, author.id).deliver }
+    subject(:email) { UserMailer.creatorship_notification(work_creatorship.id, author.id) }
 
     let(:author) { create(:user) }
     let(:second_author) { create(:user) }
     let(:work) { create(:work, authors: [author.default_pseud, second_author.default_pseud]) }
     let(:work_creatorship) { Creatorship.find_by(creation_id: work.id, pseud_id: second_author.default_pseud.id) }
+
+    context "when the creation is unavailable" do
+      before { work_creatorship.creation.delete }
+
+      include_examples "it retries and fails on", ActionView::Template::Error
+    end
+
+    context "when the pseud being invited is unavailable" do
+      before { work_creatorship.pseud.delete }
+
+      include_examples "it retries and fails on", NoMethodError
+    end
 
     # Test the headers
     it_behaves_like "an email with a valid sender"
@@ -74,12 +106,24 @@ describe UserMailer do
   end
 
   describe "creatorship_notification_archivist" do
-    subject(:email) { UserMailer.creatorship_notification_archivist(work_creatorship.id, author.id).deliver }
+    subject(:email) { UserMailer.creatorship_notification_archivist(work_creatorship.id, author.id) }
 
     let(:author) { create(:user) }
     let(:second_author) { create(:user) }
     let(:work) { create(:work, authors: [author.default_pseud, second_author.default_pseud]) }
     let(:work_creatorship) { Creatorship.find_by(creation_id: work.id, pseud_id: second_author.default_pseud.id) }
+
+    context "when the creation is unavailable" do
+      before { work_creatorship.creation.delete }
+
+      include_examples "it retries and fails on", ActionView::Template::Error
+    end
+
+    context "when the pseud being invited is unavailable" do
+      before { work_creatorship.pseud.delete }
+
+      include_examples "it retries and fails on", NoMethodError
+    end
 
     # Test the headers
     it_behaves_like "an email with a valid sender"
@@ -499,6 +543,18 @@ describe UserMailer do
     let(:work) { create(:work, summary: "<p>Paragraph <u>one</u>.</p><p>Paragraph 2.</p>") }
     let(:chapter) { create(:chapter, work: work, posted: true, summary: "<p><b>Another</b> HTML summary.</p>") }
     let(:subscription) { create(:subscription, subscribable: work) }
+
+    context "when the user is unavailable" do
+      before { subscription.user.delete }
+
+      include_examples "it retries and fails on", NoMethodError
+    end
+
+    context "when the user's preferences are unavailable" do
+      before { subscription.user.preference.delete }
+
+      include_examples "it retries and fails on", NoMethodError
+    end
 
     # Test the headers
     it_behaves_like "an email with a valid sender"
