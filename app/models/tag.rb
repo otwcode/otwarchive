0d7c8b9131d8d1b4c2500164314b2b6619d0e592
tararosenthal <tararosenthal@gmail.com>
2022-05-10 18:07:30 -0500

    AO3-6328 Allow tags to be sorted by number of uses

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -41,9 +41,8 @@ class Tag < ApplicationRecord
       Tag.transaction do
         batch.each do |id|
           value = REDIS_GENERAL.get("tag_update_#{id}_value")
-          sql = []
-          sql.push("taggings_count_cache = #{value}") unless value.blank?
-          Tag.where(id: id).update_all(sql.join(",")) unless sql.empty?
+          next if value.blank?
+          Tag.where(id: id).update(taggings_count_cache: value.to_i)
         end
         REDIS_GENERAL.srem("tag_update", batch)
       end
@@ -66,8 +65,17 @@ class Tag < ApplicationRecord
   end
 
   def write_taggings_to_redis(value)
+    # Atomically set the value while extracting the old value.
+    old_redis_value = REDIS_GENERAL.getset("tag_update_#{id}_value", value)
+
+    # If the value hasn't changed from the saved version or the REDIS version,
+    # there's no need to write an update to the database, so let's just bail
+    # out.
+    return value if value == old_redis_value && value == taggings_count_cache
+
+    # If we've reached here, then the value has changed, and we need to make
+    # sure that the new value is written to the database.
     REDIS_GENERAL.sadd("tag_update", id)
-    REDIS_GENERAL.set("tag_update_#{id}_value", value)
     value
   end
 
