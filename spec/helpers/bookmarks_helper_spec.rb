9ff826eaaedfb1dc5fd575c721cc6eb7e6338a02
Sarken <sarken@gmail.com>
2022-05-17 23:35:30 -0400

    AO3-6325 Failing tests for css_classes_for_bookmark_blurb_short

---
+++ spec/helpers/bookmarks_helper_spec.rb
@@ -0,0 +1,84 @@
+require "spec_helper"
+
+describe BookmarksHelper do
+  let(:user) { create(:user) }
+  let(:external_work_bookmark) { create(:external_work_bookmark, pseud: user.default_pseud) }
+  let(:series_bookmark) { create(:series_bookmark, pseud: user.default_pseud) }
+  let(:work_bookmark) { create(:bookmark, pseud: user.default_pseud) }
+  let(:external_work) { external_work_bookmark.bookmarkable }
+  let(:series) { series_bookmark.bookmarkable }
+  let(:work) { work_bookmark.bookmarkable }
+
+
+  describe "#css_classes_for_bookmark_blurb" do
+    context "when bookmarkable is ExternalWork" do
+    end
+
+    context "when bookmarkable is Series" do
+    end
+
+    context "when bookmarkable is Work" do
+    end
+  end
+
+  describe "#css_classes_for_bookmarkable_blurb" do 
+    context "when bookmarkable is ExternalWork" do
+    end
+
+    context "when bookmarkable is Series" do
+    end
+
+    context "when bookmarkable is Work" do
+    end
+  end
+
+  describe "#css_classes_for_bookmark_blurb_short" do 
+    let(:default_classes) { "user short blurb group" }
+
+    context "when logged in as bookmarker" do
+      context "when bookmarkable is ExternalWork" do
+        it "returns string with default classes, bookmarker info, and ownership indicator" do
+          result = helper.css_classes_for_bookmark_blurb_short(external_work_bookmark)
+          expect(result).to eq("own #{default_classes} user-#{user.id}")
+        end
+      end
+
+      context "when bookmarkable is Series" do
+        it "returns string with default classes, bookmarker info, and ownership indicator" do
+          result = helper.css_classes_for_bookmark_blurb_short(series_bookmark)
+          expect(result).to eq("own #{default_classes} user-#{user.id}")
+        end
+      end
+
+      context "when bookmarkable is Work" do
+        it "returns string with default classes, bookmarker info, and ownership indicator" do
+          result = helper.css_classes_for_bookmark_blurb_short(work_bookmark)
+          expect(result).to eq("own #{default_classes} user-#{user.id}")
+        end
+      end
+    end
+
+    context "when not logged in as bookmarker" do
+      context "when bookmarkable is ExternalWork" do
+        it "returns string with default classes and bookmarker info" do
+          result = helper.css_classes_for_bookmark_blurb_short(external_work_bookmark)
+          expect(result).to eq("own #{default_classes} user-#{user.id}")
+        end
+      end
+
+      context "when bookmarkable is Series" do
+        it "returns string with default classes and bookmarker info" do
+          result = helper.css_classes_for_bookmark_blurb_short(series_bookmark)
+          expect(result).to eq("own #{default_classes} user-#{user.id}")
+        end
+      end
+
+      context "when bookmarkable is Work" do
+        it "returns string with default classes and bookmarker info" do
+          result = helper.css_classes_for_bookmark_blurb_short(work_bookmark)
+          expect(result).to eq("own #{default_classes} user-#{user.id}")
+        end
+      end
+    end
+  end
+end
