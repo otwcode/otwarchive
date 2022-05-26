78af7df60d49c1fb20240b0552230a08dc089988
EchoEkhi <echoekhi@gmail.com>
2022-05-26 08:24:16 +0100

    AO3-6290 Pleasing the hound

--- spec/helpers/mailer_helper_spec.rb
+++ spec/helpers/mailer_helper_spec.rb
@@ -14,7 +14,7 @@ describe MailerHelper do
   describe "#creation_title" do
     context "when creation is a work" do
       it "returns the work title" do
-        expect(creation_title(work)).to eq("#{work.title}")
+        expect(creation_title(work)).to eq(work.title)
       end
     end
 
