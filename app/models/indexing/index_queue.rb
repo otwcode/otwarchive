4dbfe9750edf56b0e18b56af205575e67eeecd93
tararosenthal <tararosenthal@gmail.com>
2022-06-12 01:39:46 -0500

    AO3-6328 Hound fixes

--- app/models/indexing/index_queue.rb
+++ app/models/indexing/index_queue.rb
@@ -12,7 +12,7 @@ class IndexQueue
   end
 
   def self.get_key(klass, label)
-    if (klass.is_a?(Class))
+    if klass.is_a? Class
       klass = klass < Tag ? Tag : klass
     end
     "index:#{klass.to_s.underscore}:#{label}"
