3a304e380b882b96f47ceb7b2d1a94df0185c25d
irrationalpie7 <76858438+irrationalpie7@users.noreply.github.com>
2022-06-14 15:51:52 -0700

    AO3-6358 Overwrite local configuration files in Docker init.sh (#4261)

--- script/docker/init.sh
+++ script/docker/init.sh
@@ -5,9 +5,9 @@ set -ex
 # Change directory to root of the repo
 cd "$(dirname "$0")/../.."
 
-cp -n config/docker/database.yml config/database.yml
-cp -n config/docker/redis.yml config/redis.yml
-cp -n config/docker/local.yml config/local.yml
+cp -b config/docker/database.yml config/database.yml
+cp -b config/docker/redis.yml config/redis.yml
+cp -b config/docker/local.yml config/local.yml
 
 docker-compose up -d
 
