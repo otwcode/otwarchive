9acc2c58ac4da7a348e2a1775d37bc03c75ad119
weeklies <80141759+weeklies@users.noreply.github.com>
2022-07-12 14:37:45 +0800

    AO3-6144 Prevent protected users' works from being cited as related works (#4244)
    * AO3-6144 Prevent protected users' works from being cited as related works
    
    * Fix return statement
    
    * Hound fixes
    
    * AO3-6144 Feedback

--- app/models/work.rb
+++ app/models/work.rb
@@ -180,6 +180,21 @@ class Work < ApplicationRecord
     end
   end
 
+  validate :new_parents_can_be_cited
+
+  def new_parents_can_be_cited
+    return if self.new_parent.blank?
+
+    parent = self.new_parent[:parent]
+    return unless parent.respond_to?(:pseuds)
+    return if parent.anonymous? || parent.unrevealed?
+
+    users = parent.pseuds.collect(&:user).uniq
+    users.each do |user|
+      self.errors.add(:base, ts("You can't use the related works function to cite works by #{user.login}.")) if user.protected_user
+    end
+  end
+
   enum comment_permissions: {
     enable_all: 0,
     disable_anon: 1,
