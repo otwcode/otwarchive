ea7298ab193361b4f018d447e581939a07d4f079
Brian Austin <brianjaustin@gmail.com>
2022-07-24 18:27:46 -0400

    AO3-6361 Record admin email change

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -136,9 +136,22 @@ describe User do
       expect(existing_user.renamed_at).to eq(Time.current)
     end
 
-    it "does not set renamed_at when username is not changed" do
-      existing_user.update(email: "newemail@example.com")
-      expect(existing_user.renamed_at).to be_nil
+    context "email is changed" do
+      before do
+        existing_user.update!(email: "newemail@example.com")
+        existing_user.reload
+      end
+
+      it "does not set renamed_at" do
+        expect(existing_user.renamed_at).to be_nil
+      end
+
+      it "creates a log item" do
+        log_item = existing_user.log_items.last
+        expect(log_item.action).to eq(ArchiveConfig.ACTION_NEW_EMAIL)
+        expect(log_item.admin_id).to be_nil
+        expect(log_item.note).to eq("Change made by user")
+      end
     end
 
     context "username was recently changed" do
@@ -171,6 +184,23 @@ describe User do
       it "allows another rename" do
         expect(existing_user.update(login: "new")).to be_truthy
         expect(existing_user.login).to eq("new")
+      end
+    end
+
+    context "as an admin" do
+      let(:admin) { create(:admin) }
+
+      before do
+        User.current_user = admin
+        existing_user.update!(email: "new_email@example.com", profile: nil)
+        existing_user.reload
+      end
+
+      it "saves an admin log item" do
+        log_item = existing_user.log_items.last
+        expect(log_item.action).to eq(ArchiveConfig.ACTION_NEW_EMAIL)
+        expect(log_item.admin_id).to eq(admin.id)
+        expect(log_item.note).to eq("Change made by admin #{admin.login}")
       end
     end
   end
