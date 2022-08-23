cca7be383630d530f5f0b462e413e5cc2d0e06c0
EchoEkhi <echoekhi@gmail.com>
2022-08-23 15:24:23 +0100

    AO3-6365 Add tests

--- app/policies/mute_policy.rb
+++ app/policies/mute_policy.rb
@@ -1,4 +1,4 @@
-class Muteolicy < ApplicationPolicy
+class MutePolicy < ApplicationPolicy
   def index?
     user_has_roles?(%w[policy_and_abuse support superadmin])
   end
---
+++ features/step_definitions/mute_steps.rb
@@ -0,0 +1,21 @@
+Given "the user {string} has muted the user {string}" do |muter, muted|
+  muter = ensure_user(muter)
+  muted = ensure_user(muted)
+  Mute.create!(muter: muter, muted: muted)
+end
+
+Given "there are {int} muted users per page" do |amount|
+  allow(Mute).to receive(:per_page).and_return(amount)
+end
+
+Then "the user {string} should have a mute for {string}" do |muter, muted|
+  muter = User.find_by(login: muter)
+  muted = User.find_by(login: muted)
+  expect(Mute.find_by(muter: muter, muted: muted)).to be_present
+end
+
+Then "the user {string} should not have a mute for {string}" do |muter, muted|
+  muter = User.find_by(login: muter)
+  muted = User.find_by(login: muted)
+  expect(Mute.find_by(muter: muter, muted: muted)).to be_blank
+end
