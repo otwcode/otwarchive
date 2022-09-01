9af98d3e31b9eddabf60958144eb6700843c74ba
Brian Austin <brianjaustin@gmail.com>
2022-09-01 15:02:55 -0400

    Move wrangler message

--- app/models/concerns/wrangleable.rb
+++ app/models/concerns/wrangleable.rb
@@ -10,7 +10,7 @@ module Wrangleable
 
   def update_last_wrangling_activity
     current_user = User.current_user
-    return unless current_user.respond_to?(:is_tag_wrangler?) && current_user&.is_tag_wrangler?
+    return unless current_user.respond_to?(:is_tag_wrangler?) && current_user.is_tag_wrangler?
 
     last_activity = LastWranglingActivity.find_or_create_by(user: current_user)
     last_activity.touch
