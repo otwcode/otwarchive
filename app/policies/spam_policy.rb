6cd1eaf594ae74e5fab3a411109a2c8e6b225375
Cesium <cindydxz@gmail.com>
2022-09-14 15:03:02 -0700

    fix indents in spam policy

--- app/policies/spam_policy.rb
+++ app/policies/spam_policy.rb
@@ -1,9 +1,9 @@
 class SpamPolicy < ApplicationPolicy
-    MANAGE_SPAM = %w[superadmin policy_and_abuse].freeze
+  MANAGE_SPAM = %w[superadmin policy_and_abuse].freeze
 
-   def index?
-     user_has_roles?(MANAGE_SPAM)
-   end
+  def index?
+    user_has_roles?(MANAGE_SPAM)
+  end
 
-   alias bulk_update? index?
- end
+  alias bulk_update? index?
+end
