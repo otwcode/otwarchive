1eab6fcb22d8a8b943445d50e64f2fb36a51ed05
Ceithir <ceathramh@gmail.com>
2022-11-08 17:35:16 +0100

    Hound
    ?

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -196,9 +196,7 @@ class TagsController < ApplicationController
       else
         flash[:notice] = ts('Tag was successfully created.')
       end
-      if !@tag.canonical? # If tag already canonical, do not uncanonize it
-        @tag.update_attribute(:canonical, tag_params[:canonical])
-      end
+      @tag.update_attribute(:canonical, tag_params[:canonical]) unless @tag.canonical? # If tag already canonical, do not uncanonize it
       redirect_to edit_tag_path(@tag)
     else
       render(action: 'new') && return
