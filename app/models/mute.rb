229dc5b1160743338e3e6f2713c112df9ec742f6
EchoEkhi <echoekhi@gmail.com>
2022-11-26 19:12:40 +0000

    AO3-6365 Fix translations

--- app/models/mute.rb
+++ app/models/mute.rb
@@ -8,30 +8,28 @@ class Mute < ApplicationRecord
   validates :muted_id, uniqueness: { scope: :muter_id }
 
   validate :check_self
-  validate :check_official, if: :muted
-  validate :check_mute_limit
-
-  after_create :update_cache
-  after_destroy :update_cache
-
   def check_self
     errors.add(:muted, :self) if muted == muter
   end
 
+  validate :check_official, if: :muted
   def check_official
     errors.add(:muted, :official) if muted.official
   end
 
+  validate :check_mute_limit
   def check_mute_limit
     errors.add(:muted, :limit) if muter.muted_users.count >= ArchiveConfig.MAX_MUTED_USERS
+  end
+
+  after_create :update_cache
+  after_destroy :update_cache
+  def update_cache
+    Rails.cache.write(mute_css_key(muter), mute_css_uncached(muter))
   end
 
   def muted_byline=(byline)
     pseuds = Pseud.parse_byline(byline, assume_matching_login: true)
     self.muted = pseuds.first.user unless pseuds.empty?
-  end
-
-  def update_cache
-    Rails.cache.write(mute_css_key(muter), mute_css_uncached(muter))
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -73,12 +73,6 @@ class User < ApplicationRecord
   has_many :mutes_as_muter, class_name: "Mute", dependent: :delete_all, inverse_of: :muter, foreign_key: :muter_id
   has_many :muted_users, through: :mutes_as_muter, source: :muted
 
-  # The mute (if it exists) with this user as the muter and
-  # User.current_user as the muted:
-  has_one :mute_of_current_user,
-          -> { where(muted: User.current_user) },
-          class_name: "Mute", foreign_key: :muter_id, inverse_of: :muter
-
   # The mute (if it exists) with User.current_user as the muter and this
   # user as the muted:
   has_one :mute_by_current_user,
