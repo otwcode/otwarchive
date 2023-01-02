f9c1679a54026863f553ebe91b2bacef711819f5
Sarken <sarken@gmail.com>
2023-01-02 05:40:08 -0500

    AO3-6428 Fix downloads and attachments of 0-posted-chapter works
    AO3-6428 Create an instance variable for draft chapter inclusion
    
    AO3-6428 Tests

--- spec/support/shared_examples/mailer_shared_examples.rb
+++ spec/support/shared_examples/mailer_shared_examples.rb
@@ -38,3 +38,21 @@ shared_examples "it retries and fails on" do |error|
     end
   end
 end
+
+shared_examples "an email with a deleted work attached" do
+  it "has html and txt attachments" do
+    expect(email.attachments.length).to eq(2)
+    expect(email.attachments).to contain_exactly(
+      an_object_having_attributes(filename: "#{work.title}.html"),
+      an_object_having_attributes(filename: "#{work.title}.txt")
+    )
+  end
+
+  it "includes draft chapters in attachments" do
+    download = Download.new(work, mime_type: "text/html", include_draft_chapters: true)
+    html = DownloadWriter.new(download).generate_html
+    encoded_html = ::Mail::Encodings::Base64.encode(html)
+    expect(email.attachments["#{work.title}.html"].body.raw_source).to eq(encoded_html)
+    expect(email.attachments["#{work.title}.txt"].body.raw_source).to eq(encoded_html)
+  end
+end
