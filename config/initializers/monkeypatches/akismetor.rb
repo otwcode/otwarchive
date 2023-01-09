c54bf790ce69aa03b24758056291cd8e4f70c6d2
redsummernight <redsummernight@users.noreply.github.com>
2023-01-08 21:08:13 -0500

    Update TODO

--- config/initializers/monkeypatches/akismetor.rb
+++ config/initializers/monkeypatches/akismetor.rb
@@ -1,7 +1,7 @@
 # Patch Akismetor, which is not Ruby 3 compatible, as it uses URI.escape:
 # https://github.com/freerobby/akismetor/blob/1cef6c0e237dd69b3f6ae1ee8d719c0862cd77e4/lib/akismetor.rb#L53-L56
 #
-# TODO: Migrate away from this library and handle the API calls ourselves.
+# TODO: AO3-6454 Remove this gem and handle the API calls ourselves.
 
 class Akismetor
   def attributes_for_post
