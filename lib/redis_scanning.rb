155d0b867cd99c7dfb97a549bac3a84c0de697ef
tickinginstant <tickinginstant@users.noreply.github.com>
2023-01-20 18:23:47 -0500

    AO3-6462 Make scan_set_in_batches return more consistent batch sizes. (#4438)

--- lib/redis_scanning.rb
+++ lib/redis_scanning.rb
@@ -1,11 +1,5 @@
 module RedisScanning
-  def scan_set_in_batches(redis, key, batch_size:)
-    cursor = "0"
-
-    loop do
-      cursor, batch = redis.sscan(key, cursor, count: batch_size)
-      yield batch unless batch.empty?
-      break if cursor == "0"
-    end
+  def scan_set_in_batches(redis, key, batch_size:, &block)
+    redis.sscan_each(key, count: batch_size).each_slice(batch_size, &block)
   end
 end
