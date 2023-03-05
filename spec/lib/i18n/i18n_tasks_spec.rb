9e34d339c1d753fe488e3c448646931e2a67930d
Bilka <Bilka2@users.noreply.github.com>
2023-03-05 16:28:52 +0100

    AO3-6003 Changed config to search files of all locales
    But tests run only for the English files.

--- spec/lib/i18n/i18n_tasks_spec.rb
+++ spec/lib/i18n/i18n_tasks_spec.rb
@@ -3,29 +3,29 @@ require "./lib/i18n_newlines_tasks.rb"
 
 describe I18n do
   let(:i18n) { I18n::Tasks::BaseTask.new }
-  let(:missing_keys) { i18n.missing_keys }
-  let(:unused_keys) { i18n.unused_keys }
-  let(:newlines) { i18n.extend(I18nNewlinesTasks).newlines }
+  let(:missing_keys) { i18n.missing_keys(locales: ["en"], types: [:used, :plural]) }
+  let(:unused_keys) { i18n.unused_keys(locales: ["en"]) }
+  let(:newlines) { i18n.extend(I18nNewlinesTasks).newlines(locales: ["en"]) }
 
-  it "does not have missing keys" do
+  it "en does not have missing keys" do
     expect(missing_keys).to be_empty,
-                            "Missing #{missing_keys.leaves.count} i18n keys, run `i18n-tasks missing' to show them"
+                            "Missing #{missing_keys.leaves.count} i18n keys, run `bundle exec i18n-tasks missing -l en -t used,plural' to show them"
   end
 
-  it "does not have unused keys" do
+  it "en does not have unused keys" do
     expect(unused_keys).to be_empty,
-                           "#{unused_keys.leaves.count} unused i18n keys, run `i18n-tasks unused' to show them"
+                           "#{unused_keys.leaves.count} unused i18n keys, run `i18n-tasks unused -l en' to show them"
   end
 
-  it "files are normalized" do
-    non_normalized = i18n.non_normalized_paths
+  it "en files are normalized" do
+    non_normalized = i18n.non_normalized_paths.select { |path| path.end_with?("en.yml") }
     error_message = "The following files need to be normalized:\n" \
                     "#{non_normalized.map { |path| "  #{path}" }.join("\n")}\n" \
-                    "Please run `i18n-tasks normalize' to fix"
+                    "Please run `i18n-tasks normalize -l en' to fix"
     expect(non_normalized).to be_empty, error_message
   end
 
-  it "does not have values with newlines" do
+  it "en does not have values with newlines" do
     error_message = "The following locale keys have values that contain newlines:\n" \
                     "#{newlines.key_names(root: true)}"
     expect(newlines).to be_empty, error_message
