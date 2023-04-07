be5d939c96bc5063f5f1c292f78171bf652e8917
calm <forceofcalm@gmail.com>
2023-04-07 11:43:46 -0400

    removed abuse mailer preview

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -16,12 +16,7 @@ class UserMailerPreview < ApplicationMailerPreview
     second_creatorship, first_creator = creatorship_notification_data
     UserMailer.creatorship_request(second_creatorship.id, first_creator.id)
   end
-
-  # Sends an email to the user when they submit an abuse report
-  # def abuse_report
-  #   UserMailer.abuse_report(AbuseReport.last.id)
-  # end
-
+  
   private
 
   def creatorship_notification_data
