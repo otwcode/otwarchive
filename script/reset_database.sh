041076c99f8b807a68d57a65a8c362d3d37b89c9
redsummernight <redsummernight@users.noreply.github.com>
2023-04-08 15:07:59 -0400

    Move all reset code into the case statement

--- script/reset_database.sh
+++ script/reset_database.sh
@@ -3,24 +3,21 @@
 set -ex
 
 case "${RAILS_ENV}" in
-test) ;;
-development) ;;
+test)
+  bundle install
+  bundle exec rake db:reset_and_migrate
+  ;;
+development)
+  bundle install
+  bundle exec rake db:otwseed
+  bundle exec rake skins:load_site_skins
+  bundle exec rake search:index_tags
+  bundle exec rake search:index_works
+  bundle exec rake search:index_pseuds
+  bundle exec rake search:index_bookmarks
+  ;;
 *)
   echo "Only supported in test and development (e.g. 'RAILS_ENV=test ./script/reset_database.sh')"
   exit 1
   ;;
 esac
-
-bundle install
-
-if [ "${RAILS_ENV}" = "test" ] ; then
-  bundle exec rake db:reset_and_migrate
-  exit 0
-fi
-
-bundle exec rake db:otwseed
-bundle exec rake skins:load_site_skins
-bundle exec rake search:index_tags
-bundle exec rake search:index_works
-bundle exec rake search:index_pseuds
-bundle exec rake search:index_bookmarks
