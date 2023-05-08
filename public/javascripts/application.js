6b7a82be9e6e10ddabf5b777f28f6f5415983221
Sarken <sarken@gmail.com>
2023-05-07 23:37:37 -0400

    AO3-6524 Remove JavaScript for removing final commas in IE8 and lower

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -13,9 +13,6 @@ $j(document).ready(function() {
     setupDropdown();
     updateCachedTokens();
 
-    // remove final comma from comma lists in older browsers
-    $j('.commas li:last-child').addClass('last');
-
     // add clear to items on the splash page in older browsers
     $j('.splash').children('div:nth-of-type(odd)').addClass('odd');
 
