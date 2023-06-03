e80ea077fcbdf334e6615f8f6374532c0a8ad649
Sarken <sarken@gmail.com>
2023-06-02 21:35:59 -0400

    AO3-5499 Update routes.rb to remove send_activation

--- config/routes.rb
+++ config/routes.rb
@@ -199,7 +199,6 @@ Otwarchive::Application.routes.draw do
         get :confirm_delete_user_creations
         post :destroy_user_creations
         post :activate
-        post :send_activation
         get :check_user
       end
       collection do
