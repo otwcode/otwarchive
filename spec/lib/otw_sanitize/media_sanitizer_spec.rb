5bbd45151aa8f94abefd432efb34a433d9bdc4e8
sarken <sarken@gmail.com>
2023-06-19 04:15:51 -0400

    Revert "AO3-6199 Rename variable and method for banned media embed sources" (#4534)
    Revert "AO3-6199 Rename variable and method for banned media embed sources (#4356)"
    
    This reverts commit 1db913a1352446ac58fea4fecf8b28aff4327878.

--- spec/lib/otw_sanitize/media_sanitizer_spec.rb
+++ spec/lib/otw_sanitize/media_sanitizer_spec.rb
@@ -101,11 +101,11 @@ describe OtwSanitize::MediaSanitizer do
 
       context "given a blacklisted source" do
         before do
-          ArchiveConfig.BANNED_MULTIMEDIA_SRCS = ["google.com"]
+          ArchiveConfig.BLACKLISTED_MULTIMEDIA_SRCS = ["google.com"]
         end
 
         after do
-          ArchiveConfig.BANNED_MULTIMEDIA_SRCS = []
+          ArchiveConfig.BLACKLISTED_MULTIMEDIA_SRCS = []
         end
 
         it "strips the source element" do
