af5dcf3299e5821bdd70e25f613b3c9a08067f2d
tickinginstant <tickinginstant@users.noreply.github.com>
2023-07-10 19:45:02 -0400

    AO3-4867 & AO3-4868 More OwnedTagSet tests.

--- spec/models/owned_tag_set_spec.rb
+++ spec/models/owned_tag_set_spec.rb
@@ -7,8 +7,8 @@ describe OwnedTagSet do
   let(:user) { create(:user) }
 
   describe "#owner_changes=" do
-    context "when assigning a user that is not an owner" do
-      it "makes the user an owner" do
+    context "given a user that is not an owner" do
+      it "makes the user an owner when assigning their login" do
         owned_tag_set.update(owner_changes: user.login)
         expect(owned_tag_set.owners.reload).to include(user.default_pseud)
       end
@@ -16,39 +16,59 @@ describe OwnedTagSet do
       context "when there is another user with a pseud of the same name" do
         let!(:other_pseud) { create(:pseud, name: user.login) }
 
-        it "makes the first user an owner" do
+        it "makes the first user an owner when assigning their login" do
           owned_tag_set.update(owner_changes: user.login)
           expect(owned_tag_set.owners.reload).to include(user.default_pseud)
+          expect(owned_tag_set.owners.reload).not_to include(other_pseud)
+        end
+
+        it "makes the second user an owner when assigning the full byline for the other user's pseud" do
+          owned_tag_set.update(owner_changes: "#{other_pseud.name} (#{other_pseud.user.login})")
+          expect(owned_tag_set.owners.reload).not_to include(user.default_pseud)
+          expect(owned_tag_set.owners.reload).to include(other_pseud)
         end
       end
     end
 
-    context "when assigning a user that is an owner" do
+    context "given a user that is a co-owner" do
       before do
         owned_tag_set.add_owner(user.default_pseud)
         owned_tag_set.save
         owned_tag_set.reload
       end
 
-      it "removes the user as an owner" do
+      it "removes the user as an owner when assigning their login" do
         owned_tag_set.update(owner_changes: user.login)
         expect(owned_tag_set.owners.reload).not_to include(user.default_pseud)
       end
 
-      context "when there is another user with a pseud of the same name" do
+      context "when there is another user with a pseud of the same name who is also a co-owner" do
         let!(:other_pseud) { create(:pseud, name: user.login) }
 
-        it "removes the first user as an owner" do
+        before do
+          owned_tag_set.add_owner(other_pseud)
+          owned_tag_set.save
+          owned_tag_set.reload
+        end
+
+        it "removes the first user as an owner when assigning their login" do
           owned_tag_set.update(owner_changes: user.login)
           expect(owned_tag_set.owners.reload).not_to include(user.default_pseud)
+          expect(owned_tag_set.owners.reload).to include(other_pseud)
+        end
+
+        it "removes the second user an owner when assigning the full byline for the other user's pseud" do
+          owned_tag_set.update(owner_changes: "#{other_pseud.name} (#{other_pseud.user.login})")
+          expect(owned_tag_set.owners.reload).to include(user.default_pseud)
+          expect(owned_tag_set.owners.reload).not_to include(other_pseud)
         end
       end
     end
   end
 
   describe "#moderator_changes=" do
-    context "when assigning a user that is not a moderator" do
-      it "makes the user a moderator" do
+    context "given a user that is not a moderator" do
+      it "makes the user a moderator when assigning their login" do
         owned_tag_set.update(moderator_changes: user.login)
         expect(owned_tag_set.moderators.reload).to include(user.default_pseud)
       end
@@ -56,31 +76,51 @@ describe OwnedTagSet do
       context "when there is another user with a pseud of the same name" do
         let!(:other_pseud) { create(:pseud, name: user.login) }
 
-        it "makes the first user a moderator" do
+        it "makes the first user a moderator when assigning their login" do
           owned_tag_set.update(moderator_changes: user.login)
           expect(owned_tag_set.moderators.reload).to include(user.default_pseud)
+          expect(owned_tag_set.moderators.reload).not_to include(other_pseud)
+        end
+
+        it "makes the second user a moderator when assigning the full byline for the other user's pseud" do
+          owned_tag_set.update(moderator_changes: "#{other_pseud.name} (#{other_pseud.user.login})")
+          expect(owned_tag_set.moderators.reload).not_to include(user.default_pseud)
+          expect(owned_tag_set.moderators.reload).to include(other_pseud)
         end
       end
     end
 
-    context "when assigning a user that is a moderator" do
+    context "given a user that is a moderator" do
       before do
         owned_tag_set.add_moderator(user.default_pseud)
         owned_tag_set.save
         owned_tag_set.reload
       end
 
-      it "removes the user as a moderator" do
+      it "removes the user as a moderator when assigning their login" do
         owned_tag_set.update(moderator_changes: user.login)
         expect(owned_tag_set.moderators.reload).not_to include(user.default_pseud)
       end
 
-      context "when there is another user with a pseud of the same name" do
+      context "when there is another user with a pseud of the same name who is also a moderator" do
         let!(:other_pseud) { create(:pseud, name: user.login) }
 
-        it "removes the first user as a moderator" do
+        before do
+          owned_tag_set.add_moderator(other_pseud)
+          owned_tag_set.save
+          owned_tag_set.reload
+        end
+
+        it "removes the first user as a moderator when assigning their login" do
           owned_tag_set.update(moderator_changes: user.login)
           expect(owned_tag_set.moderators.reload).not_to include(user.default_pseud)
+          expect(owned_tag_set.moderators.reload).to include(other_pseud)
+        end
+
+        it "removes the second user a moderator when assigning the full byline for the other user's pseud" do
+          owned_tag_set.update(moderator_changes: "#{other_pseud.name} (#{other_pseud.user.login})")
+          expect(owned_tag_set.moderators.reload).to include(user.default_pseud)
+          expect(owned_tag_set.moderators.reload).not_to include(other_pseud)
         end
       end
     end
