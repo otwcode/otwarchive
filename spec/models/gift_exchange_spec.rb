c42ac588d2ddee37d22d5018c03f389173916b8e
Brian Austin <brianjaustin@gmail.com>
2023-07-11 21:16:34 -0400

    Allowed fix + rubocop rennovations

--- spec/models/gift_exchange_spec.rb
+++ spec/models/gift_exchange_spec.rb
@@ -19,5 +19,27 @@ describe GiftExchange do
     it "succeeds with a valid gift exchange" do
       expect(challenge.save).to be_truthy
     end
+
+    context "when allowed offers is bellow required offers" do
+      before do
+        challenge.offers_num_allowed = challenge.requests_num_required - 1
+      end
+
+      it "sets allowed offers to the same number as required" do
+        challenge.save!
+        expect(challenge.offers_num_allowed).to eq(challenge.offers_num_required)
+      end
+    end
+
+    context "when allowed requests is below required requests" do
+      before do
+        challenge.requests_num_allowed = challenge.requests_num_required - 1
+      end
+
+      it "sets allowed requests to same number as required" do
+        challenge.save!
+        expect(challenge.requests_num_allowed).to eq(challenge.requests_num_required)
+      end
+    end
   end
 end
