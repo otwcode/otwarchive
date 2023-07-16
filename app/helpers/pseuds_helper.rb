otwcode/otwarchive

commit 09c7c2f064b72f7feb6b30f9ab60d837bfd477f4

AO3-5901 Limit number of pseuds on profile and sidebar (#4554)
* AO3-5901 Define a profile scope for profile and sidebar display

* AO3-5901 Limit number of pseuds shown in the user sidebar

* AO3-5901 Limit number of pseuds shown on the user's profile

* AO3-5901 Introduce a profile presenter

The view was a bit confusing, pulling information from various parts of the user entity, the profile, and the preferences.

In extracting it to a presenter, I realised that if the preference was set to show it, we were showing the birthday section even if the field itself was empty, so I changed that to not show the section.

Refactor some specs to remove duplication.

Update ignore file for Brakeman to ignore the newly introduced false positive.

* AO3-5901 Get rid of deprecation messages

These yaks aren't going to shave themselves...

* AO3-5901 Always show the currently selected pseud

Fixes a long-standing bug where the intention of the code was always to show the current pseud at the top of the pseud switcher.

* AO3-5901 pseud cukes

* AO3-5901 Remove profile request spec

The maximum number of pseuds on the profile page is now tested in `pseuds.feature`.

* AO3-5901 Expand cuke with user page scenarios

And remove user request spec, as per review.

* AO3-5901 Different text for profile pseud link

Show different link text, depending on whether there are more pseuds in total than there are shown on profile.

* AO3-5901 Newly surfaced Hydra barkings

I mean Hound. Yes, that's what I mean.

* Double quoting strings
* Using `let` like DHH intended us to instead of instance variables
* Assertive language in spec descriptions
* Explicit expectations ("Do not use `be` without an argument")

* Fixes from cesy's commit cf8568d0e422f0782cd5e23cf0b0b200b419287b which could not be cherry-picked

Co-authored-by: Cesy <cesy.avon@gmail.com>

* Dynamically load pseuds on profile page from commit 9e68b3e

app/helpers/pseuds_helper.rb
@@ -1,11 +1,42 @@
 module PseudsHelper
-  
-  # Prints array of pseuds with links to user pages
-  # used on Profile page
-  def print_pseud_list(pseuds)
-    pseuds.includes(:user).collect { |pseud| span_if_current(pseud.name, [pseud.user, pseud]) }.join(", ").html_safe
+  # Returns a list of pseuds, with links to each pseud.
+  #
+  # Used on Profile page, and by ProfileController#pseuds.
+  #
+  # The pseuds argument should be a single page of the user's pseuds, generated
+  # by calling user.pseuds.paginate(page: 1) or similar. This allows us to
+  # insert a remote link to dynamically insert the next page of pseuds.
+  def print_pseud_list(user, pseuds, first: true)
+    links = pseuds.map do |pseud|
+      link_to(pseud.name, [user, pseud])
+    end
+
+    difference = pseuds.total_entries - pseuds.length - pseuds.offset
+
+    if difference.positive?
+      links << link_to(
+        t("profile.pseud_list.more_pseuds", count: difference),
+        pseuds_user_profile_path(user, page: pseuds.next_page),
+        remote: true, id: "more_pseuds"
+      )
+    end
+
+    more_pseuds_connector = tag.span(
+      t("support.array.last_word_connector"),
+      id: "more_pseuds_connector"
+    )
+
+    if first
+      to_sentence(links,
+                  last_word_connector: more_pseuds_connector)
+    else
+      links.unshift("")
+      to_sentence(links,
+                  last_word_connector: more_pseuds_connector,
+                  two_words_connector: more_pseuds_connector)
+    end
   end
-  
+
   # used in the sidebar
   def print_pseud_selector(pseuds)
     pseuds -= [@pseud] if @pseud && @pseud.new_record?

app/views/users/_sidebar.html.erb
@@ -5,9 +5,10 @@
   <li><%= span_if_current ts("Profile"), user_profile_path(@user) %></li>
   <% if @user.pseuds.size > 1 %>
     <li class="pseud" aria-haspopup="true">
-      <a href="#" title="<%= ts("Pseud Switcher") %>"><%= ts("Pseuds") %></a>
+      <% pseud_link_text = current_page?(@user) ? ts("Pseuds") : (@pseud ? @pseud.name : @user.login) %>
+      <a href="#" title="<%= ts("Pseud Switcher") %>"><%= pseud_link_text %></a>
       <ul class="expandable secondary">
-        <%= print_pseud_selector(@user.pseuds) %>
+        <%= print_pseud_selector(@user.pseuds.abbreviated_list) %>
         <li><%= span_if_current ts("All Pseuds (%{pseud_number})", :pseud_number => @user.pseuds.count), user_pseuds_path(@user) %></li>
       </ul>
     </li>

features/other_a/pseuds.feature
@@ -134,3 +134,33 @@ Scenario: Comments reflect pseud changes immediately
     And I view the work "Interesting" with comments
   Then I should see "after (myself)" within ".comment h4.byline"
     And I should not see "before (myself)"
+
+Scenario: Many pseuds
+
+  Given there are 3 pseuds per page
+    And "Zaphod" has the pseud "Slartibartfast"
+    And "Zaphod" has the pseud "Agrajag"
+    And "Zaphod" has the pseud "Betelgeuse"
+    And I am logged in as "Zaphod"
+
+  When I view my profile
+  Then I should see "Zaphod" within "dl.meta"
+    And I should see "Agrajag" within "dl.meta"
+    And I should see "Betelgeuse" within "dl.meta"
+    And I should not see "Slartibartfast" within "dl.meta"
+    And I should see "1 more pseud" within "dl.meta"
+
+  When I go to my user page
+  Then I should see "Zaphod" within "ul.expandable"
+    And I should see "Agrajag" within "ul.expandable"
+    And I should see "Betelgeuse" within "ul.expandable"
+    And I should not see "Slartibartfast" within "ul.expandable"
+    And I should see "All Pseuds (4)" within "ul.expandable"
+
+  When I go to my "Slartibartfast" pseud page
+  Then I should see "Slartibartfast" within "li.pseud > a"
+    And I should not see "Slartibartfast" within "ul.expandable"
+
+  When there are 10 pseuds per page
+    And I view my profile
+  Then I should see "Zaphod, Agrajag, Betelgeuse, and Slartibartfast" within "dl.meta"

app/controllers/profile_controller.rb
@@ -1,14 +1,14 @@
 class ProfileController < ApplicationController
+  before_action :load_user_and_pseuds
 
   def show
     @user = User.find_by(login: params[:user_id])
-    if @user.nil?
-      flash[:error] = ts("Sorry, there's no user by that name.")
-      redirect_to '/' and return
-    elsif @user.profile.nil?
+    if @user.profile.nil?
       Profile.create(user_id: @user.id)
       @user.reload
     end
+
+    @profile = ProfilePresenter.new(@user.profile)
     #code the same as the stuff in users_controller
     if current_user.respond_to?(:subscriptions)
       @subscription = current_user.subscriptions.where(subscribable_id: @user.id,
@@ -18,4 +18,27 @@ class ProfileController < ApplicationController
     @page_subtitle = ts("%{username} - Profile", username: @user.login)
   end
 
+  def pseuds
+    respond_to do |format|
+      format.html do
+        redirect_to user_pseuds_path(@user)
+      end
+
+      format.js
+    end
+  end
+
+  private
+
+  def load_user_and_pseuds
+    @user = User.find_by(login: params[:user_id])
+
+    if @user.nil?
+      flash[:error] = ts("Sorry, there's no user by that name.")
+      redirect_to root_path
+      return
+    end
+
+    @pseuds = @user.pseuds.default_alphabetical.paginate(page: params[:page])
+  end
 end

app/decorators/profile_presenter.rb
@@ -1,0 +1,13 @@
+class ProfilePresenter < SimpleDelegator
+  def created_at
+    user.created_at.to_date
+  end
+
+  def date_of_birth
+    super if user.preference.try(:date_of_birth_visible)
+  end
+
+  def email
+    user.email if user.preference.try(:email_visible)
+  end
+end

app/models/pseud.rb
@@ -86,6 +86,10 @@ class Pseud < ApplicationRecord
   after_update :expire_caches
   after_commit :reindex_creations, :touch_comments
 
+  scope :alphabetical, -> { order(:name) }
+  scope :default_alphabetical, -> { order(is_default: :desc).alphabetical }
+  scope :abbreviated_list, -> { default_alphabetical.limit(ArchiveConfig.ITEMS_PER_PAGE) }
+
   def self.not_orphaned
     where("user_id != ?", User.orphan_account)
   end

app/views/profile/pseuds.js.erb
@@ -1,0 +1,2 @@
+$j("#more_pseuds_connector").remove();
+$j("#more_pseuds").replaceWith("<%= j print_pseud_list(@user, @pseuds, first: false) %>")

app/views/profile/show.html.erb
@@ -2,37 +2,37 @@
   <%= render 'users/header' %>
 
   <!--main content-->
-  <% unless @user.profile.title.blank? %>
-    <h3 class="heading"><%=h @user.profile.title %></h3>
+  <% if @profile.title.present? %>
+    <h3 class="heading"><%=h @profile.title %></h3>
   <% end %>
 
   <div class="wrapper">
     <dl class="meta">
-      <dt class="pseuds"><%= link_to ts("My pseuds:"), user_pseuds_path(@user) %></dt>
-      <dd class="pseuds"><%= print_pseud_list(@user.pseuds) %></dd>
+      <dt class="pseuds"><%= ts("My pseuds:") %></dt>
+      <dd class="pseuds"><%= print_pseud_list(@user, @pseuds) %></dd>
       <dt><%= ts("I joined on:") %></dt>
-      <dd><%= ts("%{date}", :date => l(@user.created_at.to_date)) %></dd>
+      <dd><%= l(@profile.created_at) %></dd>
       <dt><%= ts("My user ID is:") %></dt>
       <dd><%= @user.id %></dd>
-      <% if @user.profile.location? %>
+      <% if @profile.location %>
         <dt class="location"><%=h ts("I live in:") %></dt>
-        <dd><%=h @user.profile.location %></dd>
+        <dd><%=h @profile.location %></dd>
       <% end %>
-      <% if @user.preference.try(:date_of_birth_visible) %>
+      <% if @profile.date_of_birth %>
         <dt class="birthday"><%=h ts("My birthday:") %></dt>
-        <dd class="birthday"><%=l(@user.profile.date_of_birth) unless @user.profile.date_of_birth.blank? %></dd>
+        <dd class="birthday"><%=l(@profile.date_of_birth) %></dd>
       <% end %>
-      <% if @user.preference.try(:email_visible) %>
+      <% if @profile.email %>
         <dt class="email"><%=h ts("My email address:") %></dt>
-        <dd class="email"><%= mail_to @user.email, nil, :encode => "hex" %></dd>
+        <dd class="email"><%= mail_to @profile.email, nil, :encode => "hex" %></dd>
       <% end %>
     </dl>
   </div>
 
-  <% unless @user.profile.about_me.blank? %>
+  <% if @profile.about_me.present? %>
     <div class="bio module">
       <h3 class="heading"><%=h ts("Bio") %></h3>
-      <blockquote class="userstuff"><%=raw sanitize_field(@user.profile, :about_me) %></blockquote>
+      <blockquote class="userstuff"><%=raw sanitize_field(@profile, :about_me) %></blockquote>
     </div>
   <% end %>
   <!--/content-->

commit d99747574ed348866cdeed6cda507b7636612582

AO3-5901 Pseud switcher text on the New Pseud page (#4579)
* AO3-5901 Pseud switcher text on the New Pseud page

Provide fallback text for the edge case of the New Pseud page, where `@pseud` is defined, but doesn't have a `name` yet.

* Update app/views/users/_sidebar.html.erb

Co-authored-by: sarken <sarken@gmail.com>

* AO3-5901 Ensure pseud switcher shows current pseud

When we started limiting the number of pseuds in the sidebar, the way to guarantee that the current pseud would show up in the sidebar was to show it at the top of the pseud switcher.

However, [AO3-6249](https://otwarchive.atlassian.net/browse/AO3-6249) decided that the pseud switcher should always say "Pseuds" at the top.

We broke this behaviour in https://github.com/otwcode/otwarchive/pull/4554.

Fixing it in c3e4ab9c04e0299d20799f90a224b7d24b2f7a6b resulted in the current pseud not showing in the sidebar in some conditions.

Move or add the current pseud at the beginning of the list, whether or not it is present in the abbreviated list.

We are aware that this could make the abbreviated list in the pseud switcher show `ITEMS_PER_PAGE` + 1 pseuds in some cases, discuss with @sarken if you disagree.

* AO3-5901 Don't use instance vars in helpers

The Hound has arisen from its slumber as soon as I touched some ancient scrolls, and so I've tried to appease it with the following sacrifices:
- add spaces after and before curly bois (`{` and `}`)
- IN-TER-PO-LATE! IN-TER-PO-LATE! (AKA prefer interpolation to string concatenation)
- stop using instance vars in helper method

The latter change has morally forced my hand to:
-  separate the method that determines which pseuds to show in the sidebar selector from the method that outputs the HTML
- remove the `print_` prefix from the method that I touched, as [per precedent](50f7b7e2c7c78aad7b2e5651f379cf52c28061cf)

* AO3-5901 Remove code unused since 2013

app/helpers/pseuds_helper.rb
@@ -37,9 +37,15 @@ module PseudsHelper
     end
   end
 
+  def pseuds_for_sidebar(user, pseud)
+    pseuds = user.pseuds.abbreviated_list - [pseud]
+    pseuds = pseuds.sort
+    pseuds = [pseud] + pseuds if pseud && !pseud.new_record?
+    pseuds
+  end
+
   # used in the sidebar
-  def print_pseud_selector(pseuds)
-    pseuds -= [@pseud] if @pseud && @pseud.new_record?
-    list = pseuds.sort.collect {|pseud| "<li>" + span_if_current(pseud.name, [pseud.user, pseud]) + "</li>"}.join("").html_safe
+  def pseud_selector(pseuds)
+    pseuds.collect { |pseud| "<li>#{span_if_current(pseud.name, [pseud.user, pseud])}</li>" }.join.html_safe
   end
 end

app/helpers/users_helper.rb
@@ -15,17 +15,6 @@ module UsersHelper
     current_user.is_a?(User) ? current_user.maintained_collections.present? : false
   end
 
-  def sidebar_pseud_link_text(user, pseud)
-    text = if current_page?(user)
-             ts('Pseuds')
-           elsif pseud.present? && !pseud.new_record?
-             pseud.name
-           else
-             user.login
-           end
-    (text + ' &#8595;').html_safe
-  end
-
   # Prints user pseuds with links to anchors for each pseud on the page and the description as the title
   def print_pseuds(user)
     user.pseuds.collect(&:name).join(', ')

app/views/users/_sidebar.html.erb
@@ -5,10 +5,9 @@
   <li><%= span_if_current ts("Profile"), user_profile_path(@user) %></li>
   <% if @user.pseuds.size > 1 %>
     <li class="pseud" aria-haspopup="true">
-      <% pseud_link_text = current_page?(@user) ? ts("Pseuds") : (@pseud ? @pseud.name : @user.login) %>
-      <a href="#" title="<%= ts("Pseud Switcher") %>"><%= pseud_link_text %></a>
+      <a href="#" title="<%= ts("Pseud Switcher") %>"><%= ts("Pseuds") %></a>
       <ul class="expandable secondary">
-        <%= print_pseud_selector(@user.pseuds.abbreviated_list) %>
+        <%= pseud_selector(pseuds_for_sidebar(@user, @pseud)) %>
         <li><%= span_if_current ts("All Pseuds (%{pseud_number})", :pseud_number => @user.pseuds.count), user_pseuds_path(@user) %></li>
       </ul>
     </li>

features/other_a/pseuds.feature
@@ -158,8 +158,8 @@ Scenario: Many pseuds
     And I should see "All Pseuds (4)" within "ul.expandable"
 
   When I go to my "Slartibartfast" pseud page
-  Then I should see "Slartibartfast" within "li.pseud > a"
-    And I should not see "Slartibartfast" within "ul.expandable"
+  Then I should see "Pseuds" within "li.pseud > a"
+    And I should see "Slartibartfast" within "ul.expandable"
 
   When I go to my pseuds page
   Then I should not see "Zaphod (Zaphod)" within "ul.pseud.index"