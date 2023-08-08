f8c67b7cb49066e6adc9b192abd262c3c871bcca
Ceithir <ceathramh@gmail.com>
2023-08-08 12:23:20 +0200

    Remove explicit foreign key
    Similar migrations don't appear to set one

--- db/migrate/20230717161221_add_fnok_user_id_to_log_items.rb
+++ db/migrate/20230717161221_add_fnok_user_id_to_log_items.rb
@@ -8,8 +8,7 @@ class AddFnokUserIdToLogItems < ActiveRecord::Migration[6.1]
 
         pt-online-schema-change D=#{database},t=log_items \\
           --alter "ADD `fnok_user_id` int DEFAULT NULL,
-                  ADD INDEX `index_log_items_on_fnok_user_id` (`fnok_user_id`),
-                  ADD CONSTRAINT `fk_rails_6a47a753ee" FOREIGN KEY (`fnok_user_id`) REFERENCES `users` (`id`)" \\
+                  ADD INDEX `index_log_items_on_fnok_user_id` (`fnok_user_id`)" \\
           --no-drop-old-table \\
           -uroot --ask-pass --chunk-size=5k --max-flow-ctl 0 --pause-file /tmp/pauseme \\
           --max-load Threads_running=15 --critical-load Threads_running=100 \\
@@ -23,7 +22,6 @@ class AddFnokUserIdToLogItems < ActiveRecord::Migration[6.1]
     else
       add_column :log_items, :fnok_user_id, :integer, nullable: true, default: nil
       add_index :log_items, :fnok_user_id
-      add_foreign_key :log_items, :users, column: :fnok_user_id
     end
   end
 
@@ -35,8 +33,7 @@ class AddFnokUserIdToLogItems < ActiveRecord::Migration[6.1]
         Schema Change Command:
 
         pt-online-schema-change D=#{database},t=log_items \\
-          --alter "DROP FOREIGN KEY fk_rails_6a47a753ee,
-                  DROP COLUMN `fnok_user_id`"\\
+          --alter "DROP COLUMN `fnok_user_id`"\\
           --no-drop-old-table \\
           -uroot --ask-pass --chunk-size=5k --max-flow-ctl 0 --pause-file /tmp/pauseme \\
           --max-load Threads_running=15 --critical-load Threads_running=100 \\
