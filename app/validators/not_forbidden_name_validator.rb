1433ccf557f3ac573b9933ac82f7d3ce0a843b01
Brian Austin <brianjaustin@gmail.com>
2023-08-24 14:04:25 -0400

    fix: convince i18n-tasks that message is used

--- app/validators/not_forbidden_name_validator.rb
+++ app/validators/not_forbidden_name_validator.rb
@@ -3,6 +3,7 @@ class NotForbiddenNameValidator < ActiveModel::EachValidator
     return if value.nil?
     return unless ArchiveConfig.FORBIDDEN_USERNAMES.include?(value.downcase)
 
+    # i18n-tasks-use t("activerecord.errors.messages.forbidden")
     record.errors.add(attribute, :forbidden, **options.merge(value: value))
   end
 end
