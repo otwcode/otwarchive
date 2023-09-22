b47c9194db58020110801abc11fb02726809813f
Brian Austin <brianjaustin@gmail.com>
2023-09-21 22:00:42 -0400

    doggo

--- config/initializers/monkeypatches/mailers_controller.rb
+++ config/initializers/monkeypatches/mailers_controller.rb
@@ -1,12 +1,11 @@
-Rails.application.config.after_initialize do
-  module MailersController
-    extend ActiveSupport::Concern
+module MailersController
+  extend ActiveSupport::Concern
 
-    included do
-      # Hide the dev mark in mailer previews.
-      skip_rack_dev_mark
-    end
+  included do
+    # Hide the dev mark in mailer previews.
+    skip_rack_dev_mark
   end
-
+end
+Rails.application.config.after_initialize do
   ::Rails::MailersController.include MailersController
 end
