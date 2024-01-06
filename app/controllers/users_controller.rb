f9707d0385cfb1b70860f8bf49358973d64d7c8e
Claire Carden <smclairecarden@gmail.com>
2024-01-05 20:44:28 -0600

    Updated browser title for the Edit Profile page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,6 +35,7 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
+    @page_title = ts("Edit Profile")
     authorize @user.profile if logged_in_as_admin?
   end
 
@@ -126,10 +127,11 @@ class UsersController < ApplicationController
   end
 
   def update
-    authorize @user.profile if logged_in_as_admin?
 
+    authorize @user.profile if logged_in_as_admin?
     if @user.profile.update(profile_params)
       if logged_in_as_admin? && @user.profile.ticket_url.present?
+
         link = view_context.link_to("Ticket ##{@user.profile.ticket_number}", @user.profile.ticket_url)
         AdminActivity.log_action(current_admin, @user, action: "edit profile", summary: link)
       end
