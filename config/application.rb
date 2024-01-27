1067cebf513afcef0514f853ce83ad0c1ca09a76
Brian Austin <brianjaustin@gmail.com>
2024-01-27 12:20:46 -0500

    Enable some safer new defaults

--- config/application.rb
+++ config/application.rb
@@ -128,5 +128,7 @@ module Otwarchive
                                                   authentication: ArchiveConfig.SMTP_AUTHENTICATION
                                                 })
     end
+
+    config.active_support.disable_to_s_conversion = true
   end
 end
