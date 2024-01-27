b33bbec5429ceaf987b7e80a93406a28beb32f56
Brian Austin <brianjaustin@gmail.com>
2024-01-27 18:36:17 -0500

    Fix tests

--- config/initializers/new_framework_defaults_7_0.rb
+++ config/initializers/new_framework_defaults_7_0.rb
@@ -91,7 +91,6 @@ Rails.application.config.action_dispatch.default_headers = {
   "Referrer-Policy" => "strict-origin-when-cross-origin"
 }
 
-
 # ** Please read carefully, this must be configured in config/application.rb **
 # Change the format of the cache entry.
 # Changing this default means that all new cache entries added to the cache
@@ -100,7 +99,6 @@ Rails.application.config.action_dispatch.default_headers = {
 # and you have no plans to rollback.
 # When you're ready to change format, add this to `config/application.rb` (NOT this file):
 #  config.active_support.cache_format_version = 7.0
-
 
 # Cookie serializer: 2 options
 #
--- features/support/formatter.rb
+++ features/support/formatter.rb
@@ -1,9 +1,10 @@
 # Adapted from https://github.com/tpope/fivemat
 require "cucumber/formatter/progress"
+require_relative "./elapsed_time"
 
 module Ao3Cucumber
   class Formatter < ::Cucumber::Formatter::Progress
-    include ElapsedTime
+    include ::ElapsedTime
 
     def on_test_case_started(event)
       super
@@ -27,7 +28,7 @@ module Ao3Cucumber
       @io.puts feature.location.file
       @io.flush
       @current_feature = feature
-      @start_time = Time.now
+      @start_time = Time.current
     end
 
     def after_feature
