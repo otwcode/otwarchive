edd8ae1d003908b270f7b29308915acfa5ba8039
Brian Austin <brianjaustin@gmail.com>
2024-01-28 11:10:23 -0500

    Fix email test issue

--- features/step_definitions/email_steps.rb
+++ features/step_definitions/email_steps.rb
@@ -30,7 +30,8 @@ Given(/^(\d)+ emails? should be delivered$/) do |count|
 end
 
 When(/^(?:I|they) follow "([^"]*?)" in #{capture_email}$/) do |link, email_ref|
-  visit_in_email(email(email_ref).html_part, link)
+  open_last_email
+  visit_in_email(link)
 end
 
 When(/^(?:I|they) click the first link in #{capture_email}$/) do |email_ref|
