74eef348c24f42d6048c65cd344f4f392bc8c835
Brian Austin <13002992+brianjaustin@users.noreply.github.com>
2024-02-20 19:26:29 -0500

    AO3-6667 Avoid counting non-visible works for languages page (#4746)
    * AO3-6667 Avoid counting non-visible works for languages page
    
    * Style fixes
    
    * Clearer test descriptions

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -2,7 +2,7 @@ class LanguagesController < ApplicationController
 
   def index
     @languages = Language.default_order
-    @works_counts = Rails.cache.fetch("/v1/languages/work_counts", expires_in: 1.day) do
+    @works_counts = Rails.cache.fetch("/v1/languages/work_counts/#{current_user.present?}", expires_in: 1.day) do
       WorkQuery.new.works_per_language(@languages.count)
     end
   end
