85f42a8b92cd678dbc74d2f12f14f80116a137df
Sarken <sarken@gmail.com>
2024-02-23 04:31:48 -0500

    AO3-6634 Rename SPAM_COMMENT_ROLES to SPAM_WORK_COMMENT_ROLES for clarity

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -6,7 +6,7 @@ class CommentPolicy < ApplicationPolicy
   HIDE_TAG_COMMENT_ROLES = %w[superadmin tag_wrangling].freeze
   HIDE_WORK_COMMENT_ROLES = %w[superadmin policy_and_abuse].freeze
   SPAM_ADMIN_POST_COMMENT_ROLES = %w[superadmin board board_assistants_team communications elections policy_and_abuse support].freeze
-  SPAM_COMMENT_ROLES = %w[superadmin board policy_and_abuse support].freeze
+  SPAM_WORK_COMMENT_ROLES = %w[superadmin board policy_and_abuse support].freeze
 
   def can_destroy_comment?
     case record.ultimate_parent
@@ -43,8 +43,8 @@ class CommentPolicy < ApplicationPolicy
     case record.ultimate_parent
     when AdminPost
       user_has_roles?(SPAM_ADMIN_POST_COMMENT_ROLES)
-    else
-      user_has_roles?(SPAM_COMMENT_ROLES)
+    when Work
+      user_has_roles?(SPAM_WORK_COMMENT_ROLES)
     end
   end
 
