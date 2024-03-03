49285b4aebb5b9a9aedc79693e75e70c9d9461c4
james_ <1606304+zz9pzza@users.noreply.github.com>
2024-03-03 10:23:08 +0000

    AO3-6647 Fix paperclip with no paperclip patch (#4761)
    * Fix with no paperclip patch
    
    * Will rubocop -a fix it
    
    ---------
    
    Co-authored-by: AO3 Unicorn service account <ao3app@transformativeworks.org>
    Co-authored-by: 1606304+zz9pzza@users.noreply.github.com <james_@kiki>

--- app/models/pseud.rb
+++ app/models/pseud.rb
@@ -14,8 +14,6 @@ class Pseud < ApplicationRecord
           end,
     storage: %w(staging production).include?(Rails.env) ? :s3 : :filesystem,
     s3_protocol: "https",
-    s3_credentials: "#{Rails.root}/config/s3.yml",
-    bucket: %w(staging production).include?(Rails.env) ? YAML.load_file("#{Rails.root}/config/s3.yml")['bucket'] : "",
     default_url: "/images/skins/iconsets/default/icon_user.png"
 
   validates_attachment_content_type :icon,
