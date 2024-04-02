3e9c339dcb9109a14175768fde9372452ef18413
sarken <sarken@gmail.com>
2024-04-01 20:28:28 -0400

    AO3-6486 Add development_and_membership admin role (#4777)
    * AO3-6486 Add development_and_membership admin role
    
    * AO3-6486 Make sure the proper options are shown or hidden
    
    * AO3-6486 Code style fixes

--- app/policies/admin_banner_policy.rb
+++ app/policies/admin_banner_policy.rb
@@ -1,10 +1,16 @@
 class AdminBannerPolicy < ApplicationPolicy
+  ACCESS_AND_EDIT_ROLES = %w[superadmin board communications development_and_membership support].freeze
+  CREATE_AND_DESTROY_ROLES = %w[superadmin board communications support].freeze
+
   def index?
-    user_has_roles?(%w[superadmin board communications support])
+    user_has_roles?(ACCESS_AND_EDIT_ROLES)
+  end
+
+  def create?
+    user_has_roles?(CREATE_AND_DESTROY_ROLES)
   end
 
   alias show? index?
-  alias create? index?
   alias update? index?
-  alias destroy? index?
+  alias destroy? create?
 end
