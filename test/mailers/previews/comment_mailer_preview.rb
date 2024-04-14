f8d659f7254a665cb80921e315a5b7d029f724cf
Bilka <Bilka2@users.noreply.github.com>
2024-04-14 08:34:00 +0200

    AO3-6017 Add comments to mailer previews

--- test/mailers/previews/comment_mailer_preview.rb
+++ test/mailers/previews/comment_mailer_preview.rb
@@ -1,4 +1,5 @@
 class CommentMailerPreview < ApplicationMailerPreview
+  # Sent to a user when they get a comment on a top-level creation
   def comment_notification
     user = create(:user)
 
@@ -8,6 +9,7 @@ class CommentMailerPreview < ApplicationMailerPreview
     CommentMailer.comment_notification(user, comment)
   end
 
+  # Sent to a user when they get a comment on a top-level creation by an official user
   def comment_notification_official
     user = create(:user)
 
@@ -16,12 +18,14 @@ class CommentMailerPreview < ApplicationMailerPreview
     CommentMailer.comment_notification(user, comment)
   end
 
+  # Sent to a user when they get a comment on a top-level creation by a guest
   def comment_notification_guest
     user = create(:user)
     comment = create(:comment, :by_guest)
     CommentMailer.comment_notification(user, comment)
   end
 
+  # Sent to a user when they get a comment reply to their comment
   def comment_reply_notification
     comment = create(:comment)
 
@@ -30,6 +34,7 @@ class CommentMailerPreview < ApplicationMailerPreview
     CommentMailer.comment_reply_notification(comment, reply)
   end
 
+  # Sent to a user when they get a reply to their comment by an anonymous creator
   def comment_reply_notification_anon
     replier = create(:user)
     work = create(:work, authors: [replier.default_pseud], collections: [create(:anonymous_collection)])
@@ -39,6 +44,7 @@ class CommentMailerPreview < ApplicationMailerPreview
     CommentMailer.comment_reply_notification(comment, reply)
   end
 
+  # Sent to a user when they make a reply to a comment and they want to be notified of their own comments
   def comment_reply_sent_notification
     commenter = create(:user, login: "Exoskeleton")
 
