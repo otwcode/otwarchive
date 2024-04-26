6a7e0ed2fb08ae46ac5279f4427c8aac024d7d39
Sarken <sarken@gmail.com>
2024-04-26 01:43:23 -0400

    AO3-6719 Use // instead of %r{} for regex

--- lib/html_cleaner.rb
+++ lib/html_cleaner.rb
@@ -148,12 +148,12 @@ module HtmlCleaner
 
   # strip img tags, optionally leaving the src URL exposed
   def strip_images(value, keep_src: false)
-    value.gsub(%r{(<img .*?>)}) do |img_tag|
+    value.gsub(/(<img .*?>)/) do |img_tag|
       if keep_src
         # The cleaned field will always use double quotes (") but the fields
         # in tests aren't always cleaned, so we're allowing double or single
         # quotes here.
-        img_tag.match(%r{src=['"]([^"]+)['"]})[1]
+        img_tag.match(/src=['"]([^"]+)['"]/)[1]
       else
         ""
       end
