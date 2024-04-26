f1b260b508668c3b0992a2abcca0e864e3a253cb
Brian Austin <brianjaustin@gmail.com>
2024-04-26 01:34:09 +0000

    Better support for in-browser GitPod

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
     Bullet.rails_logger = true
     Bullet.counter_cache_enable = false
   end
+
+  # GitPod preview support
+  config.hosts << "3000-#{ENV['GITPOD_WORKSPACE_ID']}.#{ENV['GITPOD_WORKSPACE_CLUSTER_HOST']}" if ENV["GITPOD_WORKSPACE_ID"]
 end
