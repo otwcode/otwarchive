7faf826688abd8d53fa8c3fae278897b66ad4422
Bilka <Bilka2@users.noreply.github.com>
2024-04-28 20:29:32 +0200

    AO3-6493 Reviewdog

--- app/models/user.rb
+++ app/models/user.rb
@@ -572,6 +572,7 @@ class User < ApplicationRecord
 
   def send_wrangler_username_change_notification
     return unless saved_change_to_login? && login_before_last_save.present?
+
     TagWranglingAdminMailer.wrangler_username_change_notification(login_before_last_save, login).deliver_now
   end
 
