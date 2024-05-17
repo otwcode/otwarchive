225462482603170ed2ff501243f7f8b8ad87cc40
Brian Austin <13002992+brianjaustin@users.noreply.github.com>
2024-05-17 16:46:46 -0400

    AO3-6706 Support Sentinel config for Redis clients (#4787)
    * AO3-6706 Support Sentinel config for Redis clients
    
    * Fix examples

--- config/initializers/gem-plugin_config/resque.rb
+++ config/initializers/gem-plugin_config/resque.rb
@@ -1,14 +1,14 @@
-require 'resque'
+require "resque"
 
-rails_root = ENV["RAILS_ROOT"] || File.dirname(__FILE__) + "/../../.."
-rails_env = ENV["RAILS_ENV"] || "development"
+rails_root = ENV["RAILS_ROOT"] || "#{File.dirname(__FILE__)}/../../.."
+rails_env = (ENV["RAILS_ENV"] || "development").to_sym
 
- redis_configs = YAML.load_file(rails_root + '/config/redis.yml')
- Resque.redis = redis_configs['redis_resque'][rails_env]
+redis_configs = YAML.load_file("#{rails_root}/config/redis.yml", symbolize_names: true)
+Resque.redis = redis_configs[:redis_resque][rails_env]
 
- # in-process performing of jobs (for testing) doesn't require a redis server
- Resque.inline = ENV['RAILS_ENV'] == "test"
+# in-process performing of jobs (for testing) doesn't require a redis server
+Resque.inline = ENV["RAILS_ENV"] == "test"
 
- Resque.after_fork do
-   Resque.redis.client.reconnect
- end
+Resque.after_fork do
+  Resque.redis.client.reconnect
+end
