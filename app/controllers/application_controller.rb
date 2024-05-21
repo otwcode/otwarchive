ea83a19650d958113e61fac73a9d2882e5d99a29
Cesium-Ice <cindydxz@gmail.com>
2024-05-21 02:36:52 -0700

    avoid html in translation string

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -398,9 +398,10 @@ public
         # If the stated suspension end date is after the unban threshold we need to advance a day 
         suspension_end = suspension_end.next_day(1) if suspension_end > unban_theshold
         localized_suspension_end = localize(suspension_end.to_date)
-        flash[:error] = t("suspension_notice", default: "Your account has been suspended until %{suspended_until}. You may not add or edit content until your suspension has been resolved. Please <a href=\"#{new_abuse_report_path}\">contact Abuse</a> for more information.", suspended_until: localized_suspension_end).html_safe
+        flash[:error] = t("users.status.suspension_notice", suspended_until: localized_suspension_end, contact_abuse_link: view_context.link_to(t(".contact_abuse"), new_abuse_report_path)).html_safe
+        
       else
-        flash[:error] = t("ban_notice", default: "Your account has been banned. You are not permitted to add or edit archive content. Please <a href=\"#{new_abuse_report_path}\">contact Abuse</a> for more information.").html_safe
+        flash[:error] = t("users.status.ban_notice", contact_abuse_link: view_context.link_to(t(".contact_abuse"), new_abuse_report_path)).html_safe
       end
       redirect_to current_user
     end
@@ -419,7 +420,8 @@ public
     suspension_end = suspension_end.next_day(1) if suspension_end > unban_theshold
     localized_suspension_end = localize(suspension_end.to_date)
     
-    flash[:error] = t("suspension_notice", default: "Your account has been suspended until %{suspended_until}. You may not add or edit content until your suspension has been resolved. Please <a href=\"#{new_abuse_report_path}\">contact Abuse</a> for more information.", suspended_until: localized_suspension_end).html_safe
+    flash[:error] = t("users.status.suspension_notice", suspended_until: localized_suspension_end, contact_abuse_link: view_context.link_to(t(".contact_abuse"), new_abuse_report_path)).html_safe
+
     redirect_to current_user
   end
 
