0c9781ed850cddda28ce37d4519dca4fedc943d7
Brian Austin <brianjaustin@gmail.com>
2024-06-29 16:28:46 -0400

    Fix test

--- spec/requests/people_n_plus_one_spec.rb
+++ spec/requests/people_n_plus_one_spec.rb
@@ -26,9 +26,10 @@ describe "n+1 queries in the people controller" do
     end
   end
 
-  describe "#search", n_plus_one: true, pseud_search: true do
+  describe "#search", n_plus_one: true do
     context "when there are search results" do
       populate do |n|
+        PseudIndexer.prepare_for_testing
         create_list(:pseud, n, name: "nplusone")
         run_all_indexing_jobs
       end
@@ -41,13 +42,9 @@ describe "n+1 queries in the people controller" do
 
       warmup { subject.call }
 
-      # TODO: AO3-6743, ideally tis would be a constant number of queries too.
-      # However, I'm only testing that we didn't add more with ActiveStorage
-      # now, as those changes are already very involved.
-      # - Brian Austin, June 2024
-      it "produces about 1 query per pseud" do
+      it "produces a constant number of queries" do
         expect { subject.call }
-          .to perform_linear_number_of_queries(slope: 1)
+          .to perform_constant_number_of_queries
       end
     end
   end
