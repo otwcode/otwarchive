450dbeb5b9d068841d91fc5f143b9234a22390bf
Cesium <cindydxz@gmail.com>
2024-09-22 01:38:47 -0700

    see if rubocop will let me get away with not fixing all instances of ts()

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -146,8 +146,8 @@ class Collection < ApplicationRecord
                       maximum: ArchiveConfig.SUMMARY_MAX,
                       too_long: ts("must be less than %{max} characters long.", max: ArchiveConfig.SUMMARY_MAX) }
 
-  validates :header_image_url, format: { allow_blank: true, with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), message: ts("is not a valid URL.") }
-  validates :header_image_url, format: { allow_blank: true, with: /\A\S+\.(png|gif|jpg)\z/, message: ts("can only point to a gif, jpg, or png file."), multiline: true }
+  validates :header_image_url, format: { allow_blank: true, with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), message: I18n.t("collection.header_image.invalid_url") }
+  validates :header_image_url, format: { allow_blank: true, with: /\A\S+\.(png|gif|jpg)\z/, message: I18n.t("collection.header_image.invalid_file_type"), multiline: true }
 
   validates :tags_after_saving,
             length: { maximum: ArchiveConfig.COLLECTION_TAGS_MAX,
@@ -207,7 +207,7 @@ class Collection < ApplicationRecord
   end
 
   # Change membership of collection(s) from a particular pseud to the orphan account
-  def self.orphan(pseuds, collections, default=true)
+  def self.orphan(pseuds, collections, default = true)
     pseuds.each do |pseud|
       collections.each do |collection|
         if pseud && collection && collection.owners.include?(pseud)
