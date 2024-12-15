7f7ea28b03380a47df6623a7b67678e370d80043
Brian Austin <brianjaustin@gmail.com>
2024-12-14 19:50:23 -0500

    Shh Rubocop
    The message I get is about a param, but that's not what happens here.
    I think Rubocop is confused, and to be honest I disagree that
    the code is ambiguous.

--- features/step_definitions/support_steps.rb
+++ features/step_definitions/support_steps.rb
@@ -13,6 +13,8 @@ Given "{string} is a permitted Archive host" do |host|
 end
 
 Then "a Zoho ticket should be created with referer {string}" do |referer|
+  # rubocop:disable Lint/AmbiguousBlockAssociation
   expect(WebMock).to have_requested(:post, "https://desk.zoho.com/api/v1/tickets")
     .with { |req| JSON.parse(req.body)["cf"]["cf_url"] == referer }
+  # rubocop:enable Lint/AmbiguousBlockAssociation
 end
