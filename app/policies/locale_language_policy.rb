79aa4f87dd4f3d735c38cb05811d7ec7987c4d3c
Cesium-Ice <cindydxz@gmail.com>
2024-12-26 03:54:36 -0800

    fix what support admins are allowed to edit for locale languages

--- app/policies/locale_language_policy.rb
+++ app/policies/locale_language_policy.rb
@@ -14,7 +14,7 @@ class LocaleLanguagePolicy < ApplicationPolicy
   ALLOWED_ATTRIBUTES_BY_ROLES = {
     "superadmin" => %i[name short support_available abuse_support_available sortable_name],
     "translation" => %i[name short support_available abuse_support_available sortable_name],
-    "support" => %i[name short support_available sortable_name],
+    "support" => %i[support_available],
     "policy_and_abuse" => %i[abuse_support_available]
   }.freeze
 
@@ -22,12 +22,16 @@ class LocaleLanguagePolicy < ApplicationPolicy
     ALLOWED_ATTRIBUTES_BY_ROLES.values_at(*user.roles).compact.flatten
   end
 
-  def can_edit_abuse_fields?
+  def can_edit_abuse_available?
     user_has_roles?(%w[superadmin translation policy_and_abuse])
   end
 
-  def can_edit_non_abuse_fields?
+  def can_edit_support_available?
     user_has_roles?(%w[superadmin translation support])
+  end
+
+  def can_edit_other_fields?
+    user_has_roles?(%w[superadmin translation])
   end
 
   alias create? new?
