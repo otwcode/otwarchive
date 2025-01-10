208da7d378b66eaf9dfd0799cc8d65f9017298bf
Super <bmwalshy@gmail.com>
2025-01-10 15:16:46 -0500

    Create user for hidden bookmark mailer test
    Co-authored-by: Bilka <Bilka2@users.noreply.github.com>

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -55,9 +55,9 @@ class UserMailerPreview < ApplicationMailerPreview
   end
 
   def admin_hidden_bookmark_notification
-    bookmark = create(:bookmark)
-    creator_id = bookmark.pseud.user_id
-    UserMailer.admin_hidden_bookmark_notification(bookmark.id, creator_id)
+    creator = create(:user, :for_mailer_preview)
+    bookmark = create(:bookmark, pseud: creator.default_pseud)
+    UserMailer.admin_hidden_bookmark_notification(bookmark.id, creator.id)
   end
 
   def admin_hidden_series_notification
