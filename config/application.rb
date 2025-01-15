158a0e1f57cbe018981d94f2305494f13c501749
Brian Austin <brianjaustin@gmail.com>
2025-01-14 21:06:37 -0500

    Do it a better way

--- config/application.rb
+++ config/application.rb
@@ -132,6 +132,10 @@ module Otwarchive
 
     config.active_support.disable_to_s_conversion = true
 
+    # Disable ActiveStorage things that we don't need and can hit the DB hard
+    config.active_storage.analyzers = []
+    config.active_storage.previewers = []
+
     # Set ActiveStorage queue name
     config.active_storage.queues.mirror = :active_storage
     config.active_storage.queues.preview_image = :active_storage
--- config/initializers/disable_active_storage_analyzer.rb
+++
@@ -1,5 +0,0 @@
-# We don't want to fill up the queue with jobs that hammer the database
-# (and extract metadata we don't use). Ref https://stackoverflow.com/a/74879360.
-Rails.application.config.to_prepare do
-  ActiveStorage::Attachment.skip_callback(:commit, :after, :analyze_blob_later)
-end
