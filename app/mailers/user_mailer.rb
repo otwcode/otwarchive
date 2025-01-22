f545651fadec7f11e32a0b19013c03df00142199
Bilka <Bilka2@users.noreply.github.com>
2025-01-22 18:30:23 +0100

    AO3-6848 Create challenge_assignment_notification mailer preview (#4971)
    * AO3-6848 Create challenge_assignment_notification mailer preview
    
    * AO3-6848 Use sent_at from assignment
    
    * AO3-6848 Update mailer templates to current i18n standard
    
    * AO3-6848 Remove unused locale keys
    
    * AO3-6848 Reviewdog
    
    * AO3-6848 Fix test
    
    * AO3-6848 Add test for sending translated email
    
    * AO3-6848 Use translatable word connector for warnings etc
    
    * AO3-6848 Fix interpolation variable name
    
    * AO3-6848 Reformat ifs in erb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -197,14 +197,12 @@ class UserMailer < ApplicationMailer
   def challenge_assignment_notification(collection_id, assigned_user_id, assignment_id)
     @collection = Collection.find(collection_id)
     @assigned_user = User.find(assigned_user_id)
-    assignment = ChallengeAssignment.find(assignment_id)
-    @request = (assignment.request_signup || assignment.pinch_request_signup)
-    I18n.with_locale(@assigned_user.preference.locale.iso) do
-      mail(
-        to: @assigned_user.email,
-        subject: default_i18n_subject(app_name: ArchiveConfig.APP_SHORT_NAME, collection_title: @collection.title)
-      )
-    end
+    @assignment = ChallengeAssignment.find(assignment_id)
+    @request = (@assignment.request_signup || @assignment.pinch_request_signup)
+    mail(
+      to: @assigned_user.email,
+      subject: default_i18n_subject(app_name: ArchiveConfig.APP_SHORT_NAME, collection_title: @collection.title)
+    )
   end
 
   # Asks a user to validate and activate their new account
