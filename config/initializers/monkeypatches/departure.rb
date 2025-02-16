fa7bdc4a0dbbc8716fdd83974102b230568b6996
Brian Austin <13002992+brianjaustin@users.noreply.github.com>
2025-02-15 23:08:59 -0500

    AO3-6893 Patch departure the right way to prevent lock issues (#5069)
    * AO3-6893 Patch departure the right way to prevent lock issues
    
    * Suppress false positive cop
    
    It refers to a migration; this is not one, and we need to subclass ActiveRecord::Base
    this way to ensure OriginalAdapterConnection subclasses the right thing.

--- config/initializers/monkeypatches/departure.rb
+++ config/initializers/monkeypatches/departure.rb
@@ -1,11 +1,13 @@
+# Fix for https://github.com/departurerb/departure/issues/110, where advisory locks are not released after migration
+# is completed. This should be removed when a fix is available upstream.
 module Departure
-  module Migration
-    # Make all connections in the connection pool NOT use PerconaAdapter instead of the current adapter.
-    # Ref https://github.com/departurerb/departure/issues/110.
-    def reconnect_with_percona
-      ActiveRecord::Base.establish_connection(connection_config.merge(adapter: "percona"))
+  class ConnectionBase < ActiveRecord::Base # rubocop:disable Rails/ApplicationRecord
+    def self.establish_connection(config = nil)
+      super
     end
   end
+
+  class OriginalAdapterConnection < ConnectionBase; end
 end
 
 puts "WARNING: The monkeypatch #{__FILE__} was written for version 6.7.0 of the departure gem, but you are running #{Departure::VERSION}. Please update or remove the monkeypatch." unless Departure::VERSION == "6.7.0"
