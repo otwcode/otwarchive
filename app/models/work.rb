46606b6eb9cebc281171b81d44737fb9063517b8
Brian Austin <brianjaustin@gmail.com>
2025-02-18 15:59:35 -0500

    Fix merge issue (sorry Indes)

--- app/models/work.rb
+++ app/models/work.rb
@@ -1154,15 +1154,15 @@ class Work < ApplicationRecord
     return if notified_of_hiding_for_spam
 
     users.each do |user|
-      UserMailer.admin_hidden_work_notification(id, user.id).deliver_after_commit
+      I18n.with_locale(user.preference.locale.iso) do
+        UserMailer.admin_hidden_work_notification(id, user.id).deliver_after_commit
+      end
     end
   end
 
   def notify_of_hiding_for_spam
     users.each do |user|
-      I18n.with_locale(user.preference.locale.iso) do
-        UserMailer.admin_spam_work_notification(id, user.id).deliver_after_commit
-      end
+      UserMailer.admin_spam_work_notification(id, user.id).deliver_after_commit
     end
     self.notified_of_hiding_for_spam = true
   end
