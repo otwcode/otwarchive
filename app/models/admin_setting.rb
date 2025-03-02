955b820de15015b6ccdc6e71c079a00ddcd90828
Bilka <Bilka2@users.noreply.github.com>
2025-03-02 23:35:20 +0100

    Reviewdog

--- app/models/admin_setting.rb
+++ app/models/admin_setting.rb
@@ -55,12 +55,10 @@ class AdminSetting < ApplicationRecord
 
   # run hourly with the resque scheduler
   def self.check_queue
-    if self.invite_from_queue_enabled? && InviteRequest.count > 0
-      if Time.current >= self.invite_from_queue_at
-        new_time = Time.current + self.invite_from_queue_frequency.hours
-        self.first.update_attribute(:invite_from_queue_at, new_time)
-        InviteFromQueueJob.perform_now(count: invite_from_queue_number)
-      end
+    if self.invite_from_queue_enabled? && InviteRequest.count > 0 && Time.current >= self.invite_from_queue_at
+      new_time = Time.current + self.invite_from_queue_frequency.hours
+      self.first.update_attribute(:invite_from_queue_at, new_time)
+      InviteFromQueueJob.perform_now(count: invite_from_queue_number)
     end
   end
 
