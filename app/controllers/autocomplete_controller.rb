6c8b13a7f983c2974300b8afd364e2228f6937e7
Bilka <Bilka2@users.noreply.github.com>
2025-03-27 09:59:22 +0100

    Once upon a time, someone invented multithreading

--- app/controllers/autocomplete_controller.rb
+++ app/controllers/autocomplete_controller.rb
@@ -2,7 +2,7 @@ class AutocompleteController < ApplicationController
   respond_to :json
 
   skip_before_action :store_location
-  skip_around_action :set_current_user, except: [:collection_parent_name, :owned_tag_sets, :site_skins, :pseud]
+  skip_around_action :set_current_user, except: [:collection_parent_name, :owned_tag_sets, :site_skins]
   skip_before_action :sanitize_ac_params # can we dare!
 
   #### DO WE NEED THIS AT ALL? IF IT FIRES WITHOUT A TERM AND 500s BECAUSE USER DID SOMETHING WACKY SO WHAT
