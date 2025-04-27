0a341d12917f3f88d6e9581d7d19d94d30194de0
Mich <50018139+michtnt@users.noreply.github.com>
2025-04-27 23:38:48 +1000

    AO3-6845 Always display invitation results in table format (#5137)
    * AO3-6845 display invitations in table
    
    * AO3-6845 give invitation view lift up
    
    * AO3-6845 update how params is passed
    
    * AO3-6845 i18n improvement
    
    * AO3-6845 i18n improvement
    
    * AO3-6845 fix cucumber tests
    
    * AO3-6845 fix cucumber tests
    
    * AO3-6845 fix cucumber tests
    
    * AO3-6845  improve tests
    
    * AO3-6845 update tests
    
    * AO3-6845 minor improvements
    
    * AO3-6845 move comment to step

--- features/step_definitions/admin_steps.rb
+++ features/step_definitions/admin_steps.rb
@@ -362,6 +362,10 @@ When "I confirm I want to remove the pseud" do
   expect(page.accept_alert).to eq("Are you sure you want to remove the creator's pseud from this work?") if @javascript
 end
 
+When "I follow the first invitation token url" do
+  first('//td/a[href*="/invitations/"]').click
+end
+
 ### THEN
 
 Then (/^the translation information should still be filled in$/) do
