0c5d31a1a60b0962870fec3d47c0580565efa8f8
weeklies <80141759+weeklies@users.noreply.github.com>
2025-04-30 19:24:06 +0000

    Add another test

--- spec/controllers/series_controller_spec.rb
+++ spec/controllers/series_controller_spec.rb
@@ -14,6 +14,17 @@ describe SeriesController do
   end
 
   describe "PATCH #remove_user_creatorship" do
+    it "removes creatorship if there are non-user pseuds left" do
+      fake_login_known_user(user)
+      other_user_pseud = create(:user).default_pseud
+      series = create(:series, authors: [user.default_pseud, other_user_pseud])
+
+      patch :remove_user_creatorship, params: { id: series }
+
+      expect(series.pseuds.reload).to contain_exactly(other_user_pseud)
+      it_redirects_to_with_notice(series_path(series), "You have been removed as a creator from the series and its works.")
+    end
+
     it "redirects to orphan if there are no pseuds left" do
       fake_login_known_user(user)
       patch :remove_user_creatorship, params: { id: series }
