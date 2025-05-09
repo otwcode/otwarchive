306c9e1e80e9ec4929a659ee484ed8f7b82bfc72
Hamham6 <157623847+Hamham6@users.noreply.github.com>
2025-05-09 12:22:22 -0400

    media_id

--- spec/controllers/fandoms_controller_spec.rb
+++ spec/controllers/fandoms_controller_spec.rb
@@ -9,11 +9,11 @@ describe FandomsController, work_search: true do
       expect(assigns[:page_subtitle]).to eq("#{collection.title} - Fandoms")
     end
 
-    context "with medium_id param" do
-      context "when medium_id param doesn't exist" do
+    context "with media_id param" do
+      context "when media_id param doesn't exist" do
         it "raises an error" do
           expect do
-            get :index, params: { medium_id: "nonexistent" }
+            get :index, params: { media_id: "nonexistent" }
           end.to raise_error ActiveRecord::RecordNotFound
         end
       end
