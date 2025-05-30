1526593e3c7c216aebe65d7e69087297e8373cd3
Marcus Low <mlow16@outlook.com>
2025-05-29 22:30:41 -0700

    AO3-5949 Remember previous page when guest user denied access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -255,17 +255,17 @@ public
   # behavior in case the user is not authorized
   # to access the requested action.  For example, a popup window might
   # simply close itself.
-  def access_denied(options ={})
+  def access_denied(options = {})
+    destination = options[:redirect]
     if logged_in?
-      destination = options[:redirect].blank? ? user_path(current_user) : options[:redirect]
+      destination ||= user_path(current_user)
       # i18n-tasks-use t('users.reconfirm_email.access_denied.logged_in')
       flash[:error] = t(".access_denied.logged_in", default: t("application.access_denied.access_denied.logged_in")) # rubocop:disable I18n/DefaultTranslation
-      redirect_to destination
     else
-      destination = options[:redirect].blank? ? new_user_session_path : options[:redirect]
+      destination ||= new_user_session_path(return_to: request.fullpath)
       flash[:error] = ts "Sorry, you don't have permission to access the page you were trying to reach. Please log in."
-      redirect_to destination
     end
+    redirect_to destination
     false
   end
 
