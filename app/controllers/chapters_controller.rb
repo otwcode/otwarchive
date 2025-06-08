5705831c2d3a002ac54cec6ad35c969a3f2f8670
Marcus Low <mlow16@outlook.com>
2025-06-07 23:29:47 -0700

    AO3-5949 DRY up chapter create/update cancel button

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -116,14 +116,7 @@ class ChaptersController < ApplicationController
   # POST /work/:work_id/chapters
   # POST /work/:work_id/chapters.xml
   def create
-    if params[:cancel_button]
-      if params[:from] == "edit-work"
-        redirect_to edit_work_path(@work)
-      else
-        redirect_to @work
-      end
-      return
-    end
+    cancel_posting_and_redirect(@work) and return if params[:cancel_button]
 
     @chapter = @work.chapters.build(chapter_params)
     @work.wip_length = params[:chapter][:wip_length]
@@ -150,17 +143,7 @@ class ChaptersController < ApplicationController
   # PUT /work/:work_id/chapters/1
   # PUT /work/:work_id/chapters/1.xml
   def update
-    if params[:cancel_button]
-      case params[:from]
-      when "edit-work"
-        redirect_to edit_work_path(@work)
-      when "manage"
-        redirect_to manage_work_chapters_path(@work)
-      else
-        redirect_to @chapter
-      end
-      return
-    end
+    cancel_posting_and_redirect(@chapter) and return if params[:cancel_button]
 
     @chapter.attributes = chapter_params
     @work.wip_length = params[:chapter][:wip_length]
@@ -292,6 +275,17 @@ class ChaptersController < ApplicationController
   def post_chapter
     @work.update_attribute(:posted, true) unless @work.posted
     flash[:notice] = ts("Chapter has been posted!")
+  end
+
+  def cancel_posting_and_redirect(fallback)
+    case params[:from]
+    when "edit-work"
+      redirect_to edit_work_path(@work)
+    when "manage"
+      redirect_to manage_work_chapters_path(@work)
+    else
+      redirect_to fallback
+    end
   end
 
   private
