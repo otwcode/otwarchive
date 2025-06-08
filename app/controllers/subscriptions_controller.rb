efaf1b328feee15957ca9afd5f118c4072a50398
VemmyRM <kwvembu@gmail.com>
2025-06-07 23:40:22 -0400

    AO3-6936 fix subscriptions page title

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -20,6 +20,7 @@ class SubscriptionsController < ApplicationController
 
     @subscriptions = @subscriptions.to_a.sort { |a,b| a.name.downcase <=> b.name.downcase }
     @subscriptions = @subscriptions.paginate page: params[:page], per_page: ArchiveConfig.ITEMS_PER_PAGE
+    @page_subtitle = t("subscriptions.page_title", username: @user.login, subscription_type:@subscribable_type ? "#{@subscribable_type.classify} " : "")
   end
 
   # POST /subscriptions
