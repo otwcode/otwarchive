90305939a5d3613f568f55a35e079b191b03bc65
strictlyNDN <github@chvsnaane.mozmail.com>
2025-06-07 19:38:04 -0700

    AO3-7001 Permanently strip image embeds from comment notification emails

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -532,9 +532,14 @@ class Comment < ApplicationRecord
     update_attribute(:hidden_by_admin, false)
   end
 
-  def sanitized_content
+  def sanitized_content(is_mailer = "false")
     sanitize_field(self, :comment_content, image_safety_mode: use_image_safety_mode?)
   end
+
+  def sanitized_mailer_content
+    sanitize_field(self, :comment_content, image_safety_mode: true)
+  end
+
 
   def use_image_safety_mode?
     pseud_id.nil? || hidden_by_admin || parent_type.in?(ArchiveConfig.PARENTS_WITH_IMAGE_SAFETY_MODE)
