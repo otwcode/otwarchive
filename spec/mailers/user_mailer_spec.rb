e0eceda57b45d8b1b9df73ef7b4357dab02e0814
Slava <53832230+slavalamp@users.noreply.github.com>
2025-06-22 22:47:55 +0000

    AO3-5345 addressed feedback including the weirdest typo ever

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1051,7 +1051,7 @@ describe UserMailer do
     end
   end
 
-  describe "c_signup_notification" do
+  describe "invalid_signup_notification" do
     subject(:email) { UserMailer.invalid_signup_notification(collection.id, [signup.id], "test@example.com") }
 
     let(:collection) { create(:collection) }
@@ -1092,13 +1092,13 @@ describe UserMailer do
     it_behaves_like "an email with a valid sender"
 
     describe "HTML version" do
-      it "has the correct content" do
+      it "has the correct footer content" do
         expect(email).to have_html_part_content("your email address has been listed as the collection email")
       end
     end
 
     describe "text version" do
-      it "has the correct content" do
+      it "has the correct footer content" do
         expect(email).to have_text_part_content("your email address has been listed as the collection email")
       end
     end
