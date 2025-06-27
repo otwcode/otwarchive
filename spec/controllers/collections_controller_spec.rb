a5c1f042d83f9a1caacbe8824d9e3d0cef58cec3
Bilka <Bilka2@users.noreply.github.com>
2025-06-27 09:10:23 +0200

    AO3-7010 AO3-7017 Fix error when trying to access subpage of hidden unrevealed work (#5219)
    * AO3-7010 Fix error when trying to access subpage of hidden unrevealed work
    
    * AO3-7017 Restrict access to subpages for draft and restricted works

--- spec/controllers/collections_controller_spec.rb
+++ spec/controllers/collections_controller_spec.rb
@@ -19,6 +19,15 @@ describe CollectionsController do
 
       include_examples "denies access for work that isn't visible to user"
     end
+
+    context "denies access for restricted work to guest" do
+      let(:work) { create(:work, restricted: true) }
+
+      it "redirects with an error" do
+        get :index, params: { work_id: work }
+        it_redirects_to_with_error(root_path, "Sorry, you don't have permission to access the page you were trying to reach. Please log in.")
+      end
+    end
   end
 
   describe "GET #show" do
