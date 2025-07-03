bc0aa4067e6c7d9676279bfd5e61de690796e03f
Brian Austin <brianjaustin@gmail.com>
2025-07-03 20:18:29 +0200

    Fix tests

--- spec/controllers/comments/default_rails_actions_spec.rb
+++ spec/controllers/comments/default_rails_actions_spec.rb
@@ -18,7 +18,7 @@ describe CommentsController do
     end
 
     it "renders the :new template if commentable is a valid admin post" do
-        admin_post = create(:admin_post)
+        admin_post = create(:admin_post, comment_permissions: :enable_all)
         get :new, params: { admin_post_id: admin_post.id }
         expect(response).to render_template("new")
         expect(assigns(:name)).to eq(admin_post.title)
