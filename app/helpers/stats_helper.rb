7d60277ad52d6d93b7270870a0ff9ddbc0a07fb6
Anna M <47932860+anmazz@users.noreply.github.com>
2025-07-25 15:52:53 -0400

    use series_counts cte

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -10,31 +10,46 @@ module StatsHelper
       end_date = start_date.end_of_year
     end
 
-    work_cte = <<-SQL
-    -- Precompute statistics for Works
+    sql = <<-SQL
       WITH work_stats AS (
         SELECT
           c.work_id,
           MAX(c.published_at) AS last_published_chapter_date,
           COUNT(DISTINCT com.id) as comment_thread_count,
-          -- Recomputing the whole work count when All Years is selected, unfortunately
+          -- Recomputing the whole Work count when All Years is selected, unfortunately
           SUM(c.word_count) as word_count,
-        -- Work contains a chapter that was published in date range
         CASE 
+          -- Evaluate whether Work contains a chapter that was published in date range
           WHEN MAX(c.published_at) IS NOT NULL THEN TRUE 
           ELSE FALSE 
-        END AS published_in_year
+        END AS published_in_range
         FROM chapters c
         LEFT JOIN comments com
           ON com.commentable_id = c.id AND com.commentable_type = 'Chapter' AND com.depth = 0
         WHERE c.published_at BETWEEN '#{start_date}' AND '#{end_date}'
         GROUP BY c.work_id
+      ),
+      -- Gets the total posted works for a series in specified range
+      series_counts AS (
+        SELECT 
+          series.id as series_id,
+          COUNT(DISTINCT sw.id) as work_count
+        FROM series
+        INNER JOIN serial_works sw ON sw.series_id = series.id
+        INNER JOIN works ON works.id = sw.work_id
+        -- Authorship
+        INNER JOIN creatorships ON creatorships.creation_id = works.id AND creatorships.creation_type = 'Work'
+	      INNER JOIN pseuds ON pseuds.id = creatorships.pseud_id
+	      INNER JOIN users ON users.id = pseuds.user_id
+        -- Join work stats to determine whether series contains a work with a chapter published in range
+        INNER JOIN work_stats ON work_stats.work_id = works.id
+        WHERE work_stats.published_in_range = TRUE
+          -- Might want to move user_id into select? Maybe doesn't matter
+	        AND users.id = #{ActiveRecord::Base.connection.quote(user.id)}
+          AND works.posted = TRUE
+	      GROUP BY series.id
       )
-    SQL
-
-    sql = <<-SQL
-      #{work_cte}
-          (
+      (
       SELECT
         'WORK' AS type,
         works.id,
@@ -70,7 +85,7 @@ module StatsHelper
       -- Only posted works
       AND works.posted = TRUE
       -- Only works within range
-      AND work_stats.published_in_year = TRUE
+      AND work_stats.published_in_range = TRUE
       GROUP BY works.id, title, tags.name
     )
     UNION ALL
@@ -90,11 +105,13 @@ module StatsHelper
         COUNT(DISTINCT b.id) AS bookmarks_count,
         COUNT(DISTINCT s.id) AS subscriptions_count,
         NULL AS comment_thread_count,
-        COUNT(DISTINCT works.id) AS work_count
+        series_counts.work_count AS work_count
       FROM series
-      -- Tags
+      -- Series counts and info
+      INNER JOIN series_counts ON series_counts.series_id = series.id
       INNER JOIN serial_works sw ON sw.series_id = series.id
       INNER JOIN works ON works.id = sw.work_id
+      -- Tags
       INNER JOIN taggings ON taggings.taggable_id = works.id AND taggings.taggable_type = 'Work'
       INNER JOIN tags ON taggings.tagger_id = tags.id AND tags.type = 'Fandom'
       -- Authorship
@@ -104,10 +121,12 @@ module StatsHelper
       -- Bookmarks/Subsriptions on series itself
       LEFT JOIN bookmarks b ON b.bookmarkable_id = series.id AND b.bookmarkable_type = 'Series'
       LEFT JOIN subscriptions s ON s.subscribable_id = series.id AND s.subscribable_type = 'Series'
-      -- Used for dating works inside series
-      LEFT JOIN work_stats ON work_stats.work_id = works.id AND work_stats.published_in_year = TRUE
+      -- Used for published in range check
+      LEFT JOIN work_stats ON work_stats.work_id = works.id 
       -- Filters
       WHERE users.id = #{ActiveRecord::Base.connection.quote(user.id)}
+      -- Only retrieve series info if work in series had a chapter published in range
+      AND work_stats.published_in_range = TRUE
       GROUP BY series.id, series.title, tags.name
     )
     ORDER BY #{sort_column} #{sort_direction}, title
