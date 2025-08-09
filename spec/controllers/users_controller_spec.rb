2832f3a788933aba323389adb4c03b65023600c4
Katie Yang <13hy22@queensu.ca>
2025-08-09 11:07:51 -0700

    AO3-7089 Removed redundant shared example

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,34 +5,32 @@ describe UsersController do
   include LoginMacros
 
   shared_examples "an action only authorized admins can access" do |authorized_roles:|
-    before do
-      fake_login_admin(admin)
-    end
-
-    context "when logged in as an admin with no role" do
-      let(:admin) { create(:admin) }
-
+    before { fake_login_admin(admin) }
+  
+    context "with no role" do
+      let(:admin) { create(:admin, roles: []) }
+  
       it "redirects with an error" do
         subject
         it_redirects_to_with_error(root_url, "Sorry, only an authorized admin can access the page you were trying to reach.")
       end
     end
-
-    (Admin::VALID_ROLES - authorized_roles).each do |admin_role|
-      context "when logged in as an admin with role #{admin_role}" do
-        let(:admin) { create(:admin, roles: [admin_role]) }
-
+  
+    (Admin::VALID_ROLES - authorized_roles).each do |role|
+      context "with role #{role}" do
+        let(:admin) { create(:admin, roles: [role]) }
+  
         it "redirects with an error" do
           subject
           it_redirects_to_with_error(root_url, "Sorry, only an authorized admin can access the page you were trying to reach.")
         end
       end
     end
-
-    authorized_roles.each do |admin_role|
-      context "when logged in as an admin with role #{admin_role}" do
-        let(:admin) { create(:admin, roles: [admin_role]) }
-
+  
+    authorized_roles.each do |role|
+      context "with role #{role}" do
+        let(:admin) { create(:admin, roles: [role]) }
+  
         it "succeeds" do
           subject
           success
