6b35b6979e163e1063de4cfa735b3b1b75af5465
EchoEkhi <echoekhi@gmail.com>
2025-08-13 01:10:57 +0800

    rubocop

--- app/validators/email_format_validator.rb
+++ app/validators/email_format_validator.rb
@@ -6,25 +6,25 @@
 #
 class EmailFormatValidator < ActiveModel::EachValidator
   def self.email_regex
-    email_name_regex = '[A-Z0-9_\.&%\+\-\']+'
-    domain_head_regex = '(?:[A-Z0-9\-]+\.)+'
-    domain_tld_regex = '(?:[A-Z]{2,25})'
+    email_name_regex = "[A-Z0-9_\\.&%\\+\\-']+"
+    domain_head_regex = "(?:[A-Z0-9\\-]+\\.)+"
+    domain_tld_regex = "(?:[A-Z]{2,25})"
     /\A#{email_name_regex}@#{domain_head_regex}#{domain_tld_regex}\z/i
   end
 
   def validate_each(record, attribute, value)
-    if (options[:allow_blank] && value.blank?) || (value.present? && value.match(EmailFormatValidator.email_regex))
-      result = true
-    else
-      result = false
-    end
+    result = if (options[:allow_blank] && value.blank?) || (value.present? && value.match(EmailFormatValidator.email_regex))
+               true
+             else
+               false
+             end
+    
+    return if result
 
-    unless result
-      if options[:allow_blank]
-        record.errors.add(attribute, options[:message] || I18n.t("validators.email.format.allow_blank"))
-      else
-        record.errors.add(attribute, options[:message] || I18n.t("validators.email.format.no_blank"))
-      end
+    if options[:allow_blank]
+      record.errors.add(attribute, options[:message] || I18n.t("validators.email.format.allow_blank"))
+    else
+      record.errors.add(attribute, options[:message] || I18n.t("validators.email.format.no_blank"))
     end
   end
 end
