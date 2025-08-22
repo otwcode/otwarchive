9678dbaf78f75b50e20400122b026879f511ea55
Marcus Low <mlow16@outlook.com>
2025-08-22 16:12:37 -0700

    AO3-6415 Rubocop and translation normalization
    add missing translations

--- app/controllers/challenge_assignments_controller.rb
+++ app/controllers/challenge_assignments_controller.rb
@@ -67,23 +67,18 @@ class ChallengeAssignmentsController < ApplicationController
     false
   end
 
-
-
   # ACTIONS
 
   def index
     if params[:user_id] && (@user = User.find_by(login: params[:user_id]))
       if current_user == @user
         @challenge_assignments = @user.assignments.undefaulted
-
-        if params[:collection_id] && (@collection = Collection.find_by(name: params[:collection_id]))
-          @challenge_assignments = @challenge_assignments.in_collection(@collection)
-        end
-        if params[:posted] == "true"
-          @challenge_assignments = @challenge_assignments.posted
-        else
-          @challenge_assignments = @challenge_assignments.unposted
-        end
+        @challenge_assignments = @challenge_assignments.in_collection(@collection) if params[:collection_id] && (@collection = Collection.find_by(name: params[:collection_id]))
+        @challenge_assignments = if params[:posted] == "true"
+                                   @challenge_assignments.posted
+                                 else
+                                   @challenge_assignments.unposted
+                                 end
         @challenge_assignments = @challenge_assignments.order(created_at: :desc).paginate(page: params[:page])
       else
         flash[:error] = t(".access_denied_user")
