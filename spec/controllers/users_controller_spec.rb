70b78bb0172d7e3d0aaaa853596aed80560f4591
Katie Yang <13hy22@queensu.ca>
2025-08-23 15:54:35 -0700

    Deleted shared_examples "an action only authorized admins can access"

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,41 +4,6 @@ describe UsersController do
   include RedirectExpectationHelper
   include LoginMacros
 
-  shared_examples "an action only authorized admins can access" do |authorized_roles:|
-    before { fake_login_admin(admin) }
-  
-    context "with no role" do
-      let(:admin) { create(:admin, roles: []) }
-  
-      it "redirects with an error" do
-        subject
-        it_redirects_to_with_error(root_url, "Sorry, only an authorized admin can access the page you were trying to reach.")
-      end
-    end
-  
-    (Admin::VALID_ROLES - authorized_roles).each do |role|
-      context "with role #{role}" do
-        let(:admin) { create(:admin, roles: [role]) }
-  
-        it "redirects with an error" do
-          subject
-          it_redirects_to_with_error(root_url, "Sorry, only an authorized admin can access the page you were trying to reach.")
-        end
-      end
-    end
-  
-    authorized_roles.each do |role|
-      context "with role #{role}" do
-        let(:admin) { create(:admin, roles: [role]) }
-  
-        it "succeeds" do
-          subject
-          success
-        end
-      end
-    end
-  end
-
   shared_examples "blocks access for banned and suspended users" do
     context "when logged in as a banned user" do
       let(:user) { create(:user, banned: true) }
