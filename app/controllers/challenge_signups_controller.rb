a35302350a0e73a22ae474ac7e9facc516216b4c
omerfaruk-pseud <nachomaho0@gmail.com>
2025-09-06 02:26:31 +0300

    AO3-4698 add string to locale file and key to controller

--- app/controllers/challenge_signups_controller.rb
+++ app/controllers/challenge_signups_controller.rb
@@ -197,7 +197,7 @@ class ChallengeSignupsController < ApplicationController
 
   public
   def new
-    @page_subtitle = ts("New Challenge Sign-up")
+    @page_subtitle = t(".page_title")
     if (@challenge_signup = ChallengeSignup.in_collection(@collection).by_user(current_user).first)
       flash[:notice] = ts("You are already signed up for this challenge. You can edit your sign-up below.")
       redirect_to edit_collection_signup_path(@collection, @challenge_signup)
