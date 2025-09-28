8f42732a24655c7d66817d694a8df28f31051051
james_ <1606304+zz9pzza@users.noreply.github.com>
2025-09-28 13:07:37 +0100

    More frozen strings

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -173,7 +173,7 @@ module TagsHelper
 
   # Returns a nested list of sub tags
   def sub_tag_tree(tag)
-    sub_ul = ""
+    sub_ul = +""
     unless tag.direct_sub_tags.empty?
       sub_ul << "<ul class='tags tree index'>"
       tag.direct_sub_tags.order(:name).each do |sub|
@@ -191,7 +191,7 @@ module TagsHelper
   def blurb_tag_block(item, tag_groups=nil)
     tag_groups ||= item.tag_groups
     categories = ['ArchiveWarning', 'Relationship', 'Character', 'Freeform']
-    tag_block = ""
+    tag_block = +""
 
     categories.each do |category|
       if tags = tag_groups[category]
