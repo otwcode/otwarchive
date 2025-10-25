d05f5a24a8d254dc1e9082a176df245fc46e0d37
EchoEkhi <echoekhi@gmail.com>
2025-10-25 22:48:36 +0100

    add bookmark search page

--- spec/models/abuse_report_spec.rb
+++ spec/models/abuse_report_spec.rb
@@ -768,6 +768,26 @@ describe AbuseReport do
           expect(subject.creator_ids).to eq(user.id.to_s)
         end
       end
+
+      context "for the user's bookmark search page" do
+        it "returns the user's ID when the parameter is at the start" do
+          allow(subject).to receive(:url).and_return("https://archiveofourown.org/bookmarks?user_id=#{user.login}&commit=Sort+and+Filter&bookmark_search%5Bsort_column%5D=created_at&bookmark_search%5Bother_tag_names%5D=&bookmark_search%5Bother_bookmark_tag_names%5D=&bookmark_search%5Bexcluded_tag_names%5D=&bookmark_search%5Bexcluded_bookmark_tag_names%5D=&bookmark_search%5Bbookmarkable_query%5D=&bookmark_search%5Bbookmark_query%5D=&bookmark_search%5Blanguage_id%5D=&bookmark_search%5Brec%5D=0&bookmark_search%5Bwith_notes%5D=0")
+
+          expect(subject.creator_ids).to eq(user.id.to_s)
+        end
+
+        it "returns the user's ID when the parameter is in the middle" do
+          allow(subject).to receive(:url).and_return("https://archiveofourown.org/bookmarks?commit=Sort+and+Filter&user_id=#{user.login}&bookmark_search%5Bsort_column%5D=created_at&bookmark_search%5Bother_tag_names%5D=&bookmark_search%5Bother_bookmark_tag_names%5D=&bookmark_search%5Bexcluded_tag_names%5D=&bookmark_search%5Bexcluded_bookmark_tag_names%5D=&bookmark_search%5Bbookmarkable_query%5D=&bookmark_search%5Bbookmark_query%5D=&bookmark_search%5Blanguage_id%5D=&bookmark_search%5Brec%5D=0&bookmark_search%5Bwith_notes%5D=0")
+
+          expect(subject.creator_ids).to eq(user.id.to_s)
+        end
+
+        it "returns the user's ID when the parameter is at the end" do
+          allow(subject).to receive(:url).and_return("https://archiveofourown.org/bookmarks?commit=Sort+and+Filter&bookmark_search%5Bsort_column%5D=created_at&bookmark_search%5Bother_tag_names%5D=&bookmark_search%5Bother_bookmark_tag_names%5D=&bookmark_search%5Bexcluded_tag_names%5D=&bookmark_search%5Bexcluded_bookmark_tag_names%5D=&bookmark_search%5Bbookmarkable_query%5D=&bookmark_search%5Bbookmark_query%5D=&bookmark_search%5Blanguage_id%5D=&bookmark_search%5Brec%5D=0&bookmark_search%5Bwith_notes%5D=0&user_id=#{user.login}")
+
+          expect(subject.creator_ids).to eq(user.id.to_s)
+        end
+      end
     end
   end
 end
