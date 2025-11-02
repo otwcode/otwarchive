01500c7f856007752d8628082794bf1ecd5183bf
EchoEkhi <echoekhi@gmail.com>
2025-11-02 03:03:31 +0000

    rubocop

--- lib/email_canonicalizer.rb
+++ lib/email_canonicalizer.rb
@@ -3,16 +3,16 @@ module EmailCanonicalizer
   def self.canonicalize(email_to_clean)
     canonical_email = email_to_clean.downcase
     canonical_email.strip!
-    canonical_email.sub!('@googlemail.com','@gmail.com')
+    canonical_email.sub!("@googlemail.com", "@gmail.com")
 
     # strip periods from gmail addresses
-    if (matchdata = canonical_email.match(/(.+)\@gmail\.com/))
-      canonical_email = matchdata[1].gsub('.', '') + "@gmail.com"
+    if (matchdata = canonical_email.match(/(.+)@gmail\.com/))
+      canonical_email = "#{matchdata[1].gsub('.', '')}@gmail.com"
     end
 
     # strip out anything after a +
     canonical_email.sub!(/(\+.*)(@.*$)/, '\2')
 
-    return canonical_email
+    canonical_email
   end
 end
