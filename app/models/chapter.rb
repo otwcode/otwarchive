761f2c3a5bd2e5ce03efaf55f212c5a59240f394
Anna M <47932860+anmazz@users.noreply.github.com>
2025-11-03 00:47:20 -0500

    out of bounds checks

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -61,7 +61,7 @@ class Chapter < ApplicationRecord
     return if chapters.empty?
 
     chapters.delete(self)
-    insert_at = [self.position.to_i - 1, 0].max
+    insert_at = [[self.position.to_i - 1, 0].max, chapters.length].min
     chapters.insert(insert_at, self)
 
     positions_changed = false
