db7cca861738862764b9faa48be02ee24ee7bd39
Bilka <Bilka2@users.noreply.github.com>
2025-11-20 20:07:51 +0100

    AO3-6941 Improve browser page titles on comment-related pages (#5434)
    * AO3-6941 Improve browser page titles on comment-related pages
    
    * AO3-6941 Rubocop
    
    * AO3-6941 Fix lazy i18n lookup
    
    * AO3-6941 Update i18n-tasks config
    
    * AO3-6941 Expand work page title tests
    
    * AO3-6941 Fix coding style
    
    * AO3-6941 Rubocop
    
    * Update spec/controllers/chapters_controller_spec.rb

--- spec/controllers/chapters_controller_spec.rb
+++ spec/controllers/chapters_controller_spec.rb
@@ -190,31 +190,59 @@ describe ChaptersController do
     end
 
     it "assigns @page_title with fandom, author name, work title, and chapter" do
-      expect_any_instance_of(ChaptersController).to receive(:get_page_title).with("Testing", user.pseuds.first.name, "My title is long enough - Chapter 1").and_return("page title")
       get :show, params: { work_id: work.id, id: work.chapters.first.id }
-      expect(assigns[:page_title]).to eq("page title")
+      expect(assigns[:page_title]).to start_with("My title is long enough - Chapter 1 - #{user.pseuds.first.name} - Testing [")
     end
 
-    it "assigns @page_title with unrevealed work" do
+    it "assigns @page_subtitle with unrevealed work" do
       allow_any_instance_of(Work).to receive(:unrevealed?).and_return(true)
       get :show, params: { work_id: work.id, id: work.chapters.first.id }
-      expect(assigns[:page_title]).to eq("Mystery Work - Chapter 1")
+      expect(assigns[:page_subtitle]).to eq("Mystery Work - Chapter 1")
     end
 
     it "assigns @page_title with anonymous work" do
       allow_any_instance_of(Work).to receive(:anonymous?).and_return(true)
-      expect_any_instance_of(ChaptersController).to receive(:get_page_title).with("Testing", "Anonymous", "My title is long enough - Chapter 1").and_return("page title")
       get :show, params: { work_id: work.id, id: work.chapters.first.id }
-      expect(assigns[:page_title]).to eq("page title")
+      expect(assigns[:page_title]).to start_with("My title is long enough - Chapter 1 - Anonymous - Testing [")
+    end
+
+    context "when work has many authors" do
+      it "assigns @page_title with all authors" do
+        authors = create_list(:pseud, 5)
+        allow_any_instance_of(Work).to receive(:pseuds).and_return(authors)
+        get :show, params: { work_id: work.id, id: work.chapters.first.id }
+        expect(response).to have_http_status(:ok)
+        expect(assigns[:page_title]).to start_with("#{work.title} - Chapter 1 - #{authors.sort.map(&:byline).join(', ')} - #{work.fandoms.first.name} [")
+      end
     end
 
     context "when work has no fandom" do
       it "assigns @page_title with a placeholder for the fandom" do
-        allow_any_instance_of(Work).to receive(:tag_groups).and_return("Fandom" => [])
-        expect_any_instance_of(ChaptersController).to receive(:get_page_title).with("No fandom specified", user.pseuds.first.name, "#{work.title} - Chapter 1").and_return("page title")
+        allow_any_instance_of(Work).to receive(:fandoms).and_return([])
         get :show, params: { work_id: work.id, id: work.chapters.first.id }
         expect(response).to have_http_status(:ok)
-        expect(assigns[:page_title]).to eq("page title")
+        expect(assigns[:page_title]).to start_with("#{work.title} - Chapter 1 - #{user.pseuds.first.name} - No fandom specified [")
+      end
+    end
+
+    context "when work has two fandoms" do
+      it "assigns @page_title with the first fandom" do
+        first = create(:fandom, name: "The First")
+        second = create(:fandom, name: "The Second")
+        allow_any_instance_of(Work).to receive(:fandoms).and_return([first, second])
+        get :show, params: { work_id: work.id, id: work.chapters.first.id }
+        expect(response).to have_http_status(:ok)
+        expect(assigns[:page_title]).to start_with("#{work.title} - Chapter 1 - #{user.pseuds.first.name} - The First [")
+      end
+    end
+
+    context "when work has many fandoms" do
+      it "assigns @page_title with placeholder for the fandoms" do
+        fandoms = create_list(:fandom, 5)
+        allow_any_instance_of(Work).to receive(:fandoms).and_return(fandoms)
+        get :show, params: { work_id: work.id, id: work.chapters.first.id }
+        expect(response).to have_http_status(:ok)
+        expect(assigns[:page_title]).to start_with("#{work.title} - Chapter 1 - #{user.pseuds.first.name} - Multifandom [")
       end
     end
 
@@ -230,16 +258,14 @@ describe ChaptersController do
       third_chapter = create(:chapter, work: work, position: 3)
       kudo = create(:kudo, commentable: work, user: create(:user))
       tag = create(:fandom)
-      expect_any_instance_of(Work).to receive(:tag_groups).and_return("Fandom" => [tag])
-      expect_any_instance_of(ChaptersController).to receive(:get_page_title).with(tag.name, user.pseuds.first.name, "My title is long enough - Chapter 2").and_return("page title")
+      expect_any_instance_of(Work).to receive(:fandoms).and_return([tag])
       get :show, params: { work_id: work.id, id: second_chapter.id }
       expect(assigns[:work]).to eq work
-      expect(assigns[:tag_groups]).to eq "Fandom" => [tag]
       expect(assigns[:chapter]).to eq second_chapter
       expect(assigns[:chapters]).to eq [work.chapters.first, second_chapter, third_chapter]
       expect(assigns[:previous_chapter]).to eq work.chapters.first
       expect(assigns[:next_chapter]).to eq third_chapter
-      expect(assigns[:page_title]).to eq "page title"
+      expect(assigns[:page_title]).to start_with("My title is long enough - Chapter 2 - #{user.pseuds.first.name} - #{tag.name} [")
       expect(assigns[:kudos]).to eq [kudo]
       expect(assigns[:subscription]).to be_nil
     end
