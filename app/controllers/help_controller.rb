83d7e2204c15db28dcd1e982d34854a8d93c1a9f
Bilka <Bilka2@users.noreply.github.com>
2025-12-05 19:40:53 +0100

    AO3-7209 Change layout of help files to only be disabled inside modals

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -1,6 +1,6 @@
 class HelpController < ApplicationController
   before_action :users_only, only: [:first_login]
-  layout false
+  layout Proc.new { |controller| controller.request.xhr? ? false : "application" }
 
   def first_login
   end
