5a4ff646a8ef3996d4550fe45d6b47ae168a8089
strictlyNDN <github@chvsnaane.mozmail.com>
2025-12-13 15:33:07 -0800

    AO3-3245 - Add spec test

--- spec/controllers/works/works_controller_spec.rb
+++ spec/controllers/works/works_controller_spec.rb
@@ -6,6 +6,62 @@ describe WorksController do
   include LoginMacros
   include RedirectExpectationHelper
 
+  describe "GET #new" do
+    context "when collection is closed" do
+      let(:collection) { create(:collection, title: "Excalibur", collection_preference: create(:collection_preference, closed: true)) }
+      let(:user) { create(:user) }
+
+      before { fake_login_known_user(user) }
+
+      it "redirects to collection page with error for non-maintainers" do
+        get :new, params: { collection_id: collection.name }
+        it_redirects_to_with_error(collection_path(collection), "Sorry, the collection Excalibur is closed, new works cannot be added to it.")
+      end
+    end
+
+    context "when collection is closed but user is owner" do
+      let(:user) { create(:user) }
+      let(:collection) { create(:collection, title: "Excalibur", collection_preference: create(:collection_preference, closed: true)) }
+
+      before do
+        fake_login_known_user(user)
+        collection.collection_participants.create(pseud: user.default_pseud, participant_role: CollectionParticipant::OWNER)
+      end
+
+      it "allows access to new work form" do
+        get :new, params: { collection_id: collection.name }
+        expect(response).to render_template("new")
+      end
+    end
+
+    context "when collection is closed but user is maintainer" do
+      let(:user) { create(:user) }
+      let(:collection) { create(:collection, title: "Excalibur", collection_preference: create(:collection_preference, closed: true)) }
+
+      before do
+        fake_login_known_user(user)
+        collection.collection_participants.create(pseud: user.default_pseud, participant_role: CollectionParticipant::MODERATOR)
+      end
+
+      it "allows access to new work form" do
+        get :new, params: { collection_id: collection.name }
+        expect(response).to render_template("new")
+      end
+    end
+
+    context "when collection is open" do
+      let(:collection) { create(:collection, title: "Excalibur", collection_preference: create(:collection_preference, closed: false)) }
+      let(:user) { create(:user) }
+
+      before { fake_login_known_user(user) }
+
+      it "allows access to new work form" do
+        get :new, params: { collection_id: collection.name }
+        expect(response).to render_template("new")
+      end
+    end
+  end
+
   describe "GET #navigate" do
     context "denies access for work that isn't visible to user" do
       subject { get :navigate, params: { id: work.id } }
