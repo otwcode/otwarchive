685b7546879028da2443eacf41b97c363a93f097
ReverM <danael.villeneuve@gmail.com>
2025-12-15 11:13:39 -0500

    Fixed test

--- spec/controllers/challenge_requests_controller_spec.rb
+++ spec/controllers/challenge_requests_controller_spec.rb
@@ -5,11 +5,14 @@ describe ChallengeRequestsController do
   include RedirectExpectationHelper
 
   describe "index" do
-    it "should not throw a 500 error if sorting by prompter with an anonymous prompt" do
-      signup = create(:prompt_meme_signup)  
-      signup.requests.create(anonymous: "TRUE")
-      get :index, params: { collection_id: signup.collection.name, sort_column: "prompter" }
-      expect(response.status).not_to eq(500)
+    context "when there are anonymous prompts" do
+      render_views
+      it "does not throw a 500 error if sorting by prompter with an anonymous prompt" do
+        signup = create(:prompt_meme_signup)  
+        signup.requests.create(anonymous: true)
+        get :index, params: { collection_id: signup.collection.name, sort_column: "prompter" }
+        expect(response.status).not_to eq(500)
+      end
     end
   end
 end
