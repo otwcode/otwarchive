29fd12068d5fa2386ff5cefbfd80f1ac5267cefd
ReverM <danael.villeneuve@gmail.com>
2025-12-17 17:36:32 -0500

    Added run all index job to test

--- spec/controllers/challenge_requests_controller_spec.rb
+++ spec/controllers/challenge_requests_controller_spec.rb
@@ -11,6 +11,7 @@ describe ChallengeRequestsController, work_search: true do
       it "does not throw a 500 error if sorting by prompter with an anonymous prompt" do
         signup = create(:prompt_meme_signup)  
         signup.requests.create(anonymous: true)
+        run_all_indexing_jobs
         get :index, params: { collection_id: signup.collection.name, sort_column: "prompter" }
         expect(response.status).not_to eq(500)
       end
