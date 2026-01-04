09557a503aaa8f48b1cbc4929a0d2a9dd4680409
kiyazz <kiyazz123@gmail.com>
2026-01-03 20:44:35 -0800

    AO3-7249 update migration to include foreign key to users

--- db/migrate/20260103083335_create_user_past_emails.rb
+++ db/migrate/20260103083335_create_user_past_emails.rb
@@ -1,12 +1,12 @@
 class CreateUserPastEmails < ActiveRecord::Migration[7.2]
   def change
     create_table :user_past_emails do |t|
+      t.references :user, null: false, foreign_key: true
+      
       t.bigint :user_id
       t.string :email_address
       t.datetime :changed_at
-
-      add_foreign_key :user_past_emails, :users
-
+      
       t.timestamps
     end
   end
