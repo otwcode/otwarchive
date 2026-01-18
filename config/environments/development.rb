398a5bfdb72d7335db5a96f2f222581969a04677
Marcus Low <mlow16@outlook.com>
2026-01-18 00:05:18 -0800

    AO3-7231 Disable query log tags in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
   config.active_record.verbose_query_logs = true
 
   # Append comments with runtime information tags to SQL queries in logs.
-  config.active_record.query_log_tags_enabled = true
+  # config.active_record.query_log_tags_enabled = true
 
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
