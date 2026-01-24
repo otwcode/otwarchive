d7da3907ecdad98d96f03f7d5bffd0d4ce6fe242
Brian Austin <brianjaustin@gmail.com>
2026-01-24 22:48:08 +0100

    AO3-7047 Migrate foreign keys and add tests

--- db/migrate/20260124181621_change_comments_ids_to_bigint.rb
+++ db/migrate/20260124181621_change_comments_ids_to_bigint.rb
@@ -1,8 +1,11 @@
-class ChangeCommentsIdToBigint < ActiveRecord::Migration[7.2]
+class ChangeCommentsIdsToBigint < ActiveRecord::Migration[7.2]
   uses_departure! if Rails.env.staging? || Rails.env.production?
 
   def up
     change_column :comments, :id, "bigint NOT NULL AUTO_INCREMENT"
+    change_column :comments, :commentable_id, "bigint"
+    change_column :comments, :thread, "bigint"
+    change_column :inbox_comments, :feedback_comment_id, "bigint"
   end
 
   def down
