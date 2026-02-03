0735f9192b7af396e98f5f8c90616786b431b0dd
Marcus Low <mlow16@outlook.com>
2026-02-03 09:57:36 -0800

    AO3-6983 Add support form notices (#5389)
    * AO3-6983 Add support form notices
    
    * AO3-6415 Rubocop
    
    * AO3-6983 Drop caching, disallow media
    
    * AO3-6983 Update model spec
    
    * AO3-6983 Improve support notices
    
    * always show a blurb box on the management pages
      * attach edit/delete buttons to the blurb
      * remove edit/delete buttons from navigation
    * deactivate all other notices when activating a new notice
    * prevent the deletion of active admin banners
    
    * AO3-6983 Add admin activity logging
    
    * AO3-6983 Add cancel button to edit page
    
    * AO3-6983 Make admin activity specs nicer
    
    * AO3-6983 Hide edit button based on policy
    
    * AO3-6983 Drop ordering on active notice selection
    
    there should only be one
    
    * AO3-6983 Tighten DB schema
    
    * AO3-6983 Changes from code review
    
    * AO3-6983 Fix test indentation
    
    * AO3-6983 Rubocop, test fix
    
    * AO3-6983 Updates from review
    
    probably should've split this into more commits
    
    * AO3-6983 Rubocop
    
    * AO3-6983 add prefix to enum, revert to use update
    
    * AO3-6983 Fix styling of support notice
    
    * add support banner classes to the actual notice
    * separate userstuff
    * drop blockquote
    
    * AO3-6983 Drop blurb styling on show page
    
    * AO3-6983 Add test for pagination
    
    * AO3-6983 Move actions out of navigation
    
    * AO3-6983 Move actions back to navigation
    
    * AO3-6983 CSS fixes from review
    
    * Broaden notice width fix selector
    * Move margin adjustment to feedback section
    
    * AO3-6983 Display support notice when form submission is rejected
    
    * AO3-6983 Combine selectors for duplicated margin rule
    
    * AO3-6983 Add test for notice display after form rejection

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,5 +1,6 @@
 class FeedbacksController < ApplicationController
   before_action :load_support_languages
+  before_action :load_support_notice
 
   def new
     @admin_setting = AdminSetting.current
@@ -37,6 +38,10 @@ class FeedbacksController < ApplicationController
 
   def load_support_languages
     @support_languages = Language.where(support_available: true).default_order
+  end
+
+  def load_support_notice
+    @support_notice = SupportNotice.where(active: true).first
   end
 
   def feedback_params
