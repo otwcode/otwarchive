3b5839b36eeb17ca43db258870a919db7ce9fc0a
sarken <sarken@gmail.com>
2026-02-06 01:48:48 -0500

    AO3-7030 Try to find some bracket layout that'll appease Reviewdog's contradictory issues

--- app/controllers/skins_controller.rb
+++ app/controllers/skins_controller.rb
@@ -180,7 +180,8 @@ class SkinsController < ApplicationController
   private
 
   def skin_params
-    allowed_attributes = [:title, :description, :css, :role, :ie_condition,
+    allowed_attributes = [
+                          :title, :description, :css, :role, :ie_condition,
                           :unusable, :font, :base_em, :margin,
                           :paragraph_margin, :background_color,
                           :foreground_color, :headercolor, :accent_color, :icon,
@@ -188,7 +189,8 @@ class SkinsController < ApplicationController
                           skin_parents_attributes: [
                             :id, :position, :parent_skin_id, :parent_skin_title,
                             :_destroy
-                         ]]
+                          ]
+                         ]
 
     allowed_attributes += [:public] if current_user.is_a?(User) && current_user.official
 
