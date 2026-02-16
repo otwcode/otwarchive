a7d2309562c24f735a7522a3dd76a0f31006b537
gmjimenezc <gm.jmnzc@gmail.com>
2026-02-15 21:57:54 -0500

    AO3-6591: Move imported_from_url to a new table with different variants in different columns

--- db/migrate/20260215000000_create_work_import_urls.rb
+++ db/migrate/20260215000000_create_work_import_urls.rb
@@ -1,17 +1,12 @@
 class CreateWorkImportUrls < ActiveRecord::Migration[7.2]
   def change
     create_table :work_import_urls do |t|
-      t.references :work, null: false, type: :integer
-      t.string :url, null: false
-      t.string :minimal
-      t.string :minimal_no_protocol_no_www
+      t.references :work, null: false, type: :integer, index: { unique: true }
+      t.string :url, null: false, index: true
+      t.string :minimal, index: true
+      t.string :minimal_no_protocol_no_www, index: true
 
       t.timestamps
     end
-
-    add_index :work_import_urls, :work_id, unique: true
-    add_index :work_import_urls, :url
-    add_index :work_import_urls, :minimal
-    add_index :work_import_urls, :minimal_no_protocol_no_www
   end
 end
