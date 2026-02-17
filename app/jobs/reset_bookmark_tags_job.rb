dc3edaf6669b41a5c85172cb5ee5783fbdda1f81
Camilly Vitória <vitoriacamilly308@gmail.com>
2026-02-17 20:14:06 -0300

    style: apply rubocop fixes to new job files

--- app/jobs/reset_bookmark_tags_job.rb
+++ app/jobs/reset_bookmark_tags_job.rb
@@ -1,20 +1,19 @@
 class ResetBookmarkTagsJob < ApplicationJob
-    queue_as :low
+  queue_as :low
 
-    def perform
-        # Searches for non-canonical tags with zero usage count (only bookmarks or nothing)
-        Tag.where(canonical: false, taggings_count_cache: 0).find_each(batch_size: 1000) do |tag|
+  def perform
+    # Searches for non-canonical tags with zero usage count (only bookmarks or nothing)
+    Tag.where(canonical: false, taggings_count_cache: 0).find_each(batch_size: 1000) do |tag|
+      # Check if have associations (parents) or categorized types
+      has_type = tag.type.present? && tag.type != "Tag"
+      has_parents = tag.common_taggings.exists?
 
-            # Check if have associations (parents) or categorized types
-            has_type = tag.type.present? && tag.type != 'Tag'
-            has_parents = tag.common_taggings.exists?
-
-            if has_type || has_parents
-                # Removes data from the association table (Wrangling)
-                tag.common_taggings.delete_all
-                # Return the type to 'Tag' (status 'Unsorted')
-                tag.update_column(:type, 'Tag')
-            end
-        end
+      if has_type || has_parents
+        # Removes data from the association table (Wrangling)
+        tag.common_taggings.delete_all
+        # Return the type to 'Tag' (status 'Unsorted')
+        tag.update_column(:type, "Tag")
+      end
     end
+  end
 end
