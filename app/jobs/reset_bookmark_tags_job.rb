b40edf15b75803d601f54dee539d0db10808dc40
Camilly Vitória <vitoriacamilly308@gmail.com>
2026-02-27 21:48:35 -0300

    refactor(bookmarks_tags): split job into enqueue and process classes per review

--- app/jobs/reset_bookmark_tags_job.rb
+++ app/jobs/reset_bookmark_tags_job.rb
@@ -1,23 +1,17 @@
 class ResetBookmarkTagsJob < ApplicationJob
   queue_as :low
 
-  def perform(tag_ids = nil)
+  def perform(tag_ids)
     base_scope = Tag.nonsynonymous.where(taggings_count_cache: 0)
-
-    if tag_ids.nil?
-      base_scope.find_in_batches(batch_size: 1000) do |tags|
-        ResetBookmarkTagsJob.perform_later(tags.map(&:id))
-      end
-    else
-      base_scope.where(id: tag_ids).each do |tag|
-        has_type = tag.type.present? && tag.type != "Tag"
-        has_parents = tag.common_taggings.exists?
-
-        next unless has_type || has_parents
+    
+    base_scope.where(id: tag_ids).each do |tag|
+      has_type = tag.type.present? && tag.type != "Tag"
+      has_parents = tag.common_taggings.exists?
+      
+      next unless has_type || has_parents
         
-        tag.common_taggings.destroy_all
-        tag.update(type: "Tag")
-      end
+      tag.common_taggings.destroy_all
+      tag.update(type: "Tag")
     end
   end
 end
