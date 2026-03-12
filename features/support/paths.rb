3818a26321518f70349521a65c9b882c653e0f72
forceofcalm <forceofcalm@gmail.com>
2026-03-11 20:45:14 -0400

    rubocop have mercy plz

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -194,12 +194,9 @@ module NavigationHelpers
     when /^the works tagged "(.*)"$/i
       step %{all indexing jobs have been run}
       tag_works_path(Tag.find_by_name($1))
-    when /^the bookmarks tagged "(.*)"$/i
+    when /^the bookmarks (?:tagged|page for the tag) "(.*)"$/i
       step %{all indexing jobs have been run}
-      tag_bookmarks_path(Tag.find_by_name($1))
-    when /^the bookmarks page for the tag "(.*)"$/i
-      step %{all indexing jobs have been run}
-      tag_bookmarks_path(Tag.find_by_name($1))
+      tag_bookmarks_path(Tag.find_by_name(Regexp.last_match(1)))
     when /^the bookmarks in collection "(.*)"$/i
       step %{all indexing jobs have been run}
       collection_bookmarks_path(Collection.find_by(title: $1))
