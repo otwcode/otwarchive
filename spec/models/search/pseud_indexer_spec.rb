ce43d098105e178d0b35b7f33b13b60c3e8508cf
not-varram <email@varram.me>
2026-03-18 08:32:50 -0700

    Remove PseudIndexer test

--- spec/models/search/pseud_indexer_spec.rb
+++ spec/models/search/pseud_indexer_spec.rb
@@ -3,7 +3,6 @@ require "spec_helper"
 describe PseudIndexer, pseud_search: true do
   describe ".index_all" do
     it "uses configured shard count when creating the index" do
-      allow(PseudIndexer).to receive(:delete_index)
       allow(PseudIndexer).to receive(:index_from_db)
       expect(PseudIndexer).to receive(:create_index).with(shards: ArchiveConfig.PSEUD_SHARDS)
 
