67d9a24e8874cfafd867b8d30322896f06e94da1
not-varram <email@varram.me>
2026-03-17 17:47:59 -0700

    address pr comments

--- spec/models/search/tag_indexer_spec.rb
+++ spec/models/search/tag_indexer_spec.rb
@@ -3,7 +3,6 @@ require "spec_helper"
 describe TagIndexer, tag_search: true do
   describe ".index_all" do
     it "uses configured shard count when creating the index" do
-      allow(TagIndexer).to receive(:delete_index)
       allow(TagIndexer).to receive(:index_from_db)
       expect(TagIndexer).to receive(:create_index).with(shards: ArchiveConfig.TAG_SHARDS)
 
