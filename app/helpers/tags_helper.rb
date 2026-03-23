bfad47890d1859f0fe011bfd35181727d5cc951d
sarken <sarken@gmail.com>
2026-03-23 01:54:34 -0400

    AO3-7352 Rename the variable because it's not actually a link just the content of the link

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -288,10 +288,10 @@ module TagsHelper
   end
 
   def get_symbol_link(css_class, title_string)
-    link = tag.span(tag.span(title_string, class: "text"), class: css_class, title: title_string)
+    symbol = tag.span(tag.span(title_string, class: "text"), class: css_class, title: title_string)
     # Use link_to_modal rather than link_to_help_modal so we can use a title rather than an aria-label, ensuring screen readers read the link text.
     # Keep classes added by link_to_help_modal for consistency with code prior to AO3-7352.
-    content_tag(:li, link_to_modal(link, for: help_symbols_key_path, title: t("tags_helper.get_symbol_link.title"), class: "help symbol question"))
+    content_tag(:li, link_to_modal(symbol, for: help_symbols_key_path, title: t("tags_helper.get_symbol_link.title"), class: "help symbol question"))
   end
 
   # return the right warnings class
