06cb026d8965ff68f92932ac7345b82ceea5b9d8
Luis Pabon <turkeyfried@gmail.com>
2026-03-23 11:14:56 -0500

    Imported URL columns are updated on save

--- app/models/imported_url.rb
+++ app/models/imported_url.rb
@@ -1,4 +1,21 @@
-# Allows admins to track all past usernames of a user
+# Url of an imported work
 class ImportedUrl < ApplicationRecord
-  belongs_to :work
+  belongs_to :Work
+
+  def initialize(url)
+    self.original = url
+  end
+
+  before_save :set_formatted_urls
+  def set_formatted_urls
+    formatter = UrlFormatter.new(:original)
+    self.minimal = formatter.minimal
+    self.minimal_no_protocol_no_www = formatter.minimal_no_protocol_no_www
+    self.no_www = formatter.no_www
+    self.with_www = formatter.with_www
+    self.with_http = formatter.with_http
+    self.with_https = formatter.with_https
+    self.encoded = formatter.encoded
+    self.decoded = formatter.decoded
+  end
 end
