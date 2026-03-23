766791c725bd69a85342290affcc7f90c61d76fc
Luis Pabon <turkeyfried@gmail.com>
2026-03-23 13:18:21 -0500

    Use original url string not a symbol

--- app/models/imported_url.rb
+++ app/models/imported_url.rb
@@ -2,13 +2,9 @@
 class ImportedUrl < ApplicationRecord
   belongs_to :Work
 
-  def initialize(url)
-    self.original = url
-  end
-
   before_save :set_formatted_urls
   def set_formatted_urls
-    formatter = UrlFormatter.new(:original)
+    formatter = UrlFormatter.new(self.original)
     self.minimal = formatter.minimal
     self.minimal_no_protocol_no_www = formatter.minimal_no_protocol_no_www
     self.no_www = formatter.no_www
