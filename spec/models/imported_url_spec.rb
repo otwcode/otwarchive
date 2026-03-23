27519b25a099d943855ab0069afebdcdb2d6e1fb
Luis Pabon <turkeyfried@gmail.com>
2026-03-23 12:45:01 -0500

    Add test case for imported url autosave

---
+++ spec/models/imported_url_spec.rb
@@ -0,0 +1,20 @@
+require "spec_helper"
+
+describe ImportedUrl do
+  it "sets formatted urls on save" do
+    let(:url) { ImportedUrl.new }
+    formatter = UrlFormatter.new("http://www.trickster.org/llwyden/misc/cracked.html")
+    url.original = formatter.original
+    url.save
+
+    expect(url.minimal).to eq(formatter.minimal)
+    expect(url.minimal_no_protocol_no_www).to eq(formatter.minimal_no_protocol_no_www)
+    expect(url.no_www).to eq(formatter.no_www)
+    expect(url.with_www).to eq(formatter.with_www)
+    expect(url.with_http).to eq(formatter.with_http)
+    expect(url.with_https).to eq(formatter.with_https)
+    expect(url.encoded).to eq(formatter.encoded)
+    expect(url.decoded).to eq(formatter.decoded)
+    expect(create(:url)).to be_valid
+  end
+end
