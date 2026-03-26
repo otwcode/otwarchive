f5eb2d283d142a06e3732a71b5a369b67622bd1b
Hunter Smith <1946720+hunternet93@users.noreply.github.com>
2026-03-25 18:26:46 -0700

    AO3-7239 Migrate help files: series, translation link, languages help, parent works help and recipients

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -1,7 +1,11 @@
 class HelpController < ApplicationController
   HELP_ACTIONS = %i[
+    choosing_series
     first_login
+    languages_help
+    parent_works_help
     preferences_locale
+    recipients
     skins_basics
     skins_creating
     skins_parents
@@ -9,18 +13,21 @@ class HelpController < ApplicationController
     tags_fandoms
     tags_ratings
     tags_warnings
-  ]
+    translation_link
+  ].freeze
 
-  HELP_REDIRECTS = Hash[
-    HELP_ACTIONS.map {|action| [action, "/help/#{action.to_s.gsub('_', '-')}.html"]}
-  ].merge({
-    first_login: "/first_login_help",
-  })
+  HELP_REDIRECTS = HELP_ACTIONS
+    .index_with { |action| "/help/#{action.to_s.gsub('_', '-')}.html" }
+    .merge({
+             first_login: "/first_login_help"
+           }).freeze
 
   before_action :users_only, only: [:first_login]
   layout proc { |controller| controller.request.xhr? ? false : "application" } # rubocop:disable Lint/AmbiguousBlockAssociation
 
   HELP_ACTIONS.each do |action|
-    define_method(action) { }
+    define_method(action) do
+      # Intentionally empty block for help actions
+    end
   end
 end
