15f6542994912d1ea95ff32b3409ed63e52a2f12
Kylie McClanahan <kylielmcclanahan@gmail.com>
2026-03-31 16:06:47 -0700

    Added i18n locale string

--- app/controllers/skins_controller.rb
+++ app/controllers/skins_controller.rb
@@ -152,7 +152,7 @@ class SkinsController < ApplicationController
       current_user.preference.skin_id = AdminSetting.default_skin_id
       current_user.preference.save
     end
-    flash[:notice] = ts("You are now using the default site skin again!")
+    flash[:notice] = t("skins.default_skin")
     redirect_back_or_to root_path
   end
 
