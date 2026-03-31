491882653171725827e4f03177d2818ff467c921
Bilka <Bilka2@users.noreply.github.com>
2026-03-31 19:57:31 +0200

    Revert "AO3-6218 Allow certain admins to access all collection and challenge pages usually reserved for owners (#5592)"
    This reverts commit 06c28a0f8bfe07cd9c4d5d7dd96e10ab911e2b5a.

--- app/controllers/challenge_requests_controller.rb
+++ app/controllers/challenge_requests_controller.rb
@@ -7,14 +7,10 @@ class ChallengeRequestsController < ApplicationController
       flash.now[:notice] = ts("Collection could not be found")
       redirect_to "/" and return
     end
-
-    return if @collection.challenge_type == "PromptMeme" || privileged_collection_admin?
-    return if @collection.challenge_type == "GiftExchange" && @collection.challenge.user_allowed_to_see_requests_summary?(current_user)
-
-    return admin_only_access_denied if logged_in_as_admin?
-
-    flash.now[:notice] = ts("You are not allowed to view the requests summary!")
-    redirect_to collection_path(@collection) and return
+    unless @collection.challenge_type == "PromptMeme" || (@collection.challenge_type == "GiftExchange" && @collection.challenge.user_allowed_to_see_requests_summary?(current_user))
+      flash.now[:notice] = ts("You are not allowed to view the requests summary!")
+      redirect_to collection_path(@collection) and return
+    end
   end
 
   def index
--- app/policies/collection_policy.rb
+++
@@ -1,7 +0,0 @@
-class CollectionPolicy < ApplicationPolicy
-  ACCESS_ROLES = %w[support policy_and_abuse superadmin].freeze
-
-  def access?
-    user_has_roles?(ACCESS_ROLES)
-  end
-end
