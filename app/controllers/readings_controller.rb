afd5a3a0cbe539ec23f02d5daf2ea42a9f728546
Respheal <madeleine.ethridge@gmail.com>
2026-04-02 15:54:09 -0400

    AO3-7331 Adding username to the title of the Marked for Later page

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -10,11 +10,12 @@ class ReadingsController < ApplicationController
   end
 
   def index
-    @readings = @user.readings.visible
-    @page_subtitle = t(".history_page_title", username: @user.login)
     if params[:show] == 'to-read'
       @readings = @readings.where(toread: true)
-      @page_subtitle = t(".marked_for_later_page_title")
+      @page_subtitle = t(".marked_for_later_page_title", username: @user.login)
+    else 
+      @readings = @user.readings.visible
+      @page_subtitle = t(".history_page_title", username: @user.login)
     end
     @readings = @readings.order("last_viewed DESC").includes(work: :pseuds)
     @pagy, @readings = pagy(@readings)
