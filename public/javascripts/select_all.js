fddb0c2ba4ed06b5a92e1e9e38c546cd40cffacd
Scott Venkataraman <scottvenkataraman22@gmail.com>
2026-04-07 15:57:37 -0400

    Refactor select_all js file to remove repetitive functions and improve flexibility

--- public/javascripts/select_all.js
+++ public/javascripts/select_all.js
@@ -1,46 +1,29 @@
 // To be included only where needed:
 // tag_wranglings/index, tags/wrangle, admin/spam
-// TODO: Refactor to be less repetitive
 
-$j(document).ready(function(){
-  $j("#wrangle_all_select").click(function() {
-    $j("#wrangulator").find(":checkbox[name='selected_tags[]']").each(function(index, ticky) {
-        $j(ticky).prop("checked", true);
-      });
+$j(document).ready(function() {
+
+  function bindToggle(buttonId, container, checkboxName, state) {
+    $j(buttonId).click(function() {
+      $j(container)
+        .find(":checkbox[name='" + checkboxName + "']")
+        .prop("checked", state);
+    });
+  }
+
+  var toggles = [
+    ["#wrangle_all_select",    "#wrangulator", "selected_tags[]", true ],
+    ["#wrangle_all_deselect",  "#wrangulator", "selected_tags[]", false],
+    ["#canonize_all_select",   "#wrangulator", "canonicals[]",    true ],
+    ["#canonize_all_deselect", "#wrangulator", "canonicals[]",    false],
+    ["#spam_all_select",       "#spam_works",  "spam[]",          true ],
+    ["#spam_all_deselect",     "#spam_works",  "spam[]",          false],
+    ["#ham_all_select",        "#spam_works",  "ham[]",           true ],
+    ["#ham_all_deselect",      "#spam_works",  "ham[]",           false],
+  ];
+
+  $j.each(toggles, function(_, t) {
+    bindToggle(t[0], t[1], t[2], t[3]);
   });
-  $j("#wrangle_all_deselect").click(function() {
-    $j("#wrangulator").find(":checkbox[name='selected_tags[]']").each(function(index, ticky) {
-        $j(ticky).prop("checked", false);
-      });
-  });
-  $j("#canonize_all_select").click(function() {
-    $j("#wrangulator").find(":checkbox[name='canonicals[]']").each(function(index, ticky) {
-        $j(ticky).prop("checked", true);
-      });
-  });
-  $j("#canonize_all_deselect").click(function() {
-    $j("#wrangulator").find(":checkbox[name='canonicals[]']").each(function(index, ticky) {
-        $j(ticky).prop("checked", false);
-      });
-  });
-  $j("#spam_all_select").click(function() {
-    $j("#spam_works").find(":checkbox[name='spam[]']").each(function(index, ticky) {
-        $j(ticky).prop("checked", true);
-      });
-  });
-  $j("#spam_all_deselect").click(function() {
-    $j("#spam_works").find(":checkbox[name='spam[]']").each(function(index, ticky) {
-        $j(ticky).prop("checked", false);
-      });
-  });
-  $j("#ham_all_select").click(function() {
-    $j("#spam_works").find(":checkbox[name='ham[]']").each(function(index, ticky) {
-        $j(ticky).prop("checked", true);
-      });
-  });
-  $j("#ham_all_deselect").click(function() {
-    $j("#spam_works").find(":checkbox[name='ham[]']").each(function(index, ticky) {
-        $j(ticky).prop("checked", false);
-      });
-  });
-})
+
+});
