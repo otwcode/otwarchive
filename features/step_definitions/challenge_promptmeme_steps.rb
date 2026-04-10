108e9e14ddff3f3346322c3fc5fc06f6154636f3
nicolacleary <62173794+nicolacleary@users.noreply.github.com>
2026-04-10 11:54:48 +0200

    AO3-6876 Check escape chars in step_definitions

--- features/step_definitions/challenge_promptmeme_steps.rb
+++ features/step_definitions/challenge_promptmeme_steps.rb
@@ -282,8 +282,7 @@ When "I sign up for {string} with combination ESCAPE_CHARS" do |title|
   step %{I fill in the 1st field with id matching "title" with "These & < > ' ? are some HTML escape chars"}
   click_button "Submit"
   step %{I should see "Sign-up was successfully created"}
-  step %{I should see "These "}
-  step %{I should see " are some HTML escape chars"}
+  step %{I should see "These & < > ' ? are some HTML escape chars"}
 end
 
 When /^I add prompt (\d+)$/ do |number|
