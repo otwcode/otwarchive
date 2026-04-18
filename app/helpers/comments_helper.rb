b37d7c3fac64ee0ba102475bf35ac198bef4a881
Breanna Gream <breannalgream@gmail.com>
2026-04-18 16:30:18 +1000

    AO3-7404 Separate title helpers for different comment pages

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -11,7 +11,18 @@ module CommentsHelper
     else
       title = link_to(commentable.commentable_name, commentable)
     end
-    t(".page_heading_html", link: title)
+    t("comments.index.page_heading_html", link: title)
+  end
+
+  def title_for_new_comment_page(commentable)
+    if commentable.commentable_name.blank?
+      title = ""
+    elsif commentable.is_a?(Tag)
+      title = link_to_tag(commentable)
+    else
+      title = link_to(commentable.commentable_name, commentable)
+    end
+    t("comments.new.page_heading_html", link: title)
   end
 
   def link_to_comment_ultimate_parent(comment)
