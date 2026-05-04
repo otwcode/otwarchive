89e961dbde2946ba4237162cdb764acbb50b25ee
Slava <53832230+slavalamp@users.noreply.github.com>
2026-05-04 04:05:25 +0200

    AO3-5550 fix work info displaying wrong

--- app/helpers/related_works_helper.rb
+++ app/helpers/related_works_helper.rb
@@ -1,20 +1,40 @@
 module RelatedWorksHelper
-  def related_work_title(related_work, with_byline: false, relation: "other_relations")
+  def related_work_title(related_work)
+    work_link = link_to related_work.title, polymorphic_url(related_work)
+
+    if related_work.respond_to?(:unrevealed?) && related_work.unrevealed?
+      t("related_works.index.unrevealed_work")
+    elsif related_work.restricted? && !logged_in?
+      t("related_works.index.restricted_work")
+    else
+      work_link
+    end
+  end
+
+  def related_work_title_with_byline(related_work)
     work_link = link_to related_work.title, polymorphic_url(related_work)
     creator_link = byline(related_work)
 
     if related_work.respond_to?(:unrevealed?) && related_work.unrevealed?
-      t("related_works.index.unrevealed")
+      t("related_works.index.unrevealed_work")
     elsif related_work.restricted? && !logged_in?
-      if with_byline
-        t(".#{relation}.restricted_by_html", creator_link: creator_link)
-      else
-        t(".#{relation}.restricted")
-      end
-    elsif with_byline
-      t(".#{relation}.revealed_html", work_link: work_link, creator_link: creator_link)
+      t("related_works.index.restricted_work_by_html", creator_link: creator_link)
     else
-      work_link
+      t("related_works.index.revealed_work_by_html",
+        work_link: work_link,
+        creator_link: creator_link)
+    end
+  end
+
+  def related_work_relation(related_work, relation)
+    work_link = link_to related_work.title, polymorphic_url(related_work)
+
+    if related_work.respond_to?(:unrevealed?) && related_work.unrevealed?
+      t(".#{relation}.unrevealed")
+    elsif related_work.restricted? && !logged_in?
+      t(".#{relation}.restricted")
+    else
+      t(".#{relation}.revealed_html", work_link: work_link)
     end
   end
 end
