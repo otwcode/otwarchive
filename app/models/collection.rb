e2465325f020561039d38f7dfc2dde4a94a302cb
Anish Kapoor <107356645+cinderbrick1212@users.noreply.github.com>
2026-05-19 01:46:26 +0530

    [AO3-7473] Restrict allowed icon formats to specific image types

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -10,7 +10,7 @@ class Collection < ApplicationRecord
   # i18n-tasks-use t("errors.attributes.icon.invalid_format")
   # i18n-tasks-use t("errors.attributes.icon.too_large")
   validates :icon, attachment: {
-    allowed_formats: %r{image/\S+},
+    allowed_formats: %w[image/gif image/jpeg image/png],
     maximum_size: ArchiveConfig.ICON_SIZE_KB_MAX.kilobytes
   }
 
