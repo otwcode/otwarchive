80bd963d65694cf442ff88f95d907cad34d90d4d
Jaimin Patel <jaiminpatel@Jaimins-MacBook-Pro-2.local>
2026-06-09 21:34:27 -0400

    AO3-105 Add Phrase in-context editor

--- config/initializers/phraseapp_in_context_editor.rb
+++ config/initializers/phraseapp_in_context_editor.rb
@@ -1,31 +1,28 @@
 PhraseApp::InContextEditor.configure do |config|
   # Enable or disable the In-Context-Editor in general
-  if ENV['AO3_PHRASE_APP'] == 'true' || ArchiveConfig.PHRASEAPP_ENABLE == 'true' then
-    config.enabled = true 
-  else 
-    config.enabled = false 
+  if ENV["AO3_PHRASE_APP"] == "true" || ArchiveConfig.PHRASEAPP_ENABLE == "true"
+    config.enabled = true
+  else
+    config.enabled = false
   end
 
-  # Fetch your project id after creating your first project
-  # in Translation Center.
-  # You can find the project id in your project settings
-  # page (https://phraseapp.com/projects)
-  config.project_id = "47f2a1b0cf81df327878c6d89cee7af3"
-
-  # You can create and manage access tokens in your profile settings
-  # in Translation Center or via the Authorizations API
-  # (http://docs.phraseapp.com/api/v2/authorizations/).
-  config.access_token = ArchiveConfig.PHRASEAPP_TOKEN
+  # Configure with your project id and account id. You can find the
+  # project id in your project settings and account id in your account
+  # settings (https://app.phrase.com/)
+  config.project_id = "a3667b8095533c2b3f8d3ac946bb642f"
+  config.account_id = "c14670ca"
 
   # Configure an array of key names that should not be handled
   # by the In-Context-Editor.
-  config.ignored_keys = ["number.*", "breadcrumb.*"]
+  # Exclude keys from the faker, rack_dev and will_paginate gems.
+  # Exclude keys that are already translated in the rails-i18n gem.
+  config.ignored_keys = ["faker.*", "rack_dev.*", "will_paginate.*", "number.*", "date.*", "datetime.*", "support.array*", "time.*"]
 
-  # PhraseApp uses decorators to generate a unique identification key
+  # Phrase uses decorators to generate a unique identification key
   # in context of your document. However, this might result in conflicts
   # with other libraries (e.g. client-side template engines) that use a similar syntax.
   # If you encounter this problem, you might want to change this decorator pattern.
-  # More information: http://docs.phraseapp.com/guides/in-context-editor/configure/
+  # More information: https://help.phrase.com/hc/en-us/articles/5784095916188-In-Context-Editor-Strings-
   # config.prefix = "{{__"
   # config.suffix = "__}}"
 end
