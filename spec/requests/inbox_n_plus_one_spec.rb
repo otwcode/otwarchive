cc2ffbf1d0b6169d55e5b95089ce3576c58828f3
Sherin Kurian <sherinkurian.123@gmail.com>
2026-06-20 14:56:32 -0700

    add inbox n+1 specs for guest and reply comments

--- spec/requests/inbox_n_plus_one_spec.rb
+++ spec/requests/inbox_n_plus_one_spec.rb
@@ -6,18 +6,32 @@ describe "n+1 queries in the InboxController" do
   describe "#show" do
     let!(:user) { create(:user) }
 
-    context "when displaying multiple inbox comments", n_plus_one: true do
-      before { fake_login_known_user(user) }
+    shared_examples "a constant number of queries" do |*traits|
+      context "when displaying multiple inbox comments", n_plus_one: true do
+        before { fake_login_known_user(user) }
 
-      populate { |n| create_list(:inbox_comment, n, user: user) }
+        populate { |n| create_list(:inbox_comment, n, *traits, user: user) }
 
-      warmup { get user_inbox_path(user) }
+        warmup { get user_inbox_path(user) }
 
-      it "produces a constant number of queries" do
-        expect do
-          get user_inbox_path(user)
-        end.to perform_constant_number_of_queries
+        it "produces a constant number of queries" do
+          expect do
+            get user_inbox_path(user)
+          end.to perform_constant_number_of_queries
+        end
       end
+    end
+
+    context "with comments from registered users" do
+      it_behaves_like "a constant number of queries"
+    end
+
+    context "with comments from guests" do
+      it_behaves_like "a constant number of queries", :with_guest_comment
+    end
+
+    context "with reply comments" do
+      it_behaves_like "a constant number of queries", :with_reply_comment
     end
   end
 end
