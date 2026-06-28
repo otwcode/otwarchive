294ba9f3d1595fe75e9221556e21b9344d0a0b42
ASalbenblatt <ausa6999@colorado.edu>
2026-06-28 09:48:24 -0600

    AO3-4412 Adding a test to ensure there are not N+1 issues when loading inbox commets on the homepage

---
+++ spec/requests/homepage_inbox_n_plus_one_spec.rb
@@ -0,0 +1,49 @@
+require "spec_helper"
+
+describe "n+1 queries in the inbox controller for the homepage" do
+  include LoginMacros
+
+  describe "#index", n_plus_one: true do
+    context "when displaying a user's unread messages on the homepage" do
+      let!(:user) { create(:user) }
+
+      subject do
+        proc do
+          get '/'
+        end
+      end
+
+      before do
+        fake_login_known_user(user)
+      end
+
+      context "when all works are cached" do
+        populate do |n|
+          create_list(:inbox_comment, n)
+          subject.call # this caches the comments
+        end
+
+        it "produces a constant number of queries" do
+          expect { subject.call }
+            .to perform_constant_number_of_queries
+        end
+      end
+
+      context "when nothing is cached" do
+        populate do |n|
+          create_list(:inbox_comment, n)
+        end
+
+        it "performs around 9 queries per message" do
+          # TODO: Ideally, we'd like the uncached inbox listings to also have a
+          # constant number of queries, instead of the linear number of queries
+          # we're checking for here. But we also don't want to put too much
+          # unnecessary load on the databases when we have a bunch of cache hits,
+          # so this requires some care & tuning.
+          expect { subject.call }
+            .to perform_linear_number_of_queries(slope: 9).with_warming_up
+        end
+      end
+    end
+  end
+end
