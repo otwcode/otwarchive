674f247d3395616098b205b95e006396bb0af375
Bilka <Bilka2@users.noreply.github.com>
2026-07-02 21:59:15 +0200

    AO3-7525 Reviewdog

--- config/locales/rails-i18n/pluralization/zh-Hans.rb
+++ config/locales/rails-i18n/pluralization/zh-Hans.rb
@@ -1,3 +1,3 @@
-require 'rails_i18n/common_pluralizations/other'
+require "rails_i18n/common_pluralizations/other"
 
-::RailsI18n::Pluralization::Other.with_locale(:'zh-Hans')
+::RailsI18n::Pluralization::Other.with_locale(:"zh-Hans")
