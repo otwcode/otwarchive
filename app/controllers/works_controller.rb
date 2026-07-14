41d3878b41e90140271ccabc7dfe6d822ab076da
marsh <mellow.marsach@gmail.com>
2026-07-14 03:43:19 -0600

    AO3-7425 Set word count when posting draft from Edit Tag page (#5803)
    * AO3-7425 - Fix word count when posting draft from Edit Tag page
    
    * AO3-7425 - Suggested edits (simplify cucumber test, remove comment)

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -409,9 +409,11 @@ class WorksController < ApplicationController
       @work.save
       flash[:notice] = ts('Tags were successfully updated.')
       redirect_to(@work)
-    else # Save Draft
+    else
       @work.posted = true
       @work.minor_version = @work.minor_version + 1
+      @work.save
+      @work.word_count = @work.first_chapter.word_count
       @work.save
       flash[:notice] = ts('Work was successfully updated.')
       redirect_to(@work)
