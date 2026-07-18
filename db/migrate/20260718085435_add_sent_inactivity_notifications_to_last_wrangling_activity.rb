1d6fb11ca6e1b1c1699c2c5debafc4430f874aa5
Bilka <Bilka2@users.noreply.github.com>
2026-07-18 10:57:57 +0200

    AO3-6028 Update migration timestamp

--- db/migrate/20260718085435_add_sent_inactivity_notifications_to_last_wrangling_activity.rb
+++ db/migrate/20260718085435_add_sent_inactivity_notifications_to_last_wrangling_activity.rb
@@ -1,4 +1,4 @@
-class AddSentInactivityNotificationsToLastWranglingActivity < ActiveRecord::Migration[7.1]
+class AddSentInactivityNotificationsToLastWranglingActivity < ActiveRecord::Migration[8.1]
   def change
     add_column :last_wrangling_activities, :notified_inactive_wrangler, :boolean, default: false, null: false
     add_column :last_wrangling_activities, :notified_inactive_supervisors, :boolean, default: false, null: false
