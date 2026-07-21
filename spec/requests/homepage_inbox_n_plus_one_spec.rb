129a3454ef788ccd987e06614ff76a1a3fd18d6c
ASalbenblatt <ausa6999@colorado.edu>
2026-07-21 10:59:43 -0600

    AO3-4412 fixing the N+1 test for the inbox on the homepage

--- spec/requests/homepage_inbox_n_plus_one_spec.rb
+++ spec/requests/homepage_inbox_n_plus_one_spec.rb
@@ -1,48 +1,49 @@
 require "spec_helper"
 
-describe "n+1 queries in the inbox controller for the homepage" do
+describe "n+1 queries in the inbox controller for the homepage: " do
   include LoginMacros
 
-  describe "#index", n_plus_one: true do
-    context "when displaying a user's unread messages on the homepage" do
+  describe "#show", n_plus_one: true do
+    context "displaying a user's unread messages on the homepage" do
       let!(:user) { create(:user) }
 
-      subject do
-        proc do
-          get "/"
-        end
-      end
+      shared_examples "a constant number of queries" do
+        before { fake_login_known_user(user) }
 
-      before do
-        fake_login_known_user(user)
-      end
-
-      context "when all works are cached" do
         populate do |n|
-          create_list(:inbox_comment, n)
-          subject.call # this caches the comments
+          create_list(:inbox_comment, n, user: user, feedback_comment: build_feedback_comment.call)
+        end
+
+        warmup { get "/" }
+
+        it "displys the right number of comments" do
+          get "/"
+          expect(response.body.scan('id="feedback_comment_').size).to eq(current_scale.to_i)
         end
 
         it "produces a constant number of queries" do
-          expect { subject.call }
-            .to perform_constant_number_of_queries
+          expect do
+            get "/"
+          end.to perform_constant_number_of_queries
         end
       end
 
-      context "when nothing is cached" do
-        populate do |n|
-          create_list(:inbox_comment, n)
-        end
+      context "with comments from registered users" do
+        let(:build_feedback_comment) { -> { create(:comment) } }
 
-        it "performs around 9 queries per message" do
-          # TODO: Ideally, we'd like the uncached inbox listings to also have a
-          # constant number of queries, instead of the linear number of queries
-          # we're checking for here. But we also don't want to put too much
-          # unnecessary load on the databases when we have a bunch of cache hits,
-          # so this requires some care & tuning.
-          expect { subject.call }
-            .to perform_linear_number_of_queries(slope: 9).with_warming_up
-        end
+        it_behaves_like "a constant number of queries"
+      end
+
+      context "with comments from guests" do
+        let(:build_feedback_comment) { -> { create(:comment, :by_guest) } }
+
+        it_behaves_like "a constant number of queries"
+      end
+
+      context "with reply comments" do
+        let(:build_feedback_comment) { -> { create(:comment, commentable: create(:comment)) } }
+
+        it_behaves_like "a constant number of queries"
       end
     end
   end
