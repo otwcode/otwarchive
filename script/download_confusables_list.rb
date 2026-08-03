4c641095d638259104331c98f1706c5f8ffa3ee4
ömer faruk <omerfaruk@transformativeworks.org>
2026-08-04 01:54:27 +0300

    AO3-7444 finalize download script and add confusables to git

--- script/download_confusables_list.rb
+++ script/download_confusables_list.rb
@@ -1,40 +1,55 @@
 # frozen_string_literal: true
-require 'net/http'
 
-# todo finalize
-CONFUSABLES_TXT_DIRECTORY = "./confusables.txt"
+# This script is for downloading and updating Unicode's confusables list which
+# we use for checking guest comment names' likeness to forbidden names at 
+# app/validators/not_forbidden_name_validator.rb
+#
+# Run this script with:
+#   bundle exec rails r script/download_confusables_list.rb
 
-# todo add comment
+require "net/http"
+
+# The directory which the confusables are stored in the otwarchive repository.
+CONFUSABLES_DIRECTORY = "./script/confusables.txt"
+
+# Uses regex to find the line that starts with "# Version:" and returns what
+# comes after as string. Takes a string object as an argument.
 def fetch_version(text)
   text.each_line do |line|
     if (match = line.match(/^# Version: (.*)$/))
-      return match.captures
+      return match[1]
     end
   end
 end
 
+response = Net::HTTP.get_response(URI.parse("https://unicode.org/Public/security/latest/confusables.txt"))
 
-# todo catch possible errors
-response = Net::HTTP.start("unicode.org") { |http| http.get("/Public/security/latest/confusables.txt") }
-
-latest_version = fetch_version(response.body)
-unless latest_version
-  # todo raise some kind of error and exit maybe
-  puts "Could not find version data on latest list. Format might be changed making it necessary to update the script."
+if response.nil? || !response.is_a?(Net::HTTPSuccess)
+  puts "Could not get confusables.txt. Try visiting"
+  puts "https://unicode.org/Public/security/latest/confusables.txt to find"
+  puts "the error."
+  return
 end
 
-begin
-  existing_version = open(CONFUSABLES_TXT_DIRECTORY, "rb") { |file| fetch_version(file) }
-rescue => e
-  puts "Couldn't find confusables.txt #{e}"
+unless (latest_version = fetch_version(response.body))
+  puts "Could not find version data on latest list. Format might be changed"
+  puts "making it necessary to update this script."
+  return
 end
 
-if not existing_version# no confusables in directory
-  puts "We don't have confusables.txt, do you want to save the latest version (#{latest_version}) as #{CONFUSABLES_TXT_DIRECTORY}? (y/N)"
-  open(CONFUSABLES_TXT_DIRECTORY, "wb") { |file| file.write(response.body) } if gets.chomp == "y"
-elsif latest_version != existing_version
-  puts "We have the version #{existing_version} while the latest is #{latest_version}. Do you want to update? (y/N)"
-  open(CONFUSABLES_TXT_DIRECTORY, "wb") { |file| file.write(response.body) } if gets.chomp == "y"
-else # they're equal then
-  puts "We already have the latest version (#{existing_version})."
+existing_version = File.open(CONFUSABLES_DIRECTORY, "rb") { |f| fetch_version(f) } if File.exist?(CONFUSABLES_DIRECTORY)
+if !existing_version
+  puts "We don't have confusables.txt, do you want to save the latest version"
+  puts "#{latest_version} as #{CONFUSABLES_DIRECTORY}? (Y/n)"
+elsif latest_version == existing_version
+  puts "We already have the latest version which is #{existing_version}. Do you still want"
+  puts "to download? (Y/n)"
+else
+  puts "We have the version #{existing_version} while the latest is #{latest_version}. Do you want to"
+  puts "update? (Y/n)"
+end
+
+unless gets.chomp == "n"
+  File.open(CONFUSABLES_DIRECTORY, "wb") { |f| f.write(response.body) }
+  puts File.exist?(CONFUSABLES_DIRECTORY) ? "Save successful." : "Save failed."
 end
