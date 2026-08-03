a3bcd6dc83075ebb98377cd467ac59795da73c48
ömer faruk <omerfaruk@transformativeworks.org>
2026-08-03 14:41:54 +0300

    AO3-7444 implement working download script

--- script/download_confusables_list.rb
+++ script/download_confusables_list.rb
@@ -1,25 +1,40 @@
 # frozen_string_literal: true
+require 'net/http'
 
-require 'open-uri'
-download = URI.open('https://www.unicode.org/Public/security/latest/confusables.txt')
+# todo finalize
+CONFUSABLES_TXT_DIRECTORY = "./confusables.txt"
 
-download.each_line do |line|
-  latest_version = line.scan(/^# Version: (.*)$/) if line.scan(/^# Version: (.*)$/)
+# todo add comment
+def fetch_version(text)
+  text.each_line do |line|
+    if (match = line.match(/^# Version: (.*)$/))
+      return match.captures
+    end
+  end
 end
 
 
+# todo catch possible errors
+response = Net::HTTP.start("unicode.org") { |http| http.get("/Public/security/latest/confusables.txt") }
 
-
-File.foreach
-
-if true# no confusables in directory
-  puts "do you want to save the latest version (#{latest_version}) as ./confusables.txt ?"
-
-
-
-  IO.copy_stream(download, './confusables.txt')
-
-elsif # latest_version != existing
-  puts "do you want to update?"
+latest_version = fetch_version(response.body)
+unless latest_version
+  # todo raise some kind of error and exit maybe
+  puts "Could not find version data on latest list. Format might be changed making it necessary to update the script."
 end
 
+begin
+  existing_version = open(CONFUSABLES_TXT_DIRECTORY, "rb") { |file| fetch_version(file) }
+rescue => e
+  puts "Couldn't find confusables.txt #{e}"
+end
+
+if not existing_version# no confusables in directory
+  puts "We don't have confusables.txt, do you want to save the latest version (#{latest_version}) as #{CONFUSABLES_TXT_DIRECTORY}? (y/N)"
+  open(CONFUSABLES_TXT_DIRECTORY, "wb") { |file| file.write(response.body) } if gets.chomp == "y"
+elsif latest_version != existing_version
+  puts "We have the version #{existing_version} while the latest is #{latest_version}. Do you want to update? (y/N)"
+  open(CONFUSABLES_TXT_DIRECTORY, "wb") { |file| file.write(response.body) } if gets.chomp == "y"
+else # they're equal then
+  puts "We already have the latest version (#{existing_version})."
+end
