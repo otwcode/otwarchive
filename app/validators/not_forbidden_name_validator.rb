ee2a060d094f14f84959812930f98fb0d68ea973
ömer faruk <omerfaruk@transformativeworks.org>
2026-08-20 00:40:07 +0300

    AO3-7444 improve, simplify validator

--- app/validators/not_forbidden_name_validator.rb
+++ app/validators/not_forbidden_name_validator.rb
@@ -2,8 +2,11 @@ class NotForbiddenNameValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return if value.nil?
     ArchiveConfig.FORBIDDEN_USERNAMES.each do |forbidden|
-      # i18n-tasks-use t("activerecord.errors.messages.forbidden")
-      return record.errors.add(attribute, :forbidden, **options.merge(value: value)) if self.confusable?(forbidden, value)
+      # TODO still something wrong with this
+      if NotForbiddenNameValidator.confusable?(forbidden, value)
+        # i18n-tasks-use t("activerecord.errors.messages.forbidden")
+        return record.errors.add(attribute, :forbidden, **options.merge(value: value))
+      end
     end
   end
 
@@ -15,11 +18,12 @@ class NotForbiddenNameValidator < ActiveModel::EachValidator
       File.open("./script/confusables.txt", "rb") do |f|
         confusable_pairs = {}
         f.each_line do |line|
+          # Process each line, capture the part before and after the first ";" # NOTSURE if it is a line that contains confusable data.
           line =~ /^(\w*)\s;\s((?:\w*\s)*);\sMA\s/
 
           # pushing an array into array since right part of the confusable can be more than one codepoint
           # TODO maybe add an example and show what it becomes at each step
-          # we're also converting to decimal from hexadecimal since that's easier to use in ruby
+          # we're also converting to decimal integer from hexadecimal string since that's easier to use in ruby
           confusable_pairs[$1.to_i(16)] = $2.split(" ").map{ |x| x.to_i(16)} if $2.is_a?(String)
         end
         confusable_pairs
@@ -29,21 +33,28 @@ class NotForbiddenNameValidator < ActiveModel::EachValidator
     # TODO maaybe find a way to not using this array or simplifying it
     final_arr = Array.new
     string.unicode_normalize(:nfd).gsub(/\p{DI}/, "").each_codepoint.map do |codepoint|
-      (confusable_pairs[codepoint] || [codepoint]).each do |integer_codepoint|
-        final_arr.push([integer_codepoint.to_s(16).hex].pack("U"))
+      # because this gives an array for each codepoint of the initial string, we iterate over that array
+      # and add that to a final array after converting to a string character.
+      (confusable_pairs[codepoint] || [codepoint]).each do |skeleton_codepoint|
+        final_arr.push([skeleton_codepoint].pack("U"))
       end
     end
 
     # convert to string characters and add that to a new array
-    # flattening is because the array can have an array as an element
     # finally, normalize as described by the standard
-    final_arr.join.unicode_normalize(:nfd)
+    final_arr.unicode_normalize(:nfd)
   end
 
-  # not the confusable Unicode defines, also makes it uppercase and includes dialectics, blank and punctuations
+  # This is not the confusable Unicode defines, also makes it uppercase and includes dialectics, blank and punctuations
+  # A problem with making it uppercase after all that is "I" becomes "l" but "i" doesn't
+  # Problem with making it uppercase before is "m" doesn't become "rn"
+  # Workaround I found is using it both after and before
+  # TODO find another workaround
   def self.confusable?(string1, string2)
     internal_skeleton(string1).gsub(/\p{Dia}|\p{Blank}|\p{Punct}/, "").upcase ==
-      internal_skeleton(string2).gsub(/\p{Dia}|\p{Blank}|\p{Punct}/, "").upcase
+      internal_skeleton(string2).gsub(/\p{Dia}|\p{Blank}|\p{Punct}/, "").upcase ||
+      internal_skeleton(string1.upcase).gsub(/\p{Dia}|\p{Blank}|\p{Punct}/, "") ==
+        internal_skeleton(string2.upcase).gsub(/\p{Dia}|\p{Blank}|\p{Punct}/, "")
   end
 end
 
