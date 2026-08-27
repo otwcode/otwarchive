09929c4f097b754656dde834cba3f7790b93e2dc
ömer faruk <omerfaruk@transformativeworks.org>
2026-08-27 12:42:45 +0300

    AO3-7444 really add tests for casing issues

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -36,7 +36,7 @@ describe User do
         allow(ArchiveConfig).to receive(:FORBIDDEN_USERNAMES).and_return([forbidden_username])
       end
 
-      it { is_expected.not_to allow_values("admin", "admın", "ad.min", "ad min ", "adrnin", "ADMIN").for(:login) }
+      it { is_expected.not_to allow_values("admin", "admın", "ad.min", "ad min ", "adrnin", "ADMIN", "ᗅdmin").for(:login) }
     end
 
     context "with a username not confusable to a forbidden one" do
@@ -50,11 +50,11 @@ describe User do
     end
 
     context "it's internal skeleton" do
-      it "has itself in latin words" do
+      it "is the word itself for words with all Latin letters" do
         expect(NotForbiddenNameValidator.internal_skeleton("Support")).to eq("Support")
       end
 
-      it "has it's latin counterpart for cyrcillic letters" do
+      it "has it's Latin counterpart for Cyrillic letters" do
         # notice the P being uppercase since letter case standardizing isn't in internal_skeleton()
         expect(NotForbiddenNameValidator.internal_skeleton("SupРort")).to eq("SupPort")
       end
@@ -63,6 +63,10 @@ describe User do
         expect(NotForbiddenNameValidator.internal_skeleton("admin")).to eq("adrnin")
       end
 
+      it "removes default ignorable code points" do
+        # ZERO WIDTH NON-JOINER, U+200C
+        expect(NotForbiddenNameValidator.internal_skeleton("𝅳\u200c")).to eq("")
+      end
     end
 
     context "in confusable check" do
@@ -80,6 +84,24 @@ describe User do
 
       it "removes dialectics" do
         expect(NotForbiddenNameValidator.confusable?("Suppört", "Support")).to be_truthy
+      end
+
+      context "uppercase/lowercase issues" do
+        it "both I and i resolves to l" do
+          expect(NotForbiddenNameValidator.confusable?("I", "i")).to be_truthy
+          end
+
+        it "m resolves to rn" do
+          expect(NotForbiddenNameValidator.confusable?("m", "rn")).to be_truthy
+        end
+
+        it "I resolves to l" do
+          expect(NotForbiddenNameValidator.confusable?("I", "l")).to be_truthy
+        end
+
+        it "i resolves to L" do
+          expect(NotForbiddenNameValidator.confusable?("i", "L")).to be_truthy
+        end
       end
     end
   end
