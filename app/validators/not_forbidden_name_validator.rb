3a2e4931db9cd78eb75bbb51a0e200376a324044
ömer faruk <omerfaruk@transformativeworks.org>
2026-09-02 14:23:44 +0300

    AO3-7444 refactor confusable logic into its concern

--- app/validators/not_forbidden_name_validator.rb
+++ app/validators/not_forbidden_name_validator.rb
@@ -1,45 +1,13 @@
 class NotForbiddenNameValidator < ActiveModel::EachValidator
+  include Confusable
   def validate_each(record, attribute, value)
     return if value.nil?
 
     ArchiveConfig.FORBIDDEN_USERNAMES.each do |forbidden|
-      if NotForbiddenNameValidator.confusable?(forbidden, value)
+      if confusable?(forbidden, value)
         # i18n-tasks-use t("activerecord.errors.messages.forbidden")
         return record.errors.add(attribute, :forbidden, **options.merge(value: value))
       end
     end
-  end
-
-  # Internal skeleton that Unicode defines in Technical Standard #39
-  #
-  # Check the cache for confusables hashmap, if it doesn't exist create it by
-  # processing each line, capturing the parts before and after the first ";",
-  # pushing an array into array (since right part of the confusable can be
-  # more than one codepoint) and finally converting to decimal integer from
-  # hexadecimal string since that's easier to use in Ruby.
-  #
-  # Because the hashmap gives an array as the prototype for each source
-  # character, join each of those arrays (or codepoint of source character if
-  # it's not confusable) after converting them to a string. Also normalize
-  # before and after as described by the standard.
-  def self.internal_skeleton(string)
-    confusable_pairs = Rails.cache.fetch("v1/confusables_hash") do
-      File.foreach(Rails.root.join("config/confusables.txt"))
-        .filter_map { it.match(/^(\h+)\s+;\s+([\h\s]+);\s*MA/) }
-        .to_h { [it[1].to_i(16), it[2].scan(/\h+/).map { |x| x.to_i(16) }] }
-    end
-
-    string.unicode_normalize(:nfd).gsub(/\p{DI}/, "").each_codepoint.map do |codepoint|
-      (confusable_pairs[codepoint] || [codepoint]).pack("U*")
-    end.join.unicode_normalize(:nfd)
-  end
-
-  # This is stricter than the confusable Unicode defines, it's case-insensitive
-  # and also includes dialectics, blank and punctuations.
-  def self.confusable?(string1, string2)
-    internal_skeleton(string1).gsub(/\p{Dia}|\p{Blank}|\p{Punct}/, "").upcase ==
-      internal_skeleton(string2).gsub(/\p{Dia}|\p{Blank}|\p{Punct}/, "").upcase ||
-      internal_skeleton(string1.upcase).gsub(/\p{Dia}|\p{Blank}|\p{Punct}/, "").upcase ==
-        internal_skeleton(string2.upcase).gsub(/\p{Dia}|\p{Blank}|\p{Punct}/, "").upcase
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -48,62 +48,6 @@ describe User do
 
       it { is_expected.to allow_values("user", "admn", "SadminER").for(:login) }
     end
-
-    context "it's internal skeleton" do
-      it "is the word itself for words with all Latin letters" do
-        expect(NotForbiddenNameValidator.internal_skeleton("Support")).to eq("Support")
-      end
-
-      it "has it's Latin counterpart for Cyrillic letters" do
-        # notice the P being uppercase since letter case standardizing isn't in internal_skeleton()
-        expect(NotForbiddenNameValidator.internal_skeleton("SupРort")).to eq("SupPort")
-      end
-
-      it "has separated characters for confusables with combined characters" do
-        expect(NotForbiddenNameValidator.internal_skeleton("admin")).to eq("adrnin")
-      end
-
-      it "removes default ignorable code points" do
-        # ZERO WIDTH NON-JOINER, U+200C
-        expect(NotForbiddenNameValidator.internal_skeleton("𝅳\u200c")).to eq("")
-      end
-    end
-
-    context "in confusable check" do
-      it "classifies two words with a cyrillic/latin difference as confusable" do
-        expect(NotForbiddenNameValidator.confusable?("SupРort", "Support")).to be_truthy
-      end
-
-      it "removes punctuations" do
-        expect(NotForbiddenNameValidator.confusable?("Sup.port", "Support")).to be_truthy
-      end
-
-      it "removes blank characters" do
-        expect(NotForbiddenNameValidator.confusable?("Sup port", "Support")).to be_truthy
-      end
-
-      it "removes dialectics" do
-        expect(NotForbiddenNameValidator.confusable?("Suppört", "Support")).to be_truthy
-      end
-
-      context "uppercase/lowercase issues" do
-        it "both I and i resolves to l" do
-          expect(NotForbiddenNameValidator.confusable?("I", "i")).to be_truthy
-        end
-
-        it "m resolves to rn" do
-          expect(NotForbiddenNameValidator.confusable?("m", "rn")).to be_truthy
-        end
-
-        it "I resolves to l" do
-          expect(NotForbiddenNameValidator.confusable?("I", "l")).to be_truthy
-        end
-
-        it "i resolves to L" do
-          expect(NotForbiddenNameValidator.confusable?("i", "L")).to be_truthy
-        end
-      end
-    end
   end
 
   describe "#destroy" do
