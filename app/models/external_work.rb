eab5089bb7280e9b0331a6bf698c2fd4bdd55d21
Bilka <Bilka2@users.noreply.github.com>
2026-09-12 21:40:35 +0200

    AO3-7603 i18n error messages in the external work model

--- app/models/external_work.rb
+++ app/models/external_work.rb
@@ -14,22 +14,15 @@ class ExternalWork < ApplicationRecord
 
   AUTHOR_LENGTH_MAX = 500
 
-  validates_presence_of :title
-  validates_length_of :title, minimum: ArchiveConfig.TITLE_MIN,
-                              too_short: ts("must be at least %{min} characters long.",
-                                            min: ArchiveConfig.TITLE_MIN)
-  validates_length_of :title, maximum: ArchiveConfig.TITLE_MAX,
-                              too_long: ts("must be less than %{max} characters long.",
-                                           max: ArchiveConfig.TITLE_MAX)
+  validates :title,
+            presence: true,
+            length: { minimum: ArchiveConfig.TITLE_MIN, maximum: ArchiveConfig.TITLE_MAX }
 
-  validates_length_of :summary, allow_blank: true, maximum: ArchiveConfig.SUMMARY_MAX,
-                                too_long: ts("must be less than %{max} characters long.",
-                                             max: ArchiveConfig.SUMMARY_MAX)
+  validates :summary, length: { maximum: ArchiveConfig.SUMMARY_MAX, allow_blank: true }
 
-  validates :author, presence: { message: ts("can't be blank") }
-  validates_length_of :author, maximum: AUTHOR_LENGTH_MAX,
-                               too_long: ts("must be less than %{max} characters long.",
-                                            max: AUTHOR_LENGTH_MAX)
+  validates :author,
+            presence: true,
+            length: { maximum: AUTHOR_LENGTH_MAX }
 
   validates :user_defined_tags_count,
             at_most: { maximum: proc { ArchiveConfig.USER_DEFINED_TAGS_MAX } }
