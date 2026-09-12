41c15506daa141a3d797e2c81b1bb22a39bde309
Bilka <Bilka2@users.noreply.github.com>
2026-09-12 23:21:39 +0200

    AO3-7605 Fix tests

--- spec/models/work_spec.rb
+++ spec/models/work_spec.rb
@@ -96,14 +96,14 @@ describe Work do
       work = build(:work, language_id: deleted_language_id)
 
       expect(work).not_to be_valid
-      expect(work.errors.messages[:base]).to include("Language cannot be blank.")
+      expect(work.errors.messages[:language]).to include("cannot be blank.")
     end
 
     it "is not valid without a language" do
       work = build(:work, language_id: "")
 
       expect(work).not_to be_valid
-      expect(work.errors.messages[:base]).to include("Language cannot be blank.")
+      expect(work.errors.messages[:language]).to include("cannot be blank.")
     end
   end
 
